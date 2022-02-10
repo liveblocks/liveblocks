a77def26119aaa557981de244089b0dadd83a0d5
Olivier Foucherot <ofoucherot@gmail.com>
2022-02-10 10:50:46 -0500

    fix e2e install with link script

--- e2e/next-sandbox/scripts/link-liveblocks.sh
+++ e2e/next-sandbox/scripts/link-liveblocks.sh
@@ -1,16 +1,24 @@
 echo "Install all dependencies"
 npm install
+cd -
+cd ../../packages/liveblocks
+npm install
+cd -
+cd ../../packages/liveblocks-zustand
+npm install
+cd -
+cd ../../packages/liveblocks-react
+npm install
+cd -
 
 echo "Build @liveblocks/client"
 cd ../../packages/liveblocks
-npm install
 npm run build
 npm link
 
 echo "Build @liveblocks/react"
 cd -
 cd ../../packages/liveblocks-react
-npm install
 npm link @liveblocks/client ../../e2e/next-sandbox/node_modules/react
 npm run build
 npm link
@@ -19,7 +27,6 @@ npm link
 echo "Build @liveblocks/zustand"
 cd -
 cd ../../packages/liveblocks-zustand
-npm install
 npm link @liveblocks/client ../../e2e/next-sandbox/node_modules/zustand
 npm run build
 npm link
