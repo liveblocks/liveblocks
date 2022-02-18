42eb3b0e37348630207f5cb3acc675725e17ff0d
Olivier Foucherot <ofoucherot@gmail.com>
2022-02-18 11:17:38 -0500

    remove full path

--- e2e/next-sandbox/pages/_app.tsx
+++ e2e/next-sandbox/pages/_app.tsx
@@ -3,7 +3,7 @@ import { LiveblocksProvider } from "@liveblocks/react";
 import { AppProps } from "next/app";
 
 const client = createClient({
-  authEndpoint: "http://localhost:3007/api/auth",
+  authEndpoint: "/api/auth",
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
