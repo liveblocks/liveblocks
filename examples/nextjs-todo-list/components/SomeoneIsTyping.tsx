cb4e2f953a2699728f8153d2406f4e06e50492d7
Olivier Foucherot <ofoucherot@gmail.com>
2022-03-10 13:35:17 -0500

    delete old nextjs examples, add prettier

--- examples/nextjs-todo-list/components/SomeoneIsTyping.tsx
+++
@@ -1,9 +0,0 @@
-import { useOthers } from "@liveblocks/react";
-
-export default function SomeoneIsTyping() {
-  const someoneIsTyping = useOthers()
-    .toArray()
-    .some((user) => user.presence?.isTyping);
-
-  return someoneIsTyping ? <div>Someone is typing</div> : null;
-}
