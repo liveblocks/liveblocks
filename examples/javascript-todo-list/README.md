0adfe2bb6fbcd1436f6fa40184abae39c7b0dfe8
GuillaumeSalles <guillaume.salles@me.com>
2022-03-12 17:20:21 -0500

    Add vanilla javascript todo list example

---
+++ examples/javascript-todo-list/README.md
@@ -0,0 +1,23 @@
+<p align="center">
+  <a href="https://liveblocks.io">
+    <img src="https://liveblocks.io/icon-192x192.png" height="96">
+  </a>
+</p>
+
+# Multiplayer to-do list app with [Liveblocks](https://liveblocks.io)
+
+This project shows how to build a real-time to-do list that do not rely on any front-end technologies. It uses [esbuild](https://esbuild.github.io/) for the bundling.
+
+## Getting started
+
+### Run examples locally
+
+- Install all dependencies with `npm install`
+
+- Create an account on [liveblocks.io](https://liveblocks.io/dashboard)
+
+- Copy your public key from the [administration](https://liveblocks.io/dashboard/apikeys)
+
+- Replace `pk_YOUR_PUBLIC_KEY` with your own public key.
+
+- Run `npm build` and open `static/index.html`
---
+++ examples/javascript-todo-list/app.js
@@ -0,0 +1,87 @@
+import { createClient, LiveList } from "@liveblocks/client";
+
+const client = createClient({
+  publicApiKey: "pk_YOUR_PUBLIC_KEY",
+});
+
+async function run() {
+  const room = client.enter("javascript-todo-list");
+
+  const whoIsHere = document.getElementById("who_is_here");
+  const todoInput = document.getElementById("todo_input");
+  const somoeneIsTyping = document.getElementById("someone_is_typing");
+  const todosContainer = document.getElementById("todos_container");
+
+  room.subscribe("others", (others) => {
+    if (others.count === 0) {
+      whoIsHere.innerHTML = "You’re the only one here.";
+    } else if (others.count === 1) {
+      whoIsHere.innerHTML = "There is one other person here.";
+    } else {
+      whoIsHere.innerHTML = `There are ${others.count} other people here`;
+    }
+
+    somoeneIsTyping.innerHTML = others
+      .toArray()
+      .some((user) => user.presence?.isTyping)
+      ? "Someone is typing..."
+      : "";
+  });
+
+  const { root } = await room.getStorage();
+
+  let todos = root.get("todos");
+
+  if (todos == null) {
+    todos = new LiveList();
+    root.set("todos", todos);
+  }
+
+  todoInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      room.updatePresence({ isTyping: false });
+      todos.push({ text: todoInput.value });
+      todoInput.value = "";
+    } else {
+      room.updatePresence({ isTyping: true });
+    }
+  });
+
+  todoInput.addEventListener("blur", () => {
+    room.updatePresence({ isTyping: false });
+  });
+
+  function renderTodos() {
+    todosContainer.innerHTML = "";
+
+    for (let i = 0; i < todos.length; i++) {
+      const todo = todos.get(i);
+
+      const todoContainer = document.createElement("div");
+      todoContainer.classList.add("todo_container");
+
+      const todoText = document.createElement("div");
+      todoText.classList.add("todo");
+      todoText.innerHTML = todo.text;
+      todoContainer.appendChild(todoText);
+
+      const deleteButton = document.createElement("button");
+      deleteButton.classList.add("delete_button");
+      deleteButton.innerHTML = "✕";
+      deleteButton.addEventListener("click", () => {
+        todos.delete(i);
+      });
+      todoContainer.appendChild(deleteButton);
+
+      todosContainer.appendChild(todoContainer);
+    }
+  }
+
+  room.subscribe(todos, () => {
+    renderTodos();
+  });
+
+  renderTodos();
+}
+
+run();
