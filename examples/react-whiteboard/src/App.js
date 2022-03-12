03812a650d7126c06032454fcffcadad64756e78
Olivier Foucherot <ofoucherot@gmail.com>
2022-03-12 18:48:21 -0500

    trim more

--- examples/react-whiteboard/src/App.js
+++ examples/react-whiteboard/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useMemo, useCallback, useEffect } from "react";
+import { useState, useCallback, useEffect } from "react";
 import {
   useMyPresence,
   useOthers,
@@ -7,7 +7,7 @@ import {
   useMap,
   useHistory,
   useBatch,
-  useRoom,
+  useSelf,
 } from "@liveblocks/react";
 import { LiveObject } from "@liveblocks/client";
 import { nanoid } from "nanoid";
@@ -15,15 +15,19 @@ import { nanoid } from "nanoid";
 import "./App.css";
 
 import LayerComponent from "./LayerComponent";
-import SelectionBox from "./SelectionBox";
 
-import {
-  CanvasMode,
-  connectionIdToColor,
-  pointerEventToCanvasPoint,
-  findIntersectingLayersWithRectangle,
-  boundingBox,
-} from "./utils";
+const CanvasMode = {
+  None: "None",
+  SelectionNe: "SelectionNet",
+  Translating: "Translating",
+  Inserting: "Inserting",
+};
+
+const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
+
+function connectionIdToColor(connectionId) {
+  return COLORS[connectionId % COLORS.length];
+}
 
 export default function App() {
   return (
@@ -56,44 +60,31 @@ function Canvas({ layerIds, layers }) {
   const [canvasState, setState] = useState({
     mode: CanvasMode.None,
   });
-  const [camera, setCamera] = useState({ x: 0, y: 0 });
-  const [lastUsedColor, setLastUsedColor] = useState({
-    r: 252,
-    g: 142,
-    b: 42,
-  });
+
   const batch = useBatch();
   const history = useHistory();
 
-  const others = useOthers();
+  const me = useSelf();
 
-  const selectionBounds = useSelectionBounds(layers, selection);
+  const myColor = connectionIdToColor(me.connectionId);
 
-  /**
-   * Delete all the selected layers.
-   */
-  const deleteLayers = useCallback(() => {
+  const deleteSelectedLayer = useCallback(() => {
     batch(() => {
-      for (const id of selection) {
-        // Delete the layer from the layers LiveMap
-        layers.delete(id);
-        // Find the layer index in the z-index list and remove it
-        const index = layerIds.indexOf(id);
-        if (index !== -1) {
-          layerIds.delete(index);
-        }
+      // Delete the layer from the layers LiveMap
+      layers.delete(selection);
+      // Find the layer index in the z-index list and remove it
+      const index = layerIds.indexOf(selection);
+      if (index !== -1) {
+        layerIds.delete(index);
       }
     });
   }, [layerIds, layers, selection]);
 
-  /**
-   * Hook used to listen to Undo / Redo and delete selected layers
-   */
   useEffect(() => {
     function onKeyDown(e) {
       switch (e.key) {
         case "Backspace": {
-          deleteLayers();
+          deleteSelectedLayer();
           break;
         }
         case "z": {
@@ -117,44 +108,26 @@ function Canvas({ layerIds, layers }) {
     return () => {
       document.removeEventListener("keydown", onKeyDown);
     };
-  }, [selection, , history, deleteLayers]); // deleteLayers
-
-  const layerIdsToColorSelection = useMemo(() => {
-    const layerIdsToColorSelection = {};
-
-    const users = others.toArray();
-    for (const user of users) {
-      const selection = user.presence?.selection;
-      if (selection == null || selection.length === 0) {
-        continue;
-      }
-
-      for (const id of selection) {
-        layerIdsToColorSelection[id] = connectionIdToColor(user.connectionId);
-      }
-    }
-
-    return layerIdsToColorSelection;
-  }, [others]);
+  }, [selection, , history, deleteSelectedLayer]);
 
   const onLayerPointerDown = useCallback(
     (e, layerId) => {
-      if (
-        canvasState.mode === CanvasMode.Pencil ||
-        canvasState.mode === CanvasMode.Inserting
-      ) {
+      if (canvasState.mode === CanvasMode.Inserting) {
         return;
       }
 
       history.pause();
       e.stopPropagation();
-      const point = pointerEventToCanvasPoint(e, camera);
-      if (!selection.includes(layerId)) {
-        setPresence({ selection: [layerId] }, { addToHistory: true });
-      }
+      const point = {
+        x: Math.round(e.clientX),
+        y: Math.round(e.clientY),
+      };
+
+      setPresence({ selection: layerId }, { addToHistory: true });
+
       setState({ mode: CanvasMode.Translating, current: point });
     },
-    [setPresence, setState, selection, camera, history, canvasState.mode]
+    [setPresence, setState, selection, history, canvasState.mode]
   );
 
   const insertLayer = useCallback(
@@ -167,60 +140,21 @@ function Canvas({ layerIds, layers }) {
           y: position.y,
           height: 100,
           width: 100,
-          fill: lastUsedColor,
+          fill: myColor,
         });
         layerIds.push(layerId);
         layers.set(layerId, layer);
-        setPresence({ selection: [layerId] }, { addToHistory: true });
+        setPresence({ selection: layerId }, { addToHistory: true });
         setState({ mode: CanvasMode.None });
       });
     },
-    [batch, layerIds, layers, setPresence, lastUsedColor]
+    [batch, layerIds, layers, setPresence, myColor]
   );
 
   const unselectLayers = useCallback(() => {
-    setPresence({ selection: [] }, { addToHistory: true });
+    setPresence({ selection: null }, { addToHistory: true });
   }, [setPresence]);
 
-  /**
-   * Start multiselection with the selection net if the pointer move enough since pressed
-   */
-  const startMultiSelection = useCallback((current, origin) => {
-    // If the distance between the pointer position and the pointer position when it was pressed
-    if (Math.abs(current.x - origin.x) + Math.abs(current.y - origin.y) > 5) {
-      // Start multi selection
-      setState({
-        mode: CanvasMode.SelectionNet,
-        origin,
-        current,
-      });
-    }
-  }, []);
-
-  /**
-   * Update the position of the selection net and select the layers accordingly
-   */
-  const updateSelectionNet = useCallback(
-    (current, origin) => {
-      setState({
-        mode: CanvasMode.SelectionNet,
-        origin: origin,
-        current,
-      });
-      const ids = findIntersectingLayersWithRectangle(
-        layerIds,
-        layers,
-        origin,
-        current
-      );
-      setPresence({ selection: ids });
-    },
-    [layers, layerIds, setPresence]
-  );
-
-  /**
-   * Move selected layers on the canvas
-   */
   const translateSelectedLayers = useCallback(
     (point) => {
       if (canvasState.mode !== CanvasMode.Translating) {
@@ -233,14 +167,12 @@ function Canvas({ layerIds, layers }) {
           y: point.y - canvasState.current.y,
         };
 
-        for (const id of selection) {
-          const layer = layers.get(id);
-          if (layer) {
-            layer.update({
-              x: layer.get("x") + offset.x,
-              y: layer.get("y") + offset.y,
-            });
-          }
+        const layer = layers.get(selection);
+        if (layer) {
+          layer.update({
+            x: layer.get("x") + offset.x,
+            y: layer.get("y") + offset.y,
+          });
         }
 
         setState({ mode: CanvasMode.Translating, current: point });
@@ -254,36 +186,26 @@ function Canvas({ layerIds, layers }) {
       <div className="canvas">
         <svg
           className="renderer_svg"
-          onPointerDown={(e) => {
-            const point = pointerEventToCanvasPoint(e, camera);
-
-            if (canvasState.mode === CanvasMode.Inserting) {
-              return;
-            }
-
-            setState({ origin: point, mode: CanvasMode.Pressing });
-          }}
           onPointerLeave={(e) => {
             setPresence({ cursor: null });
           }}
           onPointerMove={(e) => {
             e.preventDefault();
-            const current = pointerEventToCanvasPoint(e, camera);
-            if (canvasState.mode === CanvasMode.Pressing) {
-              startMultiSelection(current, canvasState.origin);
-            } else if (canvasState.mode === CanvasMode.SelectionNet) {
-              updateSelectionNet(current, canvasState.origin);
-            } else if (canvasState.mode === CanvasMode.Translating) {
+            const current = {
+              x: Math.round(e.clientX),
+              y: Math.round(e.clientY),
+            };
+            if (canvasState.mode === CanvasMode.Translating) {
               translateSelectedLayers(current);
             }
             setPresence({ cursor: current });
           }}
           onPointerUp={(e) => {
-            const point = pointerEventToCanvasPoint(e, camera);
-            if (
-              canvasState.mode === CanvasMode.None ||
-              canvasState.mode === CanvasMode.Pressing
-            ) {
+            const point = {
+              x: Math.round(e.clientX),
+              y: Math.round(e.clientY),
+            };
+            if (canvasState.mode === CanvasMode.None) {
               unselectLayers();
               setState({
                 mode: CanvasMode.None,
@@ -312,58 +234,13 @@ function Canvas({ layerIds, layers }) {
                   mode={canvasState.mode}
                   onLayerPointerDown={onLayerPointerDown}
                   layer={layer}
-                  selectionColor={layerIdsToColorSelection[layerId]}
+                  selectionColor={selection === layerId ? "blue" : undefined}
                 />
               );
             })}
-            {/* Blue square that show the selection of the current users. */}
-            {selectionBounds && (
-              <SelectionBox
-                selection={selection}
-                bounds={selectionBounds}
-                layers={layers}
-                isAnimated={
-                  canvasState.mode !== CanvasMode.Translating &&
-                  canvasState.mode !== CanvasMode.Resizing
-                }
-              />
-            )}
-            {/* Selection net that appears when the user is selecting multiple layers at once */}
-            {/* {canvasState.mode === CanvasMode.SelectionNet &&
-              canvasState.current != null && (
-                <rect
-                  className={styles.selection_net}
-                  x={Math.min(canvasState.origin.x, canvasState.current.x)}
-                  y={Math.min(canvasState.origin.y, canvasState.current.y)}
-                  width={Math.abs(canvasState.origin.x - canvasState.current.x)}
-                  height={Math.abs(
-                    canvasState.origin.y - canvasState.current.y
-                  )}
-                />
-              )} */}
-            {/* <MultiplayerGuides /> */}
           </g>
         </svg>
       </div>
     </>
   );
-}
-
-function useSelectionBounds(layers, selection) {
-  const [bounds, setBounds] = useState(boundingBox(layers, selection));
-  const room = useRoom();
-
-  useEffect(() => {
-    function onChange() {
-      setBounds(boundingBox(layers, selection));
-    }
-
-    onChange();
-
-    // We need to subscribe to the layers map updates and to the updates on the layers themselves.
-    // If User A deletes or modified a layer that is currently selected by UserB, the selection bounds needs to be refreshed.
-    return room.subscribe(layers, onChange, { isDeep: true });
-  }, [room, layers, selection]);
-
-  return bounds;
 }
--- examples/react-whiteboard/src/LayerComponent.js
+++ examples/react-whiteboard/src/LayerComponent.js
@@ -1,37 +1,12 @@
 import { useRoom } from "@liveblocks/react";
 import React, { memo, useEffect, useState } from "react";
 
-import { CanvasMode, colorToCss } from "./utils";
-
-function Rectangle({ layer, isAnimated, onPointerDown, id, selectionColor }) {
-  const { x, y, width, height, fill } = layer;
-
-  return (
-    <rect
-      onPointerDown={(e) => onPointerDown(e, id)}
-      style={{
-        transition: isAnimated ? "all 0.1s ease" : "",
-        transform: `translate(${x}px, ${y}px)`,
-      }}
-      x={0}
-      y={0}
-      width={width}
-      height={height}
-      fill={fill ? colorToCss(fill) : "#CCC"}
-      strokeWidth={1}
-      stroke={selectionColor || "transparent"}
-    />
-  );
-}
-
-// We can use react memo because "layer" is a LiveObject and it's mutable. This component will only be re-rendered if the layer is updated.
 const LayerComponent = memo(
-  ({ layer, mode, onLayerPointerDown, id, selectionColor }) => {
+  ({ layer, onLayerPointerDown, id, selectionColor }) => {
     const [layerData, setLayerData] = useState(layer.toObject());
 
     const room = useRoom();
 
-    // Layer is a nested LiveObject inside a LiveMap, so we need to subscribe to changes made to a specific layer
     useEffect(() => {
       function onChange() {
         setLayerData(layer.toObject());
@@ -40,16 +15,22 @@ const LayerComponent = memo(
       return room.subscribe(layer, onChange);
     }, [room, layer]);
 
-    const isAnimated =
-      mode !== CanvasMode.Translating && mode !== CanvasMode.Resizing;
+    const { x, y, width, height, fill } = layerData;
 
     return (
-      <Rectangle
-        id={id}
-        layer={layerData}
-        onPointerDown={onLayerPointerDown}
-        isAnimated={isAnimated}
-        selectionColor={selectionColor}
+      <rect
+        onPointerDown={(e) => onLayerPointerDown(e, id)}
+        style={{
+          transition: "all 0.1s ease",
+          transform: `translate(${x}px, ${y}px)`,
+        }}
+        x={0}
+        y={0}
+        width={width}
+        height={height}
+        fill={fill ? fill : "#CCC"}
+        strokeWidth={1}
+        stroke={selectionColor || "transparent"}
       />
     );
   }
