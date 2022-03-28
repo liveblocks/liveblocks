4333352c66390f42dc295952c25c33928f28ef25
Olivier Foucherot <ofoucherot@gmail.com>
2022-03-28 18:21:40 -0400

    fix github workflow

--- e2e/next-sandbox/playwright.config.ts
+++ e2e/next-sandbox/playwright.config.ts
@@ -24,7 +24,7 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 1,
+  retries: process.env.CI ? 3 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
