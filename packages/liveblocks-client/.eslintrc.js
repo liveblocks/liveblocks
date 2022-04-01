3187cae605a70e9a274ebd1d84ea9fed4e98ff0f
Vincent Driessen <me@nvie.com>
2022-04-01 11:19:20 +0200

    Tweak checks

--- packages/liveblocks-client/.eslintrc.js
+++ packages/liveblocks-client/.eslintrc.js
@@ -5,23 +5,42 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   rules: {
-    // Turn this one off for now. It would be a good idea to re-enable this
-    // check later on!
+    // ----------------------------------------------------------------------
+    // NOTE: Only temporarily turned off!
+    // These checks are still GOOD IDEAS to re-enable later on, but for right
+    // now they're too noisy to enable.
+    // ----------------------------------------------------------------------
+    "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
 
+    // -------------------------------
+    // Not interested in these checks:
+    // -------------------------------
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "no-constant-condition": "off",
+
+    // ------------------------
+    // Customized default rules
+    // ------------------------
     "@typescript-eslint/no-unused-vars": [
-      "error",
+      "warn",
       // Unused variables are fine if they start with an underscore
-      { varsIgnorePattern: "^_.*" },
+      { argsIgnorePattern: "^_.*", varsIgnorePattern: "^_.*" },
     ],
 
+    // -------------------------------
+    // Custom syntax we want to forbid
+    // -------------------------------
     "no-restricted-syntax": [
       "error",
-      {
-        selector: "PrivateIdentifier",
-        message:
-          "Avoid private identifiers to reduce bundle size. Instead of using `#foo`, prefer using `private _foo`.",
-      },
+      // {
+      //   selector: "PrivateIdentifier",
+      //   message:
+      //     "Avoid private identifiers to reduce bundle size. Instead of using `#foo`, prefer using `private _foo`.",
+      // },
       // {
       //   selector: "ForOfStatement",
       //   message:
