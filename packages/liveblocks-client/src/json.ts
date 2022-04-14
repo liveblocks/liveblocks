b13487c7a62f412da5a6431cf3ae9fcdb2d2a413
Vincent Driessen <me@nvie.com>
2022-04-14 21:46:33 +0200

    Add Json datatype for describing nested tree structures

---
+++ packages/liveblocks-client/src/json.ts
@@ -0,0 +1,52 @@
+import type { LiveList } from "./LiveList";
+import type { LiveMap } from "./LiveMap";
+import type { LiveObject } from "./LiveObject";
+import type { LiveRegister } from "./LiveRegister";
+
+export type Dict<V> = { [key: string]: V | undefined };
+
+// type Tree<TLeaf> = TLeaf | TreeList<TLeaf> | TreeObject<TLeaf>;
+// type TreeList<TLeaf> = Tree<TLeaf>[];
+// type TreeObject<TLeaf> = { [key: string]: Tree<TLeaf> | undefined };
+
+/**
+ * Represents an indefinitely deep arbitrary JSON data structure. There are
+ * four types that make up the Json family:
+ *
+ * - Json         any legal JSON value
+ * - JsonScalar   any legal JSON leaf value (no lists or objects)
+ * - JsonArray    a JSON value whose outer type is an array
+ * - JsonObject   a JSON value whose outer type is an object
+ *
+ */
+export type Json = JsonScalar | JsonArray | JsonObject;
+export type JsonScalar = string | number | boolean | null | undefined;
+export type JsonArray = Json[];
+export type JsonObject = { [key: string]: Json | undefined };
+
+/**
+ * Think of LiveData as a sibling of the Json data tree, except that the nested
+ * data structure can contain a mix of Json values and LiveXxx instances.
+ */
+export type LiveData =
+  | LiveDataScalar
+  | LiveListData
+  | LiveObjectData
+
+  // Or they are LiveXxx class instances
+  | LiveObject<LiveObjectData>
+  | LiveList<LiveListData>
+  | LiveMap<LiveMapData>
+  | LiveRegister<LiveDataScalar>;
+
+export type LiveDataScalar = string | number | boolean | null | undefined;
+
+export type LiveListData = LiveData[];
+
+/**
+ * A mapping of keys to LiveData values. A LiveData value is any valid JSON
+ * value or a Live storage data structure (LiveMap, LiveList, etc.)
+ */
+export type LiveObjectData = { [key: string]: LiveData | undefined };
+
+export type LiveMapData = { [key: string]: LiveData | undefined };
