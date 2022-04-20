1a33d9273ccd004d6e7a1e86f284d9f2a8b6edc4
Vincent Driessen <me@nvie.com>
2022-04-20 21:08:41 +0200

    Fix type issues in test suite

--- packages/liveblocks-client/src/room.test.ts
+++ packages/liveblocks-client/src/room.test.ts
@@ -1114,7 +1114,10 @@ describe("room", () => {
     test("disconnect and reconnect with remote changes", async () => {
       const { assert, machine } = await prepareIsolatedStorageTest<
         never,
-        { items: LiveList<string> }
+        {
+          items?: LiveList<string>;
+          items2?: LiveList<string>;
+        }
       >(
         [
           createSerializedObject("0:0", {}),
