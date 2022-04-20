5014d082c4b0ee0ca70710d3cb589eb9a397c39c
Vincent Driessen <me@nvie.com>
2022-04-20 11:23:18 +0200

    Allow "for-all" scripts to run shell cmds as well (#155)

--- scripts/for-all-examples.sh
+++ scripts/for-all-examples.sh
@@ -15,20 +15,23 @@ err () {
 }
 
 usage () {
-    err "usage: for-all-examples.sh [-qfh] <cmd> [<arg1> [<arg2> ...]]"
+    err "usage: for-all-examples.sh [-qfh] [-c <shell-cmd>] <cmd> [<arg1> [<arg2> ...]]"
     err
     err "Runs the given script inside all of our examples directories."
     err
     err "Options:"
     err "-q    Don't print the current directory"
     err "-f    Continue, even after an error occurs"
+    err "-c    Run command with \`sh -c\` instead"
     err "-h    Show this help"
 }
 
+cmd=
 quiet=0
 force=0
-while getopts qfh flag; do
+while getopts qc:fh flag; do
     case "$flag" in
+        c) cmd="$OPTARG" ; ;;
         q) quiet=1 ; ;;
         f) force=1 ; ;;
         *) usage; exit 2;;
@@ -48,7 +51,11 @@ for dir in $(find examples -maxdepth 1 -type d | grep -Ee /); do
         fi
 
         # Run the given command
-        "$@"
+        if [ -n "$cmd" ]; then
+            sh -c "$cmd"
+        else
+            "$@"
+        fi
 
         if [ $force -eq 1 ]; then
             set -e
