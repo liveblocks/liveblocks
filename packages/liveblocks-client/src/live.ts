baf8ceb0101a51f6a2f3bccdc14782ace91ef86e
Vincent Driessen <me@nvie.com>
2022-04-21 22:40:15 +0200

    Document all the incoming message types

--- packages/liveblocks-client/src/live.ts
+++ packages/liveblocks-client/src/live.ts
@@ -5,13 +5,16 @@ import { Presence } from "./types";
  * Messages that can be sent from the server to the client.
  */
 export type ServerMessage =
-  | UpdatePresenceMessage
-  | UserJoinMessage
-  | UserLeftMessage
-  | EventMessage
-  | RoomStateMessage
-  | InitialDocumentStateMessage
-  | UpdateStorageMessage;
+  // For Presence
+  | UpdatePresenceMessage // Broadcasted
+  | UserJoinMessage // Broadcasted
+  | UserLeftMessage // Broadcasted
+  | EventMessage // Broadcasted
+  | RoomStateMessage // For a single client
+
+  // For Storage
+  | InitialDocumentStateMessage // For a single client
+  | UpdateStorageMessage; // Broadcasted
 
 export enum ServerMessageType {
   UpdatePresence = 100,
@@ -24,6 +27,11 @@ export enum ServerMessageType {
   UpdateStorage = 201,
 }
 
+/**
+ * Sent by the WebSocket server to a single client in response to the client
+ * joining the Room, to provide the initial state of the Room. The payload
+ * includes a list of all other Users that already are in the Room.
+ */
 export type RoomStateMessage = {
   type: ServerMessageType.RoomState;
   users: {
@@ -34,38 +42,99 @@ export type RoomStateMessage = {
   };
 };
 
+/**
+ * Sent by the WebSocket server and broadcasted to all clients to announce that
+ * a User updated their presence. For example, when a user moves their cursor.
+ *
+ * In most cases, the data payload will only include the fields from the
+ * Presence that have been changed since the last announcement. However, after
+ * a new user joins a room, a "full presence" will be announced so the newly
+ * connected user will get each other's user full presence at least once. In
+ * those cases, the `targetActor` field indicates the newly connected client,
+ * so all other existing clients can ignore this broadcasted message.
+ */
 export type UpdatePresenceMessage = {
   type: ServerMessageType.UpdatePresence;
+  /**
+   * The User whose Presence has changed.
+   */
   actor: number;
+  /**
+   * The partial or full Presence of a User. If the `targetActor` field is set,
+   * this will be the full Presence, otherwise it only contain the fields that
+   * have changed since the last broadcast.
+   */
   data: Presence;
+  /**
+   * If this message was sent in response to a newly joined user, this field
+   * indicates which client this message is for. Other existing clients may
+   * ignore this message if this message isn't targeted for them.
+   */
   targetActor?: number;
 };
 
+/**
+ * Sent by the WebSocket server and broadcasted to all clients to announce that
+ * a new User has joined the Room.
+ */
 export type UserJoinMessage = {
   type: ServerMessageType.UserJoined;
   actor: number;
+  /**
+   * ???
+   */
   id?: string;
+  /**
+   * ???
+   */
   info?: string;
 };
 
+/**
+ * Sent by the WebSocket server and broadcasted to all clients to announce that
+ * a new User has left the Room.
+ */
 export type UserLeftMessage = {
   type: ServerMessageType.UserLeft;
   actor: number;
 };
 
+/**
+ * Sent by the WebSocket server and broadcasted to all clients to announce that
+ * a User broadcasted an Event to everyone in the Room.
+ */
 export type EventMessage = {
   type: ServerMessageType.Event;
+  /**
+   * The User who broadcasted the Event.
+   */
   actor: number;
+  /**
+   * The arbitrary payload of the Event. This can be any JSON value. Clients
+   * will have to manually verify/decode this event.
+   */
   event: Json;
 };
 
 export type SerializedCrdtWithId = [id: string, crdt: SerializedCrdt];
 
+/**
+ * Sent by the WebSocket server to a single client in response to the client
+ * joining the Room, to provide the initial Storage state of the Room. The
+ * payload includes the entire Storage document.
+ */
 export type InitialDocumentStateMessage = {
   type: ServerMessageType.InitialStorageState;
   items: SerializedCrdtWithId[];
 };
 
+/**
+ * Sent by the WebSocket server and broadcasted to all clients to announce that
+ * a change occurred in the Storage document.
+ *
+ * The payload of this message contains a list of Ops (aka incremental
+ * mutations to make to the initially loaded document).
+ */
 export type UpdateStorageMessage = {
   type: ServerMessageType.UpdateStorage;
   ops: Op[];
@@ -160,7 +229,8 @@ export enum OpType {
 }
 
 /**
- * These operations are the payload for Storage messages only.
+ * These operations are the payload for {@link UpdateStorageMessage} messages
+ * only.
  */
 export type Op =
   | CreateObjectOp
