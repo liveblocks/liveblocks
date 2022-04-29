3c6fe35517d67e03f4042bee6ef91deb57001b7f
Vincent Driessen <me@nvie.com>
2022-04-29 08:32:19 +0200

    Fix type of MessageEvents

--- packages/liveblocks-client/src/room.ts
+++ packages/liveblocks-client/src/room.ts
@@ -197,7 +197,7 @@ export type Machine<
 > = {
   // Internal
   onClose(event: { code: number; wasClean: boolean; reason: string }): void;
-  onMessage(event: MessageEvent): void;
+  onMessage(event: MessageEvent<string>): void;
   authenticationSuccess(token: AuthenticationToken, socket: WebSocket): void;
   heartbeat(): void;
   onNavigatorOnline(): void;
