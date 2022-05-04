45e6d910d9d4a2b3fa1c79fa573ede5dc3997808
Olivier Foucherot <ofoucherot@gmail.com>
2022-05-04 18:04:50 -0400

    add more tests

--- e2e/storage-requirements/tests/list.test.ts
+++ e2e/storage-requirements/tests/list.test.ts
@@ -5,25 +5,303 @@ import { prepareTest } from "./utils";
 
 describe("LiveList confict resolution", () => {
   test("push / push", async () => {
-    const { root1, root2, assert, socketUtils } = await prepareTest<{
-      list: LiveList<string>;
-    }>({
-      list: new LiveList(),
+    const { root1, root2, assert, assertEach, socketUtils, run } =
+      await prepareTest<{
+        list: LiveList<string>;
+      }>({
+        list: new LiveList(),
+      });
+
+    await run(async () => {
+      await assert({ list: [] });
+
+      socketUtils.pauseAllSockets();
+
+      root1.get("list")?.push("A"); // Client 1 pushes "A"
+      root2.get("list")?.push("B"); // Client 2 pushes "B"
+
+      socketUtils.sendMessagesClient1(); // Client 1 push "A" sent to server
+
+      await assertEach({ list: ["A"] }, { list: ["A", "B"] });
+
+      socketUtils.sendMessagesClient2(); // Client 2 push "B" sent to server
+
+      await assert({ list: ["A", "B"] });
     });
-
-    await assert({ list: [] });
-
-    socketUtils.pauseAllSockets();
-
-    root1.get("list")?.push("A"); // Client 1 push "A"
-    root2.get("list")?.push("B"); // Client 2 push "B"
-
-    socketUtils.sendMessagesClient1(); // Client 1 push "A" sent to server
-
-    await assert({ list: ["A"] }, { list: ["A", "B"] });
-
-    socketUtils.sendMessagesClient2(); // Client 2 push "B" sent to server
-
-    await assert({ list: ["A", "B"] });
   });
+
+  test("move / set", async () => {
+    const { root1, root2, assert, assertEach, socketUtils, run } =
+      await prepareTest<{
+        list: LiveList<string>;
+      }>({
+        list: new LiveList(["A", "B"]),
+      });
+
+    await run(async () => {
+      await assert({ list: ["A", "B"] });
+
+      socketUtils.pauseAllSockets();
+
+      root1.get("list")?.move(0, 1); //  Client1 moves "A" after "B"
+      root2.get("list")?.set(0, "C"); // Client2 replaces "A" by "C"
+
+      socketUtils.sendMessagesClient1();
+
+      await assertEach({ list: ["B", "A"] }, { list: ["C", "B"] });
+
+      socketUtils.sendMessagesClient2();
+
+      // await assert({ list: ["B", "C"] }); // Expected TODO
+      await assertEach({ list: ["C", "B", "A"] }, { list: ["C", "B"] }); // Current
+    });
+  });
+
+  test("move / delete", async () => {
+    const { root1, root2, assert, assertEach, socketUtils, run } =
+      await prepareTest<{
+        list: LiveList<string>;
+      }>({
+        list: new LiveList(["A", "B"]),
+      });
+
+    await run(async () => {
+      await assert({ list: ["A", "B"] });
+
+      socketUtils.pauseAllSockets();
+
+      root1.get("list")?.move(0, 1); //  Client1 moves "A" after "B"
+      root2.get("list")?.delete(0); // Client2 deletes "A"
+
+      socketUtils.sendMessagesClient1();
+
+      await assertEach({ list: ["B", "A"] }, { list: ["B"] });
+
+      socketUtils.sendMessagesClient2();
+
+      await assert({ list: ["B"] });
+    });
+  });
+
+  test("move / insert", async () => {
+    const { root1, root2, assert, assertEach, socketUtils, run } =
+      await prepareTest<{
+        list: LiveList<string>;
+      }>({
+        list: new LiveList(["A", "B"]),
+      });
+
+    await run(async () => {
+      await assert({ list: ["A", "B"] });
+
+      socketUtils.pauseAllSockets();
+
+      root1.get("list")?.move(0, 1); //     Client1 moves "A" after "B"
+      root2.get("list")?.insert("C", 0); // Client2 insert "C" at index 0
+
+      socketUtils.sendMessagesClient1();
+
+      await assertEach({ list: ["B", "A"] }, { list: ["C", "B", "A"] });
+
+      socketUtils.sendMessagesClient2();
+
+      await assert({ list: ["C", "B", "A"] });
+    });
+  });
+
+  test("move / move", async () => {
+    const { root1, root2, assert, assertEach, socketUtils, run } =
+      await prepareTest<{
+        list: LiveList<string>;
+      }>({
+        list: new LiveList(["A", "B"]),
+      });
+
+    await run(async () => {
+      await assert({ list: ["A", "B"] });
+
+      socketUtils.pauseAllSockets();
+
+      root1.get("list")?.move(0, 1); //  Client1 moves "A" after "B"
+      root2.get("list")?.move(0, 1); //  Client2 moves "A" after "B"
+
+      socketUtils.sendMessagesClient1();
+
+      await assertEach({ list: ["B", "A"] }, { list: ["B", "A"] });
+
+      socketUtils.sendMessagesClient2();
+
+      await assert({ list: ["B", "A"] });
+    });
+  });
+
+  test("set / move", async () => {
+    const { root1, root2, assert, assertEach, socketUtils, run } =
+      await prepareTest<{
+        list: LiveList<string>;
+      }>({
+        list: new LiveList(["A", "B"]),
+      });
+
+    await run(async () => {
+      await assert({ list: ["A", "B"] });
+
+      socketUtils.pauseAllSockets();
+
+      root1.get("list")?.set(0, "C"); //  Client1 sets "A" to "C"
+      root2.get("list")?.move(0, 1); //   Client2 moves "A" after "B"
+
+      socketUtils.sendMessagesClient1();
+
+      // await assertEach({ list: ["C", "B"] }, { list: ["B", "C"] }); // Expected TODO
+      await assertEach({ list: ["C", "B"] }, { list: ["C", "B", "A"] }); // Current
+
+      socketUtils.sendMessagesClient2();
+
+      // await assert({ list: ["B", "C"] }); // Expected TODO
+      await assertEach({ list: ["C", "B"] }, { list: ["C", "B", "A"] }); // Current
+    });
+  });
+
+  test("set / delete", async () => {
+    const { root1, root2, assert, assertEach, socketUtils, run } =
+      await prepareTest<{
+        list: LiveList<string>;
+      }>({
+        list: new LiveList(["A", "B"]),
+      });
+
+    await run(async () => {
+      await assert({ list: ["A", "B"] });
+
+      socketUtils.pauseAllSockets();
+
+      root1.get("list")?.set(0, "C"); //  Client1 sets "A" to "C"
+      root2.get("list")?.delete(0); //    Client2 deletes "A"
+
+      socketUtils.sendMessagesClient1();
+
+      await assertEach({ list: ["C", "B"] }, { list: ["C", "B"] });
+
+      socketUtils.sendMessagesClient2();
+
+      await assert({ list: ["C", "B"] });
+    });
+  });
+
+  test("set / insert", async () => {
+    const { root1, root2, assert, assertEach, socketUtils, run } =
+      await prepareTest<{
+        list: LiveList<string>;
+      }>({
+        list: new LiveList(["A", "B"]),
+      });
+
+    await run(async () => {
+      await assert({ list: ["A", "B"] });
+
+      socketUtils.pauseAllSockets();
+
+      root1.get("list")?.set(0, "C"); //     Client1 sets "A" to "C"
+      root2.get("list")?.insert("D", 0); //  Client2 inserts "D" at index 0
+
+      socketUtils.sendMessagesClient1();
+
+      await assertEach({ list: ["C", "B"] }, { list: ["D", "C", "B"] });
+
+      socketUtils.sendMessagesClient2();
+
+      await assert({ list: ["D", "C", "B"] });
+    });
+  });
+
+  test("set / set", async () => {
+    const { root1, root2, assert, assertEach, socketUtils, run } =
+      await prepareTest<{
+        list: LiveList<string>;
+      }>({
+        list: new LiveList(["A", "B"]),
+      });
+
+    await run(async () => {
+      await assert({ list: ["A", "B"] });
+
+      socketUtils.pauseAllSockets();
+
+      root1.get("list")?.set(0, "C"); //  Client1 sets "A" to "C"
+      root2.get("list")?.set(0, "D"); //  Client2 sets "A" to "D"
+
+      socketUtils.sendMessagesClient1();
+
+      await assertEach({ list: ["C", "B"] }, { list: ["C", "B"] });
+
+      socketUtils.sendMessagesClient2();
+
+      await assert({ list: ["D", "B"] });
+    });
+  });
+
+  test("insert / move", async () => {});
+
+  test("insert / delete", async () => {});
+
+  test("insert / set", async () => {
+    const { root1, root2, assert, assertEach, socketUtils, run } =
+      await prepareTest<{
+        list: LiveList<string>;
+      }>({
+        list: new LiveList(["A", "B"]),
+      });
+
+    await run(async () => {
+      await assert({ list: ["A", "B"] });
+
+      socketUtils.pauseAllSockets();
+
+      root1.get("list")?.insert("C", 0); // Client1 inserts "C" at index 0
+      root2.get("list")?.set(0, "D"); //    Client2 sets "A" to "D"
+
+      socketUtils.sendMessagesClient1();
+
+      await assertEach({ list: ["C", "A", "B"] }, { list: ["C", "D", "B"] });
+
+      socketUtils.sendMessagesClient2();
+
+      await assert({ list: ["C", "D", "B"] });
+    });
+  });
+
+  test("insert / insert", async () => {});
+
+  test("delete / move", async () => {});
+
+  test("delete / insert", async () => {});
+
+  test("delete / set", async () => {
+    const { root1, root2, assert, assertEach, socketUtils, run } =
+      await prepareTest<{
+        list: LiveList<string>;
+      }>({
+        list: new LiveList(["A", "B"]),
+      });
+
+    await run(async () => {
+      await assert({ list: ["A", "B"] });
+
+      socketUtils.pauseAllSockets();
+
+      root1.get("list")?.delete(0); //   Client1 deletes "A"
+      root2.get("list")?.set(0, "C"); // Client2 sets "A" to "C"
+
+      socketUtils.sendMessagesClient1();
+
+      await assertEach({ list: ["B"] }, { list: ["C", "B"] });
+
+      socketUtils.sendMessagesClient2();
+
+      await assert({ list: ["C", "B"] });
+    });
+  });
+
+  test("delete / delete", async () => {});
 });
--- e2e/storage-requirements/tests/utils.ts
+++ e2e/storage-requirements/tests/utils.ts
@@ -11,7 +11,53 @@ import WebSocket from "ws";
 import { ClientRequestArgs } from "http";
 import { URL } from "url";
 
+import "dotenv/config";
+
 export async function prepareTest<T extends LsonObject>(initialStorage = {}) {
+  const sockets: MockWebSocket[] = [];
+
+  function createTestClient() {
+    return createClient({
+      publicApiKey: process.env.PUBLIC_LIVEBLOCKS_PUBLIC_KEY!,
+      fetchPolyfill: fetch,
+      WebSocketPolyfill: MockWebSocket,
+    });
+  }
+
+  class MockWebSocket extends WebSocket {
+    sendBuffer: any[] = [];
+    _isSendPaused = false;
+
+    constructor(
+      address: string | URL,
+      protocols: WebSocket.ClientOptions | ClientRequestArgs | undefined
+    ) {
+      super(address, protocols);
+
+      sockets.push(this);
+    }
+
+    pauseSend() {
+      this._isSendPaused = true;
+    }
+
+    resumeSend() {
+      this._isSendPaused = false;
+      for (const item of this.sendBuffer) {
+        super.send(item);
+      }
+      this.sendBuffer = [];
+    }
+
+    send(data: any) {
+      if (this._isSendPaused) {
+        this.sendBuffer.push(data);
+      } else {
+        super.send(data);
+      }
+    }
+  }
+
   const client1 = createTestClient();
   const client2 = createTestClient();
 
@@ -28,7 +74,7 @@ export async function prepareTest<T extends LsonObject>(initialStorage = {}) {
   const storageRoot2 = await client2Room.getStorage<T>();
 
   async function assert(data: any, data2?: any) {
-    await waitFor(() => {
+    const val = await waitFor(() => {
       const client1Json = objectToJson(storageRoot1.root);
       const client2Json = objectToJson(storageRoot2.root);
 
@@ -37,6 +83,15 @@ export async function prepareTest<T extends LsonObject>(initialStorage = {}) {
         JSON.stringify(client2Json) === JSON.stringify(data2 || data)
       );
     });
+
+    if (!val) {
+      expect(data).toEqual(objectToJson(storageRoot1.root));
+      expect(data2 || data).toEqual(objectToJson(storageRoot2.root));
+    }
+  }
+
+  async function assertEach(data1: any, data2: any) {
+    return assert(data1, data2);
   }
 
   const socketUtils = {
@@ -52,58 +107,23 @@ export async function prepareTest<T extends LsonObject>(initialStorage = {}) {
     },
   };
 
+  async function run(testFunc: () => {}) {
+    try {
+      await testFunc();
+    } finally {
+      client1.leave(roomName);
+      client2.leave(roomName);
+    }
+  }
+
   return {
     root1: storageRoot1.root,
     root2: storageRoot2.root,
     assert,
+    assertEach,
     socketUtils,
+    run,
   };
-}
-
-const sockets: MockWebSocket[] = [];
-
-import "dotenv/config";
-
-function createTestClient() {
-  return createClient({
-    publicApiKey: process.env.PUBLIC_LIVEBLOCKS_PUBLIC_KEY!,
-    fetchPolyfill: fetch,
-    WebSocketPolyfill: MockWebSocket,
-  });
-}
-
-class MockWebSocket extends WebSocket {
-  sendBuffer: any[] = [];
-  _isSendPaused = false;
-
-  constructor(
-    address: string | URL,
-    protocols: WebSocket.ClientOptions | ClientRequestArgs | undefined
-  ) {
-    super(address, protocols);
-
-    sockets.push(this);
-  }
-
-  pauseSend() {
-    this._isSendPaused = true;
-  }
-
-  resumeSend() {
-    this._isSendPaused = false;
-    for (const item of this.sendBuffer) {
-      super.send(item);
-    }
-    this.sendBuffer = [];
-  }
-
-  send(data: any) {
-    if (this._isSendPaused) {
-      this.sendBuffer.push(data);
-    } else {
-      super.send(data);
-    }
-  }
 }
 
 function wait(ms: number) {
@@ -113,7 +133,7 @@ function wait(ms: number) {
 async function waitFor(predicate: () => {}, delay: number = 2000) {
   const result = predicate();
   if (result) {
-    return;
+    return true;
   }
 
   const time = new Date().getTime();
@@ -121,11 +141,11 @@ async function waitFor(predicate: () => {}, delay: number = 2000) {
   while (new Date().getTime() - time < delay) {
     await wait(100);
     if (predicate()) {
-      return;
+      return true;
     }
   }
 
-  throw "TIMEOUT waiting for predicate";
+  return false;
 }
 
 function objectToJson(record: LiveObject<LsonObject>) {
