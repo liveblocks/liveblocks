b3ecd1ccafc66a18eed01e3f7042e4d9a9aefbe6
Vincent Driessen <me@nvie.com>
2022-05-04 09:42:32 +0200

    Have a standard internal API for deprecating things

--- packages/liveblocks-client/src/internal.ts
+++ packages/liveblocks-client/src/internal.ts
@@ -1,4 +1,19 @@
+/**
+ * PRIVATE / INTERNAL APIS
+ * -----------------------
+ *
+ * This module is intended for internal use only, PLEASE DO NOT RELY ON ANY OF
+ * THE EXPORTS IN HERE. These are implementation details that can change at any
+ * time and without announcement. This module purely exists to share code
+ * between the several Liveblocks packages.
+ *
+ * But since you're so deep inside Liveblocks code... we're hiring!
+ * https://join.team/liveblocks ;)
+ */
+
 export * from "./live";
 export * from "./position";
+
+export { deprecate, deprecateIf } from "./utils";
 
 export type { Resolve } from "./types";
