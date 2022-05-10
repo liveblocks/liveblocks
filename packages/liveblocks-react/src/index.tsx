e551cbc6ea4e879fb3ba1c2b96796cad1f50f7c8
Vincent Driessen <me@nvie.com>
2022-05-10 08:07:22 +0200

    Split off factory module

--- packages/liveblocks-react/src/index.tsx
+++ packages/liveblocks-react/src/index.tsx
@@ -18,7 +18,7 @@ export {
   useRedo,
   useBatch,
   useHistory,
-} from "./rooms";
+} from "./factory";
 
 // Re-exports from @liveblocks/client, for convenience
 export type { Json, JsonObject } from "@liveblocks/client";
