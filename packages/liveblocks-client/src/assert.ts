b85a0cb58863cbfc6c0f7b00b68fdae171036b4e
Vincent Driessen <me@nvie.com>
2022-05-13 15:26:50 +0200

    Add new assert() and nn() helpers

---
+++ packages/liveblocks-client/src/assert.ts
@@ -0,0 +1,35 @@
+/**
+ * Asserts that a certain condition holds. If it does not hold, will throw
+ * a runtime error in dev mode.
+ *
+ * In production, nothing is asserted and this acts as a no-op.
+ */
+export function assert(condition: boolean, errmsg: string): asserts condition {
+  if (process.env.NODE_ENV !== "production" && !condition) {
+    const err = new Error(errmsg);
+    err.name = "Assertion failure";
+    throw err;
+  }
+}
+
+/**
+ * Asserts that a given value is non-nullable. This is similar to TypeScript's
+ * `!` operator, but will throw an error at runtime (dev-mode only) indicating
+ * an incorrect assumption.
+ *
+ * Instead of:
+ *
+ *     foo!.bar
+ *
+ * Use:
+ *
+ *     nn(foo).bar
+ *
+ */
+export function nn<T>(
+  value: T,
+  errmsg: string = "Expected value to be non-nullable"
+): NonNullable<T> {
+  assert(value != null, errmsg);
+  return value as NonNullable<T>;
+}
