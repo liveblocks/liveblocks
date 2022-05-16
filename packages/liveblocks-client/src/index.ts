c6d1916bbddafa0f281493fe05102681b7b0a33e
Vincent Driessen <me@nvie.com>
2022-05-16 12:39:41 +0200

    ESLint: also auto-sort exports (#245)
    * Also sort exports, like we do with imports
    
    * Autofix ESLint complaints

--- packages/liveblocks-client/src/index.ts
+++ packages/liveblocks-client/src/index.ts
@@ -1,18 +1,16 @@
-export { LiveObject } from "./LiveObject";
-export { LiveMap } from "./LiveMap";
+export { createClient } from "./client";
+export type { Json, JsonObject } from "./json";
 export { LiveList } from "./LiveList";
+export { LiveMap } from "./LiveMap";
+export { LiveObject } from "./LiveObject";
+export type { Lson, LsonObject } from "./lson";
 export type {
+  BroadcastOptions,
+  Client,
+  History,
   Others,
   Presence,
   Room,
-  Client,
-  User,
-  BroadcastOptions,
   StorageUpdate,
-  History,
+  User,
 } from "./types";
-
-export type { Json, JsonObject } from "./json";
-export type { Lson, LsonObject } from "./lson";
-
-export { createClient } from "./client";
