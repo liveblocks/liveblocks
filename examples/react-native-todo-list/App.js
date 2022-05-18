2fd575c9cf8de6a19047dd6928b015c28be7b0d4
“Vinod <vinod.santharam@gmail.com>
2022-05-17 20:00:01 -0400

    WIP - new project - wrapper - whoisthere

---
+++ examples/react-native-todo-list/App.js
@@ -0,0 +1,78 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  TextInput,
+} from 'react-native';
+
+import {useOthers, useUpdateMyPresence} from '@liveblocks/react';
+
+function WhoIsHere() {
+  const others = useOthers();
+  return <Text>There are {others.count} other users online</Text>;
+}
+
+const App = () => {
+  const backgroundStyle = {
+    backgroundColor: 'rgb(143, 243, 243)',
+    flex: 1,
+    padding: '15%',
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'white',
+          }}>
+          <WhoIsHere></WhoIsHere>
+          <TextInput style={styles.input} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+export default App;
