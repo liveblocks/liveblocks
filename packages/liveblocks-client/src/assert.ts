df2d29c85c61eb803826e1a6f94cbebd6db9f7bd
Vincent Driessen <me@nvie.com>
2022-05-20 16:59:21 +0200

    Move assertNever() utility into assert module as well

--- packages/liveblocks-client/src/assert.ts
+++ packages/liveblocks-client/src/assert.ts
@@ -1,4 +1,27 @@
 /**
+ * Helper function that can be used to implement exhaustive switch statements
+ * with TypeScript. Example usage:
+ *
+ *    type Fruit = "🍎" | "🍌";
+ *
+ *    switch (fruit) {
+ *      case "🍎":
+ *      case "🍌":
+ *        return doSomething();
+ *
+ *      default:
+ *        return assertNever(fruit, "Unknown fruit");
+ *    }
+ *
+ * If now the Fruit union is extended (i.e. add "🍒"), TypeScript will catch
+ * this *statically*, rather than at runtime, and force you to handle the
+ * 🍒 case.
+ */
+export function assertNever(_value: never, errmsg: string): never {
+  throw new Error(errmsg);
+}
+
+/**
  * Asserts that a certain condition holds. If it does not hold, will throw
  * a runtime error in dev mode.
  *
