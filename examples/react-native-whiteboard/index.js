de6bbc43e0fcb0ba914b903acc69340c5e9da1f2
“Vinod <vinod.santharam@gmail.com>
2022-06-02 15:52:55 -0400

    test reconnection

--- examples/react-native-whiteboard/index.js
+++ examples/react-native-whiteboard/index.js
@@ -12,10 +12,7 @@ import {createClient} from '@liveblocks/client';
 import {LiveblocksProvider, RoomProvider} from '@liveblocks/react';
 
 import {decode, encode} from 'base-64';
-
-if (!global.btoa) {
-  global.btoa = encode;
-}
+import NetInfo from '@react-native-community/netinfo';
 
 if (!global.atob) {
   global.atob = decode;
@@ -25,6 +22,17 @@ window.addEventListener = () => {};
 
 const client = createClient({
   publicApiKey: 'pk_test_lOMrmwejSWLaPYQc5_JuGH-H',
+});
+
+const unsubscribe = NetInfo.addEventListener(state => {
+  if (state.type != 'none' && state.type != 'unknown') {
+    if (state.isConnected) {
+      const room = client.getRoom('react-native-whiteboard');
+    }
+  }
+
+  console.log('Connection type', state.type);
+  console.log('Is connected?', state.isConnected);
 });
 
 const Wrapper = () => {
