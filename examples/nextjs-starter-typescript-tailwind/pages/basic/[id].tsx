22cc9b0162e7231479bc24f24edc61e25dc1ac17
Le Vaillant Pierre <pierre.levaillant@liveblocks.io>
2022-06-16 09:43:15 +0200

    Improve a11y

--- examples/nextjs-starter-typescript-tailwind/pages/basic/[id].tsx
+++ examples/nextjs-starter-typescript-tailwind/pages/basic/[id].tsx
@@ -29,6 +29,8 @@ export default function MultiplayerRoom() {
               className="h-5 w-5"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
+              aria-hidden={true}
+              focusable={false}
             >
               <path d="M21 11H6.83l3.58-3.59L9 6l-6 6 6 6 1.41-1.42L6.83 13H21v-2Z" />
             </svg>
