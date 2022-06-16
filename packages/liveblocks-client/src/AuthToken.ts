735c96916563e5cbf3eb596c21258cb7e8ab5fa3
Florent Lefebvre <florent.pm.lefebvre@gmail.com>
2022-06-16 17:09:23 -0400

    Apply suggestions from code review
    Co-authored-by: Vincent Driessen <me@nvie.com>

--- packages/liveblocks-client/src/AuthToken.ts
+++ packages/liveblocks-client/src/AuthToken.ts
@@ -93,7 +93,6 @@ export function isRoomAuthToken(data: JsonObject): data is RoomAuthToken {
   return (
     typeof data.appId === "string" &&
     typeof data.roomId === "string" &&
-    // TODO: throw specific error if actor is not a number
     typeof data.actor === "number" &&
     (data.id === undefined || typeof data.id === "string") &&
     isStringList(data.scopes) &&
@@ -137,7 +136,7 @@ export function parseRoomAuthToken(
     return token;
   } else {
     throw new Error(
-      "Authentication error: we expected a room token but did not get one. Hint: if you are using a callback, ensure the room is passed when creating the token. For more information: https://liveblocks.io/docs/api-reference/liveblocks-client#createClientWithCallback"
+      "Authentication error: we expected a room token but did not get one. Hint: if you are using a callback, ensure the room is passed when creating the token. For more information: https://liveblocks.io/docs/api-reference/liveblocks-client#createClientCallback"
     );
   }
 }
