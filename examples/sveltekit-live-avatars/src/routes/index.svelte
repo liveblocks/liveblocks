d605d9f9d7144e7ee3f77624bc66850718135ca5
Vincent Driessen <me@nvie.com>
2022-06-20 17:26:28 +0200

    Also update Svelte examples with the same templates

--- examples/sveltekit-live-avatars/src/routes/index.svelte
+++ examples/sveltekit-live-avatars/src/routes/index.svelte
@@ -7,9 +7,34 @@
   let room: Room;
   let roomId = "sveltekit-live-avatars";
 
-  // Define your presence and storage types for `client.enter`
-  type Presence = {};
-  type Storage = {};
+  // Presence represents the properties that will exist on every User in the
+  // Room and that will automatically be kept in sync. Accessible through the
+  // `user.presence` property. Must be JSON-serializable.
+  type Presence = {
+    // cursor: { x: number, y: number } | null,
+    // ...
+  };
+
+  // Optionally, Storage represents the shared document that persists in the
+  // Room, even after all Users leave. Fields under Storage typically are
+  // LiveList, LiveMap, LiveObject instances, for which updates are
+  // automatically persisted and synced to all connected clients.
+  type Storage = {
+    // animals: LiveList<string>,
+    // ...
+  };
+
+  // Optionally, UserMeta represents static/readonly metadata on each User, as
+  // provided by your own custom auth backend (if used). Useful for data that
+  // will not change during a session, like a User's name or avatar.
+  // type UserMeta = {
+  //   id?: string,  // Accessible through `user.id`
+  //   info?: Json,  // Accessible through `user.info`
+  // };
+
+  // Optionally, type of custom Events your app sends when broadcasting or
+  // listening to events. Must be JSON-serializable.
+  // type Event = {};
 
   // Set up the client on load
   // Check inside src/routes/api/auth.ts for the serverless function
@@ -20,7 +45,7 @@
       authEndpoint: "/api/auth"
     });
 
-    room = client.enter<Presence, Storage>(roomId);
+    room = client.enter<Presence, Storage /* UserMeta, Event */>(roomId);
   });
 
   onDestroy(() => {
