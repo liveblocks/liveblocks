90408534c89f47409d605365a6b183af88f756a0
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2022-06-30 15:21:59 +0100

    feat: Updated Next.js and middleware

--- examples/nextjs-starter-typescript/middleware.ts
+++ examples/nextjs-starter-typescript/middleware.ts
@@ -1,22 +1,15 @@
-/**
- * Middleware will be going through a breaking change soon, this is how it
- * will work after.
- * https://github.com/vercel/next.js/pull/37382
+import { NextRequest, NextResponse } from "next/server";
+import { nanoid } from "nanoid";
 
-import { NextRequest, NextResponse } from 'next/server'
-import { nanoid } from 'nanoid'
-
-// Only run on /room with no id
-export const config = {
-  matcher: '/room',
-};
-
+// Add id to pathname
 export function middleware(request: NextRequest, response: NextResponse) {
-  // Add room id
   const roomId = nanoid();
-  return NextResponse.redirect(new URL(`/room/${roomId}`, request.url));
+  const url = request.nextUrl.clone();
+  url.pathname = `${url.pathname}/${roomId}`;
+  return NextResponse.redirect(url);
 }
 
- */
-
-export default null
+// Only run on "/basic" and "/canvas" pages that have no id
+export const config = {
+  matcher: ["/basic", "/canvas"],
+};
