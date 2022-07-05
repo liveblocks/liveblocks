daf1486364ec1fa519038e4086dfddeb3ad14454
Vincent Driessen <me@nvie.com>
2022-07-05 08:40:42 +0200

    Remove compatibility layer in `@liveblocks/react` (for 0.18) (#385)

--- packages/liveblocks-react/src/index.tsx
+++ packages/liveblocks-react/src/index.tsx
@@ -2,25 +2,5 @@ export { LiveblocksProvider, useClient } from "./client";
 
 export { createRoomContext } from "./factory";
 
-export {
-  RoomProvider,
-  useRoom,
-  useMyPresence,
-  useUpdateMyPresence,
-  useOthers,
-  useBroadcastEvent,
-  useErrorListener,
-  useEventListener,
-  useSelf,
-  useStorage,
-  useMap,
-  useList,
-  useObject,
-  useUndo,
-  useRedo,
-  useBatch,
-  useHistory,
-} from "./compat";
-
 // Re-exports from @liveblocks/client, for convenience
 export type { Json, JsonObject } from "@liveblocks/client";
