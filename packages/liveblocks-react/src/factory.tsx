bdcf800f084cf018223a3dec0ee2737500c66f33
Vincent Driessen <me@nvie.com>
2022-07-05 10:07:22 +0200

    Make sure the deprecated internals still respect the `use`-prefix for hooks

--- packages/liveblocks-react/src/factory.tsx
+++ packages/liveblocks-react/src/factory.tsx
@@ -232,13 +232,13 @@ type RoomContext<
    * @example
    * const animals = useList("animals");  // e.g. [] or ["🦁", "🐍", "🦍"]
    */
-  deprecated_useList<TValue extends Lson>(key: string): LiveList<TValue> | null;
+  useList_deprecated<TValue extends Lson>(key: string): LiveList<TValue> | null;
 
   /**
    * @deprecated We no longer recommend initializing the
    * items from the useList() hook. For details, see https://bit.ly/3Niy5aP.
    */
-  deprecated_useList<TValue extends Lson>(
+  useList_deprecated<TValue extends Lson>(
     key: string,
     items: TValue[]
   ): LiveList<TValue> | null;
@@ -253,7 +253,7 @@ type RoomContext<
    * @example
    * const shapesById = useMap("shapes");
    */
-  deprecated_useMap<TKey extends string, TValue extends Lson>(
+  useMap_deprecated<TKey extends string, TValue extends Lson>(
     key: string
   ): LiveMap<TKey, TValue> | null;
 
@@ -261,7 +261,7 @@ type RoomContext<
    * @deprecated We no longer recommend initializing the
    * entries from the useMap() hook. For details, see https://bit.ly/3Niy5aP.
    */
-  deprecated_useMap<TKey extends string, TValue extends Lson>(
+  useMap_deprecated<TKey extends string, TValue extends Lson>(
     key: string,
     entries: readonly (readonly [TKey, TValue])[] | null
   ): LiveMap<TKey, TValue> | null;
@@ -276,7 +276,7 @@ type RoomContext<
    * @example
    * const object = useObject("obj");
    */
-  deprecated_useObject<TData extends LsonObject>(
+  useObject_deprecated<TData extends LsonObject>(
     key: string
   ): LiveObject<TData> | null;
 
@@ -284,7 +284,7 @@ type RoomContext<
    * @deprecated We no longer recommend initializing the fields from the
    * useObject() hook. For details, see https://bit.ly/3Niy5aP.
    */
-  deprecated_useObject<TData extends LsonObject>(
+  useObject_deprecated<TData extends LsonObject>(
     key: string,
     initialData: TData
   ): LiveObject<TData> | null;
@@ -538,14 +538,14 @@ export function createRoomContext<
     return [root];
   }
 
-  function deprecated_useMap<TKey extends string, TValue extends Lson>(
+  function useMap_deprecated<TKey extends string, TValue extends Lson>(
     key: string
   ): LiveMap<TKey, TValue> | null;
-  function deprecated_useMap<TKey extends string, TValue extends Lson>(
+  function useMap_deprecated<TKey extends string, TValue extends Lson>(
     key: string,
     entries: readonly (readonly [TKey, TValue])[] | null
   ): LiveMap<TKey, TValue> | null;
-  function deprecated_useMap<TKey extends string, TValue extends Lson>(
+  function useMap_deprecated<TKey extends string, TValue extends Lson>(
     key: string,
     entries?: readonly (readonly [TKey, TValue])[] | null | undefined
   ): LiveMap<TKey, TValue> | null {
@@ -597,14 +597,14 @@ Please see https://bit.ly/3Niy5aP for details.`
     }
   }
 
-  function deprecated_useList<TValue extends Lson>(
+  function useList_deprecated<TValue extends Lson>(
     key: string
   ): LiveList<TValue> | null;
-  function deprecated_useList<TValue extends Lson>(
+  function useList_deprecated<TValue extends Lson>(
     key: string,
     items: TValue[]
   ): LiveList<TValue> | null;
-  function deprecated_useList<TValue extends Lson>(
+  function useList_deprecated<TValue extends Lson>(
     key: string,
     items?: TValue[] | undefined
   ): LiveList<TValue> | null {
@@ -658,14 +658,14 @@ Please see https://bit.ly/3Niy5aP for details.`
     }
   }
 
-  function deprecated_useObject<TData extends LsonObject>(
+  function useObject_deprecated<TData extends LsonObject>(
     key: string
   ): LiveObject<TData> | null;
-  function deprecated_useObject<TData extends LsonObject>(
+  function useObject_deprecated<TData extends LsonObject>(
     key: string,
     initialData: TData
   ): LiveObject<TData> | null;
-  function deprecated_useObject<TData extends LsonObject>(
+  function useObject_deprecated<TData extends LsonObject>(
     key: string,
     initialData?: TData
   ): LiveObject<TData> | null {
@@ -722,19 +722,19 @@ Please see https://bit.ly/3Niy5aP for details.`
   function useList<TKey extends Extract<keyof TStorage, string>>(
     key: TKey
   ): TStorage[TKey] | null {
-    return deprecated_useList(key) as unknown as TStorage[TKey];
+    return useList_deprecated(key) as unknown as TStorage[TKey];
   }
 
   function useMap<TKey extends Extract<keyof TStorage, string>>(
     key: TKey
   ): TStorage[TKey] | null {
-    return deprecated_useMap(key) as unknown as TStorage[TKey];
+    return useMap_deprecated(key) as unknown as TStorage[TKey];
   }
 
   function useObject<TKey extends Extract<keyof TStorage, string>>(
     key: TKey
   ): TStorage[TKey] | null {
-    return deprecated_useObject(key) as unknown as TStorage[TKey];
+    return useObject_deprecated(key) as unknown as TStorage[TKey];
   }
 
   function useHistory(): History {
@@ -837,8 +837,8 @@ Please see https://bit.ly/3Niy5aP for details.`
     useUndo,
     useUpdateMyPresence,
 
-    deprecated_useList,
-    deprecated_useMap,
-    deprecated_useObject,
+    useList_deprecated,
+    useMap_deprecated,
+    useObject_deprecated,
   };
 }
