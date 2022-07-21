0e4105f56677edcd6be6577dddcee9e61ae8078f
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2022-07-21 12:30:53 +0200

    Add delete buttons

--- examples/nextjs-spreadsheet-advanced/src/pages/index.tsx
+++ examples/nextjs-spreadsheet-advanced/src/pages/index.tsx
@@ -26,9 +26,47 @@ function PlusIcon(props: ComponentProps<"svg">) {
       {...props}
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M11 4a1 1 0 1 0-2 0v5H4a1 1 0 0 0 0 2h5v5a1 1 0 1 0 2 0v-5h5a1 1 0 1 0 0-2h-5V4Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
+function HandlerIcon(props: ComponentProps<"svg">) {
+  return (
+    <svg
+      width="16"
+      height="16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M5 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2ZM5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2ZM5 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2ZM11 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2ZM11 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2ZM11 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
+function CrossIcon(props: ComponentProps<"svg">) {
+  return (
+    <svg
+      width="16"
+      height="16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M12.707 4.707a1 1 0 0 0-1.414-1.414L8 6.586 4.707 3.293a1 1 0 0 0-1.414 1.414L6.586 8l-3.293 3.293a1 1 0 1 0 1.414 1.414L8 9.414l3.293 3.293a1 1 0 0 0 1.414-1.414L9.414 8l3.293-3.293Z"
         fill="currentColor"
       />
     </svg>
@@ -92,9 +130,18 @@ function Example() {
                       width: appendUnit(column.width),
                     }}
                   >
+                    <div className={styles.sheet_header_control}>
+                      <HandlerIcon />
+                    </div>
                     <div className={styles.sheet_header_label}>
                       {convertNumberToLetter(x)}
                     </div>
+                    <button
+                      className={styles.sheet_header_control}
+                      onClick={() => deleteColumn(x)}
+                    >
+                      <CrossIcon />
+                    </button>
                   </th>
                 ))}
               </tr>
@@ -111,7 +158,16 @@ function Example() {
                         height: appendUnit(row.height),
                       }}
                     >
+                      <div className={styles.sheet_header_control}>
+                        <HandlerIcon />
+                      </div>
                       <div className={styles.sheet_header_label}>{y}</div>
+                      <button
+                        className={styles.sheet_header_control}
+                        onClick={() => deleteRow(y)}
+                      >
+                        <CrossIcon />
+                      </button>
                     </th>
                   </tr>
                 );
