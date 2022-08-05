cfbe5d10faf9c380dc6bcd8f9d7bd98aee8a86a0
GuillaumeSalles <guillaume.salles@me.com>
2022-08-04 21:27:48 -0400

    Slate <> Liveblocks synchronization

--- examples/nextjs-block-text-editor-advanced/src/slate/Editor.tsx
+++ examples/nextjs-block-text-editor-advanced/src/slate/Editor.tsx
@@ -9,7 +9,6 @@ import {
   withReact,
   Editable,
   ReactEditor,
-  DefaultElement,
   RenderElementProps,
 } from "slate-react";
 import {
@@ -137,48 +136,50 @@ function App() {
 
         isEditingRef.current = true;
 
-        for (const update of updates) {
-          if (update.type === "LiveList" && update.node === blocks) {
-            for (const delta of update.updates) {
-              if (delta.type === "set") {
-                editor.apply({
-                  type: "remove_node",
-                  path: [delta.index],
-                  node: editor.children[delta.index],
-                  isRemote: true,
-                });
-
-                editor.apply({
-                  type: "insert_node",
-                  path: [delta.index],
-                  node: delta.item as CustomElement,
-                  isRemote: true,
-                });
-              } else if (delta.type === "move") {
-                editor.apply({
-                  type: "move_node",
-                  path: [delta.previousIndex],
-                  newPath: [delta.index],
-                  isRemote: true,
-                });
-              } else if (delta.type === "insert") {
-                editor.apply({
-                  type: "insert_node",
-                  path: [delta.index],
-                  node: delta.item as CustomElement,
-                  isRemote: true,
-                });
-              } else if (delta.type === "delete") {
-                editor.apply({
-                  type: "remove_node",
-                  path: [delta.index],
-                  node: editor.children[delta.index],
-                  isRemote: true,
-                });
+        // We want to apply every operations without intermediate normalization to avoid in
+        Editor.withoutNormalizing(editor, () => {
+          for (const update of updates) {
+            if (update.type === "LiveList" && update.node === blocks) {
+              for (const delta of update.updates) {
+                if (delta.type === "set") {
+                  editor.apply({
+                    type: "remove_node",
+                    path: [delta.index],
+                    node: editor.children[delta.index],
+                    isRemote: true,
+                  });
+                  editor.apply({
+                    type: "insert_node",
+                    path: [delta.index],
+                    node: delta.item as CustomElement,
+                    isRemote: true,
+                  });
+                } else if (delta.type === "move") {
+                  editor.apply({
+                    type: "move_node",
+                    path: [delta.previousIndex],
+                    newPath: [delta.index],
+                    isRemote: true,
+                  });
+                } else if (delta.type === "insert") {
+                  editor.apply({
+                    type: "insert_node",
+                    path: [delta.index],
+                    node: delta.item as CustomElement,
+                    isRemote: true,
+                  });
+                } else if (delta.type === "delete") {
+                  editor.apply({
+                    type: "remove_node",
+                    path: [delta.index],
+                    node: editor.children[delta.index],
+                    isRemote: true,
+                  });
+                }
               }
             }
           }
-        }
+        });
 
         isEditingRef.current = false;
       },
@@ -200,6 +201,8 @@ function App() {
     }
 
     const overIndex = editor.children.findIndex((x: any) => x.id === overId);
+
+    console.log(overId, activeId, overIndex);
 
     if (overId !== activeId && overIndex !== -1) {
       Transforms.moveNodes(editor, {
@@ -237,9 +240,11 @@ function App() {
         }
         onInsertBelow={(block: CustomElement) => {
           const path = [ReactEditor.findPath(editor, props.element)[0] + 1];
+
           Transforms.insertNodes(editor, block, {
             at: path,
           });
+
           // Defer selection to be able to focus the element we just inserted
           setTimeout(() => {
             ReactEditor.focus(editor);
@@ -276,96 +281,93 @@ function App() {
           <Slate
             editor={editor}
             value={blocks?.toArray()}
-            onChange={(value) => {
-              if (blocks == null || isEditingRef.current) {
+            onChange={() => {
+              if (blocks == null) {
                 return;
               }
 
+              // Synchronizing Liveblocks storage and presence...
+
+              // Setting a flag to make sure that we don't create an infinite loop with Storage subscriptions
               isEditingRef.current = true;
 
               room.batch(() => {
+                // If the operation is a "move_node", we assume that it's coming from the drag operation and we simply call LiveList.move
+                if (
+                  editor.operations.length === 1 &&
+                  editor.operations[0].type === "move_node" &&
+                  editor.operations[0].isRemote === false
+                ) {
+                  const moveOperation = editor.operations[0];
+                  blocks.move(moveOperation.path[0], moveOperation.newPath[0]);
+                  return;
+                }
+
+                let shouldPatchLiveblocksStorage = false;
                 for (const op of editor.operations) {
+                  // Remote operations should not patch Liveblocks state to avoid inifinite loops
                   if (op.isRemote) {
                     continue;
                   }
 
-                  switch (op.type) {
-                    case "set_selection": {
-                      if (editor.selection) {
-                        updateMyPresence({
-                          selectedBlockId: (
-                            editor.children[
-                              editor.selection.anchor.path[0]
-                            ] as CustomElement
-                          ).id,
-                        });
-                      } else {
-                        updateMyPresence({
-                          selectedBlockId: null,
-                        });
-                      }
-                      break;
+                  // Update presence based on the new selection
+                  if (op.type === "set_selection") {
+                    if (editor.selection) {
+                      updateMyPresence({
+                        selectedBlockId: (
+                          editor.children[
+                            editor.selection.anchor.path[0]
+                          ] as CustomElement
+                        ).id,
+                      });
+                    } else {
+                      updateMyPresence({
+                        selectedBlockId: null,
+                      });
                     }
+                  } else {
+                    shouldPatchLiveblocksStorage = true;
+                  }
+                }
 
-                    case "insert_node": {
-                      blocks.insert(
-                        value[op.path[0]] as CustomElement,
-                        op.path[0]
-                      );
-                      break;
-                    }
-                    case "remove_node": {
-                      if (op.path.length === 1) {
-                        // Remove top level node
-                        blocks.delete(op.path[0]);
-                      } else {
-                        blocks.set(
-                          op.path[0],
-                          value[op.path[0]] as CustomElement
-                        );
-                      }
-                      break;
-                    }
-                    case "remove_text":
-                    case "insert_text":
-                      blocks.set(
-                        op.path[0],
-                        value[op.path[0]] as CustomElement
-                      );
-                      break;
-                    case "split_node":
-                      if (op.path.length > 1) {
-                        blocks.set(
-                          op.path[0],
-                          value[op.path[0]] as CustomElement
-                        );
-                      } else {
-                        const index = op.path[0] + 1;
-                        blocks.insert(value[index] as CustomElement, index);
-                      }
-                      break;
-                    case "merge_node": {
-                      if (op.path.length === 1) {
-                        blocks.set(
-                          op.path[0] - 1,
-                          value[op.path[0] - 1] as CustomElement
-                        );
-                        blocks.delete(op.path[0]);
-                      }
-                      break;
-                    }
-                    case "move_node":
-                      if (op.path.length === 1 && op.newPath.length === 1) {
-                        blocks.move(op.path[0], op.newPath[0]);
-                      }
-                      break;
-                    case "set_node": {
-                      blocks.set(
-                        op.path[0],
-                        value[op.path[0]] as CustomElement
-                      );
-                      break;
-                    }
+                if (!shouldPatchLiveblocksStorage) {
+                  return;
+                }
+
+                // Naive algorithm to patch Liveblocks LiveList. Performance could be vastly improved
+                const children = editor.children as CustomElement[];
+
+                // Insert missing blocks
+                for (let i = 0; i < editor.children.length; i++) {
+                  const child = editor.children[i] as CustomElement;
+                  const liveblocksChildIndex = blocks.findIndex(
+                    (block) => block.id === child.id
+                  );
+
+                  if (liveblocksChildIndex === -1) {
+                    blocks.insert(child, i);
+                  }
+                }
+
+                // Delete blocks that are not in Slate children
+                for (let i = 0; i < blocks.length; i++) {
+                  const block = blocks.get(i)!;
+
+                  if (
+                    children.some((child) => child.id === block.id) === false
+                  ) {
+                    blocks.delete(i);
+                    i--;
+                  }
+                }
+
+                // At this point child that are not equals by reference needs to be replaced
+                for (let i = 0; i < children.length; i++) {
+                  const child = children[i];
+                  const block = blocks.get(i);
+
+                  if (child !== block) {
+                    blocks.set(i, child);
                   }
                 }
               });
@@ -386,6 +388,10 @@ function App() {
                 <Editable
                   renderElement={renderElement}
                   renderLeaf={Leaf}
+                  /**
+                   * Inspired by this great article from https://twitter.com/_jkrsp
+                   * https://jkrsp.com/slate-js-placeholder-per-line/
+                   **/
                   decorate={([node, path]) => {
                     if (editor.selection != null) {
                       if (
@@ -418,7 +424,10 @@ function App() {
               {createPortal(
                 <DragOverlay adjustScale={false}>
                   {activeElement && (
-                    <DragOverlayContent element={activeElement} />
+                    <DragOverlayContent
+                      element={activeElement}
+                      renderElement={renderElement}
+                    />
                   )}
                 </DragOverlay>,
                 document.body
@@ -462,7 +471,7 @@ function SortableElement({
               : undefined,
             pointerEvents: sortable.isSorting ? "none" : undefined,
             opacity: sortable.isDragging ? 0 : 1,
-          } as React.CSSProperties /* casted becase of css variable */
+          } as React.CSSProperties /* casted because of css variable */
         }
       >
         {othersByBlockId.length > 0 && (
@@ -498,7 +507,13 @@ function SortableElement({
   );
 }
 
-function DragOverlayContent({ element }: { element: CustomElement }) {
+function DragOverlayContent({
+  element,
+  renderElement,
+}: {
+  element: CustomElement;
+  renderElement: (props: RenderElementProps) => JSX.Element;
+}) {
   const editor = useEditor();
   const [value] = useState([JSON.parse(JSON.stringify(element))]); // clone
 
@@ -507,7 +522,7 @@ function DragOverlayContent({ element }: { element: CustomElement }) {
       <Slate editor={editor} value={value}>
         <Editable
           readOnly={true}
-          renderElement={DefaultElement}
+          renderElement={renderElement}
           renderLeaf={Leaf}
         />
       </Slate>
