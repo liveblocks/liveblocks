7d3058ca6c9366632d4dfa64a53d2fb9693b1052
GuillaumeSalles <guillaume.salles@me.com>
2022-08-10 19:51:52 -0400

    Fix Tooltip after radix upgrade

--- examples/nextjs-block-text-editor-advanced/pages/_app.tsx
+++ examples/nextjs-block-text-editor-advanced/pages/_app.tsx
@@ -4,10 +4,11 @@ import "../styles/themes/light-theme.css";
 import "../styles/themes/dark-theme.css";
 import "../styles/globals.css";
 import "../styles/prose.css";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <TooltipProvider>
       <Head>
         <title>Liveblocks</title>
         <meta name="robots" content="noindex" />
@@ -26,7 +27,7 @@ function App({ Component, pageProps }: AppProps) {
         />
       </Head>
       <Component {...pageProps} />
-    </>
+    </TooltipProvider>
   );
 }
 export default App;
