5fc0ef8261a25518c00b480e25ee9809631de0da
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2022-08-10 18:09:18 +0200

    Apply example guidelines

--- examples/nextjs-block-text-editor-advanced/pages/_app.tsx
+++ examples/nextjs-block-text-editor-advanced/pages/_app.tsx
@@ -9,11 +9,22 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Block Text Editor</title>
+        <title>Liveblocks</title>
         <meta name="robots" content="noindex" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
+        <link
+          href="https://liveblocks.io/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="https://liveblocks.io/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
       </Head>
-
       <Component {...pageProps} />
     </>
   );
