ca676c97a6763abdbea868d3a63ddbc506f29257
Vincent Driessen <me@nvie.com>
2022-08-17 10:37:02 +0200

    Also run this check in production!
    Not running this in production would defeat the purpose: what if it's
    the production bundle configuration that contains the configuration bug
    that leads to the duplicated inclusion of Liveblocks after all?

--- packages/liveblocks-redux/src/index.ts
+++ packages/liveblocks-redux/src/index.ts
@@ -1,18 +1,17 @@
-if (process.env.NODE_ENV !== "production") {
-  const pkg = "@liveblocks/redux";
-  const format =
-    "__PACKAGE_FORMAT__" === "__PACKAGE" + /* don't join */ "_FORMAT__"
-      ? "ESM"
-      : "__PACKAGE_FORMAT__";
-  const g = globalThis as typeof globalThis & { [key: string]: string };
-  if (g && g[pkg] !== format) {
-    if (g[pkg] === undefined) {
-      g[pkg] = format;
-    } else {
-      console.warn(
-        `${pkg} appears twice in your bundle (as ${g[pkg]} and ${format}). This can lead to hard-to-debug problems. Please see XXX for details.`
-      );
-    }
+const pkg = "@liveblocks/redux";
+const format =
+  "__PACKAGE_FORMAT__" === "__PACKAGE" + /* don't join */ "_FORMAT__"
+    ? "ESM"
+    : "__PACKAGE_FORMAT__";
+const g = globalThis as typeof globalThis & { [key: string]: string };
+if (g && g[pkg] !== format) {
+  if (g[pkg] === undefined) {
+    g[pkg] = format;
+  } else {
+    // XXX Link to documentation with more details/debugging instructions
+    console.warn(
+      `${pkg} appears twice in your bundle (as ${g[pkg]} and ${format}). This can lead to hard-to-debug problems. Please see XXX for details.`
+    );
   }
 }
 
