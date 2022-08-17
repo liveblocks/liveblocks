9e5ecd436774e44ce040e7272b60c8057088f5f7
Vincent Driessen <me@nvie.com>
2022-08-17 10:27:44 +0200

    Add duplication detection snippet to all packages

--- packages/liveblocks-client/src/index.ts
+++ packages/liveblocks-client/src/index.ts
@@ -1,3 +1,21 @@
+if (process.env.NODE_ENV !== "production") {
+  const pkg = "@liveblocks/client";
+  const format =
+    "__PACKAGE_FORMAT__" === "__PACKAGE" + /* don't join */ "_FORMAT__"
+      ? "ESM"
+      : "__PACKAGE_FORMAT__";
+  const g = globalThis as typeof globalThis & { [key: string]: string };
+  if (g && g[pkg] !== format) {
+    if (g[pkg] === undefined) {
+      g[pkg] = format;
+    } else {
+      console.warn(
+        `${pkg} appears twice in your bundle (as ${g[pkg]} and ${format}). This can lead to hard-to-debug problems. Please see XXX for details.`
+      );
+    }
+  }
+}
+
 export { createClient } from "./client";
 export { LiveList } from "./LiveList";
 export { LiveMap } from "./LiveMap";
--- packages/liveblocks-react/src/index.tsx
+++ packages/liveblocks-react/src/index.tsx
@@ -1,3 +1,21 @@
+if (process.env.NODE_ENV !== "production") {
+  const pkg = "@liveblocks/react";
+  const format =
+    "__PACKAGE_FORMAT__" === "__PACKAGE" + /* don't join */ "_FORMAT__"
+      ? "ESM"
+      : "__PACKAGE_FORMAT__";
+  const g = globalThis as typeof globalThis & { [key: string]: string };
+  if (g && g[pkg] !== format) {
+    if (g[pkg] === undefined) {
+      g[pkg] = format;
+    } else {
+      console.warn(
+        `${pkg} appears twice in your bundle (as ${g[pkg]} and ${format}). This can lead to hard-to-debug problems. Please see XXX for details.`
+      );
+    }
+  }
+}
+
 export { LiveblocksProvider, useClient } from "./client";
 
 export { createRoomContext } from "./factory";
