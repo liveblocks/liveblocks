55ff2837013ed6437a3794e46e380bd8c0977bb1
Vincent Driessen <me@nvie.com>
2022-08-22 19:58:41 +0200

    Publish ESM modules as light wrappers around CJS modules (#441)

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,17 +7,29 @@ set -eu
 ROOT="$(git rev-parse --show-toplevel)"
 
 # Target dir to place file into
-DIST="lib"
+DIST="dist"
 
 err () {
     echo "$@" >&2
 }
 
+generate_esm_wrappers () {
+    if [ -f "$DIST/internal.js" ]; then
+        npx gen-esm-wrapper "$DIST/internal.js" "$DIST/internal.mjs"
+    fi
+
+    if [ -f "$DIST/index.js" ]; then
+        npx gen-esm-wrapper "$DIST/index.js" "$DIST/index.mjs"
+    fi
+}
+
 main () {
+    generate_esm_wrappers
+
     # Copy these files into the distribution
     cp "$ROOT/LICENSE" ./README.md "$DIST/"
 
-    # Strip keys from package.json and place the result in lib/
+    # Strip keys from package.json and place the result in dist/
     echo '
     const data = require("./package.json");
 
@@ -29,16 +41,23 @@ main () {
     data.optionalDependencies = undefined;
     data.jest = undefined;
     data.prettier = undefined;
+    data.tsup = undefined;
 
     console.log(JSON.stringify(data));
     ' | node - > "$DIST/package.json"
 
-    prettier --write "$DIST/package.json"
+    node_modules/.bin/prettier --write "$DIST/package.json"
 }
 
 if [ ! -f "./package.json" ]; then
     err "This script should be run from inside a package directory"
     exit 1
+fi
+
+if [ -d lib ]; then
+    # Clean up legacy build outputs that used to get written to
+    # lib/ instead of dist/
+    rm -rf lib
 fi
 
 main
