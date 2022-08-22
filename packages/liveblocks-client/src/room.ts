28068a0f82997bfda1bf9165d20af0104772e827
Vincent Driessen <me@nvie.com>
2022-08-22 16:57:10 +0200

    Oh you, eslint

--- packages/liveblocks-client/src/room.ts
+++ packages/liveblocks-client/src/room.ts
@@ -1752,6 +1752,7 @@ function prepareCreateWebSocket(
   return (token: string): WebSocket => {
     return new ws(
       `${liveblocksServer}/?token=${token}&version=${
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore (__PACKAGE_VERSION__ will be injected by the build script)
         typeof __PACKAGE_VERSION__ === "string" ? __PACKAGE_VERSION__ : "dev"
       }`
