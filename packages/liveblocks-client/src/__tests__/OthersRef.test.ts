8b0e8285adce22e1dc5e43a58195a638a2de8601
Vincent Driessen <me@nvie.com>
2022-09-02 12:06:36 +0200

    Clean up

--- packages/liveblocks-client/src/__tests__/OthersRef.test.ts
+++ packages/liveblocks-client/src/__tests__/OthersRef.test.ts
@@ -16,10 +16,10 @@ type M = {
 describe('Read-only "others" ref cache', () => {
   describe('Tracking "others"', () => {
     it("setting alone is not enough other", () => {
-      const p = new OthersRef<P, M>();
-      p.setOther(2, { x: 1, y: 1 });
+      const others = new OthersRef<P, M>();
+      others.setOther(2, { x: 1, y: 1 });
 
-      expect(p.others).toStrictEqual(
+      expect(others.others).toStrictEqual(
         [] // NOTE: Even though .setOther() is called, this is still empty!
       );
 
@@ -27,124 +27,124 @@ describe('Read-only "others" ref cache', () => {
       // information is known for this user. Normally, this information is
       // known before the .setOther() call is made, unlike how this test case
       // is structured.
-      p.setConnection(2, "user-123", undefined);
-      expect(p.others).toStrictEqual([
+      others.setConnection(2, "user-123", undefined);
+      expect(others.others).toStrictEqual([
         { connectionId: 2, id: "user-123", presence: { x: 1, y: 1 } },
       ]);
     });
 
     it("setting other", () => {
-      const p = new OthersRef<P, M>();
-      p.setConnection(2, "user-123", undefined);
-      p.setConnection(3, "user-567", undefined);
+      const others = new OthersRef<P, M>();
+      others.setConnection(2, "user-123", undefined);
+      others.setConnection(3, "user-567", undefined);
 
-      p.setOther(2, { x: 2, y: 2 });
-      p.setOther(3, { x: 3, y: 3 });
-      p.setOther(2, { x: -2, y: -2 });
+      others.setOther(2, { x: 2, y: 2 });
+      others.setOther(3, { x: 3, y: 3 });
+      others.setOther(2, { x: -2, y: -2 });
 
-      expect(p.others).toStrictEqual([
+      expect(others.others).toStrictEqual([
         { connectionId: 2, id: "user-123", presence: { x: -2, y: -2 } },
         { connectionId: 3, id: "user-567", presence: { x: 3, y: 3 } },
       ]);
     });
 
     it("setting others removes explicitly-undefined keys", () => {
-      const p = new OthersRef<P, M>();
-      p.setConnection(2, "user-123", undefined);
-      p.setOther(2, { x: 2, y: 2, z: undefined });
+      const others = new OthersRef<P, M>();
+      others.setConnection(2, "user-123", undefined);
+      others.setOther(2, { x: 2, y: 2, z: undefined });
       //                             ^^^^^^^^^ 🔑
 
-      expect(p.others).toStrictEqual(
+      expect(others.others).toStrictEqual(
         [{ connectionId: 2, id: "user-123", presence: { x: 2, y: 2 } }]
         //                                              ^ 🔑 (no explicit undefined here)
       );
     });
 
     it("patching others ignores patches for unknown users", () => {
-      const p = new OthersRef<P, M>();
-      p.setConnection(2, "user-123", undefined);
-      p.patchOther(2, { y: 1, z: 2 }); // .setOther() not called yet for actor 2
+      const others = new OthersRef<P, M>();
+      others.setConnection(2, "user-123", undefined);
+      others.patchOther(2, { y: 1, z: 2 }); // .setOther() not called yet for actor 2
 
-      expect(p.others).toStrictEqual([]);
+      expect(others.others).toStrictEqual([]);
     });
 
     it("patching others", () => {
-      const p = new OthersRef<P, M>();
-      p.setConnection(2, "user-123", undefined);
-      p.setOther(2, { x: 2, y: 2 });
-      expect(p.others).toStrictEqual([
+      const others = new OthersRef<P, M>();
+      others.setConnection(2, "user-123", undefined);
+      others.setOther(2, { x: 2, y: 2 });
+      expect(others.others).toStrictEqual([
         { connectionId: 2, id: "user-123", presence: { x: 2, y: 2 } },
       ]);
 
-      p.patchOther(2, { y: -2, z: -2 });
-      expect(p.others).toStrictEqual([
+      others.patchOther(2, { y: -2, z: -2 });
+      expect(others.others).toStrictEqual([
         { connectionId: 2, id: "user-123", presence: { x: 2, y: -2, z: -2 } },
       ]);
 
-      p.patchOther(2, { z: undefined });
-      expect(p.others).toStrictEqual([
+      others.patchOther(2, { z: undefined });
+      expect(others.others).toStrictEqual([
         { connectionId: 2, id: "user-123", presence: { x: 2, y: -2 } },
       ]);
     });
 
     it("removing connections", () => {
-      const p = new OthersRef<P, M>();
-      p.setConnection(2, "user-123", undefined);
-      p.setOther(2, { x: 2, y: 2 });
+      const others = new OthersRef<P, M>();
+      others.setConnection(2, "user-123", undefined);
+      others.setOther(2, { x: 2, y: 2 });
 
-      expect(p.getUser(2)).toStrictEqual({
+      expect(others.getUser(2)).toStrictEqual({
         connectionId: 2,
         id: "user-123",
         presence: { x: 2, y: 2 },
       });
-      p.removeConnection(2);
+      others.removeConnection(2);
 
-      expect(p.getUser(2)).toBeUndefined();
-      expect(p.others).toStrictEqual([]);
+      expect(others.getUser(2)).toBeUndefined();
+      expect(others.others).toStrictEqual([]);
 
       // Setting other without .setConnection() will have no effect
-      p.setOther(2, { x: 2, y: 2 });
-      expect(p.getUser(2)).toBeUndefined();
-      expect(p.others).toStrictEqual([]);
+      others.setOther(2, { x: 2, y: 2 });
+      expect(others.getUser(2)).toBeUndefined();
+      expect(others.others).toStrictEqual([]);
     });
   });
 
   describe("caching", () => {
     it("caches immutable results (others)", () => {
-      const p = new OthersRef<P, M>();
-      p.setConnection(2, "user-123", undefined);
-      p.setOther(2, { x: 2, y: 2 });
+      const others = new OthersRef<P, M>();
+      others.setConnection(2, "user-123", undefined);
+      others.setOther(2, { x: 2, y: 2 });
 
-      const others1 = p.others;
-      const others2 = p.others;
+      const others1 = others.others;
+      const others2 = others.others;
       expect(others1).toBe(others2);
 
       // These are effectively no-ops
-      p.patchOther(2, { x: 2 });
-      p.patchOther(2, { y: 2, z: undefined });
+      others.patchOther(2, { x: 2 });
+      others.patchOther(2, { y: 2, z: undefined });
 
-      const others3 = p.others;
+      const others3 = others.others;
       expect(others2).toBe(others3); // No observable change!
 
-      const others4 = p.others;
-      const others5 = p.others;
+      const others4 = others.others;
+      const others5 = others.others;
       expect(others3).toBe(others4); // Others did not change
       expect(others4).toBe(others5);
 
-      p.patchOther(2, { y: -2 });
+      others.patchOther(2, { y: -2 });
 
-      const others6 = p.others;
-      const others7 = p.others;
+      const others6 = others.others;
+      const others7 = others.others;
       expect(others5).not.toBe(others6); // Others changed
       expect(others6).toBe(others7);
     });
 
     it("getUser() returns stable cache results", () => {
-      const p = new OthersRef<P, M>();
-      p.setConnection(2, "user-123", undefined);
-      p.setOther(2, { x: 2, y: 2 });
+      const others = new OthersRef<P, M>();
+      others.setConnection(2, "user-123", undefined);
+      others.setOther(2, { x: 2, y: 2 });
 
-      expect(p.getUser(2)).toBe(p.getUser(2));
+      expect(others.getUser(2)).toBe(others.getUser(2));
     });
   });
 });
