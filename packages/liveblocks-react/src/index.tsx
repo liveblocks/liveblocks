9f089d825a9635877a2af28a55833b7c9275e868
Vincent Driessen <me@nvie.com>
2022-09-07 09:34:05 +0200

    Ship `ClientSideSuspense` and `shallow` with React package, too

--- packages/liveblocks-react/src/index.tsx
+++ packages/liveblocks-react/src/index.tsx
@@ -1,5 +1,7 @@
+export { ClientSideSuspense } from "./ClientSideSuspense";
 export { createRoomContext } from "./factory";
 export type { MutationContext } from "./factory";
 
 // Re-exports from @liveblocks/client, for convenience
 export type { Json, JsonObject } from "@liveblocks/client";
+export { shallow } from "@liveblocks/client";
