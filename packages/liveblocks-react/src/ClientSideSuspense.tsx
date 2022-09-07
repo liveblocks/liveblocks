0faf9c5fd2a5e84d3d3df1d9b0b8869ff0f12695
Vincent Driessen <me@nvie.com>
2022-09-07 09:57:12 +0200

    Use the same import style as elsewhere in this project

--- packages/liveblocks-react/src/ClientSideSuspense.tsx
+++ packages/liveblocks-react/src/ClientSideSuspense.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react";
-import { Suspense, useEffect, useState } from "react";
+import * as React from "react";
 
 type Props = {
   fallback: NonNullable<ReactNode> | null;
@@ -24,17 +24,17 @@ type Props = {
  *
  */
 export function ClientSideSuspense(props: Props): ReactNode {
-  const [mounted, setMounted] = useState(false);
+  const [mounted, setMounted] = React.useState(false);
 
-  useEffect(() => {
+  React.useEffect(() => {
     // Effects are never executed on the server side. The point of this is to
     // delay the flipping of this boolean until after hydration has happened.
     setMounted(true);
   }, []);
 
   return (
-    <Suspense fallback={props.fallback}>
+    <React.Suspense fallback={props.fallback}>
       {mounted ? props.children() : props.fallback}
-    </Suspense>
+    </React.Suspense>
   );
 }
