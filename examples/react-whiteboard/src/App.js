1ed2dcf649cf15d879754bafd7ad8c7e17420a1d
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2022-09-13 15:22:16 +0100

    feat: Updated whiteboard for 0.18

--- examples/react-whiteboard/src/App.js
+++ examples/react-whiteboard/src/App.js
@@ -1,17 +1,16 @@
-import { useState, useEffect, memo } from "react";
+import { useState, memo } from "react";
 import {
-  useMyPresence,
-  useMap,
   useHistory,
   useCanUndo,
   useCanRedo,
-  useBatch,
-  useRoom,
   useOthers,
-} from "./liveblocks.config";
-import { LiveObject } from "@liveblocks/client";
-
-import "./App.css";
+  RoomProvider,
+  useStorage,
+  useMutation,
+  useSelf,
+} from './liveblocks.config'
+import { LiveMap, LiveObject } from "@liveblocks/client";
+import { ClientSideSuspense, shallow } from "@liveblocks/react";
 
 const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
 
@@ -23,79 +22,84 @@ function getRandomColor() {
   return COLORS[getRandomInt(COLORS.length)];
 }
 
-export default function App() {
-  const shapes = useMap("shapes");
-
-  if (shapes == null) {
-    return (
-      <div className="loading">
-        <img src="https://liveblocks.io/loading.svg" alt="Loading" />
-      </div>
-    );
-  }
-
-  return <Canvas shapes={shapes} />;
+export default function App({ roomId }) {
+  return (
+    <RoomProvider
+      id={roomId}
+      initialPresence={{ selectedShape: null }}
+      initialStorage={{
+        shapes: new LiveMap(),
+      }}
+    >
+      <ClientSideSuspense fallback={<Loading />}>
+        {() => <Canvas />}
+      </ClientSideSuspense>
+    </RoomProvider>
+  )
 }
 
-function Canvas({ shapes }) {
+function Canvas() {
   const [isDragging, setIsDragging] = useState(false);
+  const shapeIds = useStorage((root) => Array.from(root.shapes.keys()), shallow);
 
-  const [{ selectedShape }, setPresence] = useMyPresence();
-  const batch = useBatch();
+  const selectedShape = useSelf((me) => me.presence.selectedShape);
   const history = useHistory();
   const canUndo = useCanUndo();
   const canRedo = useCanRedo();
-  const others = useOthers();
 
-  const insertRectangle = () => {
-    batch(() => {
-      const shapeId = Date.now().toString();
-      const shape = new LiveObject({
-        x: getRandomInt(300),
-        y: getRandomInt(300),
-        fill: getRandomColor(),
-      });
-      shapes.set(shapeId, shape);
-      setPresence({ selectedShape: shapeId }, { addToHistory: true });
+  const insertRectangle = useMutation(({ root, setMyPresence }) => {
+    const shapeId = Date.now().toString();
+    const shape = new LiveObject({
+      x: getRandomInt(300),
+      y: getRandomInt(300),
+      fill: getRandomColor(),
     });
-  };
+    root.get("shapes").set(shapeId, shape);
+    setMyPresence({ selectedShape: shapeId }, { addToHistory: true });
+  }, []);
 
-  const deleteRectangle = () => {
-    shapes.delete(selectedShape);
-  };
+  const deleteRectangle = useMutation(({ root }, shapeId) => {
+    root.get("shapes").delete(shapeId);
+  }, []);
 
-  const onShapePointerDown = (e, shapeId) => {
+  const onShapePointerDown = useMutation(({ setMyPresence }, e, shapeId) => {
     history.pause();
     e.stopPropagation();
 
-    setPresence({ selectedShape: shapeId }, { addToHistory: true });
-
+    setMyPresence({ selectedShape: shapeId }, { addToHistory: true });
     setIsDragging(true);
-  };
+  }, [history]);
 
-  const onCanvasPointerUp = (e) => {
+  const onCanvasPointerUp = useMutation(({ setMyPresence }, e) => {
     if (!isDragging) {
-      setPresence({ selectedShape: null }, { addToHistory: true });
+      setMyPresence({ selectedShape: null }, { addToHistory: true });
     }
 
     setIsDragging(false);
-
     history.resume();
-  };
+  }, [isDragging, history]);
 
-  const onCanvasPointerMove = (e) => {
+  const onCanvasPointerMove = useMutation(({ root, self }, e) => {
     e.preventDefault();
-
-    if (isDragging) {
-      const shape = shapes.get(selectedShape);
-      if (shape) {
-        shape.update({
-          x: e.clientX - 50,
-          y: e.clientY - 50,
-        });
-      }
+    if (!isDragging) {
+      return;
     }
-  };
+
+    const shapeId = self.presence.selectedShape;
+    if (!shapeId) {
+      return;
+    }
+
+    const shape = root.get("shapes").get(shapeId);
+    if (!shape) {
+      return
+    }
+
+    shape.update({
+      x: e.clientX - 50,
+      y: e.clientY - 50,
+    });
+  }, [isDragging]);
 
   return (
     <>
@@ -104,31 +108,19 @@ function Canvas({ shapes }) {
         onPointerMove={onCanvasPointerMove}
         onPointerUp={onCanvasPointerUp}
       >
-        {Array.from(shapes, ([shapeId, shape]) => {
-          let selectionColor =
-            selectedShape === shapeId
-              ? "blue"
-              : others
-                  .toArray()
-                  .some((user) => user.presence?.selectedShape === shapeId)
-              ? "green"
-              : undefined;
-
+        {shapeIds.map((shapeId) => {
           return (
             <Rectangle
               key={shapeId}
               id={shapeId}
               onShapePointerDown={onShapePointerDown}
-              shape={shape}
-              selectionColor={selectionColor}
-              transition={selectedShape !== shapeId}
             />
           );
         })}
       </div>
       <div className="toolbar">
-        <button onClick={insertRectangle}>Rectangle</button>
-        <button onClick={deleteRectangle} disabled={selectedShape == null}>
+        <button onClick={() => insertRectangle()}>Rectangle</button>
+        <button onClick={() => deleteRectangle(selectedShape)} disabled={selectedShape == null}>
           Delete
         </button>
         <button onClick={history.undo} disabled={!canUndo}>
@@ -143,18 +135,12 @@ function Canvas({ shapes }) {
 }
 
 const Rectangle = memo(
-  ({ shape, id, onShapePointerDown, selectionColor, transition }) => {
-    const [{ x, y, fill }, setShapeData] = useState(shape.toObject());
+  ({ id, onShapePointerDown }) => {
+    const { x, y, fill } = useStorage((root) => root.shapes.get(id));
 
-    const room = useRoom();
-
-    useEffect(() => {
-      function onChange() {
-        setShapeData(shape.toObject());
-      }
-
-      return room.subscribe(shape, onChange);
-    }, [room, shape]);
+    const selectedByMe = useSelf((me) => me.presence.selectedShape === id);
+    const selectedByOthers = useOthers((others) => others.some(other => other.presence.selectedShape === id));
+    const selectionColor = selectedByMe ? "blue" : selectedByOthers ? "green" : "transparent";
 
     return (
       <div
@@ -162,11 +148,19 @@ const Rectangle = memo(
         className="rectangle"
         style={{
           transform: `translate(${x}px, ${y}px)`,
-          transition: transition ? "transform 120ms linear" : "none",
+          transition: !selectedByMe ? "transform 120ms linear" : "none",
           backgroundColor: fill ? fill : "#CCC",
-          borderColor: selectionColor || "transparent",
+          borderColor: selectionColor,
         }}
       />
     );
   }
 );
+
+function Loading() {
+  return (
+    <div className="loading">
+      <img src="https://liveblocks.io/loading.svg" alt="Loading" />
+    </div>
+  );
+}
