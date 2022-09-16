8165e4f21d03789eb55b5e09262e5f9475032cbc
Florent Lefebvre <florent.pm.lefebvre@gmail.com>
2022-09-16 14:04:48 -0400

    feat(react-dashboard): convert to 0.18 (#498)

--- examples/react-dashboard/src/components/Header.js
+++ examples/react-dashboard/src/components/Header.js
@@ -1,27 +1,21 @@
-import { useOthers } from "../liveblocks.config";
+import { useOthers, useOthersConnectionIds } from "../liveblocks.config";
 import Avatar from "./Avatar";
 import { COLORS_PRESENCE } from "../constants";
 import styles from "./Header.module.css";
 
 export default function Header() {
-  const others = useOthers();
+  const connectionIds = useOthersConnectionIds();
 
   return (
     <header className={styles.container}>
       <h1>Dashboard</h1>
       <div className={styles.container_avatars}>
-        {others.map(({ connectionId }) => {
-          if (!connectionId) {
-            return null;
-          }
-
-          return (
-            <Avatar
-              key={connectionId}
-              color={COLORS_PRESENCE[connectionId % COLORS_PRESENCE.length]}
-            />
-          );
-        })}
+        {connectionIds.map((connectionId) => (
+          <Avatar
+            key={connectionId}
+            color={COLORS_PRESENCE[connectionId % COLORS_PRESENCE.length]}
+          />
+        ))}
       </div>
     </header>
   );
