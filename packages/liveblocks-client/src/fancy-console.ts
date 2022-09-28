4739e021dd94c6793ce7621354fd5aa955e929f4
Vincent Driessen <me@nvie.com>
2022-09-28 19:36:15 +0200

    Fancy wrapping

--- packages/liveblocks-client/src/fancy-console.ts
+++ packages/liveblocks-client/src/fancy-console.ts
@@ -2,27 +2,34 @@ const badge =
   "background:radial-gradient(106.94% 108.33% at -10% -5%,#ff1aa3 0,#ff881a 100%);border-radius:6px;color:#fff;padding:2px 6px";
 const bold = "font-weight:600";
 
-export const log = (message: string, ...args: readonly unknown[]) =>
-  console.log("%cliveblocks", badge, message, ...args);
-export const warn = (message: string, ...args: readonly unknown[]) =>
-  console.warn("%cliveblocks", badge, message, ...args);
-export const error = (message: string, ...args: readonly unknown[]) =>
-  console.error("%cliveblocks", badge, message, ...args);
+function wrap(
+  method: "log" | "warn" | "error"
+): (message: string, ...args: readonly unknown[]) => void {
+  return typeof window === "undefined" || process.env.NODE_ENV === "test"
+    ? console[method]
+    : (message, ...args) =>
+        console[method]("%cliveblocks", badge, message, ...args);
+}
 
-export const logBold = (
-  title: string,
-  message: string,
-  ...args: readonly unknown[]
-) => console.log(`%cliveblocks%c ${title}`, badge, bold, message, ...args);
+export const log = wrap("log");
+export const warn = wrap("warn");
+export const error = wrap("error");
 
-export const warnBold = (
-  title: string,
-  message: string,
-  ...args: readonly unknown[]
-) => console.warn(`%cliveblocks%c ${title}`, badge, bold, message, ...args);
+function wrapBold(
+  method: "log" | "warn" | "error"
+): (title: string, message: string, ...args: readonly unknown[]) => void {
+  return typeof window === "undefined" || process.env.NODE_ENV === "test"
+    ? console[method]
+    : (title, message, ...args) =>
+        console[method](
+          `%cliveblocks%c ${title}`,
+          badge,
+          bold,
+          message,
+          ...args
+        );
+}
 
-export const errorBold = (
-  title: string,
-  message: string,
-  ...args: readonly unknown[]
-) => console.error(`%cliveblocks%c ${title}`, badge, bold, message, ...args);
+export const logBold = wrapBold("log");
+export const warnBold = wrapBold("warn");
+export const errorBold = wrapBold("error");
