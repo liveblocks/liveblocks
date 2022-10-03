b0a1be6c9795e709568488fe686ac9c8e9907cc9
Chris <ctnicholasdev@gmail.com>
2022-10-03 10:24:23 +0100

    Updated react-whiteboard for 0.18 (#487)
    * feat: React whiteboard updated for 0.18
    
    * feat: Updated to 0.18.0-beta4
    
    * feat: Reordered functions to match to-do list
    
    * Updated to 0.18
    
    * chore: Update to 0.18.2
    
    * feat: Updated and slightly simplified to make whiteboard tutorial easier to read

--- examples/react-whiteboard/src/index.js
+++ examples/react-whiteboard/src/index.js
@@ -1,28 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-import { LiveMap } from "@liveblocks/client";
-import { RoomProvider } from "./liveblocks.config";
-import "./index.css";
+import "./App.css";
 
 let roomId = "react-whiteboard";
-
 overrideRoomId();
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <React.StrictMode>
-    <RoomProvider
-      id={roomId}
-      initialStorage={{
-        shapes: new LiveMap(),
-      }}
-    >
-      <App />
-    </RoomProvider>
-  </React.StrictMode>
-);
+const root = createRoot(document.getElementById("root"));
+root.render(<App roomId={roomId} />);
 
 /**
  * This function is used when deploying an example on liveblocks.io.
