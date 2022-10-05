ff66aa97a84fc7f99fdaf39295d5f718f21d0b6c
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2022-10-05 09:22:04 +0200

    Add custom gray color scale

--- packages/liveblocks-devtools/tailwind.config.js
+++ packages/liveblocks-devtools/tailwind.config.js
@@ -1,4 +1,22 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          50: "#f8f9f9",
+          100: "#f1f3f4",
+          200: "#dee1e6",
+          300: "#c2c6cd",
+          400: "#9ea2a7",
+          500: "#77787d",
+          600: "#4e4f53",
+          700: "#35363a",
+          800: "#292a2d",
+          900: "#202023",
+        },
+      },
+    },
+  },
 };
