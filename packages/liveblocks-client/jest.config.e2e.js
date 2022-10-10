028ce753daa56ae631167db746060270f0a8f5e5
Vincent Driessen <me@nvie.com>
2022-10-10 15:49:25 +0200

    Fix CI tests

--- packages/liveblocks-client/jest.config.e2e.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  testEnvironment: "node",
-  preset: "ts-jest",
-  testTimeout: 30000,
-  modulePathIgnorePatterns: ["<rootDir>/dist/"],
-  testPathIgnorePatterns: ["__tests__/_.*"],
-  roots: ["<rootDir>/e2e"],
-};
