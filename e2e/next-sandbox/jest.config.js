d7cda34fb166e383e63fb3787767c61cc24106a2
Vincent Driessen <me@nvie.com>
2022-10-13 12:15:02 +0200

    Reuse our common jest setup in next-sandbox, too

--- e2e/next-sandbox/jest.config.js
+++ e2e/next-sandbox/jest.config.js
@@ -1,7 +1,13 @@
+const commonJestConfig = require("@liveblocks/jest-config");
+
 module.exports = {
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  verbose: true,
+  // Our standard Jest configuration, used by all projects in this monorepo
+  ...commonJestConfig,
+
+  // transform: {
+  //   "^.+\\.tsx?$": "ts-jest",
+  // },
+
   testTimeout: 6000000,
+  verbose: true,
 };
