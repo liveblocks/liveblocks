7d09f74730570662bc723fa33b1490743fbfba3c
Vincent Driessen <me@nvie.com>
2022-10-26 09:37:14 +0200

    Remove persist middleware from the basic test

--- packages/liveblocks-zustand/test-d/basic.test-d.ts
+++ packages/liveblocks-zustand/test-d/basic.test-d.ts
@@ -1,6 +1,5 @@
 import { createClient, LiveList } from "@liveblocks/client";
 import { middleware } from "@liveblocks/zustand";
-import { persist } from "zustand/middleware";
 
 import create from "zustand";
 
@@ -35,61 +34,56 @@ type RoomEvent = {
 const client = createClient({ authEndpoint: "/api/auth" });
 
 const useStore = create(
-  persist(
-    middleware<MyState, Presence, Storage, BaseUser, RoomEvent>(
-      (set, get, _api) => ({
-        value: 0,
-        setValue: (_newValue: number) => {
-          // Liveblocks state should be available here
-          const {
-            others: _others,
-            connection: _connection,
-            enterRoom: _enterRoom,
-            leaveRoom: _leaveRoom,
-            isStorageLoading: _isStorageLoading,
-            room: _room,
-          } = get().liveblocks;
+  middleware<MyState, Presence, Storage, BaseUser, RoomEvent>(
+    (set, get, _api) => ({
+      value: 0,
+      setValue: (_newValue: number) => {
+        // Liveblocks state should be available here
+        const {
+          others: _others,
+          connection: _connection,
+          enterRoom: _enterRoom,
+          leaveRoom: _leaveRoom,
+          isStorageLoading: _isStorageLoading,
+          room: _room,
+        } = get().liveblocks;
 
-          expectError((get().liveblocks = {})); // Readonly
+        expectError((get().liveblocks = {})); // Readonly
 
-          const liveblocksState = get().liveblocks;
+        const liveblocksState = get().liveblocks;
 
-          expectError((liveblocksState.room = {} as any)); // Readonly
+        expectError((liveblocksState.room = {} as any)); // Readonly
 
-          expectError((liveblocksState.others = {} as any)); // Readonly
+        expectError((liveblocksState.others = {} as any)); // Readonly
 
-          // Presence tests
-          expectType<Presence>(liveblocksState.others[0]!.presence);
-          expectError(liveblocksState.others[0]!.presence.nonexistingProperty);
+        // Presence tests
+        expectType<Presence>(liveblocksState.others[0]!.presence);
+        expectError(liveblocksState.others[0]!.presence.nonexistingProperty);
 
-          // UserMeta tests
-          expectType<Meta>(liveblocksState.others[0]!.info);
-          expectError(liveblocksState.others[0]!.info.nonexistingProperty);
+        // UserMeta tests
+        expectType<Meta>(liveblocksState.others[0]!.info);
+        expectError(liveblocksState.others[0]!.info.nonexistingProperty);
 
+        liveblocksState.room?.broadcastEvent({
+          type: "MESSAGE",
+          value: "string",
+        });
+
+        expectError(
           liveblocksState.room?.broadcastEvent({
-            type: "MESSAGE",
-            value: "string",
-          });
+            type: "INVALID_MESSAGE",
+          })
+        );
 
-          expectError(
-            liveblocksState.room?.broadcastEvent({
-              type: "INVALID_MESSAGE",
-            })
-          );
+        liveblocksState.room?.getStorage().then((storage) => {
+          storage.root.get("todos");
+          expectError(storage.root.get("unknown_key"));
+        });
 
-          liveblocksState.room?.getStorage().then((storage) => {
-            storage.root.get("todos");
-            expectError(storage.root.get("unknown_key"));
-          });
-
-          return set({ value: get().value });
-        },
-      }),
-      { client, storageMapping: {}, presenceMapping: {} }
-    ),
-    {
-      name: "persist-name",
-    }
+        return set({ value: get().value });
+      },
+    }),
+    { client, storageMapping: {}, presenceMapping: {} }
   )
 );
 
