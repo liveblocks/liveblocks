6cc7cfb7fcc1b740c092342c317a379b926a4a64
Vincent Driessen <me@nvie.com>
2022-10-26 10:32:47 +0200

    Express the desired outcome in the type tests

--- packages/liveblocks-zustand/test-d/basic.test-d.ts
+++ packages/liveblocks-zustand/test-d/basic.test-d.ts
@@ -3,7 +3,7 @@ import { middleware } from "@liveblocks/zustand";
 
 import create from "zustand";
 
-import { expectError, expectType } from "tsd";
+import { expectAssignable, expectError, expectType } from "tsd";
 
 type MyState = {
   value: number;
@@ -33,69 +33,87 @@ type RoomEvent = {
 
 const client = createClient({ authEndpoint: "/api/auth" });
 
-const useStore = create(
-  middleware<MyState, Presence, Storage, BaseUser, RoomEvent>(
-    (set, get, _api) => ({
+const useStore = create<MyState>()(
+  middleware(
+    (set, get, api) => ({
       value: 0,
-      setValue: (_newValue: number) => {
+      setValue: (newValue: number) => {
+        const oldValue = get().value;
+        expectType<number>(oldValue);
+
+        const liveblocks1 = api.liveblocks;
+        expectType<boolean>(liveblocks1.isStorageLoading);
+        expectAssignable<Function>(liveblocks1.enterRoom);
+        expectAssignable<Function>(liveblocks1.leaveRoom);
+        expectType<string>(liveblocks1.room!.id);
+
+        const liveblocks2 = get().liveblocks;
+        expectType<boolean>(liveblocks2.isStorageLoading);
+        expectAssignable<Function>(liveblocks2.enterRoom);
+        expectAssignable<Function>(liveblocks2.leaveRoom);
+        expectType<string>(liveblocks2.room!.id);
+
         // Liveblocks state should be available here
-        const {
-          others: _others,
-          connection: _connection,
-          enterRoom: _enterRoom,
-          leaveRoom: _leaveRoom,
-          isStorageLoading: _isStorageLoading,
-          room: _room,
-        } = get().liveblocks;
+        // const {
+        //   others: _others,
+        //   connection: _connection,
+        //   enterRoom: _enterRoom,
+        //   leaveRoom: _leaveRoom,
+        //   isStorageLoading: _isStorageLoading,
+        //   room: _room,
+        // } = get().liveblocks;
 
-        expectError((get().liveblocks = {})); // Readonly
+        // expectError((get().liveblocks = {})); // Readonly
 
-        const liveblocksState = get().liveblocks;
+        // const liveblocksState = get().liveblocks;
 
-        expectError((liveblocksState.room = {} as any)); // Readonly
+        // expectError((liveblocksState.room = {} as any)); // Readonly
 
-        expectError((liveblocksState.others = {} as any)); // Readonly
+        // expectError((liveblocksState.others = {} as any)); // Readonly
 
-        // Presence tests
-        expectType<Presence>(liveblocksState.others[0]!.presence);
-        expectError(liveblocksState.others[0]!.presence.nonexistingProperty);
+        // // Presence tests
+        // expectType<Presence>(liveblocksState.others[0]!.presence);
+        // expectError(liveblocksState.others[0]!.presence.nonexistingProperty);
 
-        // UserMeta tests
-        expectType<Meta>(liveblocksState.others[0]!.info);
-        expectError(liveblocksState.others[0]!.info.nonexistingProperty);
+        // // UserMeta tests
+        // expectType<Meta>(liveblocksState.others[0]!.info);
+        // expectError(liveblocksState.others[0]!.info.nonexistingProperty);
 
-        liveblocksState.room?.broadcastEvent({
-          type: "MESSAGE",
-          value: "string",
-        });
+        // liveblocksState.room?.broadcastEvent({
+        //   type: "MESSAGE",
+        //   value: "string",
+        // });
 
-        expectError(
-          liveblocksState.room?.broadcastEvent({
-            type: "INVALID_MESSAGE",
-          })
-        );
+        // expectError(
+        //   liveblocksState.room?.broadcastEvent({
+        //     type: "INVALID_MESSAGE",
+        //   })
+        // );
 
-        liveblocksState.room?.getStorage().then((storage) => {
-          storage.root.get("todos");
-          expectError(storage.root.get("unknown_key"));
-        });
+        // liveblocksState.room?.getStorage().then((storage) => {
+        //   storage.root.get("todos");
+        //   expectError(storage.root.get("unknown_key"));
+        // });
 
-        return set({ value: get().value });
+        // Cannot write to liveblocks key
+        // expectError(set({ liveblocks: '😈' }));
+
+        return set({ value: newValue });
       },
     }),
     { client, storageMapping: {}, presenceMapping: {} }
   )
 );
 
-const { liveblocks } = useStore.getState();
+const liveblocks3 = useStore.getState().liveblocks;
+expectType<boolean>(liveblocks3.isStorageLoading);
+expectAssignable<Function>(liveblocks3.enterRoom);
+expectAssignable<Function>(liveblocks3.leaveRoom);
+expectType<string>(liveblocks3.room!.id);
 
-expectType<(roomId: string, initialState: Partial<MyState>) => void>(
-  liveblocks.enterRoom
-);
-
-expectError((liveblocks.enterRoom = () => {})); // Readonly
-expectError((liveblocks.leaveRoom = () => {})); // Readonly
-expectError((liveblocks.connection = "open")); // Readonly
-expectError((liveblocks.others = [])); // Readonly
-expectError((liveblocks.isStorageLoading = false)); // Readonly
-expectError((liveblocks.room = {})); // Readonly
+// expectError((liveblocks.enterRoom = () => {})); // Readonly
+// expectError((liveblocks.leaveRoom = () => {})); // Readonly
+// expectError((liveblocks.connection = "open")); // Readonly
+// expectError((liveblocks.others = [])); // Readonly
+// expectError((liveblocks.isStorageLoading = false)); // Readonly
+// expectError((liveblocks.room = {})); // Readonly
