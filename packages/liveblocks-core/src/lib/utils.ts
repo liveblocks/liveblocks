55326414a9c18eaeac4eb4249293dc76073ba784
Vincent Driessen <me@nvie.com>
2022-11-07 11:21:29 +0100

    Add new mapValues() helper
    This is a common pattern we use in many places, and it will allow us to
    easily express object-mappings as expressions, rather than requiring us
    to rewrite this function again and again.

--- packages/liveblocks-core/src/lib/utils.ts
+++ packages/liveblocks-core/src/lib/utils.ts
@@ -48,10 +48,27 @@ export function keys<O extends { [key: string]: unknown }, K extends keyof O>(
 /**
  * Drop-in replacement for Object.values() that retains better types.
  */
-export function values<O extends { [key: string]: unknown }>(
+export function values<O extends Record<string, unknown>>(
   obj: O
 ): O[keyof O][] {
   return Object.values(obj) as O[keyof O][];
+}
+
+/**
+ * Creates a new object by mapping a function over all values. Keys remain the
+ * same. Think Array.prototype.map(), but for values in an object.
+ */
+export function mapValues<V, O extends Record<string, unknown>>(
+  obj: O,
+  mapFn: (value: O[keyof O], key: keyof O) => V
+): { [K in keyof O]: V } {
+  const result = {} as { [K in keyof O]: V };
+  for (const pair of Object.entries(obj)) {
+    const key: keyof O = pair[0];
+    const value = pair[1] as O[keyof O];
+    result[key] = mapFn(value, key);
+  }
+  return result;
 }
 
 /**
