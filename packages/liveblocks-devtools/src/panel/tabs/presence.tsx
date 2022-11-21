ce3fd80bb73bb6f0100debebc165b96ef34fb5f9
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2022-11-21 13:47:39 +0100

    Merge presence trees

--- packages/liveblocks-devtools/src/panel/tabs/presence.tsx
+++ packages/liveblocks-devtools/src/panel/tabs/presence.tsx
@@ -3,32 +3,17 @@ import { useMemo } from "react";
 import { Tree } from "../components/Tree";
 import { useMe, useOthers } from "../contexts/CurrentRoom";
 
-export function Me() {
-  const me = useMe();
-  const data = useMemo(() => (me ? [me] : null), [me]);
-
-  return (
-    <div className="border-light-300 dark:border-dark-300 relative flex h-1/3 flex-none border-b">
-      {data !== null ? <Tree data={data} /> : null}
-    </div>
-  );
-}
-
-export function Others() {
-  const others = useOthers();
-
-  return (
-    <div className="relative w-full flex-1">
-      {others !== null ? <Tree data={others} openByDefault={false} /> : null}
-    </div>
-  );
-}
-
 export function Presence() {
+  const me = useMe();
+  const others = useOthers();
+  const data = useMemo(
+    () => (me && others.length > 0 ? (me ? [me, ...others] : others) : null),
+    [me, others]
+  );
+
   return (
     <div className="absolute inset-0 flex h-full flex-col">
-      <Me />
-      <Others />
+      {data !== null ? <Tree data={data} openByDefault={false} /> : null}
     </div>
   );
 }
