1c09e2c7fb402afc082d367dae7386bd525a1198
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2022-12-21 17:27:34 +0000

    feat: Added confirm dialog if someone tries to create a git repo inside a git repo

--- packages/create-liveblocks-app/bin/utils/initializeGit.ts
+++ packages/create-liveblocks-app/bin/utils/initializeGit.ts
@@ -2,14 +2,10 @@ import c from "ansi-colors";
 import { loadingSpinner } from "./loadingSpinner";
 import { execAsync } from "./execAsync";
 import { execSync } from "child_process";
+import prompts, { PromptObject } from "prompts";
 
 export async function initializeGit({ appDir }: { appDir: string }) {
-  const spinner = loadingSpinner().start("Initializing git...");
-
-  const options = {
-    cwd: appDir,
-    stdio: "pipe",
-  } as const;
+  let spinner;
 
   try {
     // Check git installed
@@ -18,40 +14,68 @@ export async function initializeGit({ appDir }: { appDir: string }) {
       encoding: "utf-8",
     });
 
-    // Check if inside a repo
-    let insideRepo;
-    try {
-      const result = execSync("git rev-parse --is-inside-work-tree", {
-        cwd: appDir,
-        encoding: "utf-8",
-      });
-      insideRepo = result === "true";
-    } catch (err) {
-      // Error means not inside a repo
-      insideRepo = false;
-    }
-
-    // Skip rest of function if already inside a repo
-    if (insideRepo) {
+    const continueInit = await checkIfInsideRepo(appDir);
+    if (!continueInit) {
       return;
     }
 
+    spinner = loadingSpinner().start("Initializing git...");
+
+    const options = {
+      cwd: appDir,
+      stdio: "pipe",
+    } as const;
+
     await execAsync("git init", options);
     await execAsync("git checkout -b main", options);
-    spinner.text = "Adding files...";
+    spinner.text = "Git: Adding files...";
     await execAsync("git add -A", options);
-    spinner.text = "Making first commit...";
+    spinner.text = "Git: Making first commit...";
     await execAsync(
       'git commit -m "Initial commit from create-liveblocks-app"',
       options
     );
   } catch (err) {
-    spinner.fail(c.redBright.bold("Problem initializing git:"));
+    spinner?.fail(c.redBright.bold("Problem initializing git:"));
     console.log();
     console.log(err);
     console.log();
     process.exit(0);
   }
 
-  spinner.succeed(c.green("Git initialized!"));
+  spinner?.succeed(c.green("Git initialized!"));
+}
+
+type Question = {
+  confirmNestedRepo: boolean;
+};
+
+async function checkIfInsideRepo(appDir: string): Promise<boolean> {
+  let insideRepo;
+  try {
+    const result = execSync("git rev-parse --is-inside-work-tree", {
+      cwd: appDir,
+      encoding: "utf-8",
+    });
+    insideRepo = result === "true";
+  } catch (err) {
+    // Error means not inside a repo
+    insideRepo = false;
+  }
+
+  if (insideRepo) {
+    const question: PromptObject<keyof Question> = {
+      name: "confirmNestedRepo",
+      type: "confirm",
+      message: `Directory is already inside a git repository, continue anyway?`,
+      initial: true,
+      active: "yes",
+      inactive: "no",
+    };
+
+    const { confirmNestedRepo = true }: Question = await prompts([question]);
+    return confirmNestedRepo;
+  }
+
+  return true;
 }
