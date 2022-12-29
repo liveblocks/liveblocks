d4dad5860593f3ae4dbb8990bbfc5ff9dcf12341
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2022-12-29 15:24:21 +0000

    docs: Add secret key warning

--- starter-kits/nextjs-starter-kit/liveblocks.server.config.ts
+++ starter-kits/nextjs-starter-kit/liveblocks.server.config.ts
@@ -5,6 +5,17 @@ export const API_BASE_URL = "https://api.liveblocks.io";
 export const SECRET_API_KEY = process.env.LIVEBLOCKS_SECRET_KEY;
 
 // ============================================================================
+if (!SECRET_API_KEY) {
+  console.log();
+  console.warn(
+    "INFO: You must add your Liveblocks secret key to .env.local use the starter kit"
+  );
+  console.warn(
+    "You can find your secret keys on https://liveblocks.io/dashboard"
+  );
+  console.log();
+}
+
 if (typeof window !== "undefined") {
   console.log();
   console.error(
