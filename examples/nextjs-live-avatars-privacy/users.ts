83a4fd40605eaa9706ebc556248b99032da67394
GuillaumeSalles <guillaume.salles@me.com>
2023-01-05 11:47:44 -0500

    Avatars privacy example

---
+++ examples/nextjs-live-avatars-privacy/users.ts
@@ -0,0 +1,34 @@
+export default {
+  "id-0": {
+    picture: `https://liveblocks.io/avatars/avatar-0.png`,
+    name: "Charlie Layne",
+  },
+  "id-1": {
+    picture: `https://liveblocks.io/avatars/avatar-1.png`,
+    name: "Mislav Abha",
+  },
+  "id-2": {
+    picture: `https://liveblocks.io/avatars/avatar-2.png`,
+    name: "Tatum Paolo",
+  },
+  "id-3": {
+    picture: `https://liveblocks.io/avatars/avatar-3.png`,
+    name: "Anjali Wanda",
+  },
+  "id-4": {
+    picture: `https://liveblocks.io/avatars/avatar-4.png`,
+    name: "Jody Hekla",
+  },
+  "id-5": {
+    picture: `https://liveblocks.io/avatars/avatar-5.png`,
+    name: "Emil Joyce",
+  },
+  "id-6": {
+    picture: `https://liveblocks.io/avatars/avatar-6.png`,
+    name: "Jory Quispe",
+  },
+  "id-7": {
+    picture: `https://liveblocks.io/avatars/avatar-7.png`,
+    name: "Quinn Elton",
+  },
+} as Record<string, { picture: string; name: string }>;
