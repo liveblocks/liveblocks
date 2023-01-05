c6646b63122bf5729e043c06f763e532b2ec3eca
Vincent Driessen <me@nvie.com>
2023-01-05 23:54:35 +0100

    Move icons to their separate module

--- packages/liveblocks-devtools/src/panel/components/Tree.tsx
+++ packages/liveblocks-devtools/src/panel/components/Tree.tsx
@@ -21,6 +21,14 @@ import {
   wrapObject,
   wrapProperty,
 } from "../../lib/stringify";
+import {
+  EllipsisIcon,
+  ListIcon,
+  MapIcon,
+  ObjectIcon,
+  QuestionIcon,
+  UserIcon,
+} from "./Icons";
 
 const HIGHLIGHT_ANIMATION_DURATION = 600;
 const HIGHLIGHT_ANIMATION_DELAY = 100;
@@ -31,17 +39,15 @@ const USE_GRID_LAYOUT = false;
 
 const SPECIAL_HACK_PREFIX = "@@HACK@@ ^_^;";
 
+/**
+ * Used to generate new Json subnodes on the fly.
+ */
 function makeJsonNode(
   parentId: string,
   key: string,
   payload: Json
 ): DevTools.JsonTreeNode {
-  return {
-    type: "Json",
-    id: `${parentId}:${key}`,
-    key,
-    payload,
-  };
+  return { type: "Json", id: `${parentId}:${key}`, key, payload };
 }
 
 type ArboristTreeProps<T> = TreeApi<T>["props"];
@@ -118,108 +124,23 @@ function background(node: DevTools.TreeNode): string {
 function icon(node: DevTools.TreeNode): ReactNode {
   switch (node.type) {
     case "LiveObject":
-      return (
-        <svg
-          width="16"
-          height="16"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M0 4a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4v8a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4Zm9 0a.75.75 0 0 1 .75-.75c.57 0 1.132.2 1.559.58.43.382.691.92.691 1.503v1.334c0 .124.055.264.188.382.135.12.336.201.562.201a.75.75 0 0 1 0 1.5.851.851 0 0 0-.562.201.514.514 0 0 0-.188.382v1.334c0 .583-.261 1.121-.691 1.503a2.35 2.35 0 0 1-1.559.58.75.75 0 0 1 0-1.5c.226 0 .427-.08.562-.201a.514.514 0 0 0 .188-.382V9.333c0-.5.193-.969.52-1.333a1.993 1.993 0 0 1-.52-1.333V5.333a.515.515 0 0 0-.188-.382.851.851 0 0 0-.562-.201A.75.75 0 0 1 9 4Zm-2.75-.75a.75.75 0 0 1 0 1.5.851.851 0 0 0-.562.201.514.514 0 0 0-.188.382v1.334c0 .5-.193.969-.52 1.333.327.364.52.832.52 1.333v1.334c0 .124.055.264.188.382.135.12.336.201.562.201a.75.75 0 0 1 0 1.5c-.57 0-1.132-.2-1.559-.58A2.012 2.012 0 0 1 4 10.667V9.333a.514.514 0 0 0-.188-.382.851.851 0 0 0-.562-.201.75.75 0 0 1 0-1.5c.226 0 .427-.08.562-.201A.514.514 0 0 0 4 6.667V5.333c0-.583.261-1.121.691-1.503a2.35 2.35 0 0 1 1.559-.58Z"
-            fill="currentColor"
-          />
-        </svg>
-      );
+      return <ObjectIcon />;
 
     case "LiveList":
-      return (
-        <svg
-          width="16"
-          height="16"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M0 3.5A3.5 3.5 0 0 1 3.5 0h9A3.5 3.5 0 0 1 16 3.5v9a3.5 3.5 0 0 1-3.5 3.5h-9A3.5 3.5 0 0 1 0 12.5v-9ZM9 4a.75.75 0 0 1 .75-.75h1c.69 0 1.25.56 1.25 1.25v7c0 .69-.56 1.25-1.25 1.25h-1a.75.75 0 0 1 0-1.5h.75v-6.5h-.75A.75.75 0 0 1 9 4Zm-2.75-.75a.75.75 0 1 1 0 1.5H5.5v6.5h.75a.75.75 0 0 1 0 1.5h-1c-.69 0-1.25-.56-1.25-1.25v-7c0-.69.56-1.25 1.25-1.25h1Z"
-            fill="currentColor"
-          />
-        </svg>
-      );
+      return <ListIcon />;
 
     case "LiveMap":
-      return (
-        <svg
-          width="16"
-          height="16"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M0 3.5A3.5 3.5 0 0 1 3.5 0h9A3.5 3.5 0 0 1 16 3.5v9a3.5 3.5 0 0 1-3.5 3.5h-9A3.5 3.5 0 0 1 0 12.5v-9Zm9.126.084a.75.75 0 0 1 1.04-.208l.002.001.002.001.004.003.01.007a1.37 1.37 0 0 1 .102.078c.06.049.139.117.229.207.18.18.406.446.628.816C11.59 5.234 12 6.37 12 8c0 1.632-.41 2.766-.857 3.51-.222.37-.448.637-.628.817a2.965 2.965 0 0 1-.331.285l-.01.007-.004.002-.002.002h-.001l-.001.001a.75.75 0 0 1-.842-1.241l.019-.015c.021-.018.06-.05.111-.101.101-.102.25-.272.403-.528.303-.505.643-1.37.643-2.739 0-1.368-.34-2.234-.643-2.74a2.737 2.737 0 0 0-.403-.527 1.467 1.467 0 0 0-.13-.116.75.75 0 0 1-.198-1.033Zm-3.292-.208a.75.75 0 1 1 .823 1.256c-.021.017-.06.05-.111.101-.101.102-.25.272-.403.528C5.84 5.766 5.5 6.63 5.5 8c0 1.368.34 2.234.643 2.74.153.255.302.425.403.527.05.05.09.084.111.101l.019.015a.75.75 0 0 1-.842 1.241l-.002-.001-.002-.002-.004-.002-.01-.007a1.791 1.791 0 0 1-.102-.078 2.962 2.962 0 0 1-.229-.207 4.23 4.23 0 0 1-.628-.816C4.41 10.766 4 9.63 4 8c0-1.632.41-2.766.857-3.51a4.23 4.23 0 0 1 .628-.817 2.952 2.952 0 0 1 .331-.285l.01-.007.004-.003h.002l.001-.002h.001Z"
-            fill="currentColor"
-          />
-        </svg>
-      );
+      return <MapIcon />;
 
     case "Json":
-      return (
-        <svg
-          width="16"
-          height="16"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M3.5 0A3.5 3.5 0 0 0 0 3.5v9A3.5 3.5 0 0 0 3.5 16h9a3.5 3.5 0 0 0 3.5-3.5v-9A3.5 3.5 0 0 0 12.5 0h-9Zm1 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2ZM9 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm2.5 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"
-            fill="currentColor"
-          />
-        </svg>
-      );
+      return <EllipsisIcon />;
 
     case "User":
-      return (
-        <svg
-          width="16"
-          height="16"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M0 3.5A3.5 3.5 0 0 1 3.5 0h9A3.5 3.5 0 0 1 16 3.5v9a3.5 3.5 0 0 1-3.5 3.5h-9A3.5 3.5 0 0 1 0 12.5v-9Zm8 1A1.25 1.25 0 1 0 8 7a1.25 1.25 0 0 0 0-2.5ZM5.25 5.75a2.75 2.75 0 1 1 5.5 0 2.75 2.75 0 0 1-5.5 0ZM8 9.25c-1.38 0-2.431.296-3.224.761a4.172 4.172 0 0 0-1.587 1.634.75.75 0 0 0 1.321.71c.19-.353.502-.743 1.025-1.05.524-.307 1.303-.555 2.465-.555s1.941.248 2.465.555c.523.307.835.697 1.025 1.05a.75.75 0 0 0 1.32-.71 4.173 4.173 0 0 0-1.586-1.634C10.431 9.546 9.38 9.25 8 9.25Z"
-            fill="currentColor"
-          />
-        </svg>
-      );
+      return <UserIcon />;
 
     default:
       // e.g. future LiveXxx types
-      // XXX Replace the SVG below with a "?" icon?
-      return (
-        <svg
-          width="16"
-          height="16"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M3.5 0A3.5 3.5 0 0 0 0 3.5v9A3.5 3.5 0 0 0 3.5 16h9a3.5 3.5 0 0 0 3.5-3.5v-9A3.5 3.5 0 0 0 12.5 0h-9Zm1 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2ZM9 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm2.5 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"
-            fill="currentColor"
-          />
-        </svg>
-      );
+      return <QuestionIcon />;
   }
 }
 
