57b29aacd39140376f034ca4dd477ed65ffb210c
Florent Lefebvre <florent.pm.lefebvre@gmail.com>
2023-01-06 11:56:09 -0500

    Revert "Revert "feat(auth): ensure public auth sets cookie (#588)" (#609)"
    This reverts commit f0143d3d475588928166030e6cbcdad11c216968.

--- packages/liveblocks-core/src/room.ts
+++ packages/liveblocks-core/src/room.ts
@@ -2588,6 +2588,8 @@ async function fetchAuthEndpoint(
     headers: {
       "Content-Type": "application/json",
     },
+    // necessary for cookies
+    credentials: "include",
     body: JSON.stringify(body),
   });
   if (!res.ok) {
