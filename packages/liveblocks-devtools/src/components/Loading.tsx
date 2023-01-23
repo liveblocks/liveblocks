0bb154d40ef0909179e2bcc394283cd0e080036f
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2023-01-23 17:32:02 +0100

    Fix loading spinner on Firefox

--- packages/liveblocks-devtools/src/components/Loading.tsx
+++ packages/liveblocks-devtools/src/components/Loading.tsx
@@ -12,78 +12,14 @@ export function Loading({ className, ...props }: ComponentProps<"svg">) {
       className={cx(className, "text-light-500 dark:text-dark-500")}
       {...props}
     >
-      <g>
-        <g transform="translate(-22.5,-16)">
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M0 0H45L12.9496 32V13.44L0 0Z"
-            fill="currentColor"
-          />
-          <animateMotion
-            dur="2s"
-            repeatCount="indefinite"
-            path="M54.5 61C54.5 61 77 32 87 42C97 52 73.5 67 73.5 67"
-            calcMode="spline"
-            keyTimes="0;1"
-            keySplines="0.6 0 0.4 1"
-          />
-          <animateTransform
-            attributeName="transform"
-            type="rotate"
-            from="0 22.5 16"
-            to="180 22.5 16"
-            dur="2s"
-            repeatCount="indefinite"
-            fill="freeze"
-            additive="sum"
-            calcMode="spline"
-            keyTimes="0;1"
-            keySplines="0.8 0 0.2 1"
-          />
-        </g>
-        <g transform="translate(-22.5,-16)">
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M45 32H0L32.0504 0V18.56L45 32Z"
-            fill="currentColor"
-          />
-          <animateMotion
-            dur="2s"
-            repeatCount="indefinite"
-            path="M73.5 67C73.5 67 51 96 41 86C31 76 54.5 61 54.5 61"
-            calcMode="spline"
-            keyTimes="0;1"
-            keySplines="0.6 0 0.4 1"
-          />
-          <animateTransform
-            attributeName="transform"
-            type="rotate"
-            from="0 22.5 16"
-            to="180 22.5 16"
-            dur="2s"
-            repeatCount="indefinite"
-            fill="freeze"
-            additive="sum"
-            calcMode="spline"
-            keyTimes="0;1"
-            keySplines="0.8 0 0.2 1"
-          />
-        </g>
-        <animateTransform
-          attributeName="transform"
-          type="rotate"
-          from="0 64 64"
-          to="180 64 64"
-          dur="2s"
-          repeatCount="indefinite"
-          fill="freeze"
-          additive="sum"
-          calcMode="spline"
-          keyTimes="0;1"
-          keySplines="0.8 0 0.2 1"
-        />
+      <g
+        id="loading-group"
+        fill="currentColor"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+      >
+        <path id="loading-top" d="M96 83H51L83.0504 51V69.56L96 83Z" />
+        <path id="loading-bottom" d="M32 45H77L44.9496 77V58.44L32 45Z" />
       </g>
     </svg>
   );
