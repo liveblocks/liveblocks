f8ef7b6bcc04be074a6167c139f2faaaedbe3b29
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-01-27 21:03:39 +0000

    feat: Working general integration setup

--- packages/create-liveblocks-app/bin/templates/nextjs-starter-kit/nextjs-starter-kit-template.ts
+++ packages/create-liveblocks-app/bin/templates/nextjs-starter-kit/nextjs-starter-kit-template.ts
@@ -37,6 +37,14 @@ const NEXTJS_STARTER_KIT_GUIDE_URL =
 const NEXTJS_STARTER_KIT_REPO_DIRECTORY =
   "liveblocks/liveblocks/starter-kits/nextjs-starter-kit";
 
+// Dev url: `http://localhost:3001/integrations/general?data=${encodedData}`;
+const LIVEBLOCKS_GENERAL_INTEGRATION_URL = (encodedData: string) =>
+  `https://liveblocks.io/integrations/general?data=${encodedData}`;
+
+// Dev url: `https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fliveblocks%2Fliveblocks%2Fblob%2Fmain%2Fstarter-kits%2Fnextjs-starter-kit&redirect-url=https%3A%2F%2Fliveblocks.io%2Fdocs%2Fguides%2Fnextjs-starter-kit&developer-id=oac_cem0SgRkffaXn20Xd8wYxl8V&demo-title=Next.js%20Starter%20Kit&demo-description=Kickstart%20start%20your%20collaborative%20Next.js%20app%20with%20this%20starter%20kit&demo-url=https%3A%2F%2Fliveblocks.io%2Fdocs%2Fguides%2Fnextjs-starter-kit&demo-image=https%3A%2F%2Fliveblocks.io%2Fimages%2Fsocial-images%2Fexamples%2Flive-avatars-advanced.png&integration-ids=oac_cem0SgRkffaXn20Xd8wYxl8V&external-id=${encodedData}&project-name=${name}&repository-name=${name}`;
+const VERCEL_DEPLOYMENT_URL = (encodedData: string, projectName: string) =>
+  `https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fliveblocks%2Fliveblocks%2Fblob%2Fmain%2Fstarter-kits%2Fnextjs-starter-kit&redirect-url=https%3A%2F%2Fliveblocks.io%2Fdocs%2Fguides%2Fnextjs-starter-kit&developer-id=oac_vgAdc0379wKPfhSvnUIZ4Vc8&demo-title=Next.js%20Starter%20Kit&demo-description=Kickstart%20start%20your%20collaborative%20Next.js%20app%20with%20this%20starter%20kit&demo-url=https%3A%2F%2Fliveblocks.io%2Fdocs%2Fguides%2Fnextjs-starter-kit&demo-image=https%3A%2F%2Fliveblocks.io%2Fimages%2Fsocial-images%2Fexamples%2Flive-avatars-advanced.png&integration-ids=oac_vgAdc0379wKPfhSvnUIZ4Vc8&external-id=${encodedData}&project-name=${projectName}&repository-name=${projectName}`;
+
 const AUTH_PROVIDERS = ["demo", "github", "auth0"] as const;
 
 type Questions = {
@@ -106,7 +114,7 @@ export async function create(flags: Record<string, any>) {
       },
       name: "liveblocksSecret",
       message:
-        "Would you like to get your Liveblocks secret key automatically?",
+        "Would you like to get your Liveblocks secret key automatically (recommended)?",
       initial: true,
       active: "yes",
       inactive: "no",
@@ -182,13 +190,13 @@ export async function create(flags: Record<string, any>) {
   // Empty/create appDir repo
   await confirmDirectoryEmpty(appDir);
 
+  // === Deploy on Vercel and use Vercel integration to get secret key ===
   if (vercel) {
     const vercelSpinner = loadingSpinner("", c.whiteBright("▲")).start(
       c.whiteBright.bold(
         "Opening Vercel, continue deploying then check back..."
       )
     );
-    // Use Vercel deploy button
     const vercelData: VercelIntegrationCallback = (await server(
       async (origin) => {
         const data: VercelIntegrationData = {
@@ -211,7 +219,7 @@ export async function create(flags: Record<string, any>) {
       }
     )) as VercelIntegrationCallback;
 
-    if (vercelData.env.LIVEBLOCKS_SECRET_KEY) {
+    if (vercelData?.env?.LIVEBLOCKS_SECRET_KEY) {
       liveblocksSecretKeyValue = vercelData.env.LIVEBLOCKS_SECRET_KEY;
     }
 
@@ -223,8 +231,14 @@ export async function create(flags: Record<string, any>) {
     vercelSpinner.succeed(c.green("Vercel deployment complete"));
   }
 
+  // === Get Liveblocks secret key from general integration ==============
   if (!vercel && liveblocksSecret) {
-    // Use `/integrations/general` on liveblocks.io
+    const liveblocksSpinner = loadingSpinner().start(
+      c.whiteBright.bold(
+        "Opening Liveblocks, import your API key then check back..."
+      )
+    );
+
     const liveblocksData = (await server((origin) => {
       const data: GeneralIntegrationData = {
         env: [{ name: "LIVEBLOCKS_SECRET_KEY", type: "secret" }],
@@ -237,6 +251,12 @@ export async function create(flags: Record<string, any>) {
       const liveblocksUrl = `http://localhost:3001/integrations/general?data=${encodedData}`;
       open(liveblocksUrl);
     })) as GeneralIntegrationCallback;
+
+    if (liveblocksData?.env?.LIVEBLOCKS_SECRET_KEY) {
+      liveblocksSecretKeyValue = liveblocksData.env.LIVEBLOCKS_SECRET_KEY;
+    }
+
+    liveblocksSpinner.succeed(c.green("Liveblocks secret key added"));
   }
 
   const envVariables = [
