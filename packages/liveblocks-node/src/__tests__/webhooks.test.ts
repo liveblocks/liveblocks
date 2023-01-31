51e085b0c661c4b10c8e2f58b96ccb76abb67679
flowflorent <florent.pm.lefebvre@gmail.com>
2023-01-30 20:15:52 -0500

    tests types and use svix for signature generation

--- packages/liveblocks-node/src/__tests__/webhooks.test.ts
+++ packages/liveblocks-node/src/__tests__/webhooks.test.ts
@@ -1,12 +1,13 @@
+import { Webhook } from "svix";
+
 import { WebhookHandler } from "../webhooks";
 
 describe("WebhookHandler", () => {
-  const secret = "INSERT_HERE";
+  const secret = "whsec_sFOoBaR78ZZNyOl0TxbObFZWeo3rLg+dA";
 
   const userEnteredHeaders = {
     "webhook-id": "msg_2KvLUhLIHZtzZnNgUWv3PhGYf5f",
     "webhook-timestamp": "1674850126",
-    "webhook-signature": "v1,1GrXvGm+9QKA1c01ZQ+7KOYbNleO4Ev/zfDDwo1WAPM=",
   };
 
   const userEnteredBody = {
@@ -24,8 +25,22 @@ describe("WebhookHandler", () => {
 
   const rawUserEnteredBody = JSON.stringify(userEnteredBody);
 
+  const generateSignatureWithSvix = (
+    secret: string,
+    messageId: string,
+    timestamp: string,
+    body: string
+  ) => {
+    const webhookUtils = new Webhook(secret);
+    return webhookUtils.sign(
+      messageId,
+      new Date(parseInt(timestamp, 10) * 1000),
+      body
+    );
+  };
+
   it.each([undefined, null, "", "not_a_valid_secret"])(
-    "initialization should throw an error if the secret is not provided",
+    "initialization should throw an error if the secret is not valid",
     (invalidSecret) => {
       // @ts-expect-error: We want to test invalid secret
       expect(() => new WebhookHandler(invalidSecret)).toThrow();
@@ -38,8 +53,18 @@ describe("WebhookHandler", () => {
 
       const webhookHandler = new WebhookHandler(secret);
 
+      const headers = {
+        ...userEnteredHeaders,
+        "webhook-signature": generateSignatureWithSvix(
+          secret,
+          userEnteredHeaders["webhook-id"],
+          userEnteredHeaders["webhook-timestamp"],
+          rawUserEnteredBody
+        ),
+      };
+
       const event = webhookHandler.verifyRequest({
-        headers: userEnteredHeaders,
+        headers,
         rawBody: rawUserEnteredBody,
       });
 
@@ -49,12 +74,6 @@ describe("WebhookHandler", () => {
     });
 
     it('should verify a "storageUpdated" event', () => {
-      const headersStorageUpdated = {
-        "webhook-id": "msg_2KvOK6yK9FO0U0nIyJYkM3jPwBs",
-        "webhook-timestamp": "1674851522",
-        "webhook-signature": "v1,WMFarHwdjZ0i6Ikfr2UecMFfgwGAY8WmmSZgdmx5w9M=",
-      };
-
       const storageUpdated = {
         data: {
           appId: "605a50b01a36d5ea7a2e9104",
@@ -63,8 +82,18 @@ describe("WebhookHandler", () => {
         },
         type: "storageUpdated",
       };
-
       const rawStorageUpdated = JSON.stringify(storageUpdated);
+
+      const headersStorageUpdated = {
+        "webhook-id": "msg_2KvOK6yK9FO0U0nIyJYkM3jPwBs",
+        "webhook-timestamp": "1674851522",
+        "webhook-signature": generateSignatureWithSvix(
+          secret,
+          "msg_2KvOK6yK9FO0U0nIyJYkM3jPwBs",
+          "1674851522",
+          rawStorageUpdated
+        ),
+      };
 
       const now = 1674851522000;
       const dateNowSpy = jest.spyOn(Date, "now").mockReturnValue(now);
@@ -82,11 +111,6 @@ describe("WebhookHandler", () => {
     });
 
     it('should verify a "userLeft" event', () => {
-      const headersUserLeft = {
-        "webhook-id": "msg_2KvOUwNvJ8ozHKdSJRPdqJwSuiu",
-        "webhook-timestamp": "1674851609",
-        "webhook-signature": "v1,kBjs3zOeRkb9C/Fa2QxLNGMnKDrio09/gjm8OkNooS4=",
-      };
       const userLeft = {
         data: {
           appId: "605a50b01a36d5ea7a2e9104",
@@ -102,6 +126,17 @@ describe("WebhookHandler", () => {
 
       const rawUserLeft = JSON.stringify(userLeft);
 
+      const headersUserLeft = {
+        "webhook-id": "msg_2KvOUwNvJ8ozHKdSJRPdqJwSuiu",
+        "webhook-timestamp": "1674851609",
+        "webhook-signature": generateSignatureWithSvix(
+          secret,
+          "msg_2KvOUwNvJ8ozHKdSJRPdqJwSuiu",
+          "1674851609",
+          rawUserLeft
+        ),
+      };
+
       const dateNowSpy = jest.spyOn(Date, "now").mockReturnValue(1674851609000);
 
       const webhookHandler = new WebhookHandler(secret);
@@ -116,7 +151,44 @@ describe("WebhookHandler", () => {
       dateNowSpy.mockRestore();
     });
 
-    it("should verify a request and fail if the signature is invalid", () => {
+    it("should verify an event with multiple signatures", () => {
+      const dateNowSpy = jest.spyOn(Date, "now").mockReturnValue(1674850126000);
+
+      const webhookHandler = new WebhookHandler(secret);
+
+      const anotherLegacySecret = "whsec_2KvOJ6yK9FO0hElL0JYkM3jPwBs"
+
+      const signature = [
+        generateSignatureWithSvix(
+          secret,
+          userEnteredHeaders["webhook-id"],
+          userEnteredHeaders["webhook-timestamp"],
+          rawUserEnteredBody
+        ),
+        generateSignatureWithSvix(
+          anotherLegacySecret,
+          userEnteredHeaders["webhook-id"],
+          userEnteredHeaders["webhook-timestamp"],
+          rawUserEnteredBody
+        ),
+      ].join(" ");
+
+      const headers = {
+        ...userEnteredHeaders,
+        "webhook-signature": signature,
+      };
+
+      const event = webhookHandler.verifyRequest({
+        headers,
+        rawBody: rawUserEnteredBody,
+      });
+
+      expect(event).toEqual(userEnteredBody);
+
+      dateNowSpy.mockRestore();
+    });
+
+    it("should throw if the signature is invalid", () => {
       const dateNowSpy = jest.spyOn(Date, "now").mockReturnValue(1674850126000);
 
       const webhookHandler = new WebhookHandler(secret);
@@ -133,7 +205,7 @@ describe("WebhookHandler", () => {
       dateNowSpy.mockRestore();
     });
 
-    it("should verify a request and fail if the timestamp is invalid", () => {
+    it("should throw if the timestamp is invalid", () => {
       const dateNowSpy = jest.spyOn(Date, "now").mockReturnValue(1674850126000);
 
       const webhookHandler = new WebhookHandler(secret);
@@ -141,6 +213,12 @@ describe("WebhookHandler", () => {
         webhookHandler.verifyRequest({
           headers: {
             ...userEnteredHeaders,
+            "webhook-signature": generateSignatureWithSvix(
+              secret,
+              userEnteredHeaders["webhook-id"],
+              userEnteredHeaders["webhook-timestamp"],
+              rawUserEnteredBody
+            ),
             "webhook-timestamp": "invalid_timestamp",
           },
           rawBody: rawUserEnteredBody,
@@ -150,14 +228,22 @@ describe("WebhookHandler", () => {
       dateNowSpy.mockRestore();
     });
 
-    it("should verify a request and fail if timestamp is above future threshold", () => {
+    it("should throw if timestamp is above future threshold", () => {
       const tenMinutesAgo = 1674850126000 - 10 * 60 * 1000;
       const dateNowSpy = jest.spyOn(Date, "now").mockReturnValue(tenMinutesAgo);
 
       const webhookHandler = new WebhookHandler(secret);
       expect(() =>
         webhookHandler.verifyRequest({
-          headers: userEnteredHeaders,
+          headers: {
+            ...userEnteredHeaders,
+            "webhook-signature": generateSignatureWithSvix(
+              secret,
+              userEnteredHeaders["webhook-id"],
+              userEnteredHeaders["webhook-timestamp"],
+              rawUserEnteredBody
+            ),
+          },
           rawBody: rawUserEnteredBody,
         })
       ).toThrowError("Timestamp in the future");
@@ -165,7 +251,7 @@ describe("WebhookHandler", () => {
       dateNowSpy.mockRestore();
     });
 
-    it("should verify a request and fail if timestamp is below past threshold", () => {
+    it("should throw if timestamp is below past threshold", () => {
       const tenMinutesFromNow = 1674850126000 + 10 * 60 * 1000;
       const dateNowSpy = jest
         .spyOn(Date, "now")
@@ -174,10 +260,57 @@ describe("WebhookHandler", () => {
       const webhookHandler = new WebhookHandler(secret);
       expect(() =>
         webhookHandler.verifyRequest({
-          headers: userEnteredHeaders,
+          headers: {
+            ...userEnteredHeaders,
+            "webhook-signature": generateSignatureWithSvix(
+              secret,
+              userEnteredHeaders["webhook-id"],
+              userEnteredHeaders["webhook-timestamp"],
+              rawUserEnteredBody
+            ),
+          },
           rawBody: rawUserEnteredBody,
         })
       ).toThrowError("Timestamp too old");
+
+      dateNowSpy.mockRestore();
+    });
+
+    it("should throw if the event type is not supported", () => {
+      const dateNowSpy = jest.spyOn(Date, "now").mockReturnValue(1674851522000);
+
+      const body = {
+        data: {
+          appId: "605a50b01a36d5ea7a2e9104",
+          roomId: "hero-grid-12-01-2022",
+          updatedAt: "2023-01-27T20:27:48.744Z",
+        },
+        type: "unsupportedEventType",
+      };
+
+      const rawBody = JSON.stringify(body);
+
+      const headers = {
+        "webhook-id": "msg_2KvOK6yK9FO0U0nIyJYkM3jPwBs",
+        "webhook-timestamp": "1674851522",
+        "webhook-signature": generateSignatureWithSvix(
+          secret,
+          "msg_2KvOK6yK9FO0U0nIyJYkM3jPwBs",
+          "1674851522",
+          rawBody
+        ),
+      };
+
+      const webhookHandler = new WebhookHandler(secret);
+
+      expect(() =>
+        webhookHandler.verifyRequest({
+          headers,
+          rawBody,
+        })
+      ).toThrowError(
+        "Unknown event type, please upgrade to a higher version of @liveblocks/node"
+      );
 
       dateNowSpy.mockRestore();
     });
