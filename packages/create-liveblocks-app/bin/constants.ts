7d511411a709fe3549b9aaa3720cbbd9378e2bd2
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-02-01 10:53:30 +0000

    feat: Remove redirect url

--- packages/create-liveblocks-app/bin/constants.ts
+++ packages/create-liveblocks-app/bin/constants.ts
@@ -32,14 +32,14 @@ export const NEXTJS_STARTER_KIT_VERCEL_DEPLOYMENT_URL = (
   encodedData: string,
   projectName: string
 ) =>
-  `https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fliveblocks%2Fliveblocks%2Fblob%2Fmain%2Fstarter-kits%2Fnextjs-starter-kit&redirect-url=https%3A%2F%2Fliveblocks.io%2Fdocs%2Fguides%2Fnextjs-starter-kit&developer-id=oac_vgAdc0379wKPfhSvnUIZ4Vc8&demo-title=Liveblocks%20Starter%20Kit&demo-description=Kickstart%20your%20collaborative%20app%20with%20Liveblocks%20and%20Next.js&demo-url=https%3A%2F%2Fliveblocks.io%2Fdocs%2Fguides%2Fnextjs-starter-kit&demo-image=https%3A%2F%2Fliveblocks.io%2Fimages%2Fintegrations%2Fnextjs-starter-kit-preview.png&integration-ids=oac_vgAdc0379wKPfhSvnUIZ4Vc8&external-id=${encodedData}&project-name=${projectName}&repository-name=${projectName}`;
+  `https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fliveblocks%2Fliveblocks%2Fblob%2Fmain%2Fstarter-kits%2Fnextjs-starter-kit&developer-id=oac_vgAdc0379wKPfhSvnUIZ4Vc8&demo-title=Liveblocks%20Starter%20Kit&demo-description=Kickstart%20your%20collaborative%20app%20with%20Liveblocks%20and%20Next.js&demo-url=https%3A%2F%2Fliveblocks.io%2Fdocs%2Fguides%2Fnextjs-starter-kit&demo-image=https%3A%2F%2Fliveblocks.io%2Fimages%2Fintegrations%2Fnextjs-starter-kit-preview.png&integration-ids=oac_vgAdc0379wKPfhSvnUIZ4Vc8&external-id=${encodedData}&project-name=${projectName}&repository-name=${projectName}`;
 
 export const NEXTJS_STARTER_KIT_VERCEL_DEPLOYMENT_URL_DEV = (
   encodedData: string,
   projectName: string
 ) => {
   console.warn("Remove DEV URL");
-  return `https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fliveblocks%2Fliveblocks%2Fblob%2Fmain%2Fstarter-kits%2Fnextjs-starter-kit&redirect-url=https%3A%2F%2Fliveblocks.io%2Fdocs%2Fguides%2Fnextjs-starter-kit&developer-id=oac_cem0SgRkffaXn20Xd8wYxl8V&demo-title=Liveblocks%20Starter%20Kit&demo-description=Kickstart%20your%20collaborative%20app%20with%20Liveblocks%20and%20Next.js&demo-url=https%3A%2F%2Fliveblocks.io%2Fdocs%2Fguides%2Fnextjs-starter-kit&demo-image=https%3A%2F%2Fliveblocks.io%2Fimages%2Fintegrations%2Fnextjs-starter-kit-preview.png&integration-ids=oac_cem0SgRkffaXn20Xd8wYxl8V&external-id=${encodedData}&project-name=${projectName}&repository-name=${projectName}`;
+  return `https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fliveblocks%2Fliveblocks%2Fblob%2Fmain%2Fstarter-kits%2Fnextjs-starter-kit&developer-id=oac_cem0SgRkffaXn20Xd8wYxl8V&demo-title=Liveblocks%20Starter%20Kit&demo-description=Kickstart%20your%20collaborative%20app%20with%20Liveblocks%20and%20Next.js&demo-url=https%3A%2F%2Fliveblocks.io%2Fdocs%2Fguides%2Fnextjs-starter-kit&demo-image=https%3A%2F%2Fliveblocks.io%2Fimages%2Fintegrations%2Fnextjs-starter-kit-preview.png&integration-ids=oac_cem0SgRkffaXn20Xd8wYxl8V&external-id=${encodedData}&project-name=${projectName}&repository-name=${projectName}`;
 };
 
 export const NEXTJS_STARTER_KIT_GUIDE_URL =
