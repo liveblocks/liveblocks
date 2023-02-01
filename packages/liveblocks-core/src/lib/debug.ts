95dfdbcd4b2b73ac6e229750baca633ff39db197
Eric Meier <eric@meier.sh>
2023-02-01 12:51:31 +0000

    Log stack traces of functions that caused storage op rejections

---
+++ packages/liveblocks-core/src/lib/debug.ts
@@ -0,0 +1,16 @@
+export function captureStackTrace(
+  msg: string,
+  traceRoot?: () => unknown
+): string | undefined {
+  // Hack: Normally browsers will add the name of the error before the stack trace (default Error).
+  // To customize this, we set the name of the error to the message we want to display.
+  const errorLike: { name: string; stack?: string } = { name: msg };
+
+  // Error.captureStackTrace is non-standard and only available in certain browsers/runtimes.
+  if (typeof Error.captureStackTrace !== "function") {
+    return undefined;
+  }
+
+  Error.captureStackTrace(errorLike, traceRoot);
+  return errorLike.stack;
+}
--- packages/liveblocks-core/src/lib/fancy-console.ts
+++ packages/liveblocks-core/src/lib/fancy-console.ts
@@ -14,7 +14,7 @@ function wrap(
         console[method]("%cLiveblocks", badge, message, ...args);
 }
 
-// export const log = wrap("log");
+export const log = wrap("log");
 export const warn = wrap("warn");
 export const error = wrap("error");
 
@@ -35,5 +35,5 @@ function wrapWithTitle(
 }
 
 // export const logWithTitle = wrapWithTitle("log");
-// export const warnWithTitle = wrapWithTitle("warn");
+export const warnWithTitle = wrapWithTitle("warn");
 export const errorWithTitle = wrapWithTitle("error");
