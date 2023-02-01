84a8a35ed47b37cb099196e0865b21680dbf0ed0
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-02-01 12:09:56 +0000

    feat: Prevent prettier warning

--- packages/create-liveblocks-app/bin/templates/nextjs-starter-kit/auth-provider-code.ts
+++ packages/create-liveblocks-app/bin/templates/nextjs-starter-kit/auth-provider-code.ts
@@ -38,7 +38,6 @@ export const githubAuthProvider = `
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
-    
 `;
 
 export const auth0AuthProvider = `
