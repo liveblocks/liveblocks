53ed635dd610cc98a7aeb722fe4cb715bad6d510
GuillaumeSalles <guillaume.salles@me.com>
2023-02-02 21:59:03 -0500

    Migrate javascript-live-cursor to typescript

--- examples/javascript-live-cursors/README.md
+++ examples/javascript-live-cursors/README.md
@@ -25,5 +25,5 @@ This example shows how to build live cursors with [Liveblocks](https://liveblock
 - Install all dependencies with `npm install`
 - Create an account on [liveblocks.io](https://liveblocks.io/dashboard)
 - Copy your **public** key from the [dashboard](https://liveblocks.io/dashboard/apikeys)
-- Replace `pk_YOUR_PUBLIC_KEY` in [`app.js`](./app.js) by your **public** key
+- Replace `pk_YOUR_PUBLIC_KEY` in [`app.ts`](./app.ts) by your **public** key
 - Run `npm run build` and open `static/index.html` in your browser
--- examples/javascript-live-cursors/app.ts
+++ examples/javascript-live-cursors/app.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@liveblocks/client";
 
-let PUBLIC_KEY = "pk_YOUR_PUBLIC_KEY";
+let PUBLIC_KEY =
+  "pk_dev_94sjZ6Tho2cCgs4Ej9erNJjs95XLroOArvvheXC9Zi8waQzUy2BMNb7Ix1aX6cCe";
 let roomId = "javascript-live-cursors";
 
 overrideApiKeyAndRoomId();
@@ -13,13 +14,23 @@ if (!/^pk_(live|test)/.test(PUBLIC_KEY)) {
 }
 
 const client = createClient({
+  throttle: 16,
   publicApiKey: PUBLIC_KEY,
 });
 
-const room = client.enter(roomId, { initialPresence: { cursor: null } });
+type Presence = {
+  cursor: {
+    x: number;
+    y: number;
+  } | null;
+};
 
-const cursorsContainer = document.getElementById("cursors-container");
-const text = document.getElementById("text");
+const room = client.enter<Presence>(roomId, {
+  initialPresence: { cursor: null },
+});
+
+const cursorsContainer = document.getElementById("cursors-container")!;
+const text = document.getElementById("text")!;
 
 room.subscribe("my-presence", (presence) => {
   const cursor = presence?.cursor ?? null;
@@ -81,11 +92,15 @@ function updateCursor(user) {
   }
 }
 
-function getCursorOrCreate(connectionId) {
-  let cursor = document.getElementById(`cursor-${connectionId}`);
+function getCursorOrCreate(connectionId): HTMLElement {
+  let cursor: HTMLElement | null = document.getElementById(
+    `cursor-${connectionId}`
+  );
 
   if (cursor == null) {
-    cursor = document.getElementById("cursor-template").cloneNode(true);
+    cursor = document
+      .getElementById("cursor-template")!
+      .cloneNode(true) as HTMLElement;
     cursor.id = `cursor-${connectionId}`;
     cursor.style.fill = COLORS[connectionId % COLORS.length];
     cursorsContainer.appendChild(cursor);
@@ -97,7 +112,7 @@ function getCursorOrCreate(connectionId) {
 function deleteCursor(user) {
   const cursor = document.getElementById(`cursor-${user.connectionId}`);
   if (cursor) {
-    cursor.parentNode.removeChild(cursor);
+    cursor.parentNode!.removeChild(cursor);
   }
 }
 
