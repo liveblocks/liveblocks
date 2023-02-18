c224675d4ba16ad23bdb4959637a08c6ef977d6e
flowflorent <florent.pm.lefebvre@gmail.com>
2023-02-17 19:40:44 -0500

    add help to release

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,3 @@
-GITHUB_URL="https://github.com/liveblocks/liveblocks"
 PACKAGE_DIRS=(
     "packages/liveblocks-core"
     "packages/liveblocks-client"
@@ -7,6 +6,18 @@ PACKAGE_DIRS=(
     "packages/liveblocks-redux"
     "packages/liveblocks-zustand"
 )
+
+usage () {
+    err "usage: release.sh [-V <version>] [-t <tag>] [-h]"
+    err
+    err ""
+    err "Create a release for the CI to publish."
+    err
+    err "Options:"
+    err "-V <version>  Set version to publish (default: prompt)"
+    err "-t <tag>      Sets the tag to use on NPM (default: latest)"
+    err "-h            Show this help"
+}
 
 VERSION=
 TAG=
@@ -18,6 +29,12 @@ while getopts V:t:h flag; do
         *) usage; exit 2;;
     esac
 done
+
+if [ "$#" -ne 0 ]; then
+    err "Unknown arguments: $@"
+    usage
+    exit 2
+fi
 
 check_is_valid_version () {
     if ! [[ "$VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+(-[a-z0-9]+)?$ ]]; then
