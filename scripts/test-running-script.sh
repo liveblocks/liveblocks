4696dbb2373b22c8c9feb71a0516123ae85b4904
flowflorent <florent.pm.lefebvre@gmail.com>
2023-02-21 16:43:08 -0500

    simpplify

--- scripts/test-running-script.sh
+++ scripts/test-running-script.sh
@@ -1,17 +1,3 @@
 echo "Running test scripts..."
 
 git current-branch
-
-VERSION=
-TAG=
-while getopts V:t:h flag; do
-    case "$flag" in
-        V) VERSION=$OPTARG;;
-        t) TAG=$OPTARG;;
-        *) usage; exit 2;;
-    esac
-done
-shift "$(($OPTIND - 1))"
-
-echo Version "$VERSION"
-echo Tag "$TAG"
