82a00d81cede6ffbfcf70f353a9cc5a2d72a64ce
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-02-22 12:27:30 +0000

    feat: A basic initial/solved interactive tutorial repo

---
+++ learn/react/getting-started/presence/initial/pages/index.tsx
@@ -0,0 +1,104 @@
+import React, { useMemo } from "react";
+import { useRouter } from "next/router";
+import { RoomProvider, useOthers, useMyPresence } from "../liveblocks.config";
+import Cursor from "../components/Cursor";
+import styles from "./index.module.css";
+
+/**
+ * This file shows how to add basic live cursors on your product.
+ */
+
+const COLORS = [
+  "#E57373",
+  "#9575CD",
+  "#4FC3F7",
+  "#81C784",
+  "#FFF176",
+  "#FF8A65",
+  "#F06292",
+  "#7986CB",
+];
+
+function Example() {
+  /**
+   * useMyPresence returns the presence of the current user and a function to update it.
+   * updateMyPresence is different than the setState function returned by the useState hook from React.
+   * You don't need to pass the full presence object to update it.
+   * See https://liveblocks.io/docs/api-reference/liveblocks-react#useMyPresence for more information
+   */
+  const [{ cursor }, updateMyPresence] = useMyPresence();
+
+  /**
+   * Return all the other users in the room and their presence (a cursor position in this case)
+   */
+  const others = useOthers();
+
+  return (
+    <main
+      className={styles.container}
+      onPointerMove={(event) => {
+        event.preventDefault();
+        // Update the user cursor position on every pointer move
+        updateMyPresence({
+          cursor: {
+            x: Math.round(event.clientX),
+            y: Math.round(event.clientY),
+          },
+        });
+      }}
+      onPointerLeave={() =>
+        // When the pointer goes out, set cursor to null
+        updateMyPresence({
+          cursor: null,
+        })
+      }
+    >
+      <h1>Initial</h1>
+      <div className={styles.text}>
+        {cursor
+          ? `${cursor.x} × ${cursor.y}`
+          : "Move your cursor to broadcast its position to other people in the room."}
+      </div>
+
+      {
+        /**
+         * Iterate over other users and display a cursor based on their presence
+         */
+        others.map(({ connectionId, presence }) => {
+          if (presence.cursor === null) {
+            return null;
+          }
+
+          return (
+            <Cursor
+              key={`cursor-${connectionId}`}
+              // connectionId is an integer that is incremented at every new connections
+              // Assigning a color with a modulo makes sure that a specific user has the same colors on every clients
+              color={COLORS[connectionId % COLORS.length]}
+              x={presence.cursor.x}
+              y={presence.cursor.y}
+            />
+          );
+        })
+      }
+    </main>
+  );
+}
+
+export default function Page() {
+  const roomId = "nextjs-live-cursors";
+
+  return (
+    <RoomProvider
+      id={roomId}
+      /**
+       * Initialize the cursor position to null when joining the room
+       */
+      initialPresence={{
+        cursor: null,
+      }}
+    >
+      <Example />
+    </RoomProvider>
+  );
+}
