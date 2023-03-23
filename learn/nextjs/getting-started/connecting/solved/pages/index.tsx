1f3ca1c10032da87024c3d12cbb92c2d66d1af5d
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-03-23 16:16:12 +0000

    fix: Fixed full refresh issue 2

--- learn/nextjs/getting-started/connecting/solved/pages/index.tsx
+++ learn/nextjs/getting-started/connecting/solved/pages/index.tsx
@@ -1,34 +1,10 @@
 import React from "react";
 import { RoomProvider } from "../liveblocks.config";
 
-export default function App () {
-  if (!RoomProvider) {
+export default function Page() {
+  if (typeof RoomProvider === "undefined") {
     return <div>Not connected</div>
   }
 
   return <div>Connected to Liveblocks!</div>
 }
-
-/*
-function Example() {
-  return <h1>Welcome</h1>;
-}
-
-export default function Page() {
-  const roomId = "{% ROOM_ID %}";
-
-  return (
-    <RoomProvider
-      id={roomId}
-      /**
-       * Initialize the cursor position to null when joining the room
-
-      initialPresence={{
-        cursor: null,
-      }}
-    >
-      <Example />
-    </RoomProvider>
-  );
-}
-  */
