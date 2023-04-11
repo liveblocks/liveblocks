7984c1ab0490559af84b5bad369d5320fe3db23a
Le Vaillant Pierre <pierre.levaillant@liveblocks.io>
2023-04-11 17:36:27 +0200

    Add confettis to welcome step 🎊

--- tutorial/nextjs/getting-started/welcome/solved-diff/app.tsx
+++ tutorial/nextjs/getting-started/welcome/solved-diff/app.tsx
@@ -1,3 +1,11 @@
+import React from "react";
+import Confettis from "./components/Confettis";
+
 export default function App() {
-  return <h1>Let’s start!</h1>;
+  return (
+    <>
+      <h1>Let’s start!</h1>
+      <Confettis />
+    </>
+  );
 }
