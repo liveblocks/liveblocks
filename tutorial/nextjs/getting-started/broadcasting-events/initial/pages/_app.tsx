b00046f617dcf411794123e14f6bc4ba044cfb6c
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-04-14 15:12:01 +0100

    feat: Token addition required for theme change problem

--- tutorial/nextjs/getting-started/broadcasting-events/initial/pages/_app.tsx
+++ tutorial/nextjs/getting-started/broadcasting-events/initial/pages/_app.tsx
@@ -5,10 +5,10 @@ import "../styles/global.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script id="liveblocks-default-scripts-{% RANDOM_ID %}">
+      <Script id=/* TOKEN_STRING_RANDOM_ID */"{% RANDOM_ID %}-liveblocks-default-script">
         {`{% DEFAULT_SCRIPTS %}`}
       </Script>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
