5811cc5f3bd4c206cb61ef0cb0361ad8d753ba62
Vincent Driessen <me@nvie.com>
2023-04-26 09:54:59 +0200

    Implement a simple yet generic abstract FSM implementation

---
+++ packages/liveblocks-core/src/__tests__/fsm.test.ts
@@ -0,0 +1,415 @@
+import { FiniteStateMachine as FSM } from "../fsm";
+
+describe("finite state machine", () => {
+  test("cannot start before there is at least an initial state", () => {
+    const fsm = new FSM(null).addEvent("x").addEvent("y");
+    expect(() => fsm.start()).toThrow("No states defined yet");
+  });
+
+  test("cannot use FSM when it hasn't started yet", () => {
+    const fsm = new FSM(null);
+    expect(() => fsm.transition({ type: "SOME_EVENT" })).toThrow(
+      "Machine hasn't started yet"
+    );
+  });
+
+  test("cannot use FSM when it hasn't started yet", () => {
+    const fsm = new FSM(null)
+      .addState({ name: "red" })
+      .addState({ name: "yellow" })
+      .addState({ name: "green" });
+
+    expect(() => fsm.currentStateName).toThrow("Machine hasn't started yet");
+  });
+
+  test("error when there is an nonexisting target state", () => {
+    const fsm = new FSM(null)
+      .addEvent("SOME_EVENT")
+      .addState({ name: "initial" })
+      .addTransitions("initial", {
+        SOME_EVENT: () => "i-am-not-a-valid-state-name",
+      })
+      .start();
+
+    expect(() => {
+      fsm.transition({ type: "SOME_EVENT" });
+    }).toThrow('Invalid next state name: "i-am-not-a-valid-state-name"');
+  });
+
+  test("initial state", () => {
+    const fsm = new FSM(null)
+      .addState({ name: "red" })
+      .addState({ name: "yellow" })
+      .addState({ name: "green" })
+      .start();
+
+    expect(fsm.currentStateName).toBe("red");
+  });
+
+  test("transitionIfPossible never errors when target state does not exist", () => {
+    const fsm = new FSM(null)
+      .addEvent("INVALID")
+      .addEvent("ONLY_WHEN_RED")
+      .addEvent("ONLY_WHEN_GREEN")
+
+      .addState({ name: "red" })
+      .addState({ name: "green" })
+
+      .addTransitions("red", {
+        INVALID: () => "i-am-not-a-valid-state-name",
+        ONLY_WHEN_RED: () => "green",
+      })
+
+      .addTransitions("green", {
+        ONLY_WHEN_GREEN: () => "red",
+      })
+
+      .start();
+
+    expect(() => fsm.transition({ type: "UNKNOWN_EVENT" })).toThrow(
+      'Unknown event "UNKNOWN_EVENT"'
+    );
+
+    expect(() => fsm.transition({ type: "INVALID" })).toThrow(
+      'Invalid next state name: "i-am-not-a-valid-state-name"'
+    );
+
+    expect(fsm.currentStateName).toBe("red");
+
+    expect(() => fsm.transition({ type: "ONLY_WHEN_GREEN" })).toThrow(
+      'Event "ONLY_WHEN_GREEN" is not allowed from state "red"'
+    );
+    expect(() =>
+      fsm.transitionIfPossible({ type: "ONLY_WHEN_GREEN" })
+    ).not.toThrow();
+    expect(fsm.currentStateName).toBe("red"); // Doesn't change the state
+
+    fsm.transitionIfPossible({ type: "ONLY_WHEN_RED" }); // Acts like .transition()
+    expect(fsm.currentStateName).toBe("green");
+
+    fsm.transitionIfPossible({ type: "ONLY_WHEN_GREEN" });
+
+    // Still fails (because this is a configuration error)
+    expect(() => fsm.transitionIfPossible({ type: "INVALID" })).toThrow(
+      'Invalid next state name: "i-am-not-a-valid-state-name"'
+    );
+  });
+
+  test("executes onEnter directly when starting", () => {
+    const onEnterRed = jest.fn();
+    const onEnterGreen = jest.fn();
+    const onEnterYellow = jest.fn();
+
+    const fsm = new FSM(null)
+      .addState({ name: "red", onEnter: onEnterRed })
+      .addState({ name: "yellow", onEnter: onEnterYellow })
+      .addState({ name: "green", onEnter: onEnterGreen });
+
+    expect(onEnterRed).not.toBeCalled();
+    expect(onEnterYellow).not.toBeCalled();
+    expect(onEnterGreen).not.toBeCalled();
+
+    fsm.start();
+
+    expect(onEnterRed).toBeCalledTimes(1);
+    expect(onEnterYellow).not.toBeCalled();
+    expect(onEnterGreen).not.toBeCalled();
+  });
+
+  test("executes onExit directly when stopping", () => {
+    const onExitRed = jest.fn();
+    const onExitGreen = jest.fn();
+    const onExitYellow = jest.fn();
+
+    const fsm = new FSM(null)
+      .addState({ name: "red", onExit: onExitRed })
+      .addState({ name: "yellow", onExit: onExitYellow })
+      .addState({ name: "green", onExit: onExitGreen });
+
+    fsm.start();
+
+    expect(onExitRed).not.toBeCalled();
+    expect(onExitYellow).not.toBeCalled();
+    expect(onExitGreen).not.toBeCalled();
+
+    fsm.stop();
+
+    expect(onExitRed).toBeCalledTimes(1);
+    expect(onExitYellow).not.toBeCalled();
+    expect(onExitGreen).not.toBeCalled();
+  });
+
+  test("executes onEnter after onExit", () => {
+    const calls: string[] = [];
+
+    const enterGreen = () => void calls.push("entered green");
+    const enterRed = () => void calls.push("entered red");
+    const enterYellow = () => void calls.push("entered yellow");
+    const exitGreen = () => void calls.push("exited green");
+    const exitYellow = () => void calls.push("exited red");
+    const exitRed = () => void calls.push("exited yellow");
+
+    const fsm = new FSM(null)
+      .addEvent("TO_RED")
+      .addEvent("TO_GREEN")
+      .addEvent("STAY_GREEN_LONGER")
+      .addEvent("BE_CAREFUL")
+
+      .addState({ name: "red", onEnter: enterRed, onExit: exitYellow })
+      .addState({ name: "yellow", onEnter: enterYellow, onExit: exitRed })
+      .addState({ name: "green", onEnter: enterGreen, onExit: exitGreen })
+
+      .addTransitions("green", {
+        STAY_GREEN_LONGER: () => "green",
+        BE_CAREFUL: () => "yellow",
+      })
+
+      .addTransitions("red", {
+        TO_GREEN: () => "green",
+      })
+
+      .addTransitions("yellow", {
+        TO_RED: () => "redd",
+      })
+
+      .start();
+
+    expect(fsm.currentStateName).toEqual("red");
+    expect(calls).toEqual(["entered red"]);
+
+    fsm.transition({ type: "TO_GREEN" });
+    expect(fsm.currentStateName).toEqual("green");
+    expect(calls).toEqual(["entered red", "exited red", "entered green"]);
+
+    // No enter/exit events happen when staying in the same state explicitly
+    fsm.transition({ type: "STAY_GREEN_LONGER" });
+    expect(fsm.currentStateName).toEqual("green");
+    fsm.transition({ type: "STAY_GREEN_LONGER" });
+    expect(fsm.currentStateName).toEqual("green");
+    expect(calls).toEqual(["entered red", "exited red", "entered green"]);
+
+    expect(() => fsm.transition({ type: "TO_RED" })).toThrow(
+      'Event "TO_RED" is not allowed from state "green"'
+    );
+    expect(fsm.currentStateName).toEqual("green");
+
+    expect(fsm.can("TO_RED")).toBe(false);
+    expect(fsm.can("BE_CAREFUL")).toBe(true);
+
+    fsm.transition({ type: "BE_CAREFUL" });
+    expect(fsm.currentStateName).toEqual("yellow");
+    expect(calls).toEqual([
+      "entered red",
+      "exited red",
+      "entered green",
+      "exited green",
+      "entered yellow",
+    ]);
+  });
+
+  test("states with cleanup functions", () => {
+    const calls: string[] = [];
+
+    const onEnter = () => {
+      calls.push("light!");
+      return () => {
+        calls.push("darkness!");
+      };
+    };
+
+    const fsm = new FSM(null)
+      .addState({ name: "off" })
+      .addState({ name: "on", onEnter })
+
+      .addTransitions("on", {
+        TOGGLE: () => "off",
+      })
+
+      .addTransitions("off", {
+        TOGGLE: () => "on",
+      })
+
+      .start();
+
+    expect(fsm.currentStateName).toEqual("off");
+    expect(calls).toEqual([]);
+
+    fsm.transition({ type: "TOGGLE" });
+    expect(fsm.currentStateName).toEqual("on");
+    expect(calls).toEqual(["light!"]);
+
+    fsm.transition({ type: "TOGGLE" });
+    expect(fsm.currentStateName).toEqual("off");
+    expect(calls).toEqual(["light!", "darkness!"]);
+
+    fsm.transition({ type: "TOGGLE" });
+    expect(fsm.currentStateName).toEqual("on");
+    expect(calls).toEqual(["light!", "darkness!", "light!"]);
+
+    fsm.stop();
+    expect(calls).toEqual(["light!", "darkness!", "light!", "darkness!"]);
+  });
+
+  // describe("promise-based states", () => {
+  //   test("normal flow", async () => {
+  //     const calls: string[] = [];
+
+  //     function fromPromise<T>(config: {
+  //       promiseFactory: (context: T) => Promise<unknown>;
+  //       onDone?: /* EventName */ string;
+  //       onError?: /* EventName */ string;
+  //     }) {
+  //       function generatedOnEnter(context: T) {
+  //         let cancelled = false;
+
+  //         const promise = config.promiseFactory(context);
+  //         promise.then(
+  //           (_result) => {
+  //             if (!cancelled && config.onDone && fsm.can(config.onDone)) {
+  //               fsm.transition(config.onDone);
+  //             }
+  //           },
+  //           (_error) => {
+  //             if (!cancelled && config.onError && fsm.can(config.onError)) {
+  //               fsm.transition(config.onError);
+  //             }
+  //           }
+  //         );
+
+  //         return () => {
+  //           cancelled = true;
+  //         };
+  //       }
+  //       return generatedOnEnter;
+  //     }
+
+  //     function somePromise() {
+  //       calls.push("promise started");
+  //       return new Promise((resolve) => {
+  //         setTimeout(() => {
+  //           calls.push("promise done");
+  //           return resolve(undefined);
+  //         }, 0);
+  //       });
+  //     }
+
+  //     const fsm = new FSM(
+  //       {},
+  //       {
+  //         running: {
+  //           initial: true,
+  //           onEnter: fromPromise({
+  //             promiseFactory: somePromise,
+  //             onDone: "DONE",
+  //             onError: "ERROR",
+  //           }),
+  //           on: {
+  //             DONE: "resolved",
+  //             ERROR: "rejected",
+  //           },
+  //         },
+
+  //         resolved: {},
+  //         rejected: {},
+  //       }
+  //     );
+
+  //     expect(fsm.currentStateName).toEqual("running");
+  //     expect(calls).toEqual(["promise started"]);
+
+  //     return new Promise((resolve) => {
+  //       setTimeout(() => {
+  //         expect(calls).toEqual(["promise started", "promise done"]);
+  //         expect(fsm.currentStateName).toEqual("resolved");
+  //         resolve(undefined);
+  //       }, 0);
+  //     });
+  //   });
+
+  //   test("cancelling promises", async () => {
+  //     const calls: string[] = [];
+
+  //     function fromPromise<T>(config: {
+  //       promiseFactory: (context: T) => Promise<unknown>;
+  //       onDone?: /* EventName */ string;
+  //       onError?: /* EventName */ string;
+  //     }) {
+  //       function generatedOnEnter(context: T) {
+  //         let cancelled = false;
+
+  //         const promise = config.promiseFactory(context);
+  //         promise.then(
+  //           (_result) => {
+  //             if (!cancelled && config.onDone && fsm.can(config.onDone)) {
+  //               fsm.transition(config.onDone);
+  //             }
+  //           },
+  //           (_error) => {
+  //             if (!cancelled && config.onError && fsm.can(config.onError)) {
+  //               fsm.transition(config.onError);
+  //             }
+  //           }
+  //         );
+
+  //         return () => {
+  //           cancelled = true;
+  //         };
+  //       }
+  //       return generatedOnEnter;
+  //     }
+
+  //     function somePromise() {
+  //       calls.push("promise started");
+  //       return new Promise((resolve) => {
+  //         setTimeout(() => {
+  //           calls.push("promise done");
+  //           return resolve(undefined);
+  //         }, 0);
+  //       });
+  //     }
+
+  //     const fsm = new FSM(
+  //       {},
+  //       {
+  //         running: {
+  //           initial: true,
+  //           onEnter: fromPromise({
+  //             promiseFactory: somePromise,
+  //             onDone: "DONE",
+  //             onError: "ERROR",
+  //           }),
+  //           on: {
+  //             DONE: "resolved",
+  //             ERROR: "rejected",
+  //             ABORT: "rejected",
+  //           },
+  //         },
+
+  //         resolved: {},
+  //         rejected: {
+  //           on: {
+  //             DONE: () => {
+  //               throw new Error("Should never get triggered");
+  //             },
+  //             ABORT: "rejected",
+  //           },
+  //         },
+  //       }
+  //     );
+
+  //     expect(fsm.currentStateName).toEqual("running");
+  //     expect(calls).toEqual(["promise started"]);
+
+  //     // Immediately move out of the state, which should "cancel" the promise
+  //     fsm.transition("ABORT");
+
+  //     return new Promise((resolve) => {
+  //       setTimeout(() => {
+  //         expect(calls).toEqual(["promise started", "promise done"]);
+  //         expect(fsm.currentStateName).toEqual("rejected");
+  //         resolve(undefined);
+  //       }, 0);
+  //     });
+  //   });
+  // });
+});
---
+++ packages/liveblocks-core/src/fsm.ts
@@ -0,0 +1,261 @@
+/**
+ * Finite State Machine (FSM) implementation.
+ */
+
+// XXX Tidy up this class before publishing
+
+// Trick taken from the React codebase
+type CleanupFn = () => void;
+
+type BaseState<TContext> = {
+  name: string;
+  onEnter?: (context: TContext) => void | CleanupFn;
+  onExit?: (context: TContext) => void;
+};
+type BaseEvent = { type: string };
+
+enum RunningState {
+  NOT_STARTED_YET, // Machine can be set up during this phase
+  STARTED,
+  STOPPED,
+}
+
+type Action<
+  TContext,
+  TEvent extends BaseEvent,
+  TState extends BaseState<TContext>
+> = (event: TEvent, context: Readonly<TContext>) => TState["name"];
+
+export class FiniteStateMachine<
+  TContext,
+  TEvent extends BaseEvent,
+  TState extends BaseState<TContext>
+> {
+  // Indicates whether this state machine is still being configured, has
+  // started, or has terminated
+  private runningState: RunningState;
+
+  private context: TContext;
+  private states: Map<string, TState>;
+  private allowedTransitions: Map<
+    string,
+    Map<string, Action<TContext, TEvent, TState>>
+  >;
+  private currentStateOrNull: TState | null;
+  private cleanupFn: (() => void) | undefined;
+
+  // Used to provide better error messages
+  private knownEvents: Set<string>;
+
+  /**
+   * Returns the initial state, which is defined by the first call made to
+   * .addState().
+   */
+  private get initialState(): TState {
+    // Return the first state ever defined as the initial state
+    const result = this.states.values()[Symbol.iterator]().next();
+    if (result.done) {
+      throw new Error("No states defined yet");
+    } else {
+      return result.value;
+    }
+  }
+
+  private get currentState(): TState {
+    if (this.currentStateOrNull === null) {
+      throw new Error("State machine is not configured yet");
+    }
+    return this.currentStateOrNull;
+  }
+
+  /**
+   * Starts the machine by entering the initial state.
+   */
+  public start(): this {
+    if (this.runningState !== RunningState.NOT_STARTED_YET) {
+      throw new Error("State machine has already started");
+    }
+
+    this.runningState = RunningState.STARTED;
+    this.currentStateOrNull = this.initialState;
+    this.enter();
+    return this;
+  }
+
+  /**
+   * Stops the state machine. Stopping the state machine will call exit
+   * handlers for the current state, but not enter a new state.
+   */
+  public stop(): void {
+    if (this.runningState !== RunningState.STARTED) {
+      throw new Error("Cannot stop a state machine that isn't started yet");
+    }
+    this.runningState = RunningState.STOPPED;
+    this.exit();
+    this.currentStateOrNull = null;
+  }
+
+  constructor(initialContext: TContext) {
+    this.runningState = RunningState.NOT_STARTED_YET;
+    this.currentStateOrNull = null;
+    this.states = new Map();
+    this.knownEvents = new Set();
+    this.allowedTransitions = new Map();
+    this.context = initialContext;
+  }
+
+  /**
+   * Declares the existence of an event type.
+   *
+   * XXX Not sure if this method will eventually be needed.
+   */
+  public addEvent(eventType: TEvent["type"]): this {
+    if (this.runningState !== RunningState.NOT_STARTED_YET) {
+      throw new Error("Already started");
+    }
+    this.knownEvents.add(eventType);
+    return this;
+  }
+
+  /**
+   * Define an explicit finite state in the state machine. States can contain
+   * onEnter and onExit handlers which will automatically get triggered when
+   * state transitions around this state happen.
+   */
+  public addState(state: TState): this {
+    if (this.runningState !== RunningState.NOT_STARTED_YET) {
+      throw new Error("Already started");
+    }
+    this.states.set(state.name, state);
+    return this;
+  }
+
+  public addTransitions(
+    srcStateName: TState["name"],
+    mapping: {
+      //
+      // NOTE: I'm exploring the idea of making it super explicit, by letting
+      // TypeScript force you to handle each event explicitly from every
+      // possible state, so that you make a very explicit decision in each
+      // scenario. If that is ultimately too annoying, we can make these
+      // explicit `null`s optional instead.
+      //
+      // Alternatively, we could distinguish `undefined` as "ignore", and
+      // `null` as throw-and-explicitly-forbid the transition.
+      //
+      [E in TEvent as E["type"]]: Action<TContext, E, TState> | null;
+      //                        ^ Add a `?` here if too annoying
+    }
+  ): this {
+    if (this.runningState !== RunningState.NOT_STARTED_YET) {
+      throw new Error("Already started");
+    }
+
+    let map = this.allowedTransitions.get(srcStateName);
+    if (map === undefined) {
+      map = new Map();
+      this.allowedTransitions.set(srcStateName, map);
+    }
+
+    for (const [type, ev] of Object.entries(mapping)) {
+      if (ev !== null && ev !== undefined) {
+        map.set(type, ev as Action<TContext, TEvent, TState>);
+      }
+    }
+    return this;
+  }
+
+  public get currentStateName(): string {
+    if (this.runningState === RunningState.NOT_STARTED_YET) {
+      throw new Error("Machine hasn't started yet");
+    }
+    if (this.runningState === RunningState.STOPPED) {
+      throw new Error("Machine already stopped");
+    }
+    return this.currentState.name;
+  }
+
+  private getTransition(
+    eventName: TEvent["type"]
+  ): Action<TContext, TEvent, TState> | undefined {
+    return this.allowedTransitions.get(this.currentStateName)?.get(eventName);
+  }
+
+  /**
+   * Checks to see if the given event can be handled in the current state.
+   *
+   * XXX Not sure if this method will eventually be needed.
+   */
+  public can(eventName: TEvent["type"]): boolean {
+    return this.getTransition(eventName) !== undefined;
+  }
+
+  /**
+   * Exits the current state, and executes any necessary onExit handlers.
+   * Call this before changing the current state to the next state.
+   */
+  private exit() {
+    this.cleanupFn?.();
+    this.cleanupFn = undefined;
+    this.currentState.onExit?.(this.context);
+  }
+
+  /**
+   * Enters the current state, and executes any necessary onEnter handlers.
+   * Call this directly _after_ setting the current state to the next state.
+   */
+  private enter() {
+    const cleanupFn = this.currentState.onEnter?.(this.context);
+    if (typeof cleanupFn === "function") {
+      this.cleanupFn = cleanupFn;
+    }
+  }
+
+  /**
+   * Sends an event to the machine, which may cause an internal state
+   * transition to happen. When that happens, will trigger side effects.
+   */
+  public transition(event: TEvent): void {
+    const action = this.getTransition(event.type);
+    if (action === undefined) {
+      if (this.knownEvents.has(event.type)) {
+        // XXX Fail silently instead?
+        throw new Error(
+          `Event ${JSON.stringify(
+            event.type
+          )} is not allowed from state ${JSON.stringify(
+            this.currentState.name
+          )}`
+        );
+      } else {
+        throw new Error(`Unknown event ${JSON.stringify(event.type)}`);
+      }
+    }
+
+    const nextStateName = action(event, this.context);
+    const nextState = this.states.get(nextStateName);
+    if (nextState === undefined) {
+      throw new Error(
+        `Invalid next state name: ${JSON.stringify(nextStateName)}`
+      );
+    }
+
+    if (nextState !== this.currentState) {
+      this.exit();
+      this.currentStateOrNull = nextState;
+      this.enter();
+    }
+  }
+
+  /**
+   * Like .transition(), but will not throw if the event cannot be handled by
+   * the current state.
+   *
+   * XXX Not sure if this method will eventually be needed.
+   */
+  public transitionIfPossible(event: TEvent) {
+    if (this.can(event.type)) {
+      this.transition(event);
+    }
+  }
+}
