0af8b8ce920b97468e3f00fea6be7b1dd01ba484
GuillaumeSalles <guillaume.salles@me.com>
2023-04-28 18:37:29 -0400

    Add vercel edge config on nuxt example

--- examples/nuxtjs-live-avatars/nuxt.config.ts
+++ examples/nuxtjs-live-avatars/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: "vercel-edge",
+  },
   app: {
     head: {
       title: "Liveblocks",
