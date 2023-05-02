e762ad966f295c947937c1b3f3e037ef0eb1826f
Vincent Driessen <me@nvie.com>
2023-05-02 10:08:27 +0200

    Ensure we never rely on the browser-based API accidentally in the future

--- packages/liveblocks-core/.eslintrc.js
+++ packages/liveblocks-core/.eslintrc.js
@@ -25,6 +25,11 @@ module.exports = {
         message:
           "Non-null assertions mask real problems. Please use `nn(...)` (from src/assert.ts) instead.",
       },
+      {
+        selector: "TSTypeReference[typeName.name='WebSocket']",
+        message:
+          "Please don't rely on the WebSocket type directly. Instead use IWebSocket, which is a minimal subset that is also compatible with NodeJS's WebSocket API.",
+      },
     ],
 
     // ----------------------------------------------------------------------
