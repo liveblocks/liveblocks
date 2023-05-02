6559786001b389db2883c4fc4ec1eb4c92d4a3fe
Vincent Driessen <me@nvie.com>
2023-05-02 10:02:26 +0200

    Define an IWebSocket type
    ...which is a minimal subset of both browser- and NodeJS based WebSocket
    APIs, so that both implementations are assignable to the IWebSocket API.

--- packages/liveblocks-core/src/types/WebsocketCloseCodes.ts
+++ packages/liveblocks-core/src/types/WebsocketCloseCodes.ts
@@ -1,3 +1,52 @@
+export interface IWebSocketEvent {
+  type: string;
+}
+
+export interface IWebSocketCloseEvent extends IWebSocketEvent {
+  readonly code: number;
+  readonly wasClean: boolean;
+  readonly reason: string;
+}
+
+export interface IWebSocketMessageEvent extends IWebSocketEvent {
+  readonly data: string | Buffer | ArrayBuffer | readonly Buffer[];
+}
+
+export interface IWebSocketInstance {
+  readonly CONNECTING: number; // 0
+  readonly OPEN: number; // 1
+  readonly CLOSING: number; // 2
+  readonly CLOSED: number; // 3
+
+  readonly readyState: number;
+
+  addEventListener(type: "close", listener: (this: IWebSocketInstance, ev: IWebSocketCloseEvent) => unknown): void; // prettier-ignore
+  addEventListener(type: "message", listener: (this: IWebSocketInstance, ev: IWebSocketMessageEvent) => unknown): void; // prettier-ignore
+  addEventListener(type: string, listener: (this: IWebSocketInstance, ev: IWebSocketEvent) => unknown): void; // prettier-ignore
+  //                     ^^^^^^ Includes "open" and "error"
+
+  removeEventListener(type: "close", listener: (this: IWebSocketInstance, ev: IWebSocketCloseEvent) => unknown): void; // prettier-ignore
+  removeEventListener(type: "message", listener: (this: IWebSocketInstance, ev: IWebSocketMessageEvent) => unknown): void; // prettier-ignore
+  removeEventListener(type: string, listener: (this: IWebSocketInstance, ev: IWebSocketEvent) => unknown): void; // prettier-ignore
+  //                        ^^^^^^ Includes "open" and "error"
+
+  close(): void;
+  send(data: string): void;
+}
+
+/**
+ * Either the browser-based WebSocket API or Node.js' WebSocket API (from the
+ * 'ws' package).
+ *
+ * This type defines the minimal WebSocket API that Liveblocks needs from
+ * a WebSocket implementation, and is a minimal subset of the browser-based
+ * WebSocket APIs and Node.js' WebSocket API so that both implementations are
+ * assignable to this type.
+ */
+export interface IWebSocket {
+  new (address: string): IWebSocketInstance;
+}
+
 export enum WebsocketCloseCodes {
   CLOSE_ABNORMAL = 1006,
 
