8aa094f5d149f3e24e230b270a66a569a2e25071
Vincent Driessen <me@nvie.com>
2023-05-03 12:49:58 +0200

    The liveblocks-redux package has too many explicit and implicit anys, let's ignore these all for now

--- packages/liveblocks-redux/.eslintrc.js
+++ packages/liveblocks-redux/.eslintrc.js
@@ -16,26 +16,17 @@ module.exports = {
     "@typescript-eslint/no-non-null-assertion": "off",
     eqeqeq: "off",
 
+    // Ideally, enable these lint rules again later, as they are useful
+    // to catch bugs
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+
     // ----------------------------------------------------------------------
     // Extra rules for this project specifically
     // ----------------------------------------------------------------------
     /* None yet 😇 ! */
   },
-
-  overrides: [
-    {
-      files: ["src/__tests__/**"],
-
-      // Special config for test files
-      rules: {
-        // Ideally, enable these lint rules again later, as they are useful
-        // to catch bugs
-        "@typescript-eslint/no-unsafe-argument": "off",
-        "@typescript-eslint/no-unsafe-assignment": "off",
-        "@typescript-eslint/no-unsafe-call": "off",
-        "@typescript-eslint/no-unsafe-member-access": "off",
-        "@typescript-eslint/no-unsafe-return": "off",
-      },
-    },
-  ],
 };
