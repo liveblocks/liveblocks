32a3e0acaa23801df062f00244d446d6a269b9d7
Chris Nicholas <ctnicholasdev@gmail.com>
2023-05-10 10:10:02 +0100

    Improve config generation + match interactive tutorial (#866)

--- packages/create-liveblocks-app/bin/templates/init/init-template.ts
+++ packages/create-liveblocks-app/bin/templates/init/init-template.ts
@@ -4,14 +4,21 @@ import fs from "fs";
 import { initPrompts } from "./init-prompts";
 import { configGeneration } from "./config-generation";
 export async function create(flags: Record<string, any>) {
-  const { framework, suspense, typescript } = await initPrompts(flags);
+  const { framework, suspense, typescript, comments } = await initPrompts(
+    flags
+  );
 
   const appDir = process.cwd();
 
   const filesToWrite: { location: string; content: string }[] = [];
 
   const configFileName = "liveblocks.config." + (typescript ? "ts" : "js");
-  const configFile = configGeneration({ framework, suspense, typescript });
+  const configFile = configGeneration({
+    framework,
+    suspense,
+    typescript,
+    comments,
+  });
 
   // === Add config file ==================================================
   filesToWrite.push({
@@ -25,6 +32,7 @@ export async function create(flags: Record<string, any>) {
   });
 
   // === Final console messages ==========================================
+  console.log();
   console.log(c.bold.magentaBright(`✨ ${configFileName} has been generated!`));
   console.log();
 }
