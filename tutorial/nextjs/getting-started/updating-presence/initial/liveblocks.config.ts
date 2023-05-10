8a7a7db29b85cbad0ff894ddb5082085b7e5e446
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-05-10 12:37:10 +0100

    feat: Adding suspense and config file to updating presence page

--- tutorial/nextjs/getting-started/updating-presence/initial/liveblocks.config.ts
+++ tutorial/nextjs/getting-started/updating-presence/initial/liveblocks.config.ts
@@ -5,9 +5,39 @@ const client = createClient({
   publicApiKey: "{% LIVEBLOCKS_PUBLIC_KEY %}",
 });
 
+// Presence type
 type Presence = {
   cursor: { x: number; y: number } | null;
 };
 
-export const { RoomProvider, useMyPresence } =
-  createRoomContext<Presence>(client);
+type Storage = {};
+type UserMeta = {};
+type RoomEvent = {};
+
+export const {
+  suspense: {
+    RoomProvider,
+    useRoom,
+    useMyPresence,
+    useUpdateMyPresence,
+    useSelf,
+    useOthers,
+    useOthersMapped,
+    useOthersConnectionIds,
+    useOther,
+    useBroadcastEvent,
+    useEventListener,
+    useErrorListener,
+    useStorage,
+    useObject,
+    useMap,
+    useList,
+    useBatch,
+    useHistory,
+    useUndo,
+    useRedo,
+    useCanUndo,
+    useCanRedo,
+    useMutation,
+  },
+} = createRoomContext<Presence, Storage, UserMeta, RoomEvent>(client);
