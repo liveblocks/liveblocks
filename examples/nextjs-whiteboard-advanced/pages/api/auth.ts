ac3061d99747fd21fb12609f762d21be65aa3a23
Vincent Driessen <me@nvie.com>
2023-05-16 13:45:06 +0200

    DEBUGGGGGGGING WITH AN APP

--- examples/nextjs-whiteboard-advanced/pages/api/auth.ts
+++ examples/nextjs-whiteboard-advanced/pages/api/auth.ts
@@ -3,16 +3,24 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const API_KEY = process.env.LIVEBLOCKS_SECRET_KEY;
 
+let n = 0;
+
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
   if (!API_KEY) {
-    return res.status(403).end();
+    return res.status(401).end();
   }
 
   const room = req.body.room;
 
+  // Simulate infrastructure issues: for every 10 requests, the 5 first ones
+  // made will fail
+  if (n++ % 10 < 5) {
+    return res.status(502).end();
+  }
+
   const response = await authorize({
-    room,
     secret: API_KEY,
+    room,
     userId: `user-${Math.floor(Math.random() * 10)}`,
   });
 
