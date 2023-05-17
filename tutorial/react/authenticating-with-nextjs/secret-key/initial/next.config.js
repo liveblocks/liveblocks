8cb724a8d0dcc7ce2d06ef971896a933a6b86264
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-05-17 11:56:00 +0100

    feat: Converted all getting started from next -> react

--- tutorial/react/authenticating-with-nextjs/secret-key/initial/next.config.js
+++ tutorial/react/authenticating-with-nextjs/secret-key/initial/next.config.js
@@ -2,5 +2,5 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
-module.exports = nextConfig
+};
+module.exports = nextConfig;
---
+++ tutorial/react/authenticating-with-nextjs/secret-key/solved-diff/App.tsx
@@ -0,0 +1,22 @@
+import { LiveObject } from "@liveblocks/client";
+import { RoomProvider } from "./liveblocks.config";
+import { Room } from "./Room";
+import { ClientSideSuspense } from "@liveblocks/react";
+
+export default function App() {
+  const roomId = "{% ROOM_ID %}";
+
+  return (
+    <RoomProvider
+      id={roomId}
+      initialPresence={{}}
+      initialStorage={{
+        person: new LiveObject({ name: "Marie", age: 30 }),
+      }}
+    >
+      <ClientSideSuspense fallback={<div>Loading...</div>}>
+        {() => <Room />}
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+}
