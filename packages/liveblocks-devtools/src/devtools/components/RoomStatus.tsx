ddae9f5545fb8b1c02930ed7c132558ee593df2d
Vincent Driessen <me@nvie.com>
2023-06-07 15:01:07 +0200

    Prepare a new release of the Liveblocks DevTools that will be able to correctly display colors for new room statuses

--- packages/liveblocks-devtools/src/devtools/components/RoomStatus.tsx
+++ packages/liveblocks-devtools/src/devtools/components/RoomStatus.tsx
@@ -28,15 +28,19 @@ export function RoomStatus({ className, ...props }: ComponentProps<"div">) {
   const currentStatus = useStatus();
   const statusContent = useMemo(() => {
     switch (currentStatus) {
-      case "open":
+      case "connected":
+      case "open": // Sent by old clients (prior to 1.1)
         return <Ping className="text-green-500 dark:text-green-400" />;
 
       case "connecting":
-      case "authenticating":
+      case "reconnecting":
+      case "authenticating": // Sent by old clients (prior to 1.1)
+      case "unavailable": // Sent by old clients (prior to 1.1)
         return <Ping className="text-orange-500 dark:text-orange-400" />;
 
-      case "closed":
-      case "failed":
+      case "disconnected":
+      case "closed": // Sent by old clients (prior to 1.1)
+      case "failed": // Sent by old clients (prior to 1.1)
         return (
           <Ping className="text-red-500 dark:text-red-400" animate={false} />
         );
