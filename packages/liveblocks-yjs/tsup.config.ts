7b705f44fb2acce5740992a9b2038d8b84dce6d5
Jonathan Rowny <jrowny@gmail.com>
2023-06-16 00:24:41 -0700

    change back to cjs only

--- packages/liveblocks-yjs/tsup.config.ts
+++ packages/liveblocks-yjs/tsup.config.ts
@@ -3,11 +3,10 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   dts: true,
-  splitting: false,
+  splitting: true,
   clean: true,
-  bundle: true,
-  platform: "browser",
-  format: ["cjs", "esm"],
+  target: "es2015",
+  format: ["cjs"],
 
   // Perhaps enable later?
   // "minify": true,
