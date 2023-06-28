6da6bdbdb106e4824ae630f3c1a70d88a111c384
Steven Fabre <hello@stevenfabre.com>
2023-06-28 14:43:23 +0200

    Lexical + Yjs guide
    Signed-off-by: Steven Fabre <hello@stevenfabre.com>

--- examples/nextjs-lexical/pages/index.tsx
+++ examples/nextjs-lexical/pages/index.tsx
@@ -1,132 +1,10 @@
 "use client";
 
 import { useMemo } from "react";
-import { RoomProvider, useRoom } from "../liveblocks.config";
-import "@liveblocks/react";
+import { RoomProvider } from "../liveblocks.config";
 import { useRouter } from "next/router";
-import * as Y from "yjs";
-import LiveblocksProvider from "@liveblocks/yjs";
-import { $getRoot, $createParagraphNode, $createTextNode, LexicalEditor } from 'lexical';
-import { LexicalComposer } from '@lexical/react/LexicalComposer';
-import { ContentEditable } from '@lexical/react/LexicalContentEditable';
-import { PlainTextPlugin } from '@lexical/react/LexicalPlainTextPlugin';
-import { CollaborationPlugin } from "@lexical/react/LexicalCollaborationPlugin";
-import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
-import { Provider } from '@lexical/yjs';
 import { ClientSideSuspense } from "@liveblocks/react";
-
-const USER_INFO = [
-  {
-    name: "Charlie Layne",
-    color: "#D583F0",
-    avatar: "https://liveblocks.io/avatars/avatar-1.png",
-  },
-  {
-    name: "Mislav Abha",
-    color: "#F08385",
-    avatar: "https://liveblocks.io/avatars/avatar-2.png",
-  },
-  {
-    name: "Tatum Paolo",
-    color: "#F0D885",
-    avatar: "https://liveblocks.io/avatars/avatar-3.png",
-  },
-  {
-    name: "Anjali Wanda",
-    color: "#85EED6",
-    avatar: "https://liveblocks.io/avatars/avatar-4.png",
-  },
-  {
-    name: "Jody Hekla",
-    color: "#85BBF0",
-    avatar: "https://liveblocks.io/avatars/avatar-5.png",
-  },
-  {
-    name: "Emil Joyce",
-    color: "#8594F0",
-    avatar: "https://liveblocks.io/avatars/avatar-6.png",
-  },
-  {
-    name: "Jory Quispe",
-    color: "#85DBF0",
-    avatar: "https://liveblocks.io/avatars/avatar-7.png",
-  },
-  {
-    name: "Quinn Elton",
-    color: "#87EE85",
-    avatar: "https://liveblocks.io/avatars/avatar-8.png",
-  },
-];
-
-function initialEditorState(editor: LexicalEditor): void {
-  const root = $getRoot();
-  const paragraph = $createParagraphNode();
-  const text = $createTextNode();
-  paragraph.append(text);
-  root.append(paragraph);
-}
-
-function Editor() {
-
-  const room = useRoom();
-  const user = USER_INFO[Math.floor(Math.random() * USER_INFO.length)]
-  const initialConfig = {
-    // NOTE: This is critical for collaboration plugin to set editor state to null. It
-    // would indicate that the editor should not try to set any default state
-    // (not even empty one), and let collaboration plugin do it instead
-    editorState: null,
-    namespace: 'Demo',
-    nodes: [],
-    onError: (error: unknown) => {
-      throw error;
-    },
-    theme: {
-      blockCursor: 'editor-block-cursor',
-      ltr: 'ltr',
-      placeholder: 'editor-placeholder',
-      paragraph: 'editor-paragraph',
-      quote: 'editor-quote',
-      cursor: 'cursor',
-      text: {
-        bold: 'editor-text-bold',
-        underline: 'editor-text-underline',
-        italic: 'editor-text-italic',
-      },
-    },
-  };
-
-  return (
-    <LexicalComposer initialConfig={initialConfig}>
-      <PlainTextPlugin
-        contentEditable={<ContentEditable className="editor-content" />}
-        placeholder={null}
-        ErrorBoundary={LexicalErrorBoundary}
-      />
-      <CollaborationPlugin
-        id="yjs-plugin"
-        cursorColor={user.color}
-        username={user.name}
-        providerFactory={(id, yjsDocMap) => {
-          const doc = new Y.Doc();
-          yjsDocMap.set(id, doc);
-          const provider = new LiveblocksProvider(room, doc) as Provider;
-          return provider;
-        }}
-        initialEditorState={initialEditorState}
-        shouldBootstrap={true}
-      />
-    </LexicalComposer>
-  );
-}
-
-function Example() {
-
-  return (
-    <div className="container">
-      <Editor />
-    </div>
-  );
-}
+import Editor from "@/src/Editor";
 
 function Loading() {
   return (
@@ -137,14 +15,16 @@ function Loading() {
 }
 
 export default function Page() {
-  const roomId = useOverrideRoomId("nextjs-todo-list-v2");
+  const roomId = useOverrideRoomId("nextjs-lexical");
 
   return (
-    <RoomProvider id={roomId} initialPresence={{}}>
-      <ClientSideSuspense fallback={<Loading />}>
-        {() => <Example />}
-      </ClientSideSuspense>
-    </RoomProvider>
+    <main>
+      <RoomProvider id={roomId} initialPresence={{}}>
+        <ClientSideSuspense fallback={<Loading />}>
+          {() => <Editor />}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </main>
   );
 }
 
@@ -152,9 +32,9 @@ export async function getStaticProps() {
   const API_KEY = process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY;
   const API_KEY_WARNING = process.env.CODESANDBOX_SSE
     ? `Add your public key from https://liveblocks.io/dashboard/apikeys as the \`NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY\` secret in CodeSandbox.\n` +
-    `Learn more: https://github.com/liveblocks/liveblocks/tree/main/examples/nextjs-live-cursors#codesandbox.`
+      `Learn more: https://github.com/liveblocks/liveblocks/tree/main/examples/nextjs-live-cursors#codesandbox.`
     : `Create an \`.env.local\` file and add your public key from https://liveblocks.io/dashboard/apikeys as the \`NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY\` environment variable.\n` +
-    `Learn more: https://github.com/liveblocks/liveblocks/tree/main/examples/nextjs-live-cursors#getting-started.`;
+      `Learn more: https://github.com/liveblocks/liveblocks/tree/main/examples/nextjs-live-cursors#getting-started.`;
 
   if (!API_KEY) {
     console.warn(API_KEY_WARNING);
