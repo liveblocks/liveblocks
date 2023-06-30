226c0c071240204d416dc5cdab69df542cae9b1e
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-06-30 11:11:31 +0100

    feat: Basic codemirror guide

--- examples/nextjs-yjs-codemirror/src/Editor.tsx
+++ examples/nextjs-yjs-codemirror/src/Editor.tsx
@@ -1,31 +1,13 @@
 import * as Y from "yjs";
-// @ts-ignore
 import { yCollab } from "y-codemirror.next";
-
 import { EditorView, basicSetup } from "codemirror";
 import { EditorState } from "@codemirror/state";
 import { javascript } from "@codemirror/lang-javascript";
-
-import * as random from "lib0/random";
 import { useCallback, useEffect, useState } from "react";
 import LiveblocksProvider from "@liveblocks/yjs";
 import { useRoom } from "@/liveblocks.config";
 import styles from "./Editor.module.css";
 import { USER_INFO } from "@/src/constants";
-
-export const usercolors = [
-  { color: "#30bced", light: "#30bced33" },
-  { color: "#6eeb83", light: "#6eeb8333" },
-  { color: "#ffbc42", light: "#ffbc4233" },
-  { color: "#ecd444", light: "#ecd44433" },
-  { color: "#ee6352", light: "#ee635233" },
-  { color: "#9ac2c9", light: "#9ac2c933" },
-  { color: "#8acb88", light: "#8acb8833" },
-  { color: "#1be7ff", light: "#1be7ff33" },
-];
-
-// select a random color for this user
-export const userColor = usercolors[random.uint32() % usercolors.length];
 
 export default function Editor() {
   const [element, setElement] = useState<HTMLElement>();
