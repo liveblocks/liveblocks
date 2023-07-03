319e629c8c364b30c4879b1cd0f3958f8e7955e9
Vincent Driessen <me@nvie.com>
2023-07-03 10:56:03 +0200

    Upgrade dependencies (#974)

---
+++ packages/liveblocks-yjs/src/__tests__/index.test.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import LiveblocksProvider from "..";
+
+describe("@liveblocks/yjs", () => {
+  test("TODO", () => {
+    // TODO: Add a real test here
+    expect(LiveblocksProvider).toEqual(LiveblocksProvider);
+  });
+});
