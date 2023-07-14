b32de572596589390adb9e70b4a66401f67fc889
GuillaumeSalles <guillaume.salles@me.com>
2023-07-14 11:21:17 -0400

    Remove invalid characters in error messages

--- packages/liveblocks-core/src/crdts/LiveList.ts
+++ packages/liveblocks-core/src/crdts/LiveList.ts
@@ -872,7 +872,7 @@ export class LiveList<TItem extends Lson> extends AbstractCrdt {
     this._pool?.assertStorageIsWritable();
     if (index < 0 || index > this._items.length) {
       throw new Error(
-        `Cannot insert list item at index "${index}". index should be between 0 and ${this._items.length}`
+        `Cannot insert list item at index "${index}". index should be between 0 and ${this._items.length}`
       );
     }
 
@@ -985,7 +985,7 @@ export class LiveList<TItem extends Lson> extends AbstractCrdt {
     this._pool?.assertStorageIsWritable();
     if (index < 0 || index >= this._items.length) {
       throw new Error(
-        `Cannot delete list item at index "${index}". index should be between 0 and ${
+        `Cannot delete list item at index "${index}". index should be between 0 and ${
           this._items.length - 1
         }`
       );
@@ -1067,7 +1067,7 @@ export class LiveList<TItem extends Lson> extends AbstractCrdt {
     this._pool?.assertStorageIsWritable();
     if (index < 0 || index >= this._items.length) {
       throw new Error(
-        `Cannot set list item at index "${index}". index should be between 0 and ${
+        `Cannot set list item at index "${index}". index should be between 0 and ${
           this._items.length - 1
         }`
       );
