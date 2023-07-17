7b5f84afeab2103741d497365fce9466358dd441
Vincent Driessen <me@nvie.com>
2023-07-17 15:30:32 +0200

    Loosen dupe detection alert a bit (#1004)

--- packages/liveblocks-core/src/dupe-detection.ts
+++ packages/liveblocks-core/src/dupe-detection.ts
@@ -39,6 +39,8 @@ export function detectDupes(
 
   if (!g[pkgId]) {
     g[pkgId] = pkgBuildInfo;
+  } else if (g[pkgId] === pkgBuildInfo) {
+    // Allow it, see https://github.com/liveblocks/liveblocks/pull/1004
   } else {
     const msg = [
       `Multiple copies of Liveblocks are being loaded in your project. This will cause issues! See ${
