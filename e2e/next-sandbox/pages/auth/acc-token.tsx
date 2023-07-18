29ac6ce54231b6940701adf0f9931830005525ca
Vincent Driessen <me@nvie.com>
2023-07-18 23:06:46 +0200

    [e2e] Tweak pages

--- e2e/next-sandbox/pages/auth/acc-token.tsx
+++ e2e/next-sandbox/pages/auth/acc-token.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import { createClient } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
 
@@ -9,34 +10,81 @@ const client = createClient({
 const { RoomProvider, useSelf, useOthers } = createRoomContext(client);
 
 export default function Home() {
-  let roomId = "e2e-auth";
-  if (typeof window !== "undefined") {
-    const queryParam = window.location.search;
-    if (queryParam.split("room=").length > 1) {
-      roomId = queryParam.split("room=")[1];
+  React.useEffect(() => {
+    setText("e2e-modern-auth");
+    if (typeof window !== "undefined") {
+      const queryParam = window.location.search;
+      if (queryParam.split("room=").length > 1) {
+        setText(queryParam.split("room=")[1]);
+      }
     }
-  }
+  }, []);
+
+  const [text, setText] = React.useState("");
+  const [roomId, setRoomId] = React.useState<string | null>(null);
+
   return (
-    <RoomProvider
-      id={roomId}
-      initialPresence={() => ({ diceroll: 1 + Math.floor(Math.random() * 6) })}
-    >
-      <AccessTokenAuthSandbox />
-    </RoomProvider>
+    <>
+      <h1>Auth sandbox</h1>
+      <p>
+        This page connects to a room using an <strong>access token</strong> with
+        permissions <code>session.allow("e2e-*", session.FULL_ACCESS)</code>{" "}
+        (see <code>/api/auth/acc-token</code> backend implementation).
+      </p>
+      <hr />
+      <div>
+        Enter same room with other tokens (open these links in different tabs):
+        <ul>
+          <li>
+            <Link href={"/auth/pubkey?room=" + encodeURIComponent(text)}>
+              <a>with pubkey</a>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/auth/acc-token?room=" + encodeURIComponent(text)}>
+              <a>with access token</a>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/auth/id-token?room=" + encodeURIComponent(text)}>
+              <a>with ID token</a>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/auth/secret-legacy?room=" + encodeURIComponent(text)}>
+              <a>with legacy token</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => {
+            setText(e.currentTarget.value);
+            setRoomId(null);
+          }}
+        />
+        <input type="submit" value="enter" onClick={() => setRoomId(text)} />
+      </div>
+      <hr />
+      {roomId ? (
+        <RoomProvider
+          id={roomId}
+          initialPresence={() => ({
+            diceRoll: 1 + Math.floor(Math.random() * 6),
+          })}
+        >
+          <Sandbox />
+        </RoomProvider>
+      ) : null}
+    </>
   );
 }
 
-function AccessTokenAuthSandbox() {
+function Sandbox() {
   const me = useSelf();
   const others = useOthers();
-  return (
-    <div>
-      <h1>Access token auth sandbox</h1>
-      <p>
-        This page connects the client using a Access token (where <code>k</code>{" "}
-        = <code>"acc"</code> in the JWT token).
-      </p>
-      <pre>{JSON.stringify({ me, others }, null, 2)}</pre>
-    </div>
-  );
+  return <pre>{JSON.stringify({ me, others }, null, 2)}</pre>;
 }
--- e2e/next-sandbox/pages/auth/pubkey.tsx
+++ e2e/next-sandbox/pages/auth/pubkey.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import { nn } from "@liveblocks/core";
 import { createClient } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
@@ -16,31 +17,80 @@ const client = createClient({
 const { RoomProvider, useSelf, useOthers } = createRoomContext(client);
 
 export default function Home() {
-  let roomId = "e2e-auth";
-  if (typeof window !== "undefined") {
-    const queryParam = window.location.search;
-    if (queryParam.split("room=").length > 1) {
-      roomId = queryParam.split("room=")[1];
+  React.useEffect(() => {
+    setText("e2e-modern-auth");
+    if (typeof window !== "undefined") {
+      const queryParam = window.location.search;
+      if (queryParam.split("room=").length > 1) {
+        setText(queryParam.split("room=")[1]);
+      }
     }
-  }
+  }, []);
+
+  const [text, setText] = React.useState("");
+  const [roomId, setRoomId] = React.useState<string | null>(null);
+
   return (
-    <RoomProvider
-      id={roomId}
-      initialPresence={() => ({ diceroll: 1 + Math.floor(Math.random() * 6) })}
-    >
-      <PubkeyAuthSandbox />
-    </RoomProvider>
+    <>
+      <h1>Auth sandbox</h1>
+      <p>
+        This page connects to a room using a <strong>public key</strong>{" "}
+        directly (no JWT token).
+      </p>
+      <hr />
+      <div>
+        Enter same room with other tokens (open these links in different tabs):
+        <ul>
+          <li>
+            <Link href={"/auth/pubkey?room=" + encodeURIComponent(text)}>
+              <a>with pubkey</a>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/auth/acc-token?room=" + encodeURIComponent(text)}>
+              <a>with access token</a>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/auth/id-token?room=" + encodeURIComponent(text)}>
+              <a>with ID token</a>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/auth/secret-legacy?room=" + encodeURIComponent(text)}>
+              <a>with legacy token</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => {
+            setText(e.currentTarget.value);
+            setRoomId(null);
+          }}
+        />
+        <input type="submit" value="enter" onClick={() => setRoomId(text)} />
+      </div>
+      <hr />
+      {roomId ? (
+        <RoomProvider
+          id={roomId}
+          initialPresence={() => ({
+            diceRoll: 1 + Math.floor(Math.random() * 6),
+          })}
+        >
+          <Sandbox />
+        </RoomProvider>
+      ) : null}
+    </>
   );
 }
 
-function PubkeyAuthSandbox() {
+function Sandbox() {
   const me = useSelf();
   const others = useOthers();
-  return (
-    <div>
-      <h1>Public auth sandbox</h1>
-      <p>This page connects the client using a public API key.</p>
-      <pre>{JSON.stringify({ me, others }, null, 2)}</pre>
-    </div>
-  );
+  return <pre>{JSON.stringify({ me, others }, null, 2)}</pre>;
 }
