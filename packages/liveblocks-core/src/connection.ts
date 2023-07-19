52e6758a26a261358ebbf2f1cce20bb727ac00a0
Vincent Driessen <me@nvie.com>
2023-07-19 16:57:41 +0200

    [fsm] Defer transitioning to @ok.* state until ROOM_STATE server message has been observed

--- packages/liveblocks-core/src/connection.ts
+++ packages/liveblocks-core/src/connection.ts
@@ -1,11 +1,13 @@
 import { assertNever } from "./lib/assert";
+import { controlledPromise } from "./lib/controlledPromise";
 import type { Observable } from "./lib/EventSource";
 import { makeEventSource } from "./lib/EventSource";
 import * as console from "./lib/fancy-console";
 import type { BuiltinEvent, Patchable, Target } from "./lib/fsm";
 import { FSM } from "./lib/fsm";
 import type { Json } from "./lib/Json";
-import { withTimeout } from "./lib/utils";
+import { tryParseJson, withTimeout } from "./lib/utils";
+import { ServerMsgCode } from "./protocol/ServerMsg";
 import type {
   IWebSocketCloseEvent,
   IWebSocketEvent,
@@ -381,7 +383,17 @@ const assign = (patch: Partial<Context>) => (ctx: Patchable<Context>) =>
 
 function createConnectionStateMachine<T extends BaseAuthResult>(
   delegates: Delegates<T>,
-  enableDebugLogging: boolean
+  options: {
+    enableDebugLogging: boolean;
+    /** In protocol V7, the actor will no longer be available on the token.
+     * Instead, the `actor` will be sent to the client via a ROOM_STATE message
+     * over an established WebSocket connection. If this setting is set to
+     * `true`, the state machine will only jump to "connected" state _after_
+     * this message has been received. If this setting is `false`, the machine
+     * won't wait for the actor to be received, and instead jump to "connected"
+     * as soon as the WebSocket connection is established. */
+    waitForActorId: boolean;
+  }
 ) {
   // Create observable event sources, which this machine will call into when
   // specific events happen
@@ -563,6 +575,23 @@ function createConnectionStateMachine<T extends BaseAuthResult>(
               rej(event);
             }
 
+            const [actor$, didReceiveActor] = controlledPromise<void>();
+            if (!options.waitForActorId) {
+              // Just mark the promise as "resolved" immediately, so it won't
+              // be a blocker.
+              didReceiveActor();
+            }
+
+            /** Waits until actor is received (from the ROOM_STATE message) */
+            function waitForActorId(event: IWebSocketMessageEvent) {
+              const serverMsg = tryParseJson(event.data as string) as
+                | Record<string, Json>
+                | undefined;
+              if (serverMsg?.type === ServerMsgCode.ROOM_STATE) {
+                didReceiveActor();
+              }
+            }
+
             //
             // Part 1:
             // The `error` and `close` event handlers marked (*) are installed
@@ -570,6 +599,9 @@ function createConnectionStateMachine<T extends BaseAuthResult>(
             // When those get triggered, we reject this promise.
             //
             socket.addEventListener("message", onSocketMessage);
+            if (options.waitForActorId) {
+              socket.addEventListener("message", waitForActorId);
+            }
             socket.addEventListener("error", reject); // (*)
             socket.addEventListener("close", reject); // (*)
             socket.addEventListener("open", () => {
@@ -603,10 +635,20 @@ function createConnectionStateMachine<T extends BaseAuthResult>(
               const unsub = () => {
                 socket.removeEventListener("error", reject); // Remove (*)
                 socket.removeEventListener("close", reject); // Remove (*)
+                socket.removeEventListener("message", waitForActorId);
               };
 
-              // Resolve the promise, which will take us to the @ok.* state
-              resolve([socket, unsub]);
+              // Resolve the promise only once we received the actor ID from
+              // the server. This will act like a traffic light, going green
+              // only once the actor is received. If the machine is configured
+              // not to wait for the actor, the traffic light will already be
+              // green.
+              // All messages received in the mean time while waiting for the
+              // green light will be played back to the client after the
+              // transition to "connected".
+              void actor$.then(() => {
+                resolve([socket, unsub]);
+              });
             });
           }
         );
@@ -850,7 +892,7 @@ function createConnectionStateMachine<T extends BaseAuthResult>(
   cleanups.push(unsubscribe);
 
   // Install debug logging
-  if (enableDebugLogging) {
+  if (options.enableDebugLogging) {
     cleanups.push(enableTracing(machine));
   }
 
@@ -918,7 +960,7 @@ export class ManagedSocket<T extends BaseAuthResult> {
   constructor(delegates: Delegates<T>, enableDebugLogging: boolean = false) {
     const { machine, events, cleanups } = createConnectionStateMachine(
       delegates,
-      enableDebugLogging
+      { waitForActorId: true, enableDebugLogging }
     );
     this.machine = machine;
     this.events = events;
