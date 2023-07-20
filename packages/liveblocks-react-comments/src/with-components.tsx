f9c67d6b1fa1c66a7581902137075dbc2fe168de
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2023-07-20 08:49:11 +0200

    Add early dropdown menu

--- packages/liveblocks-react-comments/src/with-components.tsx
+++ packages/liveblocks-react-comments/src/with-components.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import type { BaseMetadata, BaseUserMeta, CommentData } from "@liveblocks/core";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import type {
   ComponentPropsWithoutRef,
   ForwardRefExoticComponent,
@@ -13,6 +14,7 @@ import type { ComposerSubmitComment } from "./components/Composer";
 import { Composer as DefaultComposer } from "./components/Composer";
 import { Time } from "./components/Time";
 import type { CommentsContext } from "./factory";
+import { EllipsisIcon } from "./icons/ellipsis";
 import { classNames } from "./utils/class-names";
 import { getInitials } from "./utils/get-initials";
 
@@ -68,8 +70,6 @@ export function withComponents<
 ): CommentsContextWithComponents<TThreadMetadata, TUserMeta> {
   const {
     suspense: { useUser },
-    editComment,
-    deleteComment,
   } = context;
   const { resolveUserName, resolveUserAvatar } = options ?? {};
 
@@ -124,6 +124,10 @@ export function withComponents<
     ({ comment, className, ...props }, forwardedRef) => {
       const [isEditing, setEditing] = useState(false);
 
+      const handleEdit = useCallback(() => {
+        setEditing(true);
+      }, []);
+
       const handleEditCancel = useCallback(() => {
         setEditing(false);
       }, []);
@@ -136,6 +140,7 @@ export function withComponents<
           //   threadId: "TODO",
           //   body,
           // });
+          console.log(body);
           setEditing(false);
         },
         []
@@ -162,11 +167,43 @@ export function withComponents<
         >
           <Avatar className="lb-comment-avatar" userId={comment.userId} />
           <Name className="lb-comment-name" userId={comment.userId} />
-          <span>
-            {/* TODO: Add "edited" label */}
-            <Time date={comment.createdAt} />
+          <span className="lb-comment-date">
+            <Time date={comment.createdAt} className="lb-comment-date-time" />
+            {comment.editedAt && (
+              <>
+                {" "}
+                <span className="lb-comment-date-edited">(edited)</span>
+              </>
+            )}
           </span>
-          {/* TODO: Add dropdown to edit and delete */}
+          {/* TODO: Only show if permissions (for now = own comments) allow edit/delete */}
+          {!isEditing && (
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger
+                className="lb-comment-button lb-comment-actions-button"
+                aria-label="Comment actions"
+              >
+                <EllipsisIcon />
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Portal>
+                {/* TODO: Share viewport padding/spacing values with the mentions suggestions inset */}
+                <DropdownMenu.Content className="lb-comment-actions">
+                  <DropdownMenu.Item
+                    className="lb-comment-action"
+                    onSelect={handleEdit}
+                  >
+                    Edit
+                  </DropdownMenu.Item>
+                  <DropdownMenu.Item
+                    className="lb-comment-action"
+                    onSelect={handleDelete}
+                  >
+                    Delete
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Portal>
+            </DropdownMenu.Root>
+          )}
           {isEditing ? (
             <DefaultComposer.Form onCommentSubmit={handleEditSubmit}>
               <DefaultComposer.Body
