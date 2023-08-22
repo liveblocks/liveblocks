c30213071dce15540d9930a5e7c2ffe0393a50c4
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-08-22 11:04:53 +0100

    feat: Monaco example complete

--- examples/nextjs-yjs-monaco/src/components/CollaborativeEditor.tsx
+++ examples/nextjs-yjs-monaco/src/components/CollaborativeEditor.tsx
@@ -2,29 +2,22 @@
 
 import * as Y from "yjs";
 import LiveblocksProvider from "@liveblocks/yjs";
-import {
-  AwarenessList,
-  TypedLiveblocksProvider,
-  UserAwareness,
-  useRoom,
-  useSelf,
-} from "@/liveblocks.config";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { TypedLiveblocksProvider, useRoom } from "@/liveblocks.config";
+import { useCallback, useEffect, useState } from "react";
 import styles from "./CollaborativeEditor.module.css";
 import { Avatars } from "@/components/Avatars";
 import { Editor } from "@monaco-editor/react";
 import { editor } from "monaco-editor";
 import { MonacoBinding } from "y-monaco";
 import { Awareness } from "y-protocols/awareness";
+import { Cursors } from "@/components/Cursors";
+import { Toolbar } from "@/components/Toolbar";
 
 // Collaborative text editor with simple rich text, live cursors, and live avatars
 export function CollaborativeEditor() {
-  const [editorRef, setEditorRef] = useState<editor.IStandaloneCodeEditor>();
   const room = useRoom();
-  const [awarenessUsers, setAwarenessUsers] = useState<AwarenessList>([]);
-
-  // Get user info from Liveblocks authentication endpoint
-  const userInfo = useSelf((me) => me.info);
+  const [provider, setProvider] = useState<TypedLiveblocksProvider>();
+  const [editorRef, setEditorRef] = useState<editor.IStandaloneCodeEditor>();
 
   // Set up Liveblocks Yjs provider and attach Monaco editor
   useEffect(() => {
@@ -36,14 +29,7 @@ export function CollaborativeEditor() {
       yDoc = new Y.Doc();
       const yText = yDoc.getText("monaco");
       yProvider = new LiveblocksProvider(room, yDoc);
-
-      // Add userInfo to Yjs awareness and set list of online users
-      const localUser: UserAwareness["user"] = userInfo;
-      yProvider.awareness.setLocalStateField("user", localUser);
-      yProvider.awareness.on("change", () =>
-        setAwarenessUsers([...yProvider.awareness.getStates()] as AwarenessList)
-      );
-      setAwarenessUsers([...yProvider.awareness.getStates()] as AwarenessList);
+      setProvider(yProvider);
 
       // Attach Yjs to Monaco
       binding = new MonacoBinding(
@@ -59,39 +45,17 @@ export function CollaborativeEditor() {
       yProvider?.destroy();
       binding?.destroy();
     };
-  }, [editorRef, room, userInfo]);
+  }, [editorRef, room]);
 
   const handleOnMount = useCallback((e: editor.IStandaloneCodeEditor) => {
     setEditorRef(e);
   }, []);
 
-  // Insert awareness colours into cursors
-  const styleSheet = useMemo(() => {
-    let cursorStyles = "";
-
-    for (const [clientId, client] of awarenessUsers) {
-      if (client?.user) {
-        cursorStyles += `
-          .yRemoteSelection-${clientId}, 
-          .yRemoteSelectionHead-${clientId}  {
-            --user-color: ${client.user.color};
-          }
-          
-          .yRemoteSelectionHead-${clientId}::after {
-            content: "${client.user.name}";
-          }
-        `;
-      }
-    }
-
-    return { __html: cursorStyles };
-  }, [awarenessUsers]);
-
   return (
     <div className={styles.container}>
-      <style dangerouslySetInnerHTML={styleSheet} />
+      {provider ? <Cursors yProvider={provider} /> : null}
       <div className={styles.editorHeader}>
-        <div className={styles.editorFileName}>file.ts</div>
+        <div>{editorRef ? <Toolbar editor={editorRef} /> : null}</div>
         <Avatars />
       </div>
       <div className={styles.editorContainer}>
---
+++ examples/nextjs-yjs-monaco/src/components/Cursors.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useMemo, useState } from "react";
+import {
+  AwarenessList,
+  TypedLiveblocksProvider,
+  UserAwareness,
+  useSelf,
+} from "@/liveblocks.config";
+
+type Props = {
+  yProvider: TypedLiveblocksProvider;
+};
+
+export function Cursors({ yProvider }: Props) {
+  // Get user info from Liveblocks authentication endpoint
+  const userInfo = useSelf((me) => me.info);
+
+  const [awarenessUsers, setAwarenessUsers] = useState<AwarenessList>([]);
+
+  // Add userInfo to Yjs awareness and set list of online users
+  useEffect(() => {
+    // Add user info to Yjs awareness
+    const localUser: UserAwareness["user"] = userInfo;
+    yProvider.awareness.setLocalStateField("user", localUser);
+
+    // On changes, update `awarenessUsers`
+    function setUsers() {
+      setAwarenessUsers([...yProvider.awareness.getStates()] as AwarenessList);
+    }
+    yProvider.awareness.on("change", setUsers);
+    setUsers();
+
+    return () => {
+      yProvider.awareness.off("change", setUsers);
+    };
+  }, [yProvider]);
+
+  // Insert awareness info into cursors with styles
+  const styleSheet = useMemo(() => {
+    let cursorStyles = "";
+
+    for (const [clientId, client] of awarenessUsers) {
+      if (client?.user) {
+        cursorStyles += `
+          .yRemoteSelection-${clientId}, 
+          .yRemoteSelectionHead-${clientId}  {
+            --user-color: ${client.user.color};
+          }
+          
+          .yRemoteSelectionHead-${clientId}::after {
+            content: "${client.user.name}";
+          }
+        `;
+      }
+    }
+
+    return { __html: cursorStyles };
+  }, [awarenessUsers]);
+
+  return <style dangerouslySetInnerHTML={styleSheet} />;
+}
