3266c42103ba5fb3afe40d611de10fda2f35b3d2
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-08-27 17:06:38 +0100

    feat: Use AvatarStack

--- starter-kits/nextjs-starter-kit/components/CodeEditor/SidePanelOthers.tsx
+++ starter-kits/nextjs-starter-kit/components/CodeEditor/SidePanelOthers.tsx
@@ -1,6 +1,6 @@
 import { shallow } from "@liveblocks/client";
 import { useOthers } from "../../liveblocks.config";
-import { Avatar, AvatarEllipsis } from "../../primitives/Avatar";
+import { AvatarStack } from "../../primitives/AvatarStack";
 import styles from "./SidePanelAvatars.module.css";
 
 type Props = {
@@ -10,32 +10,26 @@ type Props = {
 const MAX_AVATARS = 2;
 
 export function SidePanelAvatars({ fileName }: Props) {
-  const othersInFile = useOthers(
+  const avatarsInFile = useOthers(
     (others) =>
-      others.filter((other) => other.presence.currentFile === fileName),
+      others
+        // Filter those currently in file
+        .filter((other) => other.presence.currentFile === fileName)
+
+        // Convert to Avatar format
+        .map(({ info }) => ({
+          name: info.name,
+          color: info.color,
+          src: info.avatar,
+        })),
     shallow
   );
 
   return (
-    <div className={styles.sidePanelAvatars}>
-      {othersInFile.slice(0, MAX_AVATARS).map((other) => (
-        <Avatar
-          size={20}
-          key={other.connectionId}
-          className={styles.sidePanelAvatar}
-          name={other.info.name}
-          color={other.info.color}
-          src={other.info.avatar}
-        />
-      ))}
-
-      {othersInFile.length > MAX_AVATARS ? (
-        <AvatarEllipsis
-          size={20}
-          className={styles.sidePanelAvatar}
-          ellipsis={othersInFile.length - MAX_AVATARS}
-        />
-      ) : null}
-    </div>
+    <AvatarStack
+      avatars={avatarsInFile}
+      size={16}
+      className={styles.sidePanelAvatarStack}
+    />
   );
 }
