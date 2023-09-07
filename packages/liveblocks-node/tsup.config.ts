5f2707585a5aa5dc21c2edd2154d6fa8f3144104
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2023-09-07 10:52:52 +0200

    Add comment to tsup.config.ts

--- packages/liveblocks-node/tsup.config.ts
+++ packages/liveblocks-node/tsup.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  dts: {
-    resolve: ["@liveblocks/core"],
-  },
-  noExternal: ["@liveblocks/core"],
   splitting: true,
   clean: true,
   target: "es2020",
@@ -17,4 +13,16 @@ export default defineConfig({
     const pkg = require("./package.json");
     options.define.__VERSION__ = JSON.stringify(pkg.version);
   },
+
+  /**
+   * Only types should be imported from @liveblocks/core, because this package
+   * is made to be used in Node.js, and @liveblocks/core has browser-specific code.
+   *
+   * The `dts.resolve` option allows tsup to resolve and inline types (which allows us
+   * to mark it as a devDependency) while `noExternal` prevents it from bundling code by mistake.
+   */
+  dts: {
+    resolve: ["@liveblocks/core"],
+  },
+  noExternal: ["@liveblocks/core"],
 });
