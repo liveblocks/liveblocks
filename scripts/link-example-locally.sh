1cb607eb09ae843279a62d4b8d851c9fd3de1abb
Vincent Driessen <me@nvie.com>
2023-09-08 10:14:00 +0200

    Make committing-to-Git an opt-in flag

--- scripts/link-example-locally.sh
+++ scripts/link-example-locally.sh
@@ -5,6 +5,31 @@ fulldir="$(pwd)"
 rootdir="$(git root)"
 reldir="${fulldir#"$rootdir"/}"
 
+err () {
+    echo "$@" >&2
+}
+
+usage () {
+    err "usage: link-example-locally.sh [-ch]"
+    err
+    err "Run this script from within an example directory. It will turn that example"
+    err "from an standalone NPM project into a workspace, so that it will use the"
+    err "local @liveblocks/* packages instead of the last published version on NPM."
+    err
+    err "Options:"
+    err "-c    Create Git commit with these changes (intended to be removed later)"
+    err "-h    Show this help"
+}
+
+commit=0
+while getopts ch flag; do
+    case "$flag" in
+        c) commit=1 ;;
+        *) usage; exit 2;;
+    esac
+done
+shift $(($OPTIND - 1))
+
 if [[ "$reldir" != "examples/"* || ! -f ../../package.json ]]; then
     echo "Must run this script in one of our example directories" >&2
     exit 2
@@ -30,7 +55,22 @@ if ! grep -q "$reldir" ../../package.json; then
     jq ".workspaces |= . + [\"$reldir\"]" ../../package.json | sponge ../../package.json
 fi
 
+( cd ../../ && npm i > /dev/null )
+
+err "All good! Current example is now a local NPM workspace."
+
 # Step 5: Capture these changes in a Git commit, so you can easily undo this
 # later when you're done testing, by simply removing this commit from the
 # history.
-( cd ../../ && npm i && git add --all && git commit -m "DO NOT KEEP THIS COMMIT - Link $reldir locally" )
+if [ "$commit" -eq 1 ]; then
+    if git is-dirty; then
+        git commit -qam "DO NOT KEEP THIS COMMIT - Link $reldir locally"
+        err "Changes committed to Git."
+        err ""
+        err "IMPORTANT! Please make sure to remove this commit from the Git history when you're done."
+        err ""
+    fi
+else
+    err ""
+    err "IMPORTANT! Please make sure to not commit any of these changes to Git."
+fi
