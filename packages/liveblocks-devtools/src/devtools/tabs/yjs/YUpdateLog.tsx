4e940ff150150003391efc0a3e6975af172542f5
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2023-09-11 17:03:51 +0200

    Tweak update log

--- packages/liveblocks-devtools/src/devtools/tabs/yjs/YUpdateLog.tsx
+++ packages/liveblocks-devtools/src/devtools/tabs/yjs/YUpdateLog.tsx
@@ -4,40 +4,44 @@ import { useYUpdateLog } from "../../contexts/CurrentRoom";
 import ContentDeleted from "./yflow/ContentDeleted";
 
 export function YUpdateLog() {
-  const updates = useYUpdateLog().map((update) => {
-    return (
-      <div>
-        <div>Delete Set: {update.ds.clients.size}</div>
-        <div>
-          Items:{" "}
-          <ul>
-            {update.structs.map((item) => {
-              if (item instanceof GC) {
-                return <li>Garbage collection</li>;
-              }
-              if (item instanceof Item) {
-                if (item.content instanceof ContentString) {
+  const updates = useYUpdateLog();
+
+  return (
+    <div className="w-full">
+      {updates.map((update) => {
+        return (
+          <div className="border-b border-light-300 dark:border-dark-300 bg-light-0 dark:bg-dark-0 last:border-none">
+            <div>Delete Set: {update.ds.clients.size}</div>
+            <div>
+              Items:{" "}
+              <ul className="pl-8">
+                {update.structs.map((item) => {
+                  if (item instanceof GC) {
+                    return <li>Garbage collection</li>;
+                  }
+                  if (item instanceof Item) {
+                    if (item.content instanceof ContentString) {
+                      return (
+                        <li>
+                          {item.id.client}:{item.id.clock}: {item.content.str}
+                        </li>
+                      );
+                    }
+                    if (item.content instanceof ContentDeleted) {
+                      return <li>Deleted: {item.length}</li>;
+                    }
+                  }
                   return (
                     <li>
-                      {item.id.client}:{item.id.clock}: {item.content.str}
+                      {item.id.client}:{item.id.clock}
                     </li>
                   );
-                }
-                if (item.content instanceof ContentDeleted) {
-                  return <li>Deleted: {item.length}</li>;
-                }
-              }
-              return (
-                <li>
-                  {item.id.client}:{item.id.clock}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </div>
-    );
-  });
-
-  return <div>{updates}</div>;
+                })}
+              </ul>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
