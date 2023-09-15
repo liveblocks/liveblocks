84459633fb47d4a808f35704ffc4dc525231906a
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2023-09-15 13:27:14 +0200

    Prevent duplicating icons’ SVG props

--- packages/liveblocks-react-comments/src/icons/ellipsis.tsx
+++ packages/liveblocks-react-comments/src/icons/ellipsis.tsx
@@ -1,25 +1,14 @@
 import type { ComponentProps } from "react";
 import React from "react";
 
-import { classNames } from "../utils/class-names";
+import { Icon } from "../components/internal/Icon";
 
-export function EllipsisIcon({ className, ...props }: ComponentProps<"svg">) {
+export function EllipsisIcon(props: ComponentProps<"svg">) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={20}
-      height={20}
-      viewBox="0 0 20 20"
-      fill="currentColor"
-      stroke="currentColor"
-      strokeWidth={1.5}
-      role="presentation"
-      className={classNames("lb-icon", className)}
-      {...props}
-    >
+    <Icon fill="currentColor" {...props}>
       <circle cx={5} cy={10} r={0.75} />
       <circle cx={10} cy={10} r={0.75} />
       <circle cx={15} cy={10} r={0.75} />
-    </svg>
+    </Icon>
   );
 }
