ce41e3cf240a646b744ae3a085d71d963156aff4
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2023-09-18 13:16:51 +0200

    Comments: Various fixes/tweaks (#1136)

--- packages/liveblocks-react-comments/src/icons/send.tsx
+++ packages/liveblocks-react-comments/src/icons/send.tsx
@@ -1,25 +1,12 @@
 import type { ComponentProps } from "react";
 import React from "react";
 
-import { classNames } from "../utils/class-names";
+import { Icon } from "../components/internal/Icon";
 
-export function SendIcon({ className, ...props }: ComponentProps<"svg">) {
+export function SendIcon(props: ComponentProps<"svg">) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={20}
-      height={20}
-      viewBox="0 0 20 20"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth={1.5}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      role="presentation"
-      className={classNames("lb-icon", className)}
-      {...props}
-    >
+    <Icon {...props}>
       <path d="m5 16 12-6L5 4l2 6-2 6ZM7 10h10" />
-    </svg>
+    </Icon>
   );
 }
