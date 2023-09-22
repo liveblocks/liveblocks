ab9edc6c481d8c6797d97326caa2f956183d4ed5
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2023-09-22 16:44:50 +0200

    Add icon to search input

---
+++ packages/liveblocks-react-comments/src/icons/Search.tsx
@@ -0,0 +1,12 @@
+import type { ComponentProps } from "react";
+import React from "react";
+
+import { Icon } from "../components/internal/Icon";
+
+export function SearchIcon(props: ComponentProps<"svg">) {
+  return (
+    <Icon {...props}>
+      <path d="M9 15A6 6 0 1 0 9 3a6 6 0 0 0 0 12Zm7.5 1.5-3.25-3.25" />
+    </Icon>
+  );
+}
