e4a7f48c765621d1af3dc99a3d543094e0431765
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2023-09-25 13:26:57 +0200

    Fix requestIdleCallback ponyfill

--- packages/liveblocks-react-comments/src/utils/request-idle-callback.ts
+++ packages/liveblocks-react-comments/src/utils/request-idle-callback.ts
@@ -1,17 +1,22 @@
 const IDLE_CALLBACK_FALLBACK_TIMEOUT = 100;
 
-function requestIdleCallbackFallback(callback: () => void) {
+function requestIdleCallbackFallback(
+  callback: IdleRequestCallback,
+  _options?: IdleRequestOptions
+) {
   return setTimeout(callback, IDLE_CALLBACK_FALLBACK_TIMEOUT);
 }
 
 /**
  * Ponyfill for `window.requestIdleCallback`.
  */
-export const requestIdleCallback =
-  window.requestIdleCallback ?? requestIdleCallbackFallback;
+export const requestIdleCallback: typeof window.requestIdleCallback =
+  (typeof window !== "undefined" ? window.requestIdleCallback : null) ??
+  requestIdleCallbackFallback;
 
 /**
  * Ponyfill for `window.cancelIdleCallback`.
  */
-export const cancelIdleCallback =
-  window.cancelIdleCallback ?? window.clearTimeout;
+export const cancelIdleCallback: typeof window.cancelIdleCallback =
+  (typeof window !== "undefined" ? window.cancelIdleCallback : null) ??
+  clearTimeout;
