41c2d0ff806b1c9f00868abe903255cc77f9cff7
Vincent Driessen <me@nvie.com>
2023-09-28 12:39:18 +0200

    Throw more helpful error when `node-fetch` cannot be found

--- packages/liveblocks-node/src/utils.ts
+++ packages/liveblocks-node/src/utils.ts
@@ -1,7 +1,15 @@
 export async function fetchPolyfill(): Promise<typeof fetch> {
-  return typeof globalThis.fetch !== "undefined"
-    ? globalThis.fetch
-    : ((await import("node-fetch")).default as unknown as typeof fetch);
+  if (typeof globalThis.fetch !== "undefined") {
+    return globalThis.fetch;
+  }
+
+  try {
+    return (await import("node-fetch")).default as unknown as typeof fetch;
+  } catch {
+    throw new Error(
+      "It looks like your runtime does not provide a global `fetch` API. Please install `node-fetch` as a dependency and we'll use that instead."
+    );
+  }
 }
 
 export function isNonEmpty(value: unknown): value is string {
