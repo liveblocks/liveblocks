f0340e1f0ffbfc20e7c4a2140c5f4c9fe013461b
Florent Lefebvre <florent.lefebvre@liveblocks.io>
2023-10-03 14:39:30 -0400

    Update packages/liveblocks-core/src/room.ts

--- packages/liveblocks-core/src/room.ts
+++ packages/liveblocks-core/src/room.ts
@@ -1876,6 +1876,7 @@ export function createRoom<
           case ServerMsgCode.THREAD_METADATA_UPDATED:
           case ServerMsgCode.COMMENT_REACTION_ADDED:
           case ServerMsgCode.COMMENT_REACTION_REMOVED:
+          case ServerMsgCode.COMMENT_CREATED:
           case ServerMsgCode.COMMENT_EDITED:
           case ServerMsgCode.COMMENT_DELETED: {
             eventHub.comments.notify(message);
