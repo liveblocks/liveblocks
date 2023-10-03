774337ff26a5bf9955c243c9a49b2fe13cab38a9
Vincent Driessen <me@nvie.com>
2023-10-03 17:03:52 +0200

    Fix Redux tests

--- e2e/next-sandbox/test/redux/index.test.ts
+++ e2e/next-sandbox/test/redux/index.test.ts
@@ -1,13 +1,14 @@
 import { Page, test, expect } from "@playwright/test";
 
 import {
-  expectJson,
   getJson,
   pickRandomItem,
   preparePages,
   sleep,
   waitForContentToBeEquals,
+  waitForJson,
 } from "../utils";
+import randomNumber from "../../utils/randomNumber";
 import type { JsonObject } from "@liveblocks/client";
 
 const TEST_URL = "http://localhost:3007/redux";
@@ -17,78 +18,99 @@ function pickRandomAction() {
 }
 
 test.describe("Redux", () => {
-  let pages: Page[];
+  let pages: [Page, Page];
 
   test.beforeEach(async ({}, testInfo) => {
     const roomName = `e2e-redux-${testInfo.title.replaceAll(" ", "-")}`;
     pages = await preparePages(`${TEST_URL}?room=${roomName}`);
   });
 
-  test.afterEach(async () => {
-    pages.forEach(async (page) => {
-      await page.close();
-    });
+  test.afterEach(() => {
+    return Promise.all(pages.map((page) => page.close()));
   });
 
   test("array push basic + presence", async () => {
-    await pages[0].click("#clear");
-    await expectJson(pages, "#itemsCount", 0);
+    const [page1, page2] = pages;
+    await page1.click("#clear");
+    await waitForJson(pages, "#itemsCount", 0);
 
     await sleep(3000);
-    const othersFirstPage = (await getJson(
-      pages[0],
-      "#others"
-    )) as JsonObject[];
-    const othersSecondPage = (await getJson(
-      pages[1],
-      "#others"
-    )) as JsonObject[];
+    const othersFirstPage = (await getJson(page1, "#others")) as JsonObject[];
+    const othersSecondPage = (await getJson(page2, "#others")) as JsonObject[];
 
     expect(othersFirstPage.length).toEqual(1);
     expect(othersFirstPage[0].presence).toEqual({});
     expect(othersSecondPage.length).toEqual(1);
     expect(othersSecondPage[0].presence).toEqual({});
 
-    await pages[0].click("#push");
+    await page1.click("#push");
     await waitForContentToBeEquals(pages, "#items");
 
-    await pages[0].click("#push");
+    await page1.click("#push");
     await waitForContentToBeEquals(pages, "#items");
 
-    await pages[0].click("#push");
+    await page1.click("#push");
     await waitForContentToBeEquals(pages, "#items");
 
-    await pages[0].click("#clear");
-    await expectJson(pages, "#itemsCount", 0);
+    await page1.click("#clear");
+    await waitForJson(pages, "#itemsCount", 0);
   });
 
-  // TODO: This test is flaky and occasionally fails in CI--make it more robust
-  test.skip("fuzzy", async () => {
-    await pages[0].click("#clear");
-    await expectJson(pages, "#itemsCount", 0);
+  test("fuzzy (before others are visible)", async () => {
+    const [page1, page2] = pages;
+    await page1.click("#clear");
+    await waitForJson(pages, "#itemsCount", 0);
 
     const clicks = [];
 
     for (let i = 0; i < 10; i++) {
-      // no await to create randomness
-      clicks.push(pages[0].click("#push"));
-      clicks.push(pages[1].click("#push"));
-      await sleep(50);
+      clicks.push(page1.click("#push"));
+      clicks.push(page2.click("#push"));
+      await sleep(randomNumber(20));
     }
 
     await waitForContentToBeEquals(pages, "#items");
 
     for (let i = 0; i < 30; i++) {
-      // no await to create randomness
-      clicks.push(pages[0].click(pickRandomAction()));
-      clicks.push(pages[1].click(pickRandomAction()));
-      await sleep(50);
+      clicks.push(page1.click(pickRandomAction()));
+      clicks.push(page2.click(pickRandomAction()));
+      await sleep(randomNumber(20));
     }
 
     await Promise.all(clicks);
     await waitForContentToBeEquals(pages, "#items");
 
-    await pages[0].click("#clear");
-    await expectJson(pages, "#itemsCount", 0);
+    await page1.click("#clear");
+    await waitForJson(pages, "#itemsCount", 0);
+  });
+
+  test("fuzzy (after others are visible)", async () => {
+    const [page1, page2] = pages;
+    await waitForJson(pages, "#othersCount", 1);
+
+    await page1.click("#clear");
+    await waitForJson(pages, "#itemsCount", 0);
+
+    const clicks = [];
+
+    for (let i = 0; i < 10; i++) {
+      clicks.push(page1.click("#push"));
+      clicks.push(page2.click("#push"));
+      await sleep(randomNumber(20));
+    }
+
+    await waitForContentToBeEquals(pages, "#items");
+
+    for (let i = 0; i < 30; i++) {
+      clicks.push(page1.click(pickRandomAction()));
+      clicks.push(page2.click(pickRandomAction()));
+      await sleep(randomNumber(20));
+    }
+
+    await Promise.all(clicks);
+    await waitForContentToBeEquals(pages, "#items");
+
+    await page1.click("#clear");
+    await waitForJson(pages, "#itemsCount", 0);
   });
 });
