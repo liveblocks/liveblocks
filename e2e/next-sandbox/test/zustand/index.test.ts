1fcc70732cfab72e53ba498ffcdb20db7625808e
Vincent Driessen <me@nvie.com>
2023-10-03 17:10:09 +0200

    Fix Zustand tests

--- e2e/next-sandbox/test/zustand/index.test.ts
+++ e2e/next-sandbox/test/zustand/index.test.ts
@@ -7,6 +7,7 @@ import {
   preparePages,
   sleep,
   waitForContentToBeEquals,
+  waitForJson,
 } from "../utils";
 import type { JsonObject } from "@liveblocks/client";
 
@@ -17,80 +18,76 @@ function pickRandomActionWithUndoRedo() {
 }
 
 test.describe("Zustand", () => {
-  let pages: Page[];
+  let pages: [Page, Page];
 
   test.beforeEach(async ({}, testInfo) => {
     const roomName = `e2e-zustand-${testInfo.title.replaceAll(" ", "-")}`;
     pages = await preparePages(`${TEST_URL}?room=${roomName}`);
   });
 
-  test.afterEach(async () => {
-    pages.forEach(async (page) => {
-      await page.close();
-    });
+  test.afterEach(() => {
+    return Promise.all(pages.map((page) => page.close()));
   });
 
-  test("array push basic + presence", async () => {
-    await pages[0].click("#clear");
-    await expectJson(pages, "#itemsCount", 0);
+  test.only("array push basic + presence", async () => {
+    const [page1, page2] = pages;
+    await page1.click("#clear");
+    await expectJson(page1, "#itemsCount", 0);
 
-    await sleep(3000);
-    const othersFirstPage = (await getJson(
-      pages[0],
-      "#others"
-    )) as JsonObject[];
-    const othersSecondPage = (await getJson(
-      pages[1],
-      "#others"
-    )) as JsonObject[];
+    await waitForJson(pages, "#othersCount", 1);
 
+    // XXX Use theirPresence here
+    const othersFirstPage = (await getJson(page1, "#others")) as JsonObject[];
+    const othersSecondPage = (await getJson(page2, "#others")) as JsonObject[];
     expect(othersFirstPage.length).toEqual(1);
     expect(othersFirstPage[0].presence).toEqual({});
     expect(othersSecondPage.length).toEqual(1);
     expect(othersSecondPage[0].presence).toEqual({});
 
-    await pages[0].click("#push");
+    await page1.click("#push");
     await waitForContentToBeEquals(pages, "#items");
 
-    await pages[0].click("#push");
+    await page1.click("#push");
     await waitForContentToBeEquals(pages, "#items");
 
-    await pages[0].click("#push");
+    await page1.click("#push");
     await waitForContentToBeEquals(pages, "#items");
 
-    await pages[0].click("#clear");
-    await expectJson(pages, "#itemsCount", 0);
+    await page1.click("#clear");
+    await waitForJson(pages, "#itemsCount", 0);
   });
 
   test("with enter and leave room", async () => {
-    await pages[0].click("#clear");
+    const [page1, page2] = pages;
+    await page1.click("#clear");
     await expectJson(pages, "#itemsCount", 0);
 
-    await pages[0].click("#push");
+    await page1.click("#push");
     await sleep(50);
-    await pages[0].click("#push");
+    await page1.click("#push");
     await waitForContentToBeEquals(pages, "#items");
 
-    await pages[1].click("#leave"); // Leave
+    await page2.click("#leave"); // Leave
     await sleep(500);
 
-    await pages[0].click("#push");
+    await page1.click("#push");
     await sleep(1000);
 
-    await pages[1].click("#enter"); // Enter
+    await page2.click("#enter"); // Enter
     await waitForContentToBeEquals(pages, "#items");
 
-    await pages[0].click("#clear");
+    await page1.click("#clear");
     await expectJson(pages, "#itemsCount", 0);
   });
 
-  test.skip("fuzzy", async () => {
-    await pages[0].click("#clear");
+  test("fuzzy", async () => {
+    const [page1, page2] = pages;
+    await page1.click("#clear");
     await expectJson(pages, "#itemsCount", 0);
     for (let i = 0; i < 10; i++) {
       // no await to create randomness
-      pages[0].click("#push");
-      pages[1].click("#push");
+      page1.click("#push");
+      page2.click("#push");
       await sleep(50);
     }
 
@@ -98,14 +95,14 @@ test.describe("Zustand", () => {
 
     for (let i = 0; i < 50; i++) {
       // no await to create randomness
-      pages[0].click(pickRandomActionWithUndoRedo());
-      pages[1].click(pickRandomActionWithUndoRedo());
+      page1.click(pickRandomActionWithUndoRedo());
+      page2.click(pickRandomActionWithUndoRedo());
       await sleep(50);
     }
 
     await waitForContentToBeEquals(pages, "#items");
 
-    await pages[0].click("#clear");
+    await page1.click("#clear");
     await expectJson(pages, "#itemsCount", 0);
   });
 });
