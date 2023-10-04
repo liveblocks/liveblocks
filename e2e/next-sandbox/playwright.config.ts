bd63ea21cdc4d45b4a253891ede4ee99a124b942
Vincent Driessen <me@nvie.com>
2023-10-04 18:10:23 +0200

    Play with the settings a bit

--- e2e/next-sandbox/playwright.config.ts
+++ e2e/next-sandbox/playwright.config.ts
@@ -24,9 +24,9 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 5 : 1,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 2,
+  workers: process.env.CI ? 4 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
