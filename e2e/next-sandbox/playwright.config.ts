7bbfcc68ffee2f8ae068cbc9e6930e67d561df3a
Vincent Driessen <me@nvie.com>
2023-10-04 18:36:57 +0200

    Use concurrency, the tests are now properly isolated

--- e2e/next-sandbox/playwright.config.ts
+++ e2e/next-sandbox/playwright.config.ts
@@ -26,7 +26,7 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : 2,
+  workers: process.env.CI ? 4 : 8,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
