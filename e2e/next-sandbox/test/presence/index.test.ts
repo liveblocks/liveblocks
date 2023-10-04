d4fa2f55b615412e432bd4ac5fa52c29c0ba3b92
Vincent Driessen <me@nvie.com>
2023-10-04 21:50:00 +0200

    DRY up room creation

--- e2e/next-sandbox/test/presence/index.test.ts
+++ e2e/next-sandbox/test/presence/index.test.ts
@@ -1,5 +1,7 @@
+import type { Page } from "@playwright/test";
 import { test } from "@playwright/test";
-import { expectJson, preparePage, waitForJson } from "../utils";
+import { expectJson, preparePage, preparePages, waitForJson } from "../utils";
+import { genRoomId } from "../../utils";
 
 const WIDTH = 640;
 const BG_COLOR_1 = "&bg=" + encodeURIComponent("#cafbca");
@@ -7,17 +9,23 @@ const BG_COLOR_2 = "&bg=" + encodeURIComponent("#e9ddf9");
 
 const TEST_URL = "http://localhost:3007/presence";
 
-// XXX DRY up page prep + close in a before/after test
-
 test.describe("Presence", () => {
-  test("connect A => connect B => verify others on A and B", async () => {
-    const testUrl = TEST_URL + "?room=e2e-presence-scenario1";
-    const pages = await Promise.all([
-      preparePage(testUrl + BG_COLOR_1),
-      preparePage(testUrl + BG_COLOR_2, WIDTH),
-    ]);
-    const [page1, page2] = pages;
+  let pages: [Page, Page];
 
+  test.beforeEach(async ({}, testInfo) => {
+    const roomName = genRoomId(testInfo.title);
+    pages = await preparePages(
+      `${TEST_URL}?room=${encodeURIComponent(roomName)}`
+    );
+  });
+
+  test.afterEach(() =>
+    // Close all pages
+    Promise.all(pages.map((page) => page.close()))
+  );
+
+  test("connect A => connect B => verify others on A and B", async () => {
+    const [page1, page2] = pages;
     await waitForJson(pages, "#othersCount", 1);
     await waitForJson(pages, "#theirPresence", {});
 
@@ -25,27 +33,8 @@ test.describe("Presence", () => {
     await page2.close();
   });
 
-  test("connect A => update presence A => connect B => verify presence A on B", async () => {
-    const testUrl = TEST_URL + "?room=e2e-presence-scenario2";
-    const page1 = await preparePage(testUrl + BG_COLOR_1);
-    await page1.click("#inc-foo");
-
-    const page2 = await preparePage(testUrl + BG_COLOR_2, WIDTH);
-    await waitForJson([page1, page2], "#othersCount", 1);
-
-    await expectJson(page2, "#theirPresence", { foo: 1 });
-
-    await page1.close();
-    await page2.close();
-  });
-
   test("connect A => connect B => update presence A => verify presence A on B", async () => {
-    const testUrl = TEST_URL + "?room=e2e-presence-scenario3";
-    const [page1, page2] = await Promise.all([
-      preparePage(testUrl + BG_COLOR_1),
-      preparePage(testUrl + BG_COLOR_2, WIDTH),
-    ]);
-
+    const [page1, page2] = pages;
     await waitForJson([page1, page2], "#othersCount", 1);
 
     await page1.click("#inc-foo");
@@ -58,12 +47,7 @@ test.describe("Presence", () => {
   });
 
   test("connect A => connect B => update presence B => verify presence A on B", async () => {
-    const testUrl = TEST_URL + "?room=e2e-presence-scenario3b";
-    const [page1, page2] = await Promise.all([
-      preparePage(testUrl + BG_COLOR_1),
-      preparePage(testUrl + BG_COLOR_2, WIDTH),
-    ]);
-
+    const [page1, page2] = pages;
     await waitForJson([page1, page2], "#othersCount", 1);
 
     await page2.click("#inc-foo");
@@ -77,12 +61,7 @@ test.describe("Presence", () => {
   });
 
   test("connect A => connect B => verify other on B => disconnect A => verify others is empty on B", async () => {
-    const testUrl = TEST_URL + "?room=e2e-presence-scenario4";
-    const [page1, page2] = await Promise.all([
-      preparePage(testUrl + BG_COLOR_1),
-      preparePage(testUrl + BG_COLOR_2, WIDTH),
-    ]);
-
+    const [page1, page2] = pages;
     await waitForJson([page1, page2], "#othersCount", 1);
 
     await waitForJson(page2, "#theirPresence", {});
@@ -95,12 +74,7 @@ test.describe("Presence", () => {
   });
 
   test("client B receives other update presence before initial presence", async () => {
-    const testUrl = TEST_URL + "?room=e2e-presence-scenario5";
-    const [page1, page2] = await Promise.all([
-      preparePage(testUrl + BG_COLOR_1),
-      preparePage(testUrl + BG_COLOR_2, WIDTH),
-    ]);
-
+    const [page1, page2] = pages;
     await waitForJson([page1, page2], "#othersCount", 1);
 
     await page1.click("#set-bar");
@@ -122,16 +96,9 @@ test.describe("Presence", () => {
     await page1.close();
     await page2.close();
   });
-});
 
-test.describe("Broadcast", () => {
   test("connect A => connect B => broadcast from A => verify B got event", async () => {
-    const testUrl = TEST_URL + "?room=e2e-broadcast-scenario1";
-    const [page1, page2] = await Promise.all([
-      preparePage(testUrl + BG_COLOR_1),
-      preparePage(testUrl + BG_COLOR_2, WIDTH),
-    ]);
-
+    const [page1, page2] = pages;
     await waitForJson(page1, "#othersCount", 1);
 
     await page1.click("#broadcast-emoji");
@@ -151,6 +118,22 @@ test.describe("Broadcast", () => {
       42,
     ]);
     await waitForJson(page2, "#events", [{ type: "EMOJI", emoji: "🔥" }]);
+
+    await page1.close();
+    await page2.close();
+  });
+});
+
+test.describe("Presence (w/ specific window timing)", () => {
+  test("connect A => update presence A => connect B => verify presence A on B", async () => {
+    const testUrl = TEST_URL + "?room=e2e-presence-scenario2";
+    const page1 = await preparePage(testUrl + BG_COLOR_1);
+    await page1.click("#inc-foo");
+
+    const page2 = await preparePage(testUrl + BG_COLOR_2, WIDTH);
+    await waitForJson([page1, page2], "#othersCount", 1);
+
+    await expectJson(page2, "#theirPresence", { foo: 1 });
 
     await page1.close();
     await page2.close();
--- e2e/next-sandbox/test/storage/list.test.ts
+++ e2e/next-sandbox/test/storage/list.test.ts
@@ -1,5 +1,5 @@
-import { Page, test } from "@playwright/test";
-
+import type { Page } from "@playwright/test";
+import { test } from "@playwright/test";
 import {
   expectJson,
   nanoSleep,
