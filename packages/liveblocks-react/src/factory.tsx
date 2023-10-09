b9823414ce9655925df43ec3a223a2c24f558454
Vincent Driessen <me@nvie.com>
2023-10-09 15:22:09 +0200

    Fix: await either a self event, _or_ a status change event

--- packages/liveblocks-react/src/factory.tsx
+++ packages/liveblocks-react/src/factory.tsx
@@ -761,6 +761,7 @@ export function createRoomContext<
     // promise resolves (aka until storage has loaded). After that, it will
     // render this component tree again.
     throw new Promise<void>((res) => {
+      room.events.self.subscribeOnce(() => res());
       room.events.status.subscribeOnce(() => res());
     });
   }
