f3e43ea20aebb3dbf8f94d078f82233690f680ce
Vincent Driessen <me@nvie.com>
2023-10-10 16:14:17 +0200

    Fix E2E tests to use modern auth by default (#1210)

--- e2e/next-sandbox/utils/createClient.ts
+++ e2e/next-sandbox/utils/createClient.ts
@@ -2,7 +2,7 @@ import { createClient } from "@liveblocks/client";
 
 export default function createLiveblocksClient() {
   return createClient({
-    authEndpoint: "/api/auth/legacy-token",
+    authEndpoint: "/api/auth/access-token",
     liveblocksServer: process.env.NEXT_PUBLIC_LIVEBLOCKS_SERVER,
     enableDebugLogging: true,
   } as any);
