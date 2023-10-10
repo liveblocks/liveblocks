8ddb1e2dca7c53cf21b1f2e6bfba793c86b508b9
Jonathan Rowny <jrowny@gmail.com>
2023-10-10 15:39:52 -0700

    comments

--- packages/liveblocks-core/src/room.ts
+++ packages/liveblocks-core/src/room.ts
@@ -2042,14 +2042,15 @@ export function createRoom<
   }
 
   function refreshStorage(options: { flush: boolean }) {
-    // Only add the fetch message to the outgoing message queue if it isn't
-    // already there
     const messages = context.buffer.messages;
     if (config.unstable_streamData) {
+      // instead of sending a fetch message over WS, stream over HTTP
       void streamStorage();
     } else if (
       !messages.some((msg) => msg.type === ClientMsgCode.FETCH_STORAGE)
     ) {
+      // Only add the fetch message to the outgoing message queue if it isn't
+      // already there
       messages.push({ type: ClientMsgCode.FETCH_STORAGE });
     }
 
