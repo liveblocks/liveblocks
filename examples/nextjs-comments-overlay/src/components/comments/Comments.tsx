06773d1838b24f9010fd85dde61ee0317a39cb55
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-10-18 11:19:13 +0100

    feat: Error boundary

--- examples/nextjs-comments-overlay/src/components/comments/Comments.tsx
+++ examples/nextjs-comments-overlay/src/components/comments/Comments.tsx
@@ -3,16 +3,27 @@
 import { ClientSideSuspense } from "@liveblocks/react";
 import { CommentsOverlay } from "@/components/comments/CommentsOverlay";
 import { Toolbar } from "@/components/comments/Toolbar";
+import { ErrorBoundary } from "react-error-boundary";
+import styles from "./Toolbar.module.css";
 
 export function Comments() {
   return (
-    <ClientSideSuspense fallback={null}>
-      {() => (
-        <>
-          <Toolbar />
-          <CommentsOverlay />
-        </>
-      )}
-    </ClientSideSuspense>
+    /* @ts-ignore */
+    <ErrorBoundary
+      fallback={
+        <div className={styles.toolbar}>
+          An error occurred while loading threads.
+        </div>
+      }
+    >
+      <ClientSideSuspense fallback={null}>
+        {() => (
+          <>
+            <Toolbar />
+            <CommentsOverlay />
+          </>
+        )}
+      </ClientSideSuspense>
+    </ErrorBoundary>
   );
 }
