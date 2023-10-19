d83d539011a5f7a4c2f36447a90f5823e8ae8d24
Vincent Driessen <me@nvie.com>
2023-10-19 15:28:00 +0200

    Be even more careful with type defs

--- packages/liveblocks-core/src/types/Others.ts
+++ packages/liveblocks-core/src/types/Others.ts
@@ -1,4 +1,5 @@
 import type { JsonObject } from "../lib/Json";
+import type { Resolve } from "../lib/Resolve";
 import type { BaseUserMeta } from "../protocol/BaseUserMeta";
 import type { User } from "./User";
 
@@ -10,48 +11,47 @@ export type Others<
   TUserMeta extends BaseUserMeta,
 > = readonly User<TPresence, TUserMeta>[];
 
+export type InternalOthersEvent<
+  TPresence extends JsonObject,
+  TUserMeta extends BaseUserMeta,
+> =
+  | { type: "leave"; user: User<TPresence, TUserMeta> }
+  | { type: "enter"; user: User<TPresence, TUserMeta> }
+  | {
+      type: "update";
+      user: User<TPresence, TUserMeta>;
+      updates: Partial<TPresence>;
+    }
+  | { type: "reset" };
+
+// XXX Rename this to OthersEvent eventually again
+export type ModernOthersEvent<
+  TPresence extends JsonObject,
+  TUserMeta extends BaseUserMeta,
+> = Resolve<
+  InternalOthersEvent<TPresence, TUserMeta> & {
+    others: readonly User<TPresence, TUserMeta>[];
+  }
+>;
+
+//
+// NOTE:
+// This type looks an awful lot like InternalOthersEvent, but don't change this
+// type definition or DRY this up!
+// The type LegacyOthersEvent is used in the signature of some public APIs, and
+// as such should remain backward compatible.
+//
+// XXX Probably better to codify this knowledge as a tsd test instead!
+//
 export type LegacyOthersEvent<
   TPresence extends JsonObject,
   TUserMeta extends BaseUserMeta,
 > =
-  | {
-      type: "leave";
-      user: User<TPresence, TUserMeta>;
-    }
-  | {
-      type: "enter";
-      user: User<TPresence, TUserMeta>;
-    }
+  | { type: "leave"; user: User<TPresence, TUserMeta> }
+  | { type: "enter"; user: User<TPresence, TUserMeta> }
   | {
       type: "update";
       user: User<TPresence, TUserMeta>;
       updates: Partial<TPresence>;
     }
-  | {
-      type: "reset";
-    };
-
-export type ModernOthersEvent<
-  TPresence extends JsonObject,
-  TUserMeta extends BaseUserMeta,
-> =
-  | {
-      type: "leave";
-      user: User<TPresence, TUserMeta>;
-      others: readonly User<TPresence, TUserMeta>[];
-    }
-  | {
-      type: "enter";
-      user: User<TPresence, TUserMeta>;
-      others: readonly User<TPresence, TUserMeta>[];
-    }
-  | {
-      type: "update";
-      user: User<TPresence, TUserMeta>;
-      updates: Partial<TPresence>;
-      others: readonly User<TPresence, TUserMeta>[];
-    }
-  | {
-      type: "reset";
-      others: readonly User<TPresence, TUserMeta>[];
-    };
+  | { type: "reset" };
