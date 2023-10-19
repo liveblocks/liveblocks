12cef41b23d6add1c95147c7ad46d2f6d14cd155
Vincent Driessen <me@nvie.com>
2023-10-19 12:42:13 +0200

    Do a slightly more interesting test, make it more robust

--- e2e/next-sandbox/test/comments/with-suspense.test.ts
+++ e2e/next-sandbox/test/comments/with-suspense.test.ts
@@ -23,22 +23,24 @@ test.describe("Comments", () => {
 
   test("verify A and B display same number of threads after threads are loaded", async () => {
     await waitForJson(pages, "#isLoading", false, { timeout: 10_000 });
-    await waitUntilEqualOnAllPages(pages, "#numOfThreads");
+    await waitUntilEqualOnAllPages(pages, "#numOfThreads", { interval: 250 });
   });
 
   test("verify thread creation on B is broadcasted correctly to A", async () => {
     const [page1, page2] = pages;
 
     await waitForJson(pages, "#isLoading", false, { timeout: 10_000 });
+    await waitUntilEqualOnAllPages(pages, "#numOfThreads", { interval: 250 });
 
-    const numOfThreads = (await getJson(page1, "#numOfThreads")) as number;
+    // Read starting value n
+    const n = (await getJson(page1, "#numOfThreads")) as number;
 
+    await page1.click("#create-thread");
     await page2.click("#create-thread");
-    await waitForJson(page1, "#numOfThreads", numOfThreads + 1, {
-      timeout: 10_000,
-    });
+    await page2.click("#create-thread");
+    await waitForJson(pages, "#numOfThreads", n + 3, { timeout: 10_000 });
 
     await page2.click("#delete-comment");
-    await waitUntilEqualOnAllPages(pages, "#numOfThreads", { interval: 250 });
+    await waitForJson(pages, "#numOfThreads", n + 3 - 1, { timeout: 10_000 });
   });
 });
