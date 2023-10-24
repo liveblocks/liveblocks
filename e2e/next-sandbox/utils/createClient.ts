61890d4f72c514eadd940f762e2e32fe5e1afed4
Jonathan <jrowny@gmail.com>
2023-10-24 00:13:07 -0700

    add a note to e2e readme (#1257)
    Co-authored-by: Vincent Driessen <me@nvie.com>

--- e2e/next-sandbox/utils/createClient.ts
+++ e2e/next-sandbox/utils/createClient.ts
@@ -1,11 +1,15 @@
 import { createClient } from "@liveblocks/client";
+import { nn } from "@liveblocks/core";
 
 export default function createLiveblocksClient() {
   return createClient({
     authEndpoint: "/api/auth/access-token",
 
     // @ts-expect-error - Hidden settings
-    baseUrl: process.env.NEXT_PUBLIC_LIVEBLOCKS_BASE_URL,
+    baseUrl: nn(
+      process.env.NEXT_PUBLIC_LIVEBLOCKS_BASE_URL,
+      "Please specify NEXT_PUBLIC_LIVEBLOCKS_BASE_URL env var"
+    ),
     enableDebugLogging: true,
   });
 }
