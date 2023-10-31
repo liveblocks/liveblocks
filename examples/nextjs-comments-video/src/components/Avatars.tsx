98677643642c1eb3884ae5fe5dec5bb9adf5a5b0
Chris Nicholas <ctnicholasdev@gmail.com>
2023-10-31 17:23:55 +0000

    Fix video comments example deploy (#1275)

--- examples/nextjs-comments-video/src/components/Avatars.tsx
+++ examples/nextjs-comments-video/src/components/Avatars.tsx
@@ -4,8 +4,17 @@ import { PresenceStates, useOthers, useSelf } from "@/liveblocks.config";
 import styles from "./Avatars.module.css";
 import { PauseIcon } from "@/icons/Pause";
 import { PlayIcon } from "@/icons/Play";
+import { ClientSideSuspense } from "@liveblocks/react";
 
 export function Avatars() {
+  return (
+    <ClientSideSuspense fallback={null}>
+      {() => <AvatarStack />}
+    </ClientSideSuspense>
+  );
+}
+
+function AvatarStack() {
   const users = useOthers();
   const currentUser = useSelf();
 
@@ -37,8 +46,7 @@ export function Avatars() {
 
 type AvatarProps = { src: string; name: string; state: PresenceStates };
 
-export function Avatar({ src, name, state }: AvatarProps) {
-  // TODO use `state` to show a `playing | paused | seeking` indicator
+function Avatar({ src, name, state }: AvatarProps) {
   return (
     <div className={styles.avatar} data-tooltip={name}>
       <img src={src} className={styles.avatar_picture} alt={name} />
