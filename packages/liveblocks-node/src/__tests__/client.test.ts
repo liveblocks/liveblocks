db1ae1c5f0a83e31c9aa94d1eca014e8b9d23707
Nimesh Nayaju <nimeshnayaju@gmail.com>
2023-11-03 15:44:53 -0700

    Fix lint error

--- packages/liveblocks-node/src/__tests__/client.test.ts
+++ packages/liveblocks-node/src/__tests__/client.test.ts
@@ -1,5 +1,6 @@
-import { HttpResponse, http } from "msw";
+import { http, HttpResponse } from "msw";
 import { setupServer } from "msw/node";
+
 import { ApiError, Liveblocks } from "../client";
 import { DEFAULT_BASE_URL } from "../utils";
 
