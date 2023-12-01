1ebeb94978f8b2c734ad03853efd7852da407fd6
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2023-12-01 15:00:17 +0100

    Improve batching utility and add cached version

--- packages/liveblocks-core/src/lib/__tests__/batch.test.ts
+++ packages/liveblocks-core/src/lib/__tests__/batch.test.ts
@@ -1,5 +1,5 @@
 import { sleep } from "../../__tests__/_waitUtils";
-import { Batch } from "../batch";
+import { Batch, BatchCache } from "../batch";
 
 const SOME_TIME = 5;
 const ERROR_MESSAGE = "error";
@@ -25,6 +25,8 @@ describe("Batch", () => {
 
     await expect(a).resolves.toEqual("a");
     await expect(b).resolves.toEqual("b");
+
+    // Callback should be called only once for both "a" and "b".
     expect(callback).toHaveBeenCalledWith([["a"], ["b"]]);
   });
 
@@ -37,6 +39,8 @@ describe("Batch", () => {
 
     await expect(a).resolves.toEqual("a");
     await expect(b).resolves.toEqual("b");
+
+    // Callback should be called only once for both "a" and "b".
     expect(callback).toHaveBeenCalledWith([["a"], ["b"]]);
   });
 
@@ -50,6 +54,9 @@ describe("Batch", () => {
 
     await expect(a).resolves.toEqual("a");
     await expect(b).resolves.toEqual("b");
+
+    // Callback should be called twice, once for "a" and once for "b".
+    expect(callback).toHaveBeenCalledTimes(2);
     expect(callback).toHaveBeenNthCalledWith(1, [["a"]]);
     expect(callback).toHaveBeenNthCalledWith(2, [["b"]]);
   });
@@ -66,11 +73,13 @@ describe("Batch", () => {
 
     await expect(a).resolves.toEqual("a");
     await expect(b).resolves.toEqual("b");
+
+    // Callback should be called twice, once for "a" and once for "b".
     expect(callback).toHaveBeenNthCalledWith(1, [["a"]]);
     expect(callback).toHaveBeenNthCalledWith(2, [["b"]]);
   });
 
-  test("should reject erroring batch", async () => {
+  test("should reject batch errors", async () => {
     const callback = jest.fn(() => {
       throw ERROR;
     });
@@ -79,11 +88,12 @@ describe("Batch", () => {
     const a = batch.add("a");
     const b = batch.add("b");
 
+    // All calls should reject with the error thrown by the callback.
     await expect(a).rejects.toEqual(ERROR);
     await expect(b).rejects.toEqual(ERROR);
   });
 
-  test("should reject rejected batch", async () => {
+  test("should reject batch rejections", async () => {
     const callback = jest.fn(() => {
       return Promise.reject(ERROR_MESSAGE);
     });
@@ -92,11 +102,12 @@ describe("Batch", () => {
     const a = batch.add("a");
     const b = batch.add("b");
 
+    // All calls should reject with the rejection reason returned by the callback.
     await expect(a).rejects.toEqual(ERROR_MESSAGE);
     await expect(b).rejects.toEqual(ERROR_MESSAGE);
   });
 
-  test("should reject individual calls", async () => {
+  test("should reject individual errors", async () => {
     const callback = jest.fn(() => {
       return ["a", ERROR];
     });
@@ -105,6 +116,7 @@ describe("Batch", () => {
     const a = batch.add("a");
     const b = batch.add("b");
 
+    // "a" should resolve, "b" should reject.
     await expect(a).resolves.toEqual("a");
     await expect(b).rejects.toEqual(ERROR);
   });
@@ -124,8 +136,209 @@ describe("Batch", () => {
 
     await expect(batch.add("a")).rejects.toEqual(
       new Error(
-        "Batch callback must return an array of the same length as the number of items in the batch. Expected 1, but got 0."
+        "Batch callback must return an array of the same length as the number of calls in the batch. Expected 1, but got 0."
       )
     );
+  });
+});
+
+describe("BatchCache", () => {
+  test("should cache and return results for synchronous calls", async () => {
+    const callback = jest.fn(synchronousCallback);
+    const batch = new BatchCache<string, [string]>(callback, {
+      delay: SOME_TIME,
+    });
+
+    const a = batch.add("a");
+    const b = batch.add("b");
+
+    await expect(a).resolves.toEqual("a");
+    await expect(b).resolves.toEqual("b");
+
+    // Callback should be called only once for both "a" and "b".
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith([["a"], ["b"]]);
+
+    await sleep(SOME_TIME * 1.5);
+
+    // Subsequent calls should return the cached results.
+    const subsequentA = batch.add("a");
+    const subsequentB = batch.add("b");
+
+    await expect(subsequentA).resolves.toEqual("a");
+    await expect(subsequentB).resolves.toEqual("b");
+
+    // Callback should not be called again.
+    expect(callback).toHaveBeenCalledTimes(1);
+
+    // Unless a non-cached call is made.
+    const c = batch.add("c");
+
+    // Callback should be called again for "c".
+    await expect(c).resolves.toEqual("c");
+    expect(callback).toHaveBeenCalledTimes(2);
+    expect(callback).toHaveBeenNthCalledWith(2, [["c"]]);
+  });
+
+  test("should cache and return results for asynchronous calls", async () => {
+    const callback = jest.fn(asynchronousCallback);
+    const batch = new BatchCache<string, [string]>(callback, {
+      delay: SOME_TIME,
+    });
+
+    const a = batch.add("a");
+    const b = batch.add("b");
+
+    await expect(a).resolves.toEqual("a");
+    await expect(b).resolves.toEqual("b");
+
+    // Callback should be called only once for both "a" and "b".
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith([["a"], ["b"]]);
+
+    await sleep(SOME_TIME * 1.5);
+
+    // Subsequent calls should return the cached results.
+    const subsequentA = batch.add("a");
+    const subsequentB = batch.add("b");
+
+    await expect(subsequentA).resolves.toEqual("a");
+    await expect(subsequentB).resolves.toEqual("b");
+
+    // Callback should not be called again.
+    expect(callback).toHaveBeenCalledTimes(1);
+
+    await sleep(SOME_TIME * 1.5);
+
+    // Unless a non-cached call is made.
+    const c = batch.add("c");
+
+    // Callback should be called again for "c".
+    await expect(c).resolves.toEqual("c");
+    expect(callback).toHaveBeenCalledTimes(2);
+    expect(callback).toHaveBeenNthCalledWith(2, [["c"]]);
+  });
+
+  test("should not cache batch errors", async () => {
+    const callback = jest.fn(() => {
+      throw ERROR;
+    });
+    const batch = new BatchCache<string, [string]>(callback, {
+      delay: SOME_TIME,
+    });
+
+    const a = batch.add("a");
+    const b = batch.add("b");
+
+    await expect(a).rejects.toEqual(ERROR);
+    await expect(b).rejects.toEqual(ERROR);
+
+    // Callback should be called only once for both "a" and "b".
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith([["a"], ["b"]]);
+
+    // Subsequent calls should create a new batch since the first one errored.
+    const subsequentA = batch.add("a");
+    const subsequentB = batch.add("b");
+
+    await expect(subsequentA).rejects.toEqual(ERROR);
+    await expect(subsequentB).rejects.toEqual(ERROR);
+
+    // Callback should be called again for both "a" and "b".
+    expect(callback).toHaveBeenCalledTimes(2);
+    expect(callback).toHaveBeenNthCalledWith(2, [["a"], ["b"]]);
+  });
+
+  test("should cache individual errors", async () => {
+    const callback = jest.fn(() => ["a", ERROR]);
+    const batch = new BatchCache<string, [string]>(callback, {
+      delay: SOME_TIME,
+    });
+
+    const a = batch.add("a");
+    const b = batch.add("b");
+
+    await expect(a).resolves.toEqual("a");
+    await expect(b).rejects.toEqual(ERROR);
+
+    // Callback should be called only once for both "a" and "b".
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith([["a"], ["b"]]);
+
+    // Subsequent calls should return the cached results (including errors).
+    const subsequentA = batch.add("a");
+    const subsequentB = batch.add("b");
+
+    await expect(subsequentA).resolves.toEqual("a");
+    await expect(subsequentB).rejects.toEqual(ERROR);
+
+    // Callback should not be called again.
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  test("should remove calls from the cache", async () => {
+    const callback = jest.fn(synchronousCallback);
+    const batch = new BatchCache<string, [string]>(callback, {
+      delay: SOME_TIME,
+    });
+
+    const a = batch.add("a");
+    const b = batch.add("b");
+
+    await expect(a).resolves.toEqual("a");
+    await expect(b).resolves.toEqual("b");
+
+    // Callback should be called only once for both "a" and "b".
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith([["a"], ["b"]]);
+
+    // "a" should be removed from the cache.
+    batch.remove("a");
+
+    // Subsequent call for "a" should trigger the callback again.
+    const subsequentA = batch.add("a");
+    await expect(subsequentA).resolves.toEqual("a");
+
+    // Callback should be called twice now.
+    expect(callback).toHaveBeenCalledTimes(2);
+    expect(callback).toHaveBeenNthCalledWith(2, [["a"]]);
+
+    // "b" should still be in the cache.
+    const subsequentB = batch.add("b");
+    await expect(subsequentB).resolves.toEqual("b");
+
+    // Callback should not be called again.
+    expect(callback).toHaveBeenCalledTimes(2);
+  });
+
+  test("should clear the cache", async () => {
+    const callback = jest.fn(synchronousCallback);
+    const batch = new BatchCache<string, [string]>(callback, {
+      delay: SOME_TIME,
+    });
+
+    const a = batch.add("a");
+    const b = batch.add("b");
+
+    await expect(a).resolves.toEqual("a");
+    await expect(b).resolves.toEqual("b");
+
+    // Callback should be called only once for both "a" and "b".
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith([["a"], ["b"]]);
+
+    // The cache should be cleared.
+    batch.clear();
+
+    // Subsequent calls should trigger the callback again.
+    const subsequentA = batch.add("a");
+    const subsequentB = batch.add("b");
+
+    await expect(subsequentA).resolves.toEqual("a");
+    await expect(subsequentB).resolves.toEqual("b");
+
+    // Callback should be called twice now.
+    expect(callback).toHaveBeenCalledTimes(2);
+    expect(callback).toHaveBeenNthCalledWith(2, [["a"], ["b"]]);
   });
 });
--- packages/liveblocks-core/src/lib/batch.ts
+++ packages/liveblocks-core/src/lib/batch.ts
@@ -1,3 +1,5 @@
+import { stringify } from "./stringify";
+
 const DEFAULT_SIZE = 50;
 const DEFAULT_DELAY = 100;
 
@@ -9,7 +11,19 @@ type BatchCallback<Result, Args extends unknown[]> = (
   args: Args[]
 ) => (Result | Error)[] | Promise<(Result | Error)[]>;
 
-class BatchItem<Result, Args extends unknown[]> {
+interface Options {
+  /**
+   * How many calls to batch together at most.
+   */
+  size?: number;
+
+  /**
+   * How long to wait before flushing the batch.
+   */
+  delay?: number;
+}
+
+class BatchCall<Result, Args extends unknown[]> {
   readonly args: Args;
   readonly resolve: Resolve<Result>;
   readonly reject: Reject;
@@ -21,27 +35,16 @@ class BatchItem<Result, Args extends unknown[]> {
   }
 }
 
-interface Options {
-  /**
-   * How many items to batch together at most.
-   */
-  size?: number;
-
-  /**
-   * How long to wait before flushing the batch.
-   */
-  delay?: number;
-}
-
 /**
  * Batch calls to a function, either by number of calls or by a maximum delay.
  */
 export class Batch<Result, Args extends unknown[] = []> {
-  private queue: BatchItem<Result, Args>[] = [];
+  private queue: BatchCall<Result, Args>[] = [];
   private callback: BatchCallback<Result, Args>;
   private size: number;
   private delay: number;
   private delayTimeoutId?: ReturnType<typeof setTimeout>;
+  public error = false;
 
   constructor(callback: BatchCallback<Result, Args>, options?: Options) {
     this.callback = callback;
@@ -58,59 +61,133 @@ export class Batch<Result, Args extends unknown[] = []> {
 
   private schedule() {
     if (this.queue.length === this.size) {
+      // If the queue is full, flush it immediately.
       void this.flush();
     } else if (this.queue.length === 1) {
+      // If the call is the first in the queue, schedule a flush.
       this.clearDelayTimeout();
       this.delayTimeoutId = setTimeout(() => void this.flush(), this.delay);
     }
   }
 
   private async flush(): Promise<void> {
+    // If the queue is empty, don't call the callback.
     if (this.queue.length === 0) {
       return;
     }
 
-    const items = this.queue.splice(0);
-    const args = items.map((item) => item.args);
+    // Empty the queue and get its calls.
+    const calls = this.queue.splice(0);
+    const args = calls.map((call) => call.args);
 
     try {
+      // Call the batch callback with the queued arguments.
       const results = await this.callback(args);
+      this.error = false;
 
-      items.forEach((item, index) => {
+      // Resolve or reject each call.
+      calls.forEach((call, index) => {
         const result = results?.[index];
 
         if (result instanceof Error) {
-          item.reject(result);
+          call.reject(result);
         } else if (result !== undefined) {
-          item.resolve(result);
+          call.resolve(result);
         } else {
           if (Array.isArray(results)) {
-            item.reject(
+            call.reject(
               new Error(
-                `Batch callback must return an array of the same length as the number of items in the batch. Expected ${items.length}, but got ${results.length}.`
+                `Batch callback must return an array of the same length as the number of calls in the batch. Expected ${calls.length}, but got ${results.length}.`
               )
             );
           } else {
-            item.reject(new Error("Batch callback must return an array."));
+            call.reject(new Error("Batch callback must return an array."));
           }
         }
       });
     } catch (error) {
-      items.forEach((item) => {
-        item.reject(error);
+      this.error = true;
+
+      // Reject all calls if the whole batch errored or was rejected.
+      calls.forEach((call) => {
+        call.reject(error);
       });
     }
   }
 
   add(...args: Args): Promise<Result> {
+    // Add the call to the queue and schedule a flush.
     return new Promise<Result>((resolve, reject) => {
-      this.queue.push(new BatchItem(args, resolve, reject));
+      this.queue.push(new BatchCall(args, resolve, reject));
       this.schedule();
     });
   }
 
   clear(): void {
     this.queue = [];
+    this.error = false;
     this.clearDelayTimeout();
+  }
+}
+
+/**
+ * Batch calls to a function, either by number of calls or by a maximum delay, and cache the results.
+ */
+export class BatchCache<Result, Args extends unknown[] = []> {
+  private batch: Batch<Result, Args>;
+  private cache: Map<string, Result | Error> = new Map();
+
+  constructor(callback: BatchCallback<Result, Args>, options?: Options) {
+    this.batch = new Batch(callback, options);
+  }
+
+  private getCacheKey(args: Args): string {
+    return stringify(args);
+  }
+
+  async add(...args: Args): Promise<Result> {
+    const cacheKey = this.getCacheKey(args);
+
+    // If already cached, return the result.
+    if (this.cache.has(cacheKey)) {
+      const cachedResult = this.cache.get(cacheKey);
+
+      // Unless it was an error, in that case rethrow it.
+      if (cachedResult instanceof Error) {
+        throw cachedResult;
+      }
+
+      return cachedResult as Result;
+    }
+
+    try {
+      // If not cached, add to the current batch.
+      const result = await this.batch.add(...args);
+
+      // Cache the result.
+      this.cache.set(cacheKey, result);
+
+      return result;
+    } catch (error) {
+      // If the whole batch errored, rethrow the error.
+      if (this.batch.error) {
+        throw error;
+      }
+
+      // Cache individual errors to avoid repeatedly loading the same error.
+      this.cache.set(cacheKey, error as Error);
+
+      throw error;
+    }
+  }
+
+  remove(...args: Args): void {
+    const cacheKey = this.getCacheKey(args);
+    this.cache.delete(cacheKey);
+  }
+
+  clear(): void {
+    this.batch.clear();
+    this.cache.clear();
   }
 }
