e2f40aa97e2adb442eaf6cb5aab31f101ae7a833
Nimesh Nayaju <nimeshnayaju@gmail.com>
2023-12-06 16:12:33 +0545

    Add tsd tests

--- packages/liveblocks-node/test-d/client.test-d.ts
+++ packages/liveblocks-node/test-d/client.test-d.ts
@@ -1,15 +1,57 @@
 import { expectType } from "tsd";
 import { Liveblocks } from "../src/client";
-import type { PlainLsonObject, JsonObject } from "@liveblocks/core";
+import type {
+  CommentReaction,
+  CommentBody,
+  CommentBodyBlockElement,
+  CommentData,
+} from "@liveblocks/core";
 
 async () => {
   const client = new Liveblocks({ secret: "sk_xxx" });
 
-  expectType<PlainLsonObject>(await client.getStorageDocument("document-id"));
-  expectType<PlainLsonObject>(
-    await client.getStorageDocument("document-id", "plain-lson")
-  );
-  expectType<JsonObject>(
-    await client.getStorageDocument("document-id", "json")
-  );
+  const comment = await client.getComment({
+    roomId: "room-id",
+    threadId: "th_threadId",
+    commentId: "cm_commentId",
+  });
+  expectType<"comment">(comment.type);
+  expectType<string>(comment.id);
+  expectType<string>(comment.threadId);
+  expectType<string>(comment.roomId);
+  expectType<string>(comment.userId);
+  expectType<Date>(comment.createdAt);
+  expectType<Date | undefined>(comment.editedAt);
+  expectType<CommentReaction[]>(comment.reactions);
+  expectType<Date | undefined>(comment.deletedAt);
+
+  expectType<CommentBody | undefined>(comment.body);
+  expectType<1 | undefined>(comment.body?.version);
+  expectType<CommentBodyBlockElement[] | undefined>(comment.body?.content);
+
+  const thread = await client.getThread<Record<string, string>>({
+    roomId: "room-id",
+    threadId: "th_threadId",
+  });
+  expectType<"thread">(thread.type);
+  expectType<string>(thread.id);
+  expectType<string>(thread.roomId);
+  expectType<Date>(thread.createdAt);
+  expectType<Date | undefined>(thread.updatedAt);
+  expectType<Record<string, string>>(thread.metadata);
+  expectType<CommentData[]>(thread.comments);
+
+  const commentReaction = await client.addCommentReaction({
+    roomId: "room-id",
+    threadId: "th_threadId",
+    commentId: "cm_commentId",
+    data: {
+      emoji: "👍",
+      userId: "user-id",
+    },
+  });
+
+  expectType<string>(commentReaction.emoji);
+  expectType<string>(commentReaction.userId);
+  expectType<Date>(commentReaction.createdAt);
 };
