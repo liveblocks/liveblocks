b5ac93217c7bac95577ecba41f28261d8a600b45
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2024-01-08 17:26:28 +0100

    Start adding content to InboxNotification

--- examples/nextjs-comments/src/app/page.tsx
+++ examples/nextjs-comments/src/app/page.tsx
@@ -14,6 +14,174 @@ import { ErrorBoundary } from "react-error-boundary";
 import { setCookie } from "tiny-cookie";
 import { InboxNotificationData } from "@liveblocks/client";
 
+const inboxNotificationA: InboxNotificationData = {
+  kind: "thread",
+  id: "in_I8pEUZU5ZRN4wxd4pcYVs",
+  threadId: "th_bdqY2mZelGglyljNvSEP2",
+  readAt: new Date("2023-12-07T10:07:15.201Z"),
+  notifiedAt: new Date("2023-12-10T10:07:15.201Z"),
+  thread: {
+    type: "thread",
+    id: "th_5ssRcW7YInDt6nbU_xr4D",
+    roomId: "nextjs-comments",
+    comments: [
+      {
+        type: "comment",
+        threadId: "th_5ssRcW7YInDt6nbU_xr4D",
+        roomId: "nextjs-comments",
+        id: "cm_5iocqEdxNx-HG3m-ZBJwo",
+        userId: "user-7",
+        createdAt: new Date("2023-12-06T10:07:15.201Z"),
+        reactions: [],
+        body: {
+          version: 1,
+          content: [
+            {
+              type: "paragraph",
+              children: [
+                {
+                  text: "Hello world",
+                },
+              ],
+            },
+          ],
+        },
+      },
+      {
+        type: "comment",
+        threadId: "th_5ssRcW7YInDt6nbU_xr4D",
+        roomId: "nextjs-comments",
+        id: "cm_5iocqEdxNx-HG3m-ZBJwd",
+        userId: "user-8",
+        createdAt: new Date("2023-12-08T10:07:15.201Z"),
+        reactions: [],
+        body: {
+          version: 1,
+          content: [
+            {
+              type: "paragraph",
+              children: [
+                {
+                  text: "Hello world",
+                },
+              ],
+            },
+          ],
+        },
+      },
+      {
+        type: "comment",
+        threadId: "th_5ssRcW7YInDt6nbU_xr4D",
+        roomId: "nextjs-comments",
+        id: "cm_5iocqEdxNx-HG3m-ZBJwa",
+        userId: "user-9",
+        createdAt: new Date("2023-12-10T10:07:15.201Z"),
+        reactions: [],
+        body: {
+          version: 1,
+          content: [
+            {
+              type: "paragraph",
+              children: [
+                {
+                  text: "Hello world",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    ],
+    createdAt: new Date("2023-12-06T10:07:15.201Z"),
+    metadata: {},
+    updatedAt: new Date("2023-12-10T10:07:15.201Z"),
+  },
+};
+
+const inboxNotificationB: InboxNotificationData = {
+  kind: "thread",
+  id: "in_I8pEUZU5ZRN4wxd4pcYVs",
+  threadId: "th_bdqY2mZelGglyljNvSEP2",
+  readAt: new Date("2023-12-10T10:07:15.201Z"),
+  notifiedAt: new Date("2023-12-10T10:07:15.201Z"),
+  thread: {
+    type: "thread",
+    id: "th_5ssRcW7YInDt6nbU_xr4D",
+    roomId: "nextjs-comments",
+    comments: [
+      {
+        type: "comment",
+        threadId: "th_5ssRcW7YInDt6nbU_xr4D",
+        roomId: "nextjs-comments",
+        id: "cm_5iocqEdxNx-HG3m-ZBJwo",
+        userId: "user-7",
+        createdAt: new Date("2023-12-06T10:07:15.201Z"),
+        reactions: [],
+        body: {
+          version: 1,
+          content: [
+            {
+              type: "paragraph",
+              children: [
+                {
+                  text: "Hello world",
+                },
+              ],
+            },
+          ],
+        },
+      },
+      {
+        type: "comment",
+        threadId: "th_5ssRcW7YInDt6nbU_xr4D",
+        roomId: "nextjs-comments",
+        id: "cm_5iocqEdxNx-HG3m-ZBJwd",
+        userId: "user-8",
+        createdAt: new Date("2023-12-08T10:07:15.201Z"),
+        reactions: [],
+        body: {
+          version: 1,
+          content: [
+            {
+              type: "paragraph",
+              children: [
+                {
+                  text: "Hello world",
+                },
+              ],
+            },
+          ],
+        },
+      },
+      {
+        type: "comment",
+        threadId: "th_5ssRcW7YInDt6nbU_xr4D",
+        roomId: "nextjs-comments",
+        id: "cm_5iocqEdxNx-HG3m-ZBJwa",
+        userId: "user-9",
+        createdAt: new Date("2023-12-10T10:07:15.201Z"),
+        reactions: [],
+        body: {
+          version: 1,
+          content: [
+            {
+              type: "paragraph",
+              children: [
+                {
+                  text: "Hello world",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    ],
+    createdAt: new Date("2023-12-06T10:07:15.201Z"),
+    metadata: {},
+    updatedAt: new Date("2023-12-10T10:07:15.201Z"),
+  },
+};
+
 /**
  * Displays a list of threads, along with a composer for creating
  * new threads.
@@ -24,7 +192,8 @@ function Example() {
 
   return (
     <main>
-      <InboxNotification inboxNotification={{} as InboxNotificationData} />
+      <InboxNotification inboxNotification={inboxNotificationA} />
+      <InboxNotification inboxNotification={inboxNotificationB} />
       {threads.map((thread) => (
         <Thread key={thread.id} thread={thread} className="thread" />
       ))}
--- packages/liveblocks-react-comments/src/components/InboxNotification.tsx
+++ packages/liveblocks-react-comments/src/components/InboxNotification.tsx
@@ -1,14 +1,23 @@
 "use client";
 
-import type { InboxNotificationData } from "@liveblocks/core";
+import {
+  type CommentData,
+  getMentionedIdsFromCommentBody,
+  type InboxNotificationData,
+} from "@liveblocks/core";
 import type { ComponentPropsWithoutRef, ReactNode } from "react";
-import React, { forwardRef } from "react";
+import React, { forwardRef, useMemo } from "react";
 
 import { useOverrides } from "../overrides";
 import { Timestamp } from "../primitives/Timestamp";
 import { classNames } from "../utils/class-names";
 import { Avatar, type AvatarProps } from "./internal/Avatar";
+import { List } from "./internal/List";
+import { User } from "./internal/User";
 
+const THREAD_INBOX_NOTIFICATION_MAX_COMMENTS = 3;
+
+// TODO: showDeletedComments option?
 export interface InboxNotificationProps
   extends ComponentPropsWithoutRef<"div"> {
   /**
@@ -75,6 +84,122 @@ function InboxNotificationAvatar({ className, ...props }: AvatarProps) {
   );
 }
 
+function findLastCommentWithMentionedId(
+  comments: CommentData[],
+  mentionedId: string
+) {
+  for (let i = comments.length - 1; i >= 0; i--) {
+    const comment = comments[i];
+
+    if (comment.body) {
+      const mentionedIds = getMentionedIdsFromCommentBody(comment.body);
+
+      if (mentionedIds.includes(mentionedId)) {
+        return comment;
+      }
+    }
+  }
+
+  return;
+}
+
+function generateThreadInboxNotificationContent(
+  inboxNotification: InboxNotificationData,
+  userId: string
+) {
+  const unreadComments = inboxNotification.thread.comments.filter((comment) =>
+    inboxNotification.readAt
+      ? comment.createdAt > inboxNotification.readAt &&
+        comment.createdAt <= inboxNotification.notifiedAt
+      : comment.createdAt <= inboxNotification.notifiedAt
+  );
+
+  // If the thread is read, show the last comments.
+  if (unreadComments.length === 0) {
+    return {
+      unread: false,
+      comments: inboxNotification.thread.comments.slice(
+        -THREAD_INBOX_NOTIFICATION_MAX_COMMENTS
+      ),
+      date: inboxNotification.notifiedAt,
+    };
+  }
+
+  const commentWithMention = findLastCommentWithMentionedId(
+    unreadComments,
+    userId
+  );
+
+  // If the thread contains one or more mentions for the current user, show the last comment with a mention.
+  if (commentWithMention) {
+    return {
+      unread: true,
+      comments: [commentWithMention],
+      date: commentWithMention.createdAt,
+      userId: commentWithMention.userId,
+    };
+  }
+
+  // Otherwise, show the last comments.
+  return {
+    unread: true,
+    comments: inboxNotification.thread.comments.slice(
+      -THREAD_INBOX_NOTIFICATION_MAX_COMMENTS
+    ),
+    date: inboxNotification.notifiedAt,
+  };
+}
+
+const ThreadInboxNotification = forwardRef<
+  HTMLDivElement,
+  InboxNotificationProps
+>(({ inboxNotification, ...props }, forwardedRef) => {
+  // TODO: How do we get the current user ID?
+  const { unread, comments, date } = useMemo(
+    () => generateThreadInboxNotificationContent(inboxNotification, "TODO"),
+    [inboxNotification]
+  );
+  const { avatarUserId, title } = useMemo(() => {
+    const reversedComments = [...comments].reverse();
+
+    const avatarUserId = comments[0]?.userId;
+    // TODO: Support overrides via $ instead of hard-coding English (look at CommentReaction)
+    // TODO: Don't show self in title?
+    const title = (
+      <>
+        <List
+          values={reversedComments.map((users, index) => (
+            <User key={users.id} userId={users.id} capitalize={index === 0} />
+          ))}
+          // formatRemaining={$.COMMENT_REACTION_REMAINING}
+          truncate={THREAD_INBOX_NOTIFICATION_MAX_COMMENTS}
+        />{" "}
+        commented on <span>Document</span>
+      </>
+    );
+
+    return {
+      avatarUserId,
+      title,
+    };
+  }, [comments]);
+
+  return (
+    <InboxNotificationLayout
+      aside={<InboxNotificationAvatar userId={avatarUserId} />}
+      title={title}
+      date={date}
+      unread={unread}
+      {...props}
+      ref={forwardedRef}
+    >
+      {comments.map((comment) => (
+        <p key={comment.id}>comment.id</p>
+      ))}
+    </InboxNotificationLayout>
+  );
+});
+
 /**
  * Displays a single inbox notification.
  *
@@ -89,15 +214,14 @@ export const InboxNotification = forwardRef<
   HTMLDivElement,
   InboxNotificationProps
 >(({ inboxNotification, ...props }, forwardedRef) => {
-  return (
-    <InboxNotificationLayout
-      aside={<InboxNotificationAvatar userId="TODO" />}
-      title="Lorem Ipsum commented on Lorem Ipsum"
-      date="01-01-01"
-      {...props}
-      ref={forwardedRef}
-    >
-      content: list of comments
-    </InboxNotificationLayout>
-  );
+  switch (inboxNotification.kind) {
+    case "thread":
+      return (
+        <ThreadInboxNotification
+          inboxNotification={inboxNotification}
+          {...props}
+          ref={forwardedRef}
+        />
+      );
+  }
 });
