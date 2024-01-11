ae4d9da9bd1f2fccf5516f7d537609453d997221
Nimesh Nayaju <nimeshnayaju@gmail.com>
2024-01-11 16:46:23 +0545

    Lint

--- packages/liveblocks-react/src/__tests__/useMarkThreadAsRead.test.tsx
+++ packages/liveblocks-react/src/__tests__/useMarkThreadAsRead.test.tsx
@@ -1,13 +1,13 @@
 import type { BaseMetadata, JsonObject } from "@liveblocks/core";
 import { createClient, getCacheStore } from "@liveblocks/core";
 import { act, renderHook, waitFor } from "@testing-library/react";
+import { rest } from "msw";
 import { setupServer } from "msw/node";
 import React from "react";
 
 import { createRoomContext } from "../room";
 import { dummyInboxNoficationData, dummyThreadData } from "./_dummies";
 import MockWebSocket from "./_MockWebSocket";
-import { rest } from "msw";
 
 const server = setupServer();
 
