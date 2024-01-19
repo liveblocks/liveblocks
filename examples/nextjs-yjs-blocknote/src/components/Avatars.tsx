8f1680a32858afdf0523ee932b37166ae9dc9f33
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2024-01-19 12:59:36 +0000

    feat: BlockNote base example

---
+++ examples/nextjs-yjs-blocknote/src/components/Avatars.tsx
@@ -0,0 +1,38 @@
+import { useOthers, useSelf } from "@/liveblocks.config";
+import styles from "./Avatars.module.css";
+
+export function Avatars() {
+  const users = useOthers();
+  const currentUser = useSelf();
+
+  return (
+    <div className={styles.avatars}>
+      {users.map(({ connectionId, info }) => {
+        return (
+          <Avatar key={connectionId} picture={info.picture} name={info.name} />
+        );
+      })}
+
+      {currentUser && (
+        <div className="relative ml-8 first:ml-0">
+          <Avatar
+            picture={currentUser.info.picture}
+            name={currentUser.info.name}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export function Avatar({ picture, name }: { picture: string; name: string }) {
+  return (
+    <div className={styles.avatar} data-tooltip={name}>
+      <img
+        src={picture}
+        className={styles.avatar_picture}
+        data-tooltip={name}
+      />
+    </div>
+  );
+}
