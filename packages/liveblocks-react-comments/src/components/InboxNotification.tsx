5b6d245b9e09a224e883784383b3cc6ef194ef90
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2024-01-22 12:26:27 +0100

    Rename ThreadInboxNotification* to InboxNotificationThread*

--- packages/liveblocks-react-comments/src/components/InboxNotification.tsx
+++ packages/liveblocks-react-comments/src/components/InboxNotification.tsx
@@ -4,8 +4,8 @@ import type {
   BaseMetadata,
   CommentData,
   InboxNotificationData,
+  InboxNotificationThreadData,
   ThreadData,
-  ThreadInboxNotificationData,
 } from "@liveblocks/core";
 import {
   assertNever,
@@ -32,7 +32,7 @@ import { User } from "./internal/User";
 
 const THREAD_INBOX_NOTIFICATION_MAX_COMMENTS = 3;
 
-type ThreadInboxNotificationCommentsContents = {
+type InboxNotificationThreadCommentsContents = {
   type: "comments";
   unread: boolean;
   comments: CommentData[];
@@ -40,7 +40,7 @@ type ThreadInboxNotificationCommentsContents = {
   date: Date;
 };
 
-type ThreadInboxNotificationMentionContents = {
+type InboxNotificationThreadMentionContents = {
   type: "mention";
   unread: boolean;
   comments: CommentData[];
@@ -48,9 +48,9 @@ type ThreadInboxNotificationMentionContents = {
   date: Date;
 };
 
-type ThreadInboxNotificationContents =
-  | ThreadInboxNotificationCommentsContents
-  | ThreadInboxNotificationMentionContents;
+type InboxNotificationThreadContents =
+  | InboxNotificationThreadCommentsContents
+  | InboxNotificationThreadMentionContents;
 
 export interface InboxNotificationProps
   extends ComponentPropsWithoutRef<"div"> {
@@ -219,11 +219,11 @@ function getUserIdsFromComments(comments: CommentData[]) {
   return Array.from(new Set(comments.map((comment) => comment.userId)));
 }
 
-function generateThreadInboxNotificationContents(
-  inboxNotification: ThreadInboxNotificationData,
+function generateInboxNotificationThreadContents(
+  inboxNotification: InboxNotificationThreadData,
   thread: ThreadData<BaseMetadata>,
   userId: string
-): ThreadInboxNotificationContents {
+): InboxNotificationThreadContents {
   const unreadComments = thread.comments.filter((comment) => {
     if (!comment.body) {
       return false;
@@ -280,7 +280,7 @@ function generateThreadInboxNotificationContents(
   };
 }
 
-const ThreadInboxNotification = forwardRef<
+const InboxNotificationThread = forwardRef<
   HTMLDivElement,
   InboxNotificationProps
 >(({ inboxNotification, ...props }, forwardedRef) => {
@@ -291,7 +291,7 @@ const ThreadInboxNotification = forwardRef<
 
   // [comments-unread] TODO: How do we get the current user ID?
   const { unread, date, aside, title, content } = useMemo(() => {
-    const contents = generateThreadInboxNotificationContents(
+    const contents = generateInboxNotificationThreadContents(
       inboxNotification,
       thread,
       "[comments-unread] TODO: get current user's ID"
@@ -374,7 +374,7 @@ const ThreadInboxNotification = forwardRef<
           "Unexpected thread inbox notification type"
         );
     }
-  }, [inboxNotification]);
+  }, [inboxNotification, thread]);
 
   return (
     <InboxNotificationLayout
@@ -407,7 +407,7 @@ export const InboxNotification = forwardRef<
   switch (inboxNotification.kind) {
     case "thread":
       return (
-        <ThreadInboxNotification
+        <InboxNotificationThread
           inboxNotification={inboxNotification}
           {...props}
           ref={forwardedRef}
