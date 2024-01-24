e74ca5e236a7b1837fe7cfb6ff7df3b537726de5
Le Vaillant Pierre <pierre.levaillant@liveblocks.io>
2024-01-24 15:36:30 +0100

    Increase `CircularButton`

--- examples/nextjs-comments-audio/src/components/CircularButton.tsx
+++ examples/nextjs-comments-audio/src/components/CircularButton.tsx
@@ -13,7 +13,7 @@ export function CircularButton(props: Props) {
         "group rounded-full shrink-0 flex items-center justify-center transition-opacity duration-150 ease-out disabled:pointer-events-none bg-inverse disabled:bg-quaternary",
         {
           ["size-10"]: props.size === "md",
-          ["size-12 sm:size-14"]: props.size === "lg",
+          ["size-14 sm:size-16"]: props.size === "lg",
         }
       )}
       {...props}
