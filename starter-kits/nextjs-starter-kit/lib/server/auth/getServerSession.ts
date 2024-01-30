06a6ea7ad9514d1682f108ec379f277607a6fa27
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2024-01-30 12:39:09 +0000

    feat: Update NextAuth and date-fns

--- starter-kits/nextjs-starter-kit/lib/server/auth/getServerSession.ts
+++ starter-kits/nextjs-starter-kit/lib/server/auth/getServerSession.ts
@@ -1,10 +1,10 @@
 import type { GetServerSidePropsContext } from "next";
-import { unstable_getServerSession } from "next-auth/next";
+import { getServerSession as session } from "next-auth/next";
 import { authOptions } from "../../../pages/api/auth/[...nextauth]";
 
 export function getServerSession(
   req: GetServerSidePropsContext["req"],
   res: GetServerSidePropsContext["res"]
 ) {
-  return unstable_getServerSession(req, res, authOptions);
+  return session(req, res, authOptions);
 }
