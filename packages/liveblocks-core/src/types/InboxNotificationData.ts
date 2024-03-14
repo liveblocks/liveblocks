801fe7125003ed910d7639b56835f6fad1306508
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2024-03-14 15:12:12 +0100

    Start setting up custom notifications types

--- packages/liveblocks-core/src/types/InboxNotificationData.ts
+++ packages/liveblocks-core/src/types/InboxNotificationData.ts
@@ -9,6 +9,38 @@ export type InboxNotificationThreadData = {
   readAt: Date | null;
 };
 
-export type InboxNotificationData = InboxNotificationThreadData;
+// TODO: Move and expose? And find more fitting name.
+type Activity = {
+  id: string;
+  createdAt: Date;
+  // TODO: Define `data` type
+  data: any;
+};
 
-export type InboxNotificationDataPlain = DateToString<InboxNotificationData>;
+export type InboxNotificationCustomData = {
+  kind: string;
+  id: string;
+  roomId: string;
+  subjectId?: string;
+  notifiedAt: Date;
+  readAt: Date | null;
+  activities: Activity[];
+};
+
+export type InboxNotificationData =
+  | InboxNotificationThreadData
+  | InboxNotificationCustomData;
+
+type InboxNotificationThreadDataPlain =
+  DateToString<InboxNotificationThreadData>;
+
+type InboxNotificationCustomDataPlain = Omit<
+  DateToString<InboxNotificationCustomData>,
+  "activities"
+> & {
+  activities: DateToString<Activity>[];
+};
+
+export type InboxNotificationDataPlain =
+  | InboxNotificationThreadDataPlain
+  | InboxNotificationCustomDataPlain;
