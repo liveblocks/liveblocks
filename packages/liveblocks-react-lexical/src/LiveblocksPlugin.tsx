145d0b96ae2c57a319779688d592271d58bf5e9e
Jonathan Rowny <jrowny@gmail.com>
2024-03-28 10:08:49 -0700

    base react lexical package setup

---
+++ packages/liveblocks-react-lexical/src/LiveblocksPlugin.tsx
@@ -0,0 +1,23 @@
+
+import { CollaborationPlugin } from "@lexical/react/LexicalCollaborationPlugin";
+import type { Provider } from "@lexical/yjs";
+import { useRoomContextBundle } from "@liveblocks/react";
+import LiveblocksProvider from "@liveblocks/yjs";
+import React from "react"
+import * as Y from "yjs";
+
+
+export const LiveblocksPlugin = (): JSX.Element => {
+  const { useRoom } = useRoomContextBundle();
+  const room = useRoom();
+  return (<CollaborationPlugin
+    providerFactory={(id, yjsDocMap) => {
+      const doc = new Y.Doc();
+      yjsDocMap.set(id, doc);
+      const provider = new LiveblocksProvider(room, doc) as Provider;
+      return provider;
+    }}
+    id="liveblocks-yjs-plugin"
+    shouldBootstrap={true}
+  />)
+};
---
+++ packages/liveblocks-react-lexical/src/index.ts
@@ -0,0 +1,7 @@
+import { detectDupes } from "@liveblocks/core";
+
+import { PKG_FORMAT, PKG_NAME, PKG_VERSION } from "./version";
+
+detectDupes(PKG_NAME, PKG_VERSION, PKG_FORMAT);
+
+export { LiveblocksPlugin } from "./LiveblocksPlugin";
