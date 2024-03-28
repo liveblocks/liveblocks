918a4cf294135163db82db5322c0b6ae8e0944c5
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2024-03-28 11:16:12 +0000

    feat: Copy CSS files

--- starter-kits/nextjs-starter-kit-new/app/layout.tsx
+++ starter-kits/nextjs-starter-kit-new/app/layout.tsx
@@ -2,10 +2,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "@/app/Providers";
 import { auth } from "@/auth";
-import "../styles/globals.css";
-import "../styles/text-editor.css";
+import "../styles/normalize.css";
 import "@liveblocks/react-comments/styles.css";
 import "@liveblocks/react-comments/styles/dark/media-query.css";
+import "../styles/globals.css";
+import "../styles/text-editor.css";
 import "../styles/text-editor-comments.css";
 
 const inter = Inter({ subsets: ["latin"] });
