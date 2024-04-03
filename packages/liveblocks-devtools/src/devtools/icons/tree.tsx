d1828c139e97a1637fe355d541aa30d35c919447
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2024-04-03 11:52:11 +0200

    Update TextIcon

--- packages/liveblocks-devtools/src/devtools/icons/tree.tsx
+++ packages/liveblocks-devtools/src/devtools/icons/tree.tsx
@@ -250,24 +250,17 @@ export function TrashIcon(props: ComponentProps<"svg">) {
 export function TextIcon(props: ComponentProps<"svg">) {
   return (
     <svg
-      width="20"
-      height="20"
+      width="16"
+      height="16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <rect
-        opacity=".1"
-        width="20"
-        height="20"
-        rx="4.375"
-        fill="currentColor"
-      />
       <path
-        d="M14 6H5M16 10H5M12.5 14H5"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M3.5 0A3.5 3.5 0 0 0 0 3.5v9A3.5 3.5 0 0 0 3.5 16h9a3.5 3.5 0 0 0 3.5-3.5v-9A3.5 3.5 0 0 0 12.5 0h-9ZM4 3.75a.75.75 0 0 0 0 1.5h7a.75.75 0 0 0 0-1.5H4ZM3.25 8A.75.75 0 0 1 4 7.25h8a.75.75 0 0 1 0 1.5H4A.75.75 0 0 1 3.25 8ZM4 10.75a.75.75 0 0 0 0 1.5h5a.75.75 0 0 0 0-1.5H4Z"
+        fill="currentColor"
       />
     </svg>
   );
