467504332621a10aa5826dca7f94e63c9561e513
Vincent Driessen <me@nvie.com>
2024-04-04 11:12:47 +0200

    Solve disabling buttons generally

--- e2e/next-sandbox/pages/offline.tsx
+++ e2e/next-sandbox/pages/offline.tsx
@@ -245,10 +245,8 @@ function Sandbox(_props: { roomId: string }) {
       <div style={{ display: "flex", margin: "8px 0" }}>
         <Button
           id="push"
-          onClick={() => {
-            if (!canPush) return;
-            push(nextValueToPush);
-          }}
+          enabled={canPush}
+          onClick={() => push(nextValueToPush)}
           subtitle={nextValueToPush}
         >
           Push
@@ -258,7 +256,6 @@ function Sandbox(_props: { roomId: string }) {
           id="move"
           enabled={canMove}
           onClick={() => {
-            if (!canMove) return;
             const [fromIndex, toIndex] = nextIndicesToMove;
             move(fromIndex, toIndex);
           }}
@@ -272,10 +269,7 @@ function Sandbox(_props: { roomId: string }) {
         <Button
           id="delete"
           enabled={canDelete}
-          onClick={() => {
-            if (!canDelete) return;
-            delete_(nextIndexToDelete);
-          }}
+          onClick={() => delete_(nextIndexToDelete)}
           subtitle={
             canDelete
               ? `index ${nextIndexToDelete} (${items[
@@ -287,14 +281,7 @@ function Sandbox(_props: { roomId: string }) {
           Delete
         </Button>
 
-        <Button
-          id="clear"
-          enabled={canClear}
-          onClick={() => {
-            if (!canClear) return;
-            clear();
-          }}
-        >
+        <Button id="clear" enabled={canClear} onClick={clear}>
           Clear
         </Button>
 
