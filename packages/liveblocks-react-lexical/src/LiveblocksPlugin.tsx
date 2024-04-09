3a1856ef9ebd3cf4ee483d7b28ef48a7b28ad87d
Jonathan Rowny <jrowny@gmail.com>
2024-04-09 11:20:51 -0700

    add warning and initial editor state

--- packages/liveblocks-react-lexical/src/LiveblocksPlugin.tsx
+++ packages/liveblocks-react-lexical/src/LiveblocksPlugin.tsx
@@ -3,6 +3,7 @@ import { CollaborationPlugin } from "@lexical/react/LexicalCollaborationPlugin";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import type { Provider } from "@lexical/yjs";
 import { useRoomContextBundle } from "@liveblocks/react";
+import type { LexicalEditor } from "lexical";
 import React, { useCallback, useEffect } from "react";
 import type { Doc } from "yjs";
 
@@ -22,15 +23,37 @@ export type LiveblocksPluginProps = {
    */
   allowEditsBeforeSync?: boolean
 
+  /**
+   * Modify the state with this function to set the initial state. 
+   * Ex. $createTextNode('initial text content'); 
+   * 
+   * @param editor 
+   * @returns void
+   */
+  initialEditorState?: (editor: LexicalEditor) => void;
+
 }
 
-export const LiveblocksPlugin = ({ userInfo = undefined, allowEditsBeforeSync = true }: LiveblocksPluginProps): JSX.Element => {
+export const LiveblocksPlugin = ({ userInfo = undefined, allowEditsBeforeSync = true, initialEditorState = undefined }: LiveblocksPluginProps): JSX.Element => {
   const { useSelf } = useRoomContextBundle();
   const { provider, doc } = useTextCollaboration();
   const [editor] = useLexicalComposerContext();
   const { synced } = useDocumentSyncState();
 
-  // Get user info
+  // Warn users if initialConfig.editorState, set on the composer, is not null
+  useEffect(() => {
+    // only in dev mode
+    if (process.env.NODE_ENV !== "production") {
+      // A user should not even be set an emptyState, but when passing null, getEditorState still has initial empty state
+      if (!editor.getEditorState().isEmpty()) {
+        console.warn("Warning: LiveblocksPlugin: editorState in initialConfig detected, but must be null.");
+      }
+    }
+    // we know editor is already defined as we're inside LexicalComposer, and we only want this running the first time
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // Get user info or allow override from props
   const info = useSelf(me => me.info);
   const username = userInfo?.name ?? info?.name;
   const cursorcolor = userInfo?.color ?? info?.color as string | undefined;
@@ -52,6 +75,7 @@ export const LiveblocksPlugin = ({ userInfo = undefined, allowEditsBeforeSync = 
     <>{provider &&   // NOTE: if we used suspense, we wouldn't need this provider &&
       <CollaborationPlugin
         providerFactory={providerFactor}
+        initialEditorState={initialEditorState}
         id={"liveblocks-document"}
         username={username}
         cursorColor={cursorcolor}
