1859cc8b4f942cf9bb5d6031165ad0f9174bc164
Jonathan Rowny <jrowny@gmail.com>
2024-04-15 16:11:10 -0700

    resizing

--- packages/liveblocks-react-lexical/src/LastActiveSelection.tsx
+++ packages/liveblocks-react-lexical/src/LastActiveSelection.tsx
@@ -11,61 +11,74 @@ export function LastActiveSelection() {
 
   useLayoutEffect(() => {
     const container = containerRef.current;
+    let observer: ResizeObserver | undefined;
     if (container === null) return;
 
     let markerContainer = container.parentNode?.querySelector(".lb-marker-container") as HTMLDivElement | undefined;
-    if (!markerContainer) {
-      // Look for contentEditable, then use its parent
-      const editorRef = Array.from(container.parentNode?.querySelectorAll("[contenteditable=true]") as NodeList)
-        .find((div) => !(div as HTMLElement).className.includes("lb-composer-editor"))
-      if (editorRef) {
-        markerContainer = document.createElement("div");
-        markerContainer.style.position = "absolute";
-        markerContainer.style.height = "100%";
-        markerContainer.style.width = "100%";
-        markerContainer.style.top = "0";
-        markerContainer.style.left = "0";
-        markerContainer.style.pointerEvents = "none";
-        markerContainer.className = "lb-marker-container";
-        editorRef.parentNode?.appendChild(markerContainer);
-      }
+    // Look for contentEditable, then use its parent
+    const editorRef = Array.from(container.parentNode?.querySelectorAll("[contenteditable=true]") as NodeList)
+      .find((div) => !(div as HTMLElement).className.includes("lb-composer-editor"))
+    if (editorRef && !markerContainer) {
+      markerContainer = document.createElement("div");
+      markerContainer.style.position = "absolute";
+      markerContainer.style.height = "100%";
+      markerContainer.style.width = "100%";
+      markerContainer.style.top = "0";
+      markerContainer.style.left = "0";
+      markerContainer.style.pointerEvents = "none";
+      markerContainer.className = "lb-marker-container";
+      editorRef.parentNode?.appendChild(markerContainer);
     }
-
     // If we weren't able to find the editor ref (this shouldn't really happen...) then use our own container
     if (!markerContainer) {
       markerContainer = container;
     }
 
-    // Remove all existing children of the container
-    while (markerContainer.firstChild) {
-      markerContainer.removeChild(markerContainer.firstChild);
+
+
+    function drawRects() {
+      if (!markerContainer) return;
+      // Remove all existing children of the container
+      while (markerContainer.firstChild) {
+        markerContainer.removeChild(markerContainer.firstChild);
+      }
+
+      if (selection === null) return;
+      const range = createDOMRange(
+        editor,
+        selection.anchor.node,
+        selection.anchor.offset,
+        selection.focus.node,
+        selection.focus.offset
+      );
+
+      if (range === null) return;
+      const rects = createRectsFromDOMRange(editor, range);
+
+      for (const rect of rects) {
+        const div = document.createElement("div");
+        div.style.position = "absolute";
+        div.style.top = `${rect.top - markerContainer.getBoundingClientRect().top}px`;
+        div.style.left = `${rect.left - markerContainer.getBoundingClientRect().left
+          }px`;
+        div.style.width = `${rect.width}px`;
+        div.style.height = `${rect.height}px`;
+        div.style.backgroundColor = "rgb(255, 212, 0)";
+        div.style.opacity = "0.5";
+        div.style.pointerEvents = "none";
+        markerContainer.appendChild(div);
+      }
+    }
+    drawRects();
+
+    if (editorRef) {
+      observer = new ResizeObserver(drawRects);
+      observer.observe(editorRef as HTMLElement);
     }
 
-    if (selection === null) return;
-
-    const range = createDOMRange(
-      editor,
-      selection.anchor.node,
-      selection.anchor.offset,
-      selection.focus.node,
-      selection.focus.offset
-    );
-
-    if (range === null) return;
-    const rects = createRectsFromDOMRange(editor, range);
-
-    for (const rect of rects) {
-      const div = document.createElement("div");
-      div.style.position = "absolute";
-      div.style.top = `${rect.top - markerContainer.getBoundingClientRect().top}px`;
-      div.style.left = `${rect.left - markerContainer.getBoundingClientRect().left
-        }px`;
-      div.style.width = `${rect.width}px`;
-      div.style.height = `${rect.height}px`;
-      div.style.backgroundColor = "rgb(255, 212, 0)";
-      div.style.opacity = "0.5";
-      div.style.pointerEvents = "none";
-      markerContainer.appendChild(div);
+    return () => {
+      observer?.disconnect();
+      observer = undefined;
     }
   }, [editor, selection]);
 
