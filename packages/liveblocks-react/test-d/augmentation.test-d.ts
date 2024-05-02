48b5d84244a302637fcb9cd164cd98866c5d2c3c
Vincent Driessen <me@nvie.com>
2024-05-02 10:52:59 +0200

    Add type-level tests (in test-d/)

---
+++ packages/liveblocks-react/test-d/augmentation.test-d.ts
@@ -0,0 +1,56 @@
+import type { Json } from "@liveblocks/client";
+import { useRoom, useSelf, useOthers } from "@liveblocks/react";
+import * as suspense from "@liveblocks/react/suspense";
+import { expectType } from "tsd";
+
+//
+// User-provided type augmentations
+//
+declare global {
+  namespace Liveblocks {
+    interface Presence {
+      cursor: {
+        x: number;
+        y: number;
+      };
+    }
+  }
+}
+
+// ---------------------------------------------------------
+// Hook APIs
+// ---------------------------------------------------------
+
+const room = useRoom();
+
+// Global presence is now available
+expectType<number>(room.getPresence().cursor.x);
+expectType<number>(room.getPresence().cursor.y);
+expectType<Json | undefined>(room.getPresence().notAPresenceField);
+
+// useSelf()
+{
+  const me = useSelf();
+  expectType<number | undefined>(me?.presence.cursor.x);
+  expectType<number | undefined>(me?.presence.cursor.y);
+  expectType<Json | undefined>(me?.presence.notAPresenceField);
+}
+
+// useSelf() (suspense)
+{
+  const me = suspense.useSelf();
+  expectType<number>(me.presence.cursor.x);
+  expectType<number>(me.presence.cursor.y);
+  expectType<Json | undefined>(me.presence.notAPresenceField);
+}
+
+// ---------------------------------------------------------
+
+// useOthers()
+{
+  const others = useOthers();
+  expectType<number>(others[13].presence.cursor.x);
+  expectType<number>(others[42].presence.cursor.y);
+  expectType<boolean>(others[0].canWrite);
+}
+
---
+++ packages/liveblocks-react/test-d/no-augmentation.test-d.ts
@@ -0,0 +1,20 @@
+import type { Json } from "@liveblocks/client";
+import { useRoom, useOthers /* etc etc */ } from "@liveblocks/react";
+import { expectType } from "tsd";
+
+// ---------------------------------------------------------
+// Hook APIs
+// ---------------------------------------------------------
+
+const room = useRoom();
+
+// When using imported hooks, without augmentation, the types are really generic
+expectType<Json | undefined>(room.getPresence().cursor);
+expectType<Json | undefined>(room.getPresence().notAPresenceField);
+
+// ---------------------------------------------------------
+
+const others = useOthers();
+expectType<Json | undefined>(others[13].presence.cursor);
+expectType<Json | undefined>(others[13].presence.notAPresenceField);
+expectType<boolean>(others[0].canWrite);
