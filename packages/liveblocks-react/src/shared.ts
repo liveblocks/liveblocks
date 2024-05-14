62f60233ff638034d0dcd8297c9036feba9ac1f6
Vincent Driessen <me@nvie.com>
2024-05-14 16:14:53 +0200

    Pull functions out of closure, and presort towards the _withClient() internal hooks

--- packages/liveblocks-react/src/shared.ts
+++ packages/liveblocks-react/src/shared.ts
@@ -40,145 +40,148 @@ const missingRoomInfoError = new Error(
   "resolveRoomsInfo didn't return anything for this room ID."
 );
 
+function useUser_withClient<TUserMeta extends BaseUserMeta>(
+  client: Client,
+  userId: string
+): UserState<TUserMeta["info"]> {
+  const usersStore = client[kInternal].usersStore;
+
+  const getUserState = useCallback(() => usersStore.getState(userId), [userId]);
+
+  useEffect(() => {
+    void usersStore.get(userId);
+  }, [userId]);
+
+  const state = useSyncExternalStore(
+    usersStore.subscribe,
+    getUserState,
+    getUserState
+  );
+
+  return state
+    ? ({
+        isLoading: state.isLoading,
+        user: state.data,
+        // Return an error if `undefined` was returned by `resolveUsers` for this user ID
+        error:
+          !state.isLoading && !state.data && !state.error
+            ? missingUserError
+            : state.error,
+      } as UserState<TUserMeta["info"]>)
+    : { isLoading: true };
+}
+
+function useUserSuspense_withClient<TUserMeta extends BaseUserMeta>(
+  client: Client,
+  userId: string
+) {
+  const usersStore = client[kInternal].usersStore;
+
+  const getUserState = useCallback(() => usersStore.getState(userId), [userId]);
+  const userState = getUserState();
+
+  if (!userState || userState.isLoading) {
+    throw usersStore.get(userId);
+  }
+
+  if (userState.error) {
+    throw userState.error;
+  }
+
+  // Throw an error if `undefined` was returned by `resolveUsers` for this user ID
+  if (!userState.data) {
+    throw missingUserError;
+  }
+
+  const state = useSyncExternalStore(
+    usersStore.subscribe,
+    getUserState,
+    getUserState
+  );
+
+  return {
+    isLoading: false,
+    user: state?.data,
+    error: state?.error,
+  } as UserStateSuccess<TUserMeta["info"]>;
+}
+
+function useRoomInfo_withClient(client: Client, roomId: string): RoomInfoState {
+  const roomsInfoStore = client[kInternal].roomsInfoStore;
+
+  const getRoomInfoState = useCallback(
+    () => roomsInfoStore.getState(roomId),
+    [roomId]
+  );
+
+  useEffect(() => {
+    void roomsInfoStore.get(roomId);
+  }, [roomId]);
+
+  const state = useSyncExternalStore(
+    roomsInfoStore.subscribe,
+    getRoomInfoState,
+    getRoomInfoState
+  );
+
+  return state
+    ? ({
+        isLoading: state.isLoading,
+        info: state.data,
+        // Return an error if `undefined` was returned by `resolveRoomsInfo` for this room ID
+        error:
+          !state.isLoading && !state.data && !state.error
+            ? missingRoomInfoError
+            : state.error,
+      } as RoomInfoState)
+    : { isLoading: true };
+}
+
+function useRoomInfoSuspense_withClient(client: Client, roomId: string) {
+  const roomsInfoStore = client[kInternal].roomsInfoStore;
+
+  const getRoomInfoState = useCallback(
+    () => roomsInfoStore.getState(roomId),
+    [roomId]
+  );
+  const roomInfoState = getRoomInfoState();
+
+  if (!roomInfoState || roomInfoState.isLoading) {
+    throw roomsInfoStore.get(roomId);
+  }
+
+  if (roomInfoState.error) {
+    throw roomInfoState.error;
+  }
+
+  // Throw an error if `undefined` was returned by `resolveRoomsInfo` for this room ID
+  if (!roomInfoState.data) {
+    throw missingRoomInfoError;
+  }
+
+  const state = useSyncExternalStore(
+    roomsInfoStore.subscribe,
+    getRoomInfoState,
+    getRoomInfoState
+  );
+
+  return {
+    isLoading: false,
+    info: state?.data,
+    error: state?.error,
+  } as RoomInfoStateSuccess;
+}
+
 export function createSharedContext<
   TUserMeta extends BaseUserMeta = BaseUserMeta,
 >(client: Client): SharedContextBundle<TUserMeta> {
-  const usersStore = client[kInternal].usersStore;
-  const roomsInfoStore = client[kInternal].roomsInfoStore;
-
-  function useUser(userId: string): UserState<TUserMeta["info"]> {
-    const getUserState = useCallback(
-      () => usersStore.getState(userId),
-      [userId]
-    );
-
-    useEffect(() => {
-      void usersStore.get(userId);
-    }, [userId]);
-
-    const state = useSyncExternalStore(
-      usersStore.subscribe,
-      getUserState,
-      getUserState
-    );
-
-    return state
-      ? ({
-          isLoading: state.isLoading,
-          user: state.data,
-          // Return an error if `undefined` was returned by `resolveUsers` for this user ID
-          error:
-            !state.isLoading && !state.data && !state.error
-              ? missingUserError
-              : state.error,
-        } as UserState<TUserMeta["info"]>)
-      : { isLoading: true };
-  }
-
-  function useUserSuspense(userId: string) {
-    const getUserState = useCallback(
-      () => usersStore.getState(userId),
-      [userId]
-    );
-    const userState = getUserState();
-
-    if (!userState || userState.isLoading) {
-      throw usersStore.get(userId);
-    }
-
-    if (userState.error) {
-      throw userState.error;
-    }
-
-    // Throw an error if `undefined` was returned by `resolveUsers` for this user ID
-    if (!userState.data) {
-      throw missingUserError;
-    }
-
-    const state = useSyncExternalStore(
-      usersStore.subscribe,
-      getUserState,
-      getUserState
-    );
-
-    return {
-      isLoading: false,
-      user: state?.data,
-      error: state?.error,
-    } as UserStateSuccess<TUserMeta["info"]>;
-  }
-
-  function useRoomInfo(roomId: string): RoomInfoState {
-    const getRoomInfoState = useCallback(
-      () => roomsInfoStore.getState(roomId),
-      [roomId]
-    );
-
-    useEffect(() => {
-      void roomsInfoStore.get(roomId);
-    }, [roomId]);
-
-    const state = useSyncExternalStore(
-      roomsInfoStore.subscribe,
-      getRoomInfoState,
-      getRoomInfoState
-    );
-
-    return state
-      ? ({
-          isLoading: state.isLoading,
-          info: state.data,
-          // Return an error if `undefined` was returned by `resolveRoomsInfo` for this room ID
-          error:
-            !state.isLoading && !state.data && !state.error
-              ? missingRoomInfoError
-              : state.error,
-        } as RoomInfoState)
-      : { isLoading: true };
-  }
-
-  function useRoomInfoSuspense(roomId: string) {
-    const getRoomInfoState = useCallback(
-      () => roomsInfoStore.getState(roomId),
-      [roomId]
-    );
-    const roomInfoState = getRoomInfoState();
-
-    if (!roomInfoState || roomInfoState.isLoading) {
-      throw roomsInfoStore.get(roomId);
-    }
-
-    if (roomInfoState.error) {
-      throw roomInfoState.error;
-    }
-
-    // Throw an error if `undefined` was returned by `resolveRoomsInfo` for this room ID
-    if (!roomInfoState.data) {
-      throw missingRoomInfoError;
-    }
-
-    const state = useSyncExternalStore(
-      roomsInfoStore.subscribe,
-      getRoomInfoState,
-      getRoomInfoState
-    );
-
-    return {
-      isLoading: false,
-      info: state?.data,
-      error: state?.error,
-    } as RoomInfoStateSuccess;
-  }
-
-  const bundle: SharedContextBundle<TUserMeta> = {
-    useUser,
-    useRoomInfo,
-
+  return {
+    useUser: (userId: string) => useUser_withClient(client, userId),
+    useRoomInfo: (roomId: string) => useRoomInfo_withClient(client, roomId),
     suspense: {
-      useUser: useUserSuspense,
-      useRoomInfo: useRoomInfoSuspense,
+      useUser: (userId: string) => useUserSuspense_withClient(client, userId),
+      useRoomInfo: (roomId: string) =>
+        useRoomInfoSuspense_withClient(client, roomId),
     },
   };
-
-  return bundle;
 }
