84b5a1a24f8cf3fb77b21ed4e1e7cf39271e57a9
Vincent Driessen <me@nvie.com>
2024-05-20 16:10:42 +0200

    Tighten type tests

--- packages/liveblocks-react/test-d/hooks.test-d.ts
+++ packages/liveblocks-react/test-d/hooks.test-d.ts
@@ -1,24 +1,17 @@
-import type {
-  BaseUserMeta,
-  Json,
-  JsonObject,
-  LsonObject,
-  Room,
-  User,
-} from "@liveblocks/client";
+import type { LiveList, Room, User } from "@liveblocks/client";
 import { createClient } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
 import { expectType } from "tsd";
 
-type Presence = JsonObject;
-type Storage = LsonObject;
-type UserMeta = BaseUserMeta;
-type RoomEvent = Json;
+type MyPresence = { cursor: { x: number; y: number } | null };
+type MyStorage = { animals: LiveList<string> };
+type MyUserMeta = { id: string; info: { name: string } };
+type MyRoomEvent = { type: "emoji"; value: string };
 
-type P = Presence;
-type S = Storage;
-type U = UserMeta;
-type E = RoomEvent;
+type P = MyPresence;
+type S = MyStorage;
+type U = MyUserMeta;
+type E = MyRoomEvent;
 
 const client = createClient({ publicApiKey: "pk_whatever" });
 
@@ -31,8 +24,23 @@ const ctx = createRoomContext<P, S, U, E>(client);
 // The useRoom() hook
 expectType<Room<P, S, U, E>>(ctx.useRoom());
 
-// The useOthers() hook
+// The presence hooks
+expectType<P>(ctx.useSelf()!.presence);
 expectType<readonly User<P, U>[]>(ctx.useOthers());
+expectType<P>(ctx.useOthers()[0].presence);
+expectType<P>(ctx.useOthersMapped((u) => u.presence)[0][1]);
+expectType<readonly number[]>(ctx.useOthersConnectionIds());
+expectType<P>(ctx.useOther(123, (o) => o.presence));
+expectType<P>(ctx.useMyPresence()[0]);
+
+// The presence hooks (suspense versions)
+expectType<P>(ctx.suspense.useSelf().presence);
+expectType<readonly User<P, U>[]>(ctx.suspense.useOthers());
+expectType<P>(ctx.suspense.useOthers()[0].presence);
+expectType<P>(ctx.suspense.useOthersMapped((u) => u.presence)[0][1]);
+expectType<readonly number[]>(ctx.suspense.useOthersConnectionIds());
+expectType<P>(ctx.suspense.useOther(123, (o) => o.presence));
+expectType<P>(ctx.suspense.useMyPresence()[0]);
 
 // The useOthersListener() hook
 ctx.useOthersListener((event) => {
