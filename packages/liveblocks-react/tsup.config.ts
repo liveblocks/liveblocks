39761834a8587e8f1f2c284cf806372964778c2b
Vincent Driessen <me@nvie.com>
2024-05-28 22:31:11 +0200

    Split package into two entrypoints

--- packages/liveblocks-react/tsup.config.ts
+++ packages/liveblocks-react/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts", "src/suspense.ts"],
   dts: true,
   splitting: true,
   clean: true,
