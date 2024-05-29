94486e83726534dbc9f38ca12a85cbde52b61cae
Vincent Driessen <me@nvie.com>
2024-05-29 11:35:31 +0200

    Update entrypoints to only include the appropriate classic or Suspense hooks at the top-level

--- packages/liveblocks-react/src/index.ts
+++ packages/liveblocks-react/src/index.ts
@@ -10,10 +10,40 @@ export {
   useLiveblocksContextBundle,
   useLiveblocksContextBundleOrNull,
 } from "./liveblocks";
-export { createRoomContext, useRoomContextBundle } from "./room";
 export { useSharedContextBundle } from "./shared";
 export type { MutationContext, UseThreadsOptions } from "./types";
 
 // Re-exports from @liveblocks/client, for convenience
 export type { Json, JsonObject } from "@liveblocks/client";
 export { shallow } from "@liveblocks/client";
+
+// Export all the top-level hooks
+export {
+  createRoomContext,
+  useRoomContextBundle,
+  useBatch,
+  useBroadcastEvent,
+  useOthersListener,
+  useRoom,
+  useStatus,
+  useAddReaction,
+  useMutation,
+  useCreateThread,
+  useEditThreadMetadata,
+  useEventListener,
+  useMyPresence,
+  useStorageRoot,
+  useUpdateMyPresence,
+} from "./room";
+
+// Export the classic (non-Suspense) versions of our hooks
+// (This part differs from src/suspense.ts)
+export {
+  useOthersConnectionIds,
+  useOthersMapped,
+  useThreads,
+  useOther,
+  useOthers,
+  useStorage,
+  useSelf,
+} from "./room";
--- packages/liveblocks-react/src/suspense.ts
+++ packages/liveblocks-react/src/suspense.ts
@@ -10,10 +10,40 @@ export {
   useLiveblocksContextBundle,
   useLiveblocksContextBundleOrNull,
 } from "./liveblocks";
-export { createRoomContext, useRoomContextBundle } from "./room";
 export { useSharedContextBundle } from "./shared";
 export type { MutationContext, UseThreadsOptions } from "./types";
 
 // Re-exports from @liveblocks/client, for convenience
 export type { Json, JsonObject } from "@liveblocks/client";
 export { shallow } from "@liveblocks/client";
+
+// Export all the top-level hooks
+export {
+  createRoomContext,
+  useRoomContextBundle,
+  useBatch,
+  useBroadcastEvent,
+  useOthersListener,
+  useRoom,
+  useStatus,
+  useAddReaction,
+  useMutation,
+  useCreateThread,
+  useEditThreadMetadata,
+  useEventListener,
+  useMyPresence,
+  useStorageRoot,
+  useUpdateMyPresence,
+} from "./room";
+
+// Export the Suspense versions of our hooks
+// (This part differs from src/index.ts)
+export {
+  useOthersConnectionIdsSuspense as useOthersConnectionIds,
+  useOthersMappedSuspense as useOthersMapped,
+  useThreadsSuspense as useThreads,
+  useOtherSuspense as useOther,
+  useOthersSuspense as useOthers,
+  useStorageSuspense as useStorage,
+  useSelfSuspense as useSelf,
+} from "./room";
