a1b3637dbb8dbaee5ffe1b41b03a0b6ba94bff55
Vincent Driessen <me@nvie.com>
2024-05-30 14:53:23 +0200

    Ignore linter complaint

--- packages/liveblocks-client/src/global.ts
+++ packages/liveblocks-client/src/global.ts
@@ -9,6 +9,7 @@ declare global {
   /**
    * Namespace for user-defined Liveblocks types.
    */
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   export namespace Liveblocks {
     export interface Presence extends JsonObject {}
     export interface Storage extends LsonObject {}
