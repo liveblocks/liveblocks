56c93eaa48e33453cde9f984fd1cd8d924e79d3f
Jonathan Rowny <jrowny@gmail.com>
2024-06-03 14:11:24 -0700

    new api methods

--- packages/liveblocks-node-lexical/src/index.ts
+++ packages/liveblocks-node-lexical/src/index.ts
@@ -3,9 +3,9 @@ import type { Liveblocks } from "@liveblocks/node";
 import type {
   EditorState,
   Klass,
+  LexicalEditor,
   LexicalNode,
   LexicalNodeReplacement,
-  RootNode,
 } from "lexical";
 import { $getRoot } from "lexical";
 import {
@@ -24,6 +24,46 @@ detectDupes(PKG_NAME, PKG_VERSION, PKG_FORMAT);
 const LIVEBLOCKS_NODES = [ThreadMarkNode, MentionNode];
 
 export { $createParagraphNode, $createTextNode, $getRoot } from "lexical";
+
+// gets a sync'd headless editor
+export async function getEditor(
+  client: Liveblocks,
+  roomId: string,
+  nodes: ReadonlyArray<Klass<LexicalNode> | LexicalNodeReplacement>
+): Promise<LexicalEditor> {
+  const doc = await client.getYjsDocumentAsBinaryUpdate(roomId);
+  const update = new Uint8Array(doc);
+  return withHeadlessCollaborationEditor(
+    [...LIVEBLOCKS_NODES, ...nodes],
+    (editor, binding) => {
+      applyUpdate(binding.doc, update);
+      editor.update(() => {}, { discrete: true });
+      return editor;
+    }
+  );
+}
+
+// gets text content
+export async function getTextContent(
+  client: Liveblocks,
+  roomId: string,
+  nodes: ReadonlyArray<Klass<LexicalNode> | LexicalNodeReplacement>
+): Promise<string> {
+  const doc = await client.getYjsDocumentAsBinaryUpdate(roomId);
+  const update = new Uint8Array(doc);
+  return withHeadlessCollaborationEditor(
+    [...LIVEBLOCKS_NODES, ...nodes],
+    (editor, binding) => {
+      applyUpdate(binding.doc, update);
+      editor.update(() => {}, { discrete: true });
+      let content: string = "";
+      editor.getEditorState().read(() => {
+        content = $getRoot().getTextContent();
+      });
+      return content;
+    }
+  );
+}
 
 // get editor state
 export async function getEditorState(
@@ -48,7 +88,7 @@ export async function modifyDocument(
   client: Liveblocks,
   roomId: string,
   nodes: ReadonlyArray<Klass<LexicalNode> | LexicalNodeReplacement>,
-  modifyFn: (root: RootNode) => void
+  modifyFn: (editor: LexicalEditor) => void
 ): Promise<void> {
   const doc = await client.getYjsDocumentAsBinaryUpdate(roomId);
   const update = new Uint8Array(doc);
@@ -60,8 +100,7 @@ export async function modifyDocument(
       const beforeVector = encodeStateVectorFromUpdate(update);
       editor.update(
         () => {
-          const root = $getRoot();
-          modifyFn(root);
+          modifyFn(editor);
         },
         { discrete: true }
       );
