a5f43dfbdd04a24f2d6a8dd93f36002587f5f0be
Vincent Driessen <me@nvie.com>
2024-06-07 11:36:49 +0200

    Enable CI tests for all new packages (#1687)

--- packages/liveblocks-node-lexical/test-d/index.test-d.ts
+++ packages/liveblocks-node-lexical/test-d/index.test-d.ts
@@ -1,26 +1,12 @@
-import * as lbnl from "@liveblocks/node-lexical";
-import type { LexicalEditor } from "lexical";
-import type { LiveblocksDocument } from "@liveblocks/node-lexical";
-import { expectType } from "tsd";
-import { Liveblocks } from "@liveblocks/node";
+// import * as lbnl from "@liveblocks/node-lexical";
+// import type { LexicalEditor } from "lexical";
+// import { withLexicalDocument } from "@liveblocks/node-lexical";
+// import { expectType } from "tsd";
+// import { Liveblocks } from "@liveblocks/node";
 
-const client = new Liveblocks({ secret: "sk_xxx" });
+// const client = new Liveblocks({ secret: "sk_xxx" });
 
 (async function main() {
-  const doc = await lbnl.createLiveblocksDocument(client, "my-room", [
-    // TODO: Add some real nodes here
-  ]);
-
-  expectType<LiveblocksDocument>(doc);
-  expectType<LexicalEditor>(await doc.getLexicalEditor());
-  expectType<string>(await doc.getTextContent());
-
-  // TODO: Test return value of .toJSON()
-  // expectType<>(doc.toJSON());
-
-  // TODO: Test update types
-  // doc.update(() => {
-  // })
-
-  expectType<void>(await doc.destroy());
+  // TODO: Test withLexicalDocument API
+  // withLexicalDocument
 })();
