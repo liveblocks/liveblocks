206d2d698a565e45dfdcec4176d20752a0543b8b
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2024-06-07 10:48:32 +0200

    Simplify list of known exports/imports with regex

--- packages/liveblocks-codemod/src/transforms/update-liveblocks-config-imports.ts
+++ packages/liveblocks-codemod/src/transforms/update-liveblocks-config-imports.ts
@@ -2,106 +2,8 @@ import type { API, FileInfo, Options } from "jscodeshift";
 
 // Based on https://github.com/vercel/next.js/blob/main/packages/next-codemod
 
-// const ROOM_CONTEXT_EXPORTS = [
-//   "RoomProvider",
-//   "useRoom",
-//   "useMyPresence",
-//   "useUpdateMyPresence",
-//   "useSelf",
-//   "useOthers",
-//   "useOthersMapped",
-//   "useOthersListener",
-//   "useOthersConnectionIds",
-//   "useOther",
-//   "useBroadcastEvent",
-//   "useEventListener",
-//   "useErrorListener",
-//   "useStorage",
-//   "useObject",
-//   "useMap",
-//   "useList",
-//   "useBatch",
-//   "useHistory",
-//   "useUndo",
-//   "useRedo",
-//   "useCanUndo",
-//   "useCanRedo",
-//   "useMutation",
-//   "useStatus",
-//   "useLostConnectionListener",
-//   "useThreads",
-//   "useCreateThread",
-//   "useEditThreadMetadata",
-//   "useCreateComment",
-//   "useEditComment",
-//   "useDeleteComment",
-//   "useAddReaction",
-//   "useRemoveReaction",
-//   "useThreadSubscription",
-//   "useMarkThreadAsRead",
-//   "useRoomNotificationSettings",
-//   "useUpdateRoomNotificationSettings",
-// ];
-
-// const LIVEBLOCKS_CONTEXT_EXPORTS = [
-//   "LiveblocksProvider",
-//   "useMarkInboxNotificationAsRead",
-//   "useMarkAllInboxNotificationsAsRead",
-//   "useInboxNotifications",
-//   "useUnreadInboxNotificationsCount",
-// ];
-
-// const SHARED_CONTEXT_EXPORTS = ["useUser", "useRoomInfo"];
-
-// const NON_SUSPENSE_EXPORTS = ["RoomProvider", "LiveblocksProvider"];
-
 const CONFIG_IMPORT_REGEX = /.*liveblocks(.*)?\.config(\.(?:t|j)sx?)?/;
-
-const KNOWN_EXPORTS = [
-  "RoomProvider",
-  "useRoom",
-  "useMyPresence",
-  "useUpdateMyPresence",
-  "useSelf",
-  "useOthers",
-  "useOthersMapped",
-  "useOthersListener",
-  "useOthersConnectionIds",
-  "useOther",
-  "useBroadcastEvent",
-  "useEventListener",
-  "useErrorListener",
-  "useStorage",
-  "useObject",
-  "useMap",
-  "useList",
-  "useBatch",
-  "useHistory",
-  "useUndo",
-  "useRedo",
-  "useCanUndo",
-  "useCanRedo",
-  "useMutation",
-  "useStatus",
-  "useLostConnectionListener",
-  "useThreads",
-  "useCreateThread",
-  "useEditThreadMetadata",
-  "useCreateComment",
-  "useEditComment",
-  "useDeleteComment",
-  "useAddReaction",
-  "useRemoveReaction",
-  "useThreadSubscription",
-  "useMarkThreadAsRead",
-  "useRoomNotificationSettings",
-  "useUpdateRoomNotificationSettings",
-  "LiveblocksProvider",
-  "useMarkInboxNotificationAsRead",
-  "useMarkAllInboxNotificationsAsRead",
-  "useInboxNotifications",
-  "useUnreadInboxNotificationsCount",
-];
+const CONFIG_IMPORTS_REGEX = /^(Room|Liveblocks)?Provider|^use[A-Z]/;
 
 export default function transformer(
   file: FileInfo,
@@ -125,24 +27,24 @@ export default function transformer(
     })
     .forEach((path) => {
       const importSpecifiers = path.node.specifiers;
-      const importToChange = importSpecifiers.filter((specifier) =>
-        KNOWN_EXPORTS.includes(specifier.local.name)
+      const importsFromLiveblocksReact = importSpecifiers.filter((specifier) =>
+        CONFIG_IMPORTS_REGEX.test(specifier.local.name)
       );
-      const importsRemained = importSpecifiers.filter(
-        (specifier) => !KNOWN_EXPORTS.includes(specifier.local.name)
+      const importsFromConfig = importSpecifiers.filter(
+        (specifier) => !CONFIG_IMPORTS_REGEX.test(specifier.local.name)
       );
 
-      if (importToChange.length > 0) {
+      if (importsFromLiveblocksReact.length > 0) {
         const liveblocksReactImport = j.importDeclaration(
-          importToChange,
+          importsFromLiveblocksReact,
           j.stringLiteral("@liveblocks/react")
         );
         path.insertBefore(liveblocksReactImport);
       }
 
-      if (importsRemained.length > 0) {
+      if (importsFromConfig.length > 0) {
         const originalImport = j.importDeclaration(
-          importsRemained,
+          importsFromConfig,
           path.value.source
         );
         path.insertBefore(originalImport);
