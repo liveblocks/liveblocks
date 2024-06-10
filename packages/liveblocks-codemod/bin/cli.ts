1dbb6d4c9ec9633a0125434cd9ed656e3d715478
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2024-06-10 17:46:40 +0200

    Use gitignore

--- packages/liveblocks-codemod/bin/cli.ts
+++ packages/liveblocks-codemod/bin/cli.ts
@@ -104,7 +104,9 @@ export function runTransform({
 
   args.push("--verbose=2");
 
-  args.push("--ignore-pattern=**/{node_modules,.*}/**");
+  args.push("--ignore-config=.gitignore");
+
+  args.push("--ignore-pattern=**/node_modules/**");
 
   args.push("--extensions=tsx,ts,jsx,js");
 
