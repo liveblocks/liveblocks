6561e43b9d99ca9ad0f382451fa89e7441b67baa
Vincent Driessen <me@nvie.com>
2024-06-10 16:27:45 +0200

    Upgrade nextjs-yjs-blocknote to 2.0

--- examples/nextjs-yjs-blocknote/src/components/CollaborativeEditor.tsx
+++ examples/nextjs-yjs-blocknote/src/components/CollaborativeEditor.tsx
@@ -6,7 +6,7 @@ import { BlockNoteView, useCreateBlockNote } from "@blocknote/react";
 import "@blocknote/react/style.css";
 import * as Y from "yjs";
 import { LiveblocksYjsProvider } from "@liveblocks/yjs";
-import { useRoom, useSelf } from "@/liveblocks.config";
+import { useRoom, useSelf } from "@liveblocks/react/suspense";
 import { useEffect, useState } from "react";
 import { Toolbar } from "./Toolbar";
 import styles from "./CollaborativeEditor.module.css";
