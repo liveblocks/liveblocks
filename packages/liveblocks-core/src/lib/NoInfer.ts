3bf3b1241cbd540fd693768f031e0ddbe76e4a26
Vincent Driessen <me@nvie.com>
2024-06-10 08:19:06 +0200

    Lint + comment

--- packages/liveblocks-core/src/lib/NoInfer.ts
+++ packages/liveblocks-core/src/lib/NoInfer.ts
@@ -1,7 +1,6 @@
 /**
- * Back-port of TypeScript 5.4's built-in NoInfer utility type. We cannot use
- * the name `NoInfer` here, because in TS 5.4, it's a reserved type name.
+ * Back-port of TypeScript 5.4's built-in NoInfer utility type.
  * See https://stackoverflow.com/a/56688073/148872
  */
-export type NoInfr<A extends any> = [A][A extends any ? 0 : never];
-//          ^^^^^^ Not a typo
+export type NoInfr<A> = [A][A extends any ? 0 : never];
+//          ^^^^^^ Not a typo, the name `NoInfer` is reserved in TS 5.4+
