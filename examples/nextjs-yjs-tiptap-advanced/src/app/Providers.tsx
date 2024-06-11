bc817685fd37a6c2703cb9c7f6f1cbc00097f20e
Vincent Driessen <me@nvie.com>
2024-06-11 09:46:36 +0200

    Fix TS issue

--- examples/nextjs-yjs-tiptap-advanced/src/app/Providers.tsx
+++ examples/nextjs-yjs-tiptap-advanced/src/app/Providers.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import type { PropsWithChildren } from "react";
 import { LiveblocksProvider } from "@liveblocks/react";
 
-export function Providers({ children }) {
+export function Providers({ children }: PropsWithChildren) {
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
