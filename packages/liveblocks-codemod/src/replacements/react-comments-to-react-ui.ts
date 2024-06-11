d98d43a740c6c043358f45b9a8555b2735e87367
Vincent Driessen <me@nvie.com>
2024-06-11 11:32:54 +0200

    Don't touch package.json in codemod

--- packages/liveblocks-codemod/src/replacements/react-comments-to-react-ui.ts
+++ packages/liveblocks-codemod/src/replacements/react-comments-to-react-ui.ts
@@ -1,15 +1,4 @@
 /**
- * Before: "@liveblocks/react-comments": "^1.12.0"
- *  After: "@liveblocks/react-ui": "^1.12.0"
- */
-export function replaceReactCommentsInPackageJson(file: string) {
-  return file.replace(
-    /(["'])@liveblocks\/react-comments\1:/g,
-    "$1@liveblocks/react-ui$1:"
-  );
-}
-
-/**
  * Before: @import "@liveblocks/react-comments/styles.css"
  *  After: @import "@liveblocks/react-ui/styles.css"
  */
