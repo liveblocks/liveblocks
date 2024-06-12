83cd671d2f39e4087a0af4b8819b819048d89845
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2024-06-12 08:59:13 +0200

    Improve LiveList codemod tests

---
+++ packages/liveblocks-codemod/src/transforms/__testfixtures__/live-list-argument/unrelated.input.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable */
+// @ts-nocheck
+import React from "react";
+import { createRoomContext } from "@liveblocks/react";
+import { LiveList } from "./module";
+
+LiveList();
+
+const list = new LiveList();
+const list2 = new LiveList([]);
+const list3 = new LiveList([1, 2, 3]);
+
+// Comment
+const list4 = new LiveList();
+
+/**
+ * Comment
+ */
+const list5 = new LiveList([]);
+
+//
+// Comment
+//
+const list6 = new LiveList([1, 2, 3]);
