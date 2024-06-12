ee5f922b90bea3e0ea1fcaf49192c92642eaf89f
Vincent Driessen <me@nvie.com>
2024-06-12 12:52:29 +0200

    Update fixture to ideal output

--- packages/liveblocks-codemod/src/transforms/__testfixtures__/remove-liveblocks-config-contexts/liveblocks-no-types.config.output.tsx
+++ packages/liveblocks-codemod/src/transforms/__testfixtures__/remove-liveblocks-config-contexts/liveblocks-no-types.config.output.tsx
@@ -6,3 +6,10 @@ export const client = createClient({
   authEndpoint: "/api/liveblocks-auth",
   throttle: 16,
 });
+
+declare global {
+  interface Liveblocks {
+    // Define your custom types here
+    // See https://liveblocks.io/docs/api-reference/liveblocks-react#TypeScript
+  }
+}
