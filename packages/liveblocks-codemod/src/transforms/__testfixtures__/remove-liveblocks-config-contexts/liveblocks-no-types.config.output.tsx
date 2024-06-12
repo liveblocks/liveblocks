256087126df3ddd1050caf70ab443d8c9bbabd23
Vincent Driessen <me@nvie.com>
2024-06-12 13:05:11 +0200

    Revert "Update fixture to ideal output"
    This reverts commit ee5f922b90bea3e0ea1fcaf49192c92642eaf89f.

--- packages/liveblocks-codemod/src/transforms/__testfixtures__/remove-liveblocks-config-contexts/liveblocks-no-types.config.output.tsx
+++ packages/liveblocks-codemod/src/transforms/__testfixtures__/remove-liveblocks-config-contexts/liveblocks-no-types.config.output.tsx
@@ -6,10 +6,3 @@ export const client = createClient({
   authEndpoint: "/api/liveblocks-auth",
   throttle: 16,
 });
-
-declare global {
-  interface Liveblocks {
-    // Define your custom types here
-    // See https://liveblocks.io/docs/api-reference/liveblocks-react#TypeScript
-  }
-}
