d4c1e888f646c74cc30f66a86990d5c14e3c6d8d
Vincent Driessen <me@nvie.com>
2024-06-12 12:27:18 +0200

    Add script

---
+++ scripts/check-exports.ts
@@ -0,0 +1,212 @@
+#!/usr/bin/env bun
+
+import { relative } from "path";
+import { Project, Node, SyntaxKind } from "ts-morph";
+import { sorted } from "itertools";
+
+let numIssues = 0;
+
+type Location = {
+  file: string;
+  lineno: number;
+};
+
+type ExportedSymbol = {
+  location: Location;
+  name: string;
+  jsDoc?: string;
+};
+
+/**
+ * Strips all indentation from the raw JSDoc comment string.
+ */
+function normalizeCommentText(rawText: string): string {
+  rawText = rawText.trim();
+  if (rawText.startsWith("/**")) {
+    rawText = rawText.slice(3);
+  }
+  if (rawText.endsWith("*/")) {
+    rawText = rawText.slice(0, -2);
+  }
+  rawText = rawText.trim();
+
+  const lines = [];
+  for (const line of rawText.split("\n")) {
+    lines.push(line.replace(/^\s*\*\s*/, "").trim());
+  }
+  return lines.join("\n");
+}
+
+// Function to extract JSDoc comments from a node
+function getJSDoc(node: Node): string {
+  return node
+    .getChildrenOfKind(SyntaxKind.JSDoc)
+    .map((block) => normalizeCommentText(block.getText()))
+    .filter(Boolean)
+    .join("\n\n")
+    .trim();
+}
+
+// Function to display all exported symbols from a given file path
+function* iterExports(filePath: string): Generator<ExportedSymbol> {
+  const project = new Project({
+    tsConfigFilePath: "tsconfig.json",
+  });
+
+  const sourceFile = project.addSourceFileAtPath(filePath);
+
+  const exports = sorted(sourceFile.getExportedDeclarations(), ([name]) =>
+    name.toLowerCase()
+  );
+
+  for (const [name, declarations] of exports) {
+    for (const decl of declarations) {
+      const jsDoc = getJSDoc(decl);
+      yield {
+        location: {
+          file: decl.getSourceFile().getFilePath(),
+          lineno: decl.getStartLineNumber(),
+        },
+        name,
+        jsDoc,
+      };
+    }
+  }
+}
+
+function listExports(filePath: string): ExportedSymbol[] {
+  return Array.from(iterExports(filePath));
+}
+
+function isPrivate(sym: ExportedSymbol): boolean {
+  return /\@private/.test(sym.jsDoc);
+}
+
+function formatLocation(sym: ExportedSymbol): string {
+  const loc = sym.location;
+  return `${yellow(relative(process.cwd(), loc.file))}:${magenta(loc.lineno)}`;
+}
+
+function warn(...args: unknown[]) {
+  numIssues++;
+  return console.warn(...args);
+}
+
+const classicExports = listExports("dist/index.d.mts");
+const suspenseExports = listExports("dist/suspense.d.mts");
+
+function blue(text: string | number): string {
+  return `\x1b[34m${text}\x1b[0m`;
+}
+
+function magenta(text: string | number): string {
+  return `\x1b[35m${text}\x1b[0m`;
+}
+
+function yellow(text: string | number): string {
+  return `\x1b[33m${text}\x1b[0m`;
+}
+
+// Warn about any symbols that aren't documented yet
+for (const sym of [...classicExports, ...suspenseExports]) {
+  if (!sym.jsDoc) {
+    warn(
+      formatLocation(sym),
+      "Symbol",
+      blue(sym.name),
+      "has no JSDoc comment",
+      "💬"
+    );
+  }
+}
+
+function intersection<T>(xs: T[], ys: T[]): T[] {
+  const result = [];
+  for (const x of xs) {
+    if (ys.includes(x)) {
+      result.push(x);
+    }
+  }
+  return result;
+}
+
+function difference<T>(xs: T[], ys: T[]): T[] {
+  const result = [];
+  for (const x of xs) {
+    if (!ys.includes(x)) {
+      result.push(x);
+    }
+  }
+  return result;
+}
+
+/**
+ * Any symbol is by default expected to be exported in both ./index and
+ * ./suspense. This isn't needed if the exported symbol has an @-private
+ * directive.
+ *
+ * These symbols are expected to only exist in ./index, but not in ./suspense subpath.
+ */
+const CLASSIC_ONLY = ["createLiveblocksContext", "createRoomContext"];
+
+// Warn about any symbols that aren't documented yet
+const classicNames = classicExports.map((e) => e.name);
+const suspenseNames = suspenseExports.map((e) => e.name);
+
+for (const name of difference(classicNames, suspenseNames)) {
+  if (CLASSIC_ONLY.includes(name)) {
+    // Skip known classic-only symbols
+    continue;
+  }
+
+  const sym = classicExports.find((x) => x.name === name)!;
+  if (isPrivate(sym)) {
+    // Skip check: @private-symbols are not required to have both exports
+    continue;
+  }
+
+  warn(
+    formatLocation(sym),
+    "Symbol",
+    blue(sym.name),
+    "has no suspense export",
+    "⚠️"
+  );
+}
+
+for (const name of difference(suspenseNames, classicNames)) {
+  const sym = suspenseExports.find((x) => x.name === name)!;
+  if (isPrivate(sym)) {
+    // Skip check: @private-symbols are not required to have both exports
+    continue;
+  }
+
+  warn(
+    formatLocation(sym),
+    "Symbol",
+    blue(sym.name),
+    "has no classic export",
+    "⚠️"
+  );
+}
+
+for (const name of intersection(suspenseNames, classicNames)) {
+  const sym1 = classicExports.find((x) => x.name === name)!;
+  const sym2 = suspenseExports.find((x) => x.name === name)!;
+
+  if (sym1.jsDoc !== sym2.jsDoc) {
+    warn(
+      "Symbol",
+      blue(name),
+      "has different doc strings for classic vs suspense"
+      // TODO: Show diff here?
+    );
+  }
+}
+
+if (numIssues > 0) {
+  console.log(`Found ${numIssues} issue${numIssues !== 1 ? "s" : ""}`);
+  process.exit(2);
+} else {
+  process.exit(0);
+}
