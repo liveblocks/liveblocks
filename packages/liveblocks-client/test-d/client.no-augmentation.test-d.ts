0ba9be68aeeabc5a149de1c3f6fba4102d138030
Vincent Driessen <me@nvie.com>
2024-06-14 09:10:37 +0200

    Adjust tests to match ideal behavior (for @liveblocks/client)

--- packages/liveblocks-client/test-d/client.no-augmentation.test-d.ts
+++ packages/liveblocks-client/test-d/client.no-augmentation.test-d.ts
@@ -1,11 +1,57 @@
 import type { Json, JsonObject } from "@liveblocks/client";
 import { createClient } from "@liveblocks/client";
-import { expectType } from "tsd";
+import { expectError, expectType } from "tsd";
 
 const client = createClient({ publicApiKey: "pk_xxx" });
 
 // .enterRoom()
 {
+  {
+    // No options at all is fine
+    // XXX Enable this test as well, once all the others work!
+    // const { room } = client.enterRoom("my-room");
+    // expectType<JsonObject>(room.getPresence());
+  }
+
+  {
+    // No initial props is also fine
+    const { room } = client.enterRoom("my-room", { autoConnect: true });
+    expectType<JsonObject>(room.getPresence());
+  }
+
+  {
+    // No options at all is fine if not using the global presence
+    const { room } = client.enterRoom<JsonObject>("my-room", {
+      autoConnect: true,
+    });
+    expectType<JsonObject>(room.getPresence());
+  }
+
+  {
+    // Initial presence is required
+    expectError<{ foo: string }>(client.enterRoom("room"));
+    expectError<{ foo: string }>(client.enterRoom("room", {}));
+    expectError<{ foo: string }>(
+      client.enterRoom("room", { initialPresence: {} })
+    );
+    expectError<{ foo: string }>(
+      client.enterRoom("room", { initialPresence: { bar: "" } })
+    );
+  }
+
+  {
+    // Initial presence is not required when it has only optional fields
+    // XXX Enable this test as well, once all the others work!
+    // client.enterRoom<{ foo?: string; bar?: number }>("room");
+    client.enterRoom<{ foo?: string; bar?: number }>("room", {});
+    client.enterRoom<{ foo?: string; bar?: number }>("room", {
+      initialPresence: {},
+    });
+    client.enterRoom<{ foo?: string; bar?: number }>("room", {
+      initialPresence: { foo: "" },
+    });
+  }
+
   {
     const { room } = client.enterRoom("my-room", {
       initialPresence: { foo: null },
