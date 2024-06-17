dd21e7c1ae1384bcdede26fef7bc0d5e3292fb47
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2024-06-17 16:48:36 +0200

    Rename format variable for clarification

--- packages/liveblocks-react-ui/src/version.ts
+++ packages/liveblocks-react-ui/src/version.ts
@@ -1,6 +1,6 @@
 declare const __VERSION__: string;
-declare const TSUP_FORMAT: string;
+declare const ROLLUP_FORMAT: string;
 
 export const PKG_NAME = "@liveblocks/react-ui";
 export const PKG_VERSION = typeof __VERSION__ === "string" && __VERSION__;
-export const PKG_FORMAT = typeof TSUP_FORMAT === "string" && TSUP_FORMAT;
+export const PKG_FORMAT = typeof ROLLUP_FORMAT === "string" && ROLLUP_FORMAT;
