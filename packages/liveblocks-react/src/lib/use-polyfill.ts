6539bb0876b7669ded101fff1a1f7e876a78e24b
Vincent Driessen <me@nvie.com>
2024-06-24 17:00:48 +0200

    Add polyfill for React 19's `use` function

---
+++ packages/liveblocks-react/src/lib/use-polyfill.ts
@@ -0,0 +1,35 @@
+// import React from "react";
+
+/**
+ * Drop-in replacement for React 19's `use` hook.
+ */
+export const use =
+  // React.use ||
+  <T>(
+    promise: Promise<T> & {
+      status?: "pending" | "fulfilled" | "rejected";
+      value?: T;
+      reason?: unknown;
+    }
+  ): T => {
+    if (promise.status === "pending") {
+      throw promise;
+    } else if (promise.status === "fulfilled") {
+      return promise.value as T;
+    } else if (promise.status === "rejected") {
+      throw promise.reason;
+    } else {
+      promise.status = "pending";
+      promise.then(
+        (v) => {
+          promise.status = "fulfilled";
+          promise.value = v;
+        },
+        (e) => {
+          promise.status = "rejected";
+          promise.reason = e;
+        }
+      );
+      throw promise;
+    }
+  };
