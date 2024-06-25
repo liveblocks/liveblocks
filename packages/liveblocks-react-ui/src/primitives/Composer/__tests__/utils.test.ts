9dd95a4cf37c4b2eb2037f082cf8cdef5071f59f
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2024-06-25 17:29:52 +0200

    Add tests for commentBodyToComposerBody

--- packages/liveblocks-react-ui/src/primitives/Composer/__tests__/utils.test.ts
+++ packages/liveblocks-react-ui/src/primitives/Composer/__tests__/utils.test.ts
@@ -1,7 +1,7 @@
 import type { CommentBody } from "@liveblocks/client";
 
 import type { ComposerBody } from "../../../types";
-import { commentBodyToComposerBody } from "../utils";
+import { commentBodyToComposerBody, composerBodyToCommentBody } from "../utils";
 
 const commentBodyToComposerBodyFixtures: [string, CommentBody, ComposerBody][] =
   [
@@ -269,6 +269,276 @@ describe("commentBodyToComposerBody", () => {
     "converts %s to a composer body",
     (_, commentBody, composerBody) => {
       expect(commentBodyToComposerBody(commentBody)).toEqual(composerBody);
+    }
+  );
+});
+
+const composerBodyToCommentBodyFixtures: [string, ComposerBody, CommentBody][] =
+  [
+    // ===========================================================
+    [
+      "a composer body",
+      // ---------------------------------------------------------
+      [
+        {
+          type: "paragraph",
+          children: [
+            {
+              text: "Hello ",
+            },
+            {
+              text: "world",
+              bold: true,
+            },
+            {
+              text: " and ",
+            },
+            {
+              type: "mention",
+              id: "chris",
+              children: [
+                {
+                  text: "",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      // ---------------------------------------------------------
+      {
+        version: 1,
+        content: [
+          {
+            type: "paragraph",
+            children: [
+              { text: "Hello " },
+              { text: "world", bold: true },
+              { text: " and " },
+              { type: "mention", id: "chris" },
+            ],
+          },
+        ],
+      },
+    ],
+    // ===========================================================
+    [
+      "a composer body with multiple paragraphs",
+      // ---------------------------------------------------------
+      [
+        {
+          type: "paragraph",
+          children: [
+            {
+              text: "Hello ",
+            },
+            {
+              text: "world",
+              italic: true,
+              bold: true,
+            },
+            {
+              text: " and ",
+            },
+            {
+              type: "mention",
+              id: "vincent",
+              children: [
+                {
+                  text: "",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: "paragraph",
+          children: [
+            {
+              type: "auto-link",
+              url: "https://liveblocks.io",
+              children: [
+                {
+                  text: "https://liveblocks.io",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: "paragraph",
+          children: [
+            {
+              type: "custom-link",
+              url: "https://liveblocks.io",
+              children: [
+                {
+                  text: "Liveblocks",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      // ---------------------------------------------------------
+      {
+        version: 1,
+        content: [
+          {
+            type: "paragraph",
+            children: [
+              { text: "Hello " },
+              { text: "world", italic: true, bold: true },
+              { text: " and " },
+              { type: "mention", id: "vincent" },
+            ],
+          },
+          {
+            type: "paragraph",
+            children: [
+              {
+                type: "link",
+                url: "https://liveblocks.io",
+              },
+            ],
+          },
+          {
+            type: "paragraph",
+            children: [
+              {
+                type: "link",
+                url: "https://liveblocks.io",
+                text: "Liveblocks",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    // ===========================================================
+    [
+      "an empty composer body",
+      // ---------------------------------------------------------
+      [],
+      // ---------------------------------------------------------
+      {
+        version: 1,
+        content: [],
+      },
+    ],
+    // ===========================================================
+    [
+      "a composer body with an empty paragraph",
+      // ---------------------------------------------------------
+      [
+        {
+          type: "paragraph",
+          children: [],
+        },
+      ],
+      // ---------------------------------------------------------
+      {
+        version: 1,
+        content: [
+          {
+            type: "paragraph",
+            children: [],
+          },
+        ],
+      },
+    ],
+    // ===========================================================
+    [
+      "a composer body with empty inline elements",
+      // ---------------------------------------------------------
+      [
+        {
+          type: "paragraph",
+          children: [
+            {
+              text: "",
+            },
+            {
+              text: "",
+              bold: true,
+            },
+            {
+              text: "",
+              bold: true,
+              italic: true,
+              code: true,
+              strikethrough: true,
+            },
+          ],
+        },
+      ],
+      // ---------------------------------------------------------
+      {
+        version: 1,
+        content: [
+          {
+            type: "paragraph",
+            children: [
+              { text: "" },
+              { text: "", bold: true },
+              {
+                text: "",
+                bold: true,
+                italic: true,
+                code: true,
+                strikethrough: true,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    // ===========================================================
+    [
+      "a composer body with an unknown block element",
+      // ---------------------------------------------------------
+      [
+        {
+          // @ts-expect-error - Deliberately using an unknown block element
+          type: "unknown",
+          children: [{ text: "Hello " }, { text: "world", bold: true }],
+        },
+      ],
+      // ---------------------------------------------------------
+      {
+        version: 1,
+        content: [],
+      },
+    ],
+    // // ===========================================================
+    [
+      "a composer body with an unknown inline element",
+      // ---------------------------------------------------------
+      [
+        {
+          type: "paragraph",
+          // @ts-expect-error - Deliberately using an unknown inline element
+          children: [{ type: "unknown", code: "code" }],
+        },
+      ],
+      // ---------------------------------------------------------
+      {
+        version: 1,
+        content: [
+          {
+            type: "paragraph",
+            children: [],
+          },
+        ],
+      },
+    ],
+  ];
+
+describe("composerBodyToCommentBody", () => {
+  test.each(composerBodyToCommentBodyFixtures)(
+    "converts %s to a composer body",
+    (_, composerBody, commentBody) => {
+      expect(composerBodyToCommentBody(composerBody)).toEqual(commentBody);
     }
   );
 });
