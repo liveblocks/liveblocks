c3d48c4761c25e3b704f553a301213b73aa9f653
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2024-07-01 11:16:54 +0200

    Add hooks tests

--- packages/liveblocks-react/src/__tests__/_dummies.ts
+++ packages/liveblocks-react/src/__tests__/_dummies.ts
@@ -1,7 +1,7 @@
 import type {
   BaseMetadata,
   CommentData,
-  InboxNotificationData,
+  InboxNotificationCustomData,
   InboxNotificationThreadData,
   ThreadData,
 } from "@liveblocks/core";
@@ -12,7 +12,9 @@ import {
   createThreadId,
 } from "./_createIds";
 
-export function dummyThreadData(): ThreadData<BaseMetadata> {
+export function dummyThreadData(
+  overrides?: Partial<ThreadData<BaseMetadata>>
+): ThreadData<BaseMetadata> {
   const now = new Date();
   const threadId = createThreadId();
 
@@ -29,10 +31,13 @@ export function dummyThreadData(): ThreadData<BaseMetadata> {
     updatedAt: now,
     comments: [comment],
     resolved: false,
+    ...overrides,
   };
 }
 
-export function dummyCommentData(): CommentData {
+export function dummyCommentData(
+  overrides?: Partial<Omit<CommentData, "deletedAt">>
+): CommentData {
   const id = createCommentId();
   const threadId = createThreadId();
   const now = new Date();
@@ -50,25 +55,13 @@ export function dummyCommentData(): CommentData {
     deletedAt: undefined,
     createdAt: now,
     reactions: [],
+    ...overrides,
   };
 }
 
-export function dummyInboxNoficationData(): InboxNotificationData {
-  const id = createInboxNotificationId();
-  const threadId = createThreadId();
-  const now = new Date();
-
-  return {
-    kind: "thread",
-    roomId: "room-id",
-    id,
-    notifiedAt: now,
-    threadId,
-    readAt: null,
-  };
-}
-
-export function dummyCustomInboxNoficationData(): InboxNotificationData {
+export function dummyCustomInboxNoficationData(
+  overrides?: Partial<InboxNotificationCustomData>
+): InboxNotificationCustomData {
   const id = createInboxNotificationId();
   const now = new Date();
 
@@ -91,10 +84,13 @@ export function dummyCustomInboxNoficationData(): InboxNotificationData {
         },
       },
     ],
+    ...overrides,
   };
 }
 
-export function dummyThreadInboxNotificationData(): InboxNotificationThreadData {
+export function dummyThreadInboxNotificationData(
+  overrides?: Partial<InboxNotificationThreadData>
+): InboxNotificationThreadData {
   const id = createInboxNotificationId();
   const threadId = createThreadId();
   const now = new Date();
@@ -106,5 +102,6 @@ export function dummyThreadInboxNotificationData(): InboxNotificationThreadData 
     notifiedAt: now,
     threadId,
     readAt: null,
+    ...overrides,
   };
 }
