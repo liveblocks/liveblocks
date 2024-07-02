6cf1bbdd0b55aef053b334cbb07c68d28f33d184
Vincent Driessen <me@nvie.com>
2024-07-02 13:47:18 +0200

    XXXs notes

--- packages/liveblocks-react/src/liveblocks.tsx
+++ packages/liveblocks-react/src/liveblocks.tsx
@@ -253,8 +253,11 @@ function makeExtrasForClient<U extends BaseUserMeta, M extends BaseMetadata>(
       await autoRetry(
         () => updateInboxNotifications(),
         5,
-        // XXX: Previously we did 40000, 80000 here, but... do we really wait until over a minute? Seems too long to me.
-        [5000, 10000, 20000]
+        // XXX: Previously we did 40000, 80000 here, but... do we really wait
+        // until over a minute? Seems too long to me. Maybe instead we want to
+        // try a bit more often, and with a bit less waiting time?
+        // XXX: Proposal: change the array below to [5000, 5000, 10000, 10000, 15000, 15000] (= total timeout of 1 minute)
+        [5000, 10000, 20000, 40000, 80000]
       );
     } catch (err) {
       // Store the error in the cache as a side-effect, for non-Suspense
