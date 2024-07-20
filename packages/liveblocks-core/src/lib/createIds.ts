ae4929b2bca55ba5821db230e072dfa77264ccb3
GuillaumeSalles <guillaume.salles@me.com>
2024-07-20 18:06:54 +0200

    Replace our nanoid custom implementation by inlined nanoid v3.3.7

--- packages/liveblocks-core/src/lib/createIds.ts
+++ packages/liveblocks-core/src/lib/createIds.ts
@@ -1,20 +1,4 @@
-// Inlined version of 3.3.7 of nanoid.js
-// https://www.npmjs.com/package/nanoid/v/3.3.7?activeTab=code
-const nanoid = (t = 21) =>
-  crypto
-    .getRandomValues(new Uint8Array(t))
-    .reduce(
-      (t, e) =>
-        (t +=
-          (e &= 63) < 36
-            ? e.toString(36)
-            : e < 62
-              ? (e - 26).toString(36).toUpperCase()
-              : e < 63
-                ? "_"
-                : "-"),
-      ""
-    );
+import { nanoid } from "./nanoid";
 
 const THREAD_ID_PREFIX = "th";
 const COMMENT_ID_PREFIX = "cm";
