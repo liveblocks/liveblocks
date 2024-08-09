88c18a1edc84720577dab4c7f50acc036725bf0a
Chris Nicholas <ctnicholasdev@gmail.com>
2024-08-09 13:55:12 +0100

    feat: Deleting overlay

--- examples/nextjs-project-manager/src/components/IssueActions.tsx
+++ examples/nextjs-project-manager/src/components/IssueActions.tsx
@@ -2,14 +2,28 @@
 
 import { deleteRoom } from "@/actions/liveblocks";
 import { getRoomId } from "@/config";
+import { useState } from "react";
+import { Loading } from "@/components/Loading";
 
 export function IssueActions({ issueId }: { issueId: string }) {
+  const [deleting, setDeleting] = useState(false);
+
   return (
-    <button
-      className="text-red-600 text-sm"
-      onClick={() => deleteRoom(getRoomId(issueId))}
-    >
-      Delete issue
-    </button>
+    <>
+      {deleting ? (
+        <div className="inset-0 bg-gray-100/50 absolute">
+          <Loading />
+        </div>
+      ) : null}
+      <button
+        className="text-red-600 text-sm"
+        onClick={() => {
+          setDeleting(true);
+          deleteRoom(getRoomId(issueId));
+        }}
+      >
+        Delete issue
+      </button>
+    </>
   );
 }
