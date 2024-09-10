e425dabf64cff0fc86874ca177e8a75125b92796
Chris Nicholas <ctnicholasdev@gmail.com>
2024-09-10 13:59:30 +0100

    feat: Inbox styles

--- examples/nextjs-lexical-ai/app/components/Notifications.tsx
+++ examples/nextjs-lexical-ai/app/components/Notifications.tsx
@@ -10,31 +10,16 @@ import {
 import { InboxNotification, InboxNotificationList } from "@liveblocks/react-ui";
 import { Loading } from "./Loading";
 import * as Dialog from "@radix-ui/react-dialog";
+import { InboxIcon } from "../icons/InboxIcon";
+import { MailReadIcon } from "../icons/MailReadIcon";
+import { MailDeleteIcon } from "../icons/MailDeleteIcon";
 
 export function Notifications() {
   return (
     <Dialog.Root>
       <Dialog.Trigger className="flex relative items-center justify-between whitespace-nowrap rounded-md font-medium transition-colors w-full px-2 py-1.5 hover:bg-gray-200">
-        <div className="flex items-center gap-1.5 flex-1 text-sm">
-          <svg
-            width="20"
-            height="20"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="m3.6 9.8 1.9-4.6A2 2 0 0 1 7.3 4h5.4a2 2 0 0 1 1.8 1.2l2 4.6V13a2 2 0 0 1-2 2h-9a2 2 0 0 1-2-2V9.8Z"
-              stroke="currentColor"
-              strokeWidth="1.5"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M3.5 10h3c.3 0 .6.1.8.4l.9 1.2c.2.3.5.4.8.4h2c.3 0 .6-.1.8-.4l.9-1.2c.2-.3.5-.4.8-.4h3"
-              stroke="currentColor"
-              strokeWidth="1.5"
-              strokeLinejoin="round"
-            />
-          </svg>
+        <div className="flex items-center gap-1.5 flex-1 text-sm text-gray-700">
+          <InboxIcon className="w-5 h-5" />
           Inbox
         </div>
 
@@ -44,7 +29,7 @@ export function Notifications() {
       </Dialog.Trigger>
 
       <Dialog.Portal>
-        <Dialog.Content className="bg-white shadow-xl text-sm overflow-hidden w-[380px] z-20 fixed top-0 left-[240px] bottom-0">
+        <Dialog.Content className="bg-white shadow-xl border-r text-sm overflow-hidden w-[380px] z-20 fixed top-0 left-[240px] bottom-0">
           <ClientSideSuspense fallback={<Loading />}>
             <Inbox />
           </ClientSideSuspense>
@@ -73,19 +58,21 @@ function Inbox() {
 
   return (
     <div className="flex flex-col h-full overflow-hidden">
-      <div className="flex p-3 border-b border-border justify-end bg-muted/50 gap-1.5 flex-0">
+      <div className="flex py-1.5 px-3 border-b border-border justify-end gap-1.5 flex-0">
         <button
-          className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-8 px-4 py-2"
+          className="inline-flex gap-1.5 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 h-8 px-2 hover:bg-gray-100 text-gray-600 hover:text-gray-800"
           disabled={inboxNotifications.length === 0}
           onClick={markAllNotificationsAsRead}
         >
+          <MailReadIcon className="w-4 h-4 opacity-70" />
           Mark all as read
         </button>
         <button
-          className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-red-600 text-primary-foreground shadow hover:bg-primary/90 h-8 px-4 py-2"
+          className="inline-flex gap-1.5 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 h-8 px-2 hover:bg-gray-100 text-red-600 hover:text-red-700"
           disabled={inboxNotifications.length === 0}
           onClick={deleteAllNotifications}
         >
+          <MailDeleteIcon className="w-4 h-4 opacity-70" />
           Delete all
         </button>
       </div>
