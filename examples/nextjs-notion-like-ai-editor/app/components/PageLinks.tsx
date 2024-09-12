161abc82b2624c2107bc48df7e5c19a3d4e97d71
Chris Nicholas <ctnicholasdev@gmail.com>
2024-09-12 15:35:29 +0100

    feat: Better skeleton

--- examples/nextjs-notion-like-ai-editor/app/components/PageLinks.tsx
+++ examples/nextjs-notion-like-ai-editor/app/components/PageLinks.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import { TypedRoomDataWithInfo } from "../utils/liveblocks";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import useSWRInfinite from "swr/infinite";
 import { getRoomsAndInfo } from "../actions/liveblocks";
-import { useParams } from "next/navigation";
-import { useState } from "react";
+import { TypedRoomDataWithInfo } from "../utils/liveblocks";
 
 const PAGE_LOAD_COUNT = 10;
 const NO_CURSOR = Symbol();
 
 // Infinitely load all pages
 export function PageLinks() {
-  const params = useParams();
+  const pathname = usePathname();
   const [reachedEnd, setReachedEnd] = useState(false);
 
   // Fetch all pages for sidebar
@@ -20,7 +20,7 @@ export function PageLinks() {
     (pageIndex, previousPageData) => {
       // First set of pages requires no cursor
       if (pageIndex === 0 || !previousPageData) {
-        return NO_CURSOR; // Symbol because `undefined` halts function
+        return NO_CURSOR; // Symbol because falsy values halt fetching
       }
 
       // No cursor for next pages, reached end
@@ -62,23 +62,14 @@ export function PageLinks() {
   }
 
   if (isLoading) {
+    const skeletonLength = Array.from({ length: 6 /* PAGE_LOAD_COUNT + 1 */ });
     return (
-      <div className="flex flex-col gap-0.5 px-5 py-3">
-        <div className="h-8 flex items-center w-full">
-          <div className="bg-gray-200/60 h-5 w-40 rounded animate-pulse" />
-        </div>
-        <div className="h-8 flex items-center w-full">
-          <div className="bg-gray-200/60 h-5 w-40 rounded animate-pulse" />
-        </div>
-        <div className="h-8 flex items-center w-full">
-          <div className="bg-gray-200/60 h-5 w-40 rounded animate-pulse" />
-        </div>
-        <div className="h-8 flex items-center w-full">
-          <div className="bg-gray-200/60 h-5 w-40 rounded animate-pulse" />
-        </div>
-        <div className="h-8 flex items-center w-full">
-          <div className="bg-gray-200/60 h-5 w-40 rounded animate-pulse" />
-        </div>
+      <div className="flex flex-col gap-px px-5 py-3">
+        {skeletonLength.map((_, index) => (
+          <div key={index} className="h-8 flex items-center w-full">
+            <div className="bg-gray-200/60 h-5 w-40 max-w-full rounded animate-pulse" />
+          </div>
+        ))}
       </div>
     );
   }
@@ -98,7 +89,7 @@ export function PageLinks() {
           <PageLink
             key={room.id}
             room={room}
-            active={params.pageId === room.metadata.pageId}
+            active={pathname === `/${room.metadata.pageId}`}
           />
         ))
       )}
@@ -107,7 +98,7 @@ export function PageLinks() {
         <button
           onClick={() => setSize(size + 1)}
           disabled={isLoadingMore}
-          className="text-center py-1.5 px-3 bg-gray-200/60 transition-colors rounded text-medium text-gray-700 hover:text-gray-900 pr-2 text-sm font-medium data-[active]:bg-gray-200/80 data-[active]:text-gray-900 disabled:opacity-60"
+          className="text-center py-1.5 px-3 bg-gray-200/60 transition-colors rounded text-medium text-gray-700 hover:text-gray-900 pr-2 text-sm font-medium data-[active]:bg-gray-200/80 data-[active]:text-gray-900 disabled:opacity-70 disabled:aniamte-pulse"
         >
           {isLoadingMore ? "Loading…" : "Load more"}
         </button>
