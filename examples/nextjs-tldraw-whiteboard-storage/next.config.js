849bcca198128f50b01b63a580df938f374f2850
Chris Nicholas <ctnicholasdev@gmail.com>
2024-09-17 12:24:19 +0100

    feat: Type config

--- examples/nextjs-tldraw-whiteboard-storage/next.config.js
+++ examples/nextjs-tldraw-whiteboard-storage/next.config.js
@@ -1,11 +1,15 @@
+// @ts-check
 const path = require("path");
 
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      // Ensure that all imports of 'yjs' resolve to the same instance
+      // Ensure that all imports of `yjs` resolve to the same instance
       config.resolve.alias["yjs"] = path.resolve(__dirname, "node_modules/yjs");
     }
     return config;
   },
 };
+
+module.exports = nextConfig;
