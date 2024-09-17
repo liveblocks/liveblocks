2e7b3d983b37c2dbb408fab85b01b29929ce37df
Chris Nicholas <ctnicholasdev@gmail.com>
2024-09-17 12:22:19 +0100

    feat: Fix warning

---
+++ examples/nextjs-tldraw-whiteboard-yjs/next.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Ensure that all imports of 'yjs' resolve to the same instance
+      config.resolve.alias["yjs"] = path.resolve(__dirname, "node_modules/yjs");
+    }
+    return config;
+  },
+};
