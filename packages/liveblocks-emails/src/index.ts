1ff9b3334e7f3a7542328f6a5a05bee343b5859f
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-09-20 13:43:56 +0200

    chore: add internal functions

--- packages/liveblocks-emails/src/index.ts
+++ packages/liveblocks-emails/src/index.ts
@@ -1,5 +1,50 @@
 /* eslint-disable simple-import-sort/exports */
-import { detectDupes } from "@liveblocks/core";
+import type { CommentData, InboxNotificationData } from "@liveblocks/core";
+import { detectDupes, getMentionedIdsFromCommentBody } from "@liveblocks/core";
 
+import type { CommentDataWithBody } from "./comment-with-body";
+import { filterCommentsWithBody } from "./comment-with-body";
 import { PKG_FORMAT, PKG_NAME, PKG_VERSION } from "./version";
+
 detectDupes(PKG_NAME, PKG_VERSION, PKG_FORMAT);
+
+/** @internal */
+export const getUnreadComments = ({
+  comments,
+  inboxNotification,
+  userId,
+}: {
+  comments: CommentData[];
+  inboxNotification: InboxNotificationData;
+  userId: string;
+}): CommentDataWithBody[] => {
+  const commentsWithBody = filterCommentsWithBody(comments);
+  const readAt = inboxNotification.readAt;
+
+  return commentsWithBody
+    .filter((c) => c.userId !== userId)
+    .filter((c) =>
+      readAt
+        ? c.createdAt > readAt && c.createdAt <= inboxNotification.notifiedAt
+        : c.createdAt <= inboxNotification.notifiedAt
+    );
+};
+
+/** @internal */
+export const getLastCommentWithMention = ({
+  comments,
+  mentionedUserId,
+}: {
+  comments: CommentDataWithBody[];
+  mentionedUserId: string;
+}): CommentDataWithBody | null => {
+  return (
+    Array.from(comments)
+      .reverse()
+      .filter((c) => c.userId !== mentionedUserId)
+      .find((c) => {
+        const mentionedUserIds = getMentionedIdsFromCommentBody(c.body);
+        return mentionedUserIds.includes(mentionedUserId);
+      }) ?? null
+  );
+};
