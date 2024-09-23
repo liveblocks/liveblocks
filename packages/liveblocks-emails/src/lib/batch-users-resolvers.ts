e3c399ef81bd3df7f87a286eecb38763b3e6133e
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-09-23 16:36:15 +0200

    chore: add first draft od batch users resolver

--- packages/liveblocks-emails/src/lib/batch-users-resolvers.ts
+++ packages/liveblocks-emails/src/lib/batch-users-resolvers.ts
@@ -1,11 +1,13 @@
-import type {
-  BaseUserMeta,
-  DU,
-  OptionalPromise,
-  ResolveUsersArgs,
+import {
+  type BaseUserMeta,
+  type DU,
+  type OptionalPromise,
+  type ResolveUsersArgs,
+  stringify,
 } from "@liveblocks/core";
 
 /**
+ * @internal
  * Emit a warning only once if a condition is met, in development only.
  */
 const createDevelopmentWarning = (
@@ -30,38 +32,98 @@ const createDevelopmentWarning = (
   }
 };
 
-const getCacheKey = (): string => "";
+const getMapKey = (args: ResolveUsersArgs): string => stringify(args);
 
-type ResolveUserFunction<U extends BaseUserMeta> = (
+type ResolveUserOptionalPromise<U extends BaseUserMeta> = (
   args: ResolveUsersArgs
 ) => OptionalPromise<(U["info"] | undefined)[] | undefined>;
 
-class BatchUsersResolver<U extends BaseUserMeta> {
-  private resolveUsers: ResolveUserFunction<U> | undefined;
-  private resolvers: Map<string, ResolveUserFunction<U>>;
+type BatchUsersResolverCall<U extends BaseUserMeta> = {
+  args: ResolveUsersArgs;
+  promise: OptionalPromise<(U["info"] | undefined)[] | undefined>;
+  resolve: (
+    value: OptionalPromise<(U["info"] | undefined)[] | undefined>
+  ) => void;
+};
 
-  constructor(resolveUsers: ResolveUserFunction<U> | undefined) {
+class BatchUsersResolver<U extends BaseUserMeta> {
+  private resolveUsers: ResolveUserOptionalPromise<U> | undefined;
+  private resolveUsersPromises: Map<string, BatchUsersResolverCall<U>>;
+
+  constructor(resolveUsers: ResolveUserOptionalPromise<U> | undefined) {
     this.resolveUsers = resolveUsers;
-    this.resolvers = new Map();
+    this.resolveUsersPromises = new Map();
   }
 
-  async resolve(): Promise<void> {}
+  register = (
+    args: ResolveUsersArgs
+  ): OptionalPromise<(U["info"] | undefined)[] | undefined> => {
+    const key = getMapKey(args);
+    const existingPromise = this.resolveUsersPromises.get(key);
+    if (existingPromise) {
+      return existingPromise.promise;
+    }
+
+    let resolveFn: (
+      value: OptionalPromise<(U["info"] | undefined)[] | undefined>
+    ) => void = (): void => {};
+
+    const promise = new Promise<(U["info"] | undefined)[] | undefined>(
+      (resolve) => {
+        resolveFn = resolve;
+      }
+    );
+
+    this.resolveUsersPromises.set(key, { args, promise, resolve: resolveFn });
+
+    return promise;
+  };
+
+  async resolve(): Promise<void> {
+    const userIds = Array.from(
+      new Set(
+        Array.from(this.resolveUsersPromises.values()).flatMap(
+          (entry) => entry.args.userIds
+        )
+      )
+    );
+
+    const users = await this.resolveUsers?.({ userIds });
+
+    const usersMap = new Map<string, U["info"] | undefined>();
+    for (let i = 0; i < userIds.length; i++) {
+      const userId = userIds[i];
+      if (userId) {
+        usersMap.set(userId, users?.[i] ?? undefined);
+      }
+    }
+
+    for (const { args, resolve } of this.resolveUsersPromises.values()) {
+      const resolvedUsers = args.userIds.map((userId) => usersMap.get(userId));
+      resolve(resolvedUsers);
+    }
+  }
 }
 
 export type CreateBatchUsersResolverReturnType<U extends BaseUserMeta> = {
+  registerResolveUsers: (
+    args: ResolveUsersArgs
+  ) => OptionalPromise<(U["info"] | undefined)[] | undefined>;
   resolve: () => Promise<void>;
 };
 
 export function createBatchUsersResolver<U extends BaseUserMeta = DU>({
   resolveUsers,
-  callerFnName,
+  callerName,
 }: {
-  resolveUsers?: ResolveUserFunction<U>;
-  callerFnName: string;
+  resolveUsers?: (
+    args: ResolveUsersArgs
+  ) => OptionalPromise<(U["info"] | undefined)[] | undefined>;
+  callerName: string;
 }): CreateBatchUsersResolverReturnType<U> {
   const warnIfNoResolveUsers = createDevelopmentWarning(
     () => !resolveUsers,
-    `Set "resolverUsers" in ${callerFnName} options to specify users info`
+    `Set "resolverUsers" in ${callerName} options to specify users info`
   );
   const batchUsersResolver = new BatchUsersResolver(resolveUsers);
 
@@ -75,14 +137,8 @@ export function createBatchUsersResolver<U extends BaseUserMeta = DU>({
     }
   };
 
-  return { resolve } as const;
+  return {
+    resolve,
+    registerResolveUsers: batchUsersResolver.register,
+  } as const;
 }
-
-/*
- * const batchResolver = batchResolveUsers(resolveUsers);
- * const commentBodyPromises = commentBodies.map(
- *  body => stringifyCommentBody(body, { resolveUsers: batchResolver.get })
- * );
- * batchResolver.resolve();
- * const commentBodies = await Promise.all(commentBodyPromises);
- */
