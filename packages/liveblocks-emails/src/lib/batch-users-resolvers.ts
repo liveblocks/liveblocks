9301225236103109784199f7b32d64ddc4277b82
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-09-23 17:28:26 +0200

    refactor: move warning creator in his own file

--- packages/liveblocks-emails/src/lib/batch-users-resolvers.ts
+++ packages/liveblocks-emails/src/lib/batch-users-resolvers.ts
@@ -6,31 +6,7 @@ import {
   stringify,
 } from "@liveblocks/core";
 
-/**
- * @internal
- * Emit a warning only once if a condition is met, in development only.
- */
-const createDevelopmentWarning = (
-  condition: boolean | (() => boolean),
-  ...args: Parameters<typeof console.warn>
-) => {
-  let hasWarned = false;
-
-  if (process.env.NODE_ENV !== "production") {
-    return () => {
-      if (
-        !hasWarned &&
-        (typeof condition === "function" ? condition() : condition)
-      ) {
-        console.warn(...args);
-
-        hasWarned = true;
-      }
-    };
-  } else {
-    return () => {};
-  }
-};
+import { createDevelopmentWarning } from "./warning";
 
 const getMapKey = (args: ResolveUsersArgs): string => stringify(args);
 
