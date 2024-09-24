17831fa197293e9d708e9ec928763d312b05d763
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-09-24 09:27:26 +0200

    tests: add first test for batch users resolver

--- packages/liveblocks-emails/src/lib/__tests__/batch-users-resolver.test.ts
+++ packages/liveblocks-emails/src/lib/__tests__/batch-users-resolver.test.ts
@@ -1,5 +1,75 @@
+import type {
+  BaseUserMeta,
+  DU,
+  IUserInfo,
+  OptionalPromise,
+  ResolveUsersArgs,
+} from "@liveblocks/core";
+
+import { createBatchUsersResolver } from "../batch-users-resolver";
+
+const USERS_DB: IUserInfo[] = [
+  {
+    id: "user-0",
+    name: "Charlie Layne",
+  },
+  {
+    id: "user-1",
+    name: "Mislav Abha",
+  },
+  {
+    id: "user-2",
+    name: "Tatum Paolo",
+  },
+  {
+    id: "user-3",
+    name: "Anjali Wanda",
+  },
+];
+
 describe("batch users resolve", () => {
-  it("dummy", () => {
-    expect(2 + 2).toBe(4);
+  let resolveUsersMock: jest.Mock;
+  beforeEach(() => {
+    resolveUsersMock = jest.fn(
+      <U extends BaseUserMeta = DU>({
+        userIds,
+      }: ResolveUsersArgs): OptionalPromise<
+        (U["info"] | undefined)[] | undefined
+      > => {
+        const users: (U["info"] | undefined)[] = [];
+
+        for (const userId of userIds) {
+          const user = USERS_DB.find((u) => u.id === userId);
+          if (user) {
+            users.push(user);
+          }
+        }
+
+        return users;
+      }
+    );
+  });
+
+  it("should register and resolve users info", async () => {
+    const batchUsersResolver = createBatchUsersResolver<BaseUserMeta>({
+      resolveUsers: resolveUsersMock,
+      callerName: "test-suite",
+    });
+    const userIds = ["user-0", "user-1", "user-2"];
+    const registeredPromises = userIds.map((id) =>
+      batchUsersResolver.registerResolveUsers({ userIds: [id] })
+    );
+
+    await batchUsersResolver.resolve();
+
+    const results = await Promise.all(registeredPromises);
+
+    expect(results).toEqual([
+      [{ id: "user-0", name: "Charlie Layne" }],
+      [{ id: "user-1", name: "Mislav Abha" }],
+      [{ id: "user-2", name: "Tatum Paolo" }],
+    ]);
+    expect(resolveUsersMock).toHaveBeenCalledTimes(1);
+    expect(resolveUsersMock).toHaveBeenCalledWith({ userIds });
   });
 });
