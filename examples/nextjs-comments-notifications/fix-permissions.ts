51e56ad366b29c705e57df6ab1ea58e951e7f81c
Vincent Driessen <me@nvie.com>
2024-10-03 11:59:58 +0200

    Add fix permissions script to example

---
+++ examples/nextjs-comments-notifications/fix-permissions.ts
@@ -0,0 +1,29 @@
+import { Liveblocks } from "@liveblocks/node";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const liveblocks = new Liveblocks({
+  // @ts-expect-error - dev
+  baseUrl: "https://dev.dev-liveblocks5948.workers.dev",
+  secret: process.env.LIVEBLOCKS_SECRET_KEY!,
+});
+
+const rooms = [
+  "liveblocks:examples:nextjs-comments-notifications:design",
+  "liveblocks:examples:nextjs-comments-notifications:general-pagination",
+  "liveblocks:examples:nextjs-comments-notifications:design-pagination",
+  "liveblocks:examples:nextjs-comments-notifications:engineering-pagination",
+];
+
+await Promise.all(
+  rooms.map((room) =>
+    liveblocks.updateRoom(room, {
+      defaultAccesses: [],
+      usersAccesses: {
+        "user-0": ["room:write"],
+        "user-1": ["room:write"],
+      },
+    })
+  )
+);
