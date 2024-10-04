f99794d60d1c1fd1f4739390231658fa441c5f99
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-10-04 17:34:27 +0200

    chore: create email root component

--- examples/nextjs-comments-emails/emails/UnreadReplies.tsx
+++ examples/nextjs-comments-emails/emails/UnreadReplies.tsx
@@ -1,19 +1,14 @@
 import {
-  Body,
   Button,
   Column,
-  Container,
-  Head,
   Heading,
-  Html,
   Img,
-  Preview,
   Row,
   Section,
-  Tailwind,
   Text,
 } from "@react-email/components";
 import type { CommentEmailAsReactData } from "@liveblocks/emails";
+import { EmailRoot } from "./_components/email-root";
 import { getProps } from "./_utils/getProps";
 
 type RoomInfo = {
@@ -105,78 +100,70 @@ export default function Email(props: EmailProps) {
   const { company, roomInfo, comments } = getProps(props, previewProps);
   const previewText = getPreviewText(comments, roomInfo);
   return (
-    <Html>
-      <Head />
-      <Preview>{previewText}</Preview>
-      <Tailwind>
-        <Body className="bg-white my-auto mx-auto font-sans">
-          <Container className="py-8">
+    <EmailRoot preview={previewText}>
+      <Row>
+        <Column className="w-[32px]">
+          <div className="w-[24px] h-[24px] bg-black rounded-lg flex items-center justify-center">
+            <div className="w-[10px] h-[10px] rounded-full bg-white" />
+          </div>
+        </Column>
+        <Column>
+          <Heading as="h1" className="m-0">
+            {company.name}
+          </Heading>
+        </Column>
+      </Row>
+      <Section className="my-12">
+        <Text className="text-sm font-medium">{previewText}</Text>
+        {comments.map((comment) => (
+          <Section
+            key={comment.id}
+            className="my-4 rounded-md py-4 pl-4 pr-8"
+            // NOTE: `react-email` do not interpret correctly borders
+            // in `className` attribute w/ `tailwindcss`.
+            style={{ border: "solid 1px rgba(23, 23, 23, 0.10)" }}
+          >
             <Row>
-              <Column className="w-[32px]">
-                <div className="w-[24px] h-[24px] bg-black rounded-lg flex items-center justify-center">
-                  <div className="w-[10px] h-[10px] rounded-full bg-white" />
-                </div>
+              <Column className="w-[34px]">
+                <Img
+                  className="rounded-full bg-[hsla(0, 0%, 93%, 1)]"
+                  width={28}
+                  height={28}
+                  src={comment.author.info.avatar}
+                />
               </Column>
               <Column>
-                <Heading as="h1" className="m-0">
-                  {company.name}
-                </Heading>
+                {comment.author.info.name}{" "}
+                <span className="text-[rgba(23, 23, 23, 0.6)] text-xs">
+                  {comment.createdAt.toLocaleDateString()}
+                </span>
               </Column>
             </Row>
-            <Section className="my-12">
-              <Text className="text-sm font-medium">{previewText}</Text>
-              {comments.map((comment) => (
-                <Section
-                  key={comment.id}
-                  className="my-4 rounded-md py-4 pl-4 pr-8"
-                  // NOTE: `react-email` do not interpret correctly borders
-                  // in `className` attribute w/ `tailwindcss`.
-                  style={{ border: "solid 1px rgba(23, 23, 23, 0.10)" }}
+            <Row className="mt-1">
+              <Column className="w-[34px]" />
+              <Column>
+                {comment.reactBody}
+                <Button
+                  className="bg-[#171717] rounded-md px-4 h-[36px] text-white text-sm font-medium flex justify-center w-max flex-col"
+                  href={comment.url}
                 >
-                  <Row>
-                    <Column className="w-[34px]">
-                      <Img
-                        className="rounded-full bg-[hsla(0, 0%, 93%, 1)]"
-                        width={28}
-                        height={28}
-                        src={comment.author.info.avatar}
-                      />
-                    </Column>
-                    <Column>
-                      {comment.author.info.name}{" "}
-                      <span className="text-[rgba(23, 23, 23, 0.6)] text-xs">
-                        {comment.createdAt.toLocaleDateString()}
-                      </span>
-                    </Column>
-                  </Row>
-                  <Row className="mt-1">
-                    <Column className="w-[34px]" />
-                    <Column>
-                      {comment.reactBody}
-                      <Button
-                        className="bg-[#171717] rounded-md px-4 h-[36px] text-white text-sm font-medium flex justify-center w-max flex-col"
-                        href={comment.url}
-                      >
-                        View comment
-                      </Button>
-                    </Column>
-                  </Row>
-                </Section>
-              ))}
-              <Row>
-                <Column className="w-[26px]">
-                  <div className="w-[20px] h-[20px] bg-black rounded-md flex items-center justify-center">
-                    <div className="w-[8px] h-[8px] rounded-full bg-white" />
-                  </div>
-                </Column>
-                <Column>
-                  <Text className="text-xs font-medium">{company.name}</Text>
-                </Column>
-              </Row>
-            </Section>
-          </Container>
-        </Body>
-      </Tailwind>
-    </Html>
+                  View comment
+                </Button>
+              </Column>
+            </Row>
+          </Section>
+        ))}
+        <Row>
+          <Column className="w-[26px]">
+            <div className="w-[20px] h-[20px] bg-black rounded-md flex items-center justify-center">
+              <div className="w-[8px] h-[8px] rounded-full bg-white" />
+            </div>
+          </Column>
+          <Column>
+            <Text className="text-xs font-medium">{company.name}</Text>
+          </Column>
+        </Row>
+      </Section>
+    </EmailRoot>
   );
 }
---
+++ examples/nextjs-comments-emails/emails/_components/email-root.tsx
@@ -0,0 +1,28 @@
+import {
+  Body,
+  Container,
+  Head,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components";
+
+export function EmailRoot({
+  preview,
+  children,
+}: {
+  preview: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <Html>
+      <Head />
+      <Preview>{preview}</Preview>
+      <Tailwind>
+        <Body className="bg-white my-auto mx-auto font-sans">
+          <Container className="py-8">{children}</Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
