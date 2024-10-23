3a0f3a0ebf4d950ccb92897c9c0bb232b72001ba
Jonathan Rowny <jrowny@gmail.com>
2024-10-23 07:50:47 -0700

    fix type

--- packages/liveblocks-core/src/index.ts
+++ packages/liveblocks-core/src/index.ts
@@ -255,7 +255,8 @@ export type {
 export { WebsocketCloseCodes } from "./types/IWebSocket";
 export type { NodeMap, ParentToChildNodeMap } from "./types/NodeMap";
 export type { OptionalPromise } from "./types/OptionalPromise";
-export type { OthersEvent, TextEditorType } from "./types/Others";
+export type { OthersEvent } from "./types/Others";
+export { TextEditorType } from "./types/Others";
 export type { Patchable } from "./types/Patchable";
 export type {
   PlainLson,
