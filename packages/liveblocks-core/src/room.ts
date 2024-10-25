238941e646bad04fa66faf2c316851c56f847b31
Vincent Driessen <me@nvie.com>
2024-10-25 13:28:34 +0200

    Make signal optional again
    I briefly overlooked that these are public APIs.

--- packages/liveblocks-core/src/room.ts
+++ packages/liveblocks-core/src/room.ts
@@ -482,7 +482,7 @@ export type GetThreadsOptions<M extends BaseMetadata> = {
 
 export type GetThreadsSinceOptions = {
   since: Date;
-  signal: AbortSignal;
+  signal?: AbortSignal;
 };
 
 export type UploadAttachmentOptions = {
@@ -491,7 +491,7 @@ export type UploadAttachmentOptions = {
 
 type ListTextVersionsSinceOptions = {
   since: Date;
-  signal: AbortSignal;
+  signal?: AbortSignal;
 };
 
 /**
