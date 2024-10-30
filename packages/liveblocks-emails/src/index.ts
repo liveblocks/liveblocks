aafb63d859ee4bd38df7870a0f2841af46744c75
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-10-30 11:16:17 +0100

    Add exports

--- packages/liveblocks-emails/src/index.ts
+++ packages/liveblocks-emails/src/index.ts
@@ -14,10 +14,23 @@ export type {
   ConvertCommentBodyAsReactComponents,
 } from "./comment-body";
 export type {
+  ConvertLiveblocksTextEditorNodesAsReactComponents,
+  LiveblocksTextEditorContainerComponentProps,
+  LiveblocksTextEditorMentionComponentProps,
+  LiveblocksTextEditorTextComponentProps,
+} from "./liveblocks-text-editor";
+export type {
+  MentionEmailAsReact,
+  PrepareTextMentionNotificationEmailAsReactOptions,
+  TextMentionNotificationEmailDataAsReact,
+} from "./text-mention-notification";
+export { prepareTextMentionNotificationEmailAsReact } from "./text-mention-notification";
+export type {
   CommentEmailAsHtmlData,
   CommentEmailAsReactData,
   PrepareThreadNotificationEmailAsHtmlOptions,
   PrepareThreadNotificationEmailAsReactOptions,
+  // TODO: create a common shared type once thread notification are publicly released.
   ResolveRoomInfoArgs,
   ThreadNotificationEmailDataAsHtml,
   ThreadNotificationEmailDataAsReact,
