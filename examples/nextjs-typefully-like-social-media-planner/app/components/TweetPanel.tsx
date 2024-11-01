763fe097f6478f4936629976c9381e9f40068a80
Chris Nicholas <ctnicholasdev@gmail.com>
2024-11-01 11:31:35 +0000

    feat: Better

--- examples/nextjs-typefully-like-social-media-planner/app/components/TweetPanel.tsx
+++ examples/nextjs-typefully-like-social-media-planner/app/components/TweetPanel.tsx
@@ -11,13 +11,15 @@ import StarterKit from "@tiptap/starter-kit";
 import { LengthenIcon } from "../icons/LengthenIcon";
 import { CommentIcon } from "../icons/CommentIcon";
 import { useThreads } from "@liveblocks/react/suspense";
+import { ClientSideSuspense } from "@liveblocks/react";
+
 export function TweetPanel() {
   return (
-    <div>
+    <ClientSideSuspense fallback={null}>
       <Tweet />
       <Tweet />
       <Tweet />
-    </div>
+    </ClientSideSuspense>
   );
 }
 
@@ -31,17 +33,17 @@ function Tweet() {
   });
 
   return (
-    <div className="flex gap-2 group">
+    <div className="flex gap-2 group w-full">
       <div className="flex flex-col items-center gap-1 pb-1">
-        <div className="bg-gray-200 w-10 h-10 rounded-full overflow-hidden flex-shrink-0">
+        <div className="bg-gray-200 w-10 h-10 rounded-full overflow-hidden shrink-0">
           <img
             className="h-full w-full"
             src="https://github.com/ctnicholas.png"
           />
         </div>
-        <div className="w-[3px] bg-gray-200/80 h-full flex-grow group-last:hidden"></div>
+        <div className="w-[3px] bg-gray-200/80 h-full flex-grow group-last:hidden flex-1" />
       </div>
-      <div className="flex-1">
+      <div className="grow">
         <div className="flex gap-1 whitespace-nowrap">
           <span className="font-semibold">Chris Nicholas</span>
           <span className="text-gray-500">@ctnicholasdev</span>
@@ -52,13 +54,14 @@ function Tweet() {
             <EditorContent editor={editor} className="!outline-none" />
           </div>
 
-          {editor?.isFocused ? (
-            <div className="absolute bottom-2 left-0 right-0 flex justify-end">
-              <button>
-                <LengthenIcon className="w-5 h-5 text-gray-500 hover:text-gray-700 transition-colors" />
-              </button>
-            </div>
-          ) : null}
+          <div
+            className="absolute bottom-2 left-0 right-0 justify-end hidden group-hover:flex"
+            style={{ display: !editor?.isFocused ? "hidden" : undefined }}
+          >
+            <button>
+              <LengthenIcon className="w-5 h-5 text-gray-500 hover:text-gray-700 transition-colors" />
+            </button>
+          </div>
         </div>
       </div>
       <BubbleMenu editor={editor}>
@@ -67,7 +70,15 @@ function Tweet() {
         </button>
       </BubbleMenu>
       <FloatingComposer editor={editor} style={{ width: "350px" }} />
-      <div className="w-[300px]">
+      <div
+        className="w-[300px]"
+        style={{
+          display:
+            threads.length > 0 && threads.some((thread) => !thread.resolved)
+              ? "block"
+              : "none",
+        }}
+      >
         <AnchoredThreads threads={threads} editor={editor} />
       </div>
     </div>
--- examples/nextjs-typefully-like-social-media-planner/next.config.js
+++ examples/nextjs-typefully-like-social-media-planner/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ["yjs"],
 };
 
 module.exports = nextConfig;
