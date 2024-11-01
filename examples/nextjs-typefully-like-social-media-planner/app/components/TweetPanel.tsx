90663f3fa0ff4138b1b9b4c50fe9ae5652bbe94b
Chris Nicholas <ctnicholasdev@gmail.com>
2024-11-01 01:15:40 +0000

    feat: Refactor

--- examples/nextjs-typefully-like-social-media-planner/app/components/TweetPanel.tsx
+++ examples/nextjs-typefully-like-social-media-planner/app/components/TweetPanel.tsx
@@ -3,11 +3,14 @@
 import {
   useLiveblocksExtension,
   FloatingComposer,
+  AnchoredThreads,
 } from "@liveblocks/react-tiptap";
-import { useEditor, EditorContent } from "@tiptap/react";
+import { useEditor, EditorContent, BubbleMenu } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import { useState } from "react";
+
 import { LengthenIcon } from "../icons/LengthenIcon";
+import { CommentIcon } from "../icons/CommentIcon";
+import { useThreads } from "@liveblocks/react/suspense";
 export function TweetPanel() {
   return (
     <div>
@@ -19,7 +22,13 @@ export function TweetPanel() {
 }
 
 function Tweet() {
-  const [focused, setFocused] = useState(false);
+  const liveblocks = useLiveblocksExtension();
+  const { threads } = useThreads();
+
+  const editor = useEditor({
+    extensions: [liveblocks, StarterKit],
+    immediatelyRender: false,
+  });
 
   return (
     <div className="flex gap-2 group">
@@ -37,10 +46,14 @@ function Tweet() {
           <span className="font-semibold">Chris Nicholas</span>
           <span className="text-gray-500">@ctnicholasdev</span>
         </div>
-        <div className="relative pb-6">
-          <Editor focused={focused} setFocused={setFocused} />
-          {focused ? (
-            <div className="absolute bottom-0 left-0 right-0 flex justify-end">
+
+        <div className="relative pb-8">
+          <div>
+            <EditorContent editor={editor} className="!outline-none" />
+          </div>
+
+          {editor?.isFocused ? (
+            <div className="absolute bottom-2 left-0 right-0 flex justify-end">
               <button>
                 <LengthenIcon className="w-5 h-5 text-gray-500 hover:text-gray-700 transition-colors" />
               </button>
@@ -48,40 +61,15 @@ function Tweet() {
           ) : null}
         </div>
       </div>
-    </div>
-  );
-}
-
-function Editor({
-  focused,
-  setFocused,
-}: {
-  focused: boolean;
-  setFocused: (isFocused: boolean) => void;
-}) {
-  const liveblocks = useLiveblocksExtension();
-
-  const editor = useEditor({
-    extensions: [
-      liveblocks.configure({
-        // ...
-      }),
-      StarterKit,
-    ],
-    immediatelyRender: false,
-    onFocus({ editor, event }) {
-      setFocused(true);
-    },
-    onBlur({ editor, event }) {
-      setFocused(false);
-    },
-  });
-
-  return (
-    <div>
-      <EditorContent editor={editor} className="!outline-none" />
-      <div></div>
+      <BubbleMenu editor={editor}>
+        <button onClick={() => editor?.chain().focus().addPendingComment()}>
+          <CommentIcon />
+        </button>
+      </BubbleMenu>
       <FloatingComposer editor={editor} style={{ width: "350px" }} />
+      <div className="w-[300px]">
+        <AnchoredThreads threads={threads} editor={editor} />
+      </div>
     </div>
   );
 }
