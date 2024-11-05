ca26b72e309d22e73d28981f080031fc22174a50
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-11-05 14:00:21 +0100

    Complete TipTap interfaces based on real data

--- packages/liveblocks-emails/src/tiptap-editor.ts
+++ packages/liveblocks-emails/src/tiptap-editor.ts
@@ -1,18 +1,79 @@
-export interface SerializedTipTapNode {
+export interface SerializedTipTapBaseNode {
   type: string;
-  content?: Array<SerializedTipTapNode>;
-  text?: string;
-  attrs?: Record<string, string>;
-  marks?: Array<{ type: string; attrs?: Record<string, string> }>;
+  content?: Array<SerializedTipTapBaseNode>;
 }
 
-export interface SerializedTipTapTextNode extends SerializedTipTapNode {
-  type: "text";
-  // TODO: add format in attrs
+export interface SerializedTipTapParagraphBreakNode
+  extends SerializedTipTapBaseNode {
+  type: "paragraph";
+  content: Array<SerializedTipTapBaseNode>;
 }
-export interface SerializedTipTapMentionNode extends SerializedTipTapNode {
+
+export interface SerializedTipTapLineBreakNode
+  extends Omit<SerializedTipTapBaseNode, "content"> {
+  type: "paragraph";
+}
+
+export interface SerializedTipTapBaseMark {
+  type: string;
+  attrs: Record<string, string>;
+}
+
+export interface SerializedTipTapBoldMark extends SerializedTipTapBaseMark {
+  type: "bold";
+}
+
+export interface SerializedTipTapItalicMark extends SerializedTipTapBaseMark {
+  type: "italic";
+}
+
+export interface SerializedTipTapStrikethroughMark
+  extends SerializedTipTapBaseMark {
+  type: "strike";
+}
+
+export interface SerializedTipTapStrikethroughMark
+  extends SerializedTipTapBaseMark {
+  type: "strike";
+}
+
+export interface SerializedTipTapCommentMark extends SerializedTipTapBaseMark {
+  type: "liveblocksCommentMark";
+  attrs: {
+    threadId: string;
+  };
+}
+
+export type SerializedTipTapMark =
+  | SerializedTipTapBoldMark
+  | SerializedTipTapItalicMark
+  | SerializedTipTapStrikethroughMark;
+
+export interface SerializedTipTapTextNode extends SerializedTipTapBaseNode {
+  type: "text";
+  text: string;
+  marks?: Array<SerializedTipTapMark>;
+}
+
+export interface SerializedTipTapMentionNode extends SerializedTipTapBaseNode {
   type: "liveblocksMention";
   attrs: {
+    userId: string;
     notificationId: string;
   };
+}
+
+export type SerializedTipTapNode =
+  | SerializedTipTapParagraphBreakNode
+  | SerializedTipTapLineBreakNode
+  | SerializedTipTapMentionNode;
+
+export type SerializedTipTapRootNodeContent = Array<
+  Readonly<SerializedTipTapNode>
+>;
+
+export interface SerializedTipTapRootNode
+  extends Readonly<SerializedTipTapBaseNode> {
+  readonly type: "doc";
+  readonly content: SerializedTipTapRootNodeContent;
 }
