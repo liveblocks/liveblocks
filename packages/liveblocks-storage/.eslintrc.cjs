0e993c054872ad31637021974c3475357b27173e
Vincent Driessen <me@nvie.com>
2024-11-08 13:48:34 +0100

    Initial project scaffolding for new package

---
+++ packages/liveblocks-storage/.eslintrc.cjs
@@ -0,0 +1,17 @@
+const rulesDirPlugin = require("eslint-plugin-rulesdir");
+rulesDirPlugin.RULES_DIR = "./rules";
+
+module.exports = {
+  root: true,
+  plugins: ["eslint-plugin-rulesdir"],
+  extends: ["@liveblocks/eslint-config"],
+  // rules: {},
+  // overrides: [
+  //   {
+  //     files: ["test/**"],
+  //
+  //     // Special config for test files
+  //     rules: {},
+  //   },
+  // ],
+};
---
+++ packages/liveblocks-storage/test/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from "vitest";
+import { xxx } from "~/index.js";
+
+describe("storage", () => {
+  test("xxx", () => {
+    expect(xxx).toEqual("hi");
+  });
+});
