62e4b86fae48608888c8700ef39885a5b5a16811
Vincent Driessen <me@nvie.com>
2024-11-12 00:01:09 +0100

    Implement pending Ops list, make basic e2e test pass

--- packages/liveblocks-storage/src/types.ts
+++ packages/liveblocks-storage/src/types.ts
@@ -1,3 +1,8 @@
+declare const brand: unique symbol;
+export type Brand<T, TBrand extends string> = T & { [brand]: TBrand };
+
+export type OpId = Brand<string, "OpId">;
+
 export type OmitFirstArg<F> = F extends (
   first: any,
   ...args: infer A
