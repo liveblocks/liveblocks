2b75acc141bcef8eeb82c11c0c7aa44258b84140
Vincent Driessen <me@nvie.com>
2024-11-12 00:26:31 +0100

    Add test with randomization

--- packages/liveblocks-storage/src/utils.ts
+++ packages/liveblocks-storage/src/utils.ts
@@ -12,7 +12,7 @@ export function* chain<T>(
 
 // Inlined version of 3.3.7 of nanoid.js
 // https://www.npmjs.com/package/nanoid/v/3.3.7?activeTab=code
-const nanoid = (t = 21): string =>
+export const nanoid = (t = 21): string =>
   crypto
     .getRandomValues(new Uint8Array(t))
     .reduce(
