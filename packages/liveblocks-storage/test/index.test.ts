c29eafe5941128ccbf9c812b3378fa73dc740805
Vincent Driessen <me@nvie.com>
2024-11-11 16:25:50 +0100

    Pairing session 2

--- packages/liveblocks-storage/test/index.test.ts
+++ packages/liveblocks-storage/test/index.test.ts
@@ -64,6 +64,7 @@ describe("StoreStub", () => {
     const txn = stub.startTransaction();
     txn.delete("xyz");
     txn.delete("foo");
+    txn.set("abc", 42);
     txn.set("pqr", 3);
 
     expect(size(stub)).toEqual(2);
@@ -71,10 +72,10 @@ describe("StoreStub", () => {
 
     // Original stub is unaffected
     expect(fmt(stub)).toEqual({ abc: 1, xyz: 2 });
-    expect(fmt(txn)).toEqual({ abc: 1, pqr: 3 });
+    expect(fmt(txn)).toEqual({ abc: 42, pqr: 3 });
     txn.commit();
 
-    expect(fmt(stub)).toEqual({ abc: 1, pqr: 3 });
+    expect(fmt(stub)).toEqual({ abc: 42, pqr: 3 });
   });
 });
 
@@ -106,6 +107,81 @@ describe("Client", () => {
     } catch {}
     client.mutate.dupe("a", "c");
     expect(fmt(client)).toEqual({ a: 1, b: 3, c: 1 });
+  });
+
+  test.skip("basic random test (re-executing pending mutation should lead to new random key every time!)", () => {
+    const client1 = new Client(mutations);
+    const server = new Server(mutations);
+
+    // Client A:
+    // - Main cache: {}
+    // - Pending cache: { "a": 0.1223232 }
+    // - Pending ops: [ ["putRandom", "a"] ]
+
+    // Server:
+    // - Cache: { "b": 1 }
+
+    // Client A:
+    // - Main cache: {}
+    // - Pending cache: { "a": 0.1223232 }
+    // - Pending ops: [ ["putRandom", "a"] ]
+
+    // Server:
+    // - Cache: { "a": 0.12897239 }
+
+    // Client A:
+    // - Main cache: { "a": 0.12897239 }
+    // - Pending cache: { }
+    // - Pending ops: [ ]
+
+    client1.mutate.putRandom("a");
+
+    expect(fmt(client1)).toEqual({ a: 1 });
+    expect(fmt(server)).toEqual({});
+
+    // Simulate client A op reaching server first
+    const delta1 = server.applyOp(["put", ["a", 1]]);
+
+    // Apply the delta on both clients
+    client1.applyDelta(delta1);
+
+    expect(fmt(client1)).toEqual({ a: 1 });
+    expect(fmt(server)).toEqual({ a: 1 });
+  });
+
+  test("most basic end to end test", () => {
+    const client1 = new Client(mutations);
+    const client2 = new Client(mutations);
+    const server = new Server(mutations);
+
+    client1.mutate.put("a", 1);
+    client2.mutate.put("a", 2);
+
+    expect(fmt(client1)).toEqual({ a: 1 });
+    expect(fmt(client2)).toEqual({ a: 2 });
+    expect(fmt(server)).toEqual({});
+
+    // Simulate client A op reaching server first
+    const delta1 = server.applyOp(["put", ["a", 1]]);
+
+    // Apply the delta on both clients
+    client1.applyDelta(delta1);
+    client2.applyDelta(delta1);
+
+    expect(fmt(client1)).toEqual({ a: 1 });
+    expect(fmt(client2)).toEqual({ a: 2 });
+    expect(fmt(server)).toEqual({ a: 1 });
+
+    // Simulate client B op reaching server next
+    const delta2 = server.applyOp(["put", ["a", 2]]);
+
+    // Apply the delta on both clients
+    client1.applyDelta(delta2);
+    client2.applyDelta(delta2);
+
+    expect(fmt(client1)).toEqual({ a: 2 });
+    expect(fmt(client2)).toEqual({ a: 2 });
+    expect(fmt(server)).toEqual({ a: 2 });
   });
 });
 
