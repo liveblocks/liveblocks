3f40b3cebdbd7e8a830c0da162143f8b3bb8bbe6
Vincent Driessen <me@nvie.com>
2024-11-12 13:12:56 +0100

    Move pending ops details into Client

--- packages/liveblocks-storage/src/index.ts
+++ packages/liveblocks-storage/src/index.ts
@@ -24,30 +24,13 @@ type BoundMutations<M extends Record<string, Mutation>> = {
 
 export class Base<M extends Mutations> {
   #cache: LayeredCache;
-  #pendingOps: Op[];
   #mutations: M;
-
-  // XXX Expose `mutate` on clients only!
-  // (Currently hard because it would make pendingOps a protected field.)
-  mutate: BoundMutations<M>;
 
   constructor(mutations: M) {
     this.#mutations = mutations;
 
     this.#cache = new LayeredCache();
     this.#cache.snapshot();
-    this.#pendingOps = [];
-
-    // Bind all given mutation functions to this instance
-    this.mutate = {} as BoundMutations<M>;
-    for (const name of Object.keys(mutations)) {
-      this.mutate[name as keyof M] = ((...args: Json[]): OpId => {
-        const op: Op = [opId(), name, args];
-        this.#pendingOps.push(op);
-        const delta = this._applyOp(op);
-        return delta[0];
-      }) as any;
-    }
   }
 
   /**
@@ -76,20 +59,13 @@ export class Base<M extends Mutations> {
 
     // Acknowledge the incoming opId by removing it from the pending ops list.
     // If this opId is not found, it's from another client.
-    {
-      const index = this.#pendingOps.findIndex(([id]) => id === opId);
-      if (index >= 0) {
-        this.#pendingOps.splice(index, 1);
-      }
-    }
+    this.ack(opId);
 
     // Start a new snapshot
     stub.snapshot();
 
     // Apply all local pending ops
-    for (const pendingOp of this.#pendingOps) {
-      this._applyOp(pendingOp);
-    }
+    this.applyPendingOps();
   }
 
   /**
@@ -98,7 +74,7 @@ export class Base<M extends Mutations> {
    * successful, will return an authoritative delta, which should be sent to
    * every client.
    */
-  _applyOp(op: Op): Delta {
+  applyOp(op: Op): Delta {
     const [id, name, args] = op;
     const mutationFn = this.#mutations[name];
     if (!mutationFn) {
@@ -116,20 +92,53 @@ export class Base<M extends Mutations> {
       throw e;
     }
   }
+
+  ack(_opId: OpId): void {
+    // Ack is a no-op in the base class
+  }
+
+  applyPendingOps(): void {
+    // Applying pending ops is a no-op in the base class
+  }
 }
 
 // ----------------------------------------------------------------------------
 
 export class Client<M extends Mutations> extends Base<M> {
-  applyOp(op: Op): void {
-    super._applyOp(op);
+  #pendingOps: Op[];
+  mutate: BoundMutations<M>;
+
+  constructor(mutations: M) {
+    super(mutations);
+
+    this.#pendingOps = [];
+
+    // Bind all given mutation functions to this instance
+    this.mutate = {} as BoundMutations<M>;
+    for (const name of Object.keys(mutations)) {
+      this.mutate[name as keyof M] = ((...args: Json[]): OpId => {
+        const op: Op = [opId(), name, args];
+        this.#pendingOps.push(op);
+        const delta = this.applyOp(op);
+        return delta[0];
+      }) as any;
+    }
+  }
+
+  ack(opId: OpId): void {
+    const index = this.#pendingOps.findIndex(([id]) => id === opId);
+    if (index >= 0) {
+      this.#pendingOps.splice(index, 1);
+    }
+  }
+
+  applyPendingOps(): void {
+    for (const pendingOp of this.#pendingOps) {
+      this.applyOp(pendingOp);
+    }
   }
 }
 
 // ----------------------------------------------------------------------------
 
-export class Server<M extends Mutations> extends Base<M> {
-  applyOp(op: Op): Delta {
-    return super._applyOp(op);
-  }
-}
+export class Server<M extends Mutations> extends Base<M> {}
