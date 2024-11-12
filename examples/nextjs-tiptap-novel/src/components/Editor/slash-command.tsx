38196fa661667b62d6f4386695adbd8b239cbe24
Chris Nicholas <ctnicholasdev@gmail.com>
2024-11-12 14:50:50 +0000

    chore: Remove unusued

--- examples/nextjs-tiptap-novel/src/components/Editor/slash-command.tsx
+++ examples/nextjs-tiptap-novel/src/components/Editor/slash-command.tsx
@@ -33,7 +33,12 @@ export const suggestionItems = createSuggestionItems([
     searchTerms: ["p", "paragraph"],
     icon: <Text size={18} />,
     command: ({ editor, range }) => {
-      editor.chain().focus().deleteRange(range).toggleNode("paragraph", "paragraph").run();
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .toggleNode("paragraph", "paragraph")
+        .run();
     },
   },
   {
@@ -51,7 +56,12 @@ export const suggestionItems = createSuggestionItems([
     searchTerms: ["title", "big", "large"],
     icon: <Heading1 size={18} />,
     command: ({ editor, range }) => {
-      editor.chain().focus().deleteRange(range).setNode("heading", { level: 1 }).run();
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode("heading", { level: 1 })
+        .run();
     },
   },
   {
@@ -60,7 +70,12 @@ export const suggestionItems = createSuggestionItems([
     searchTerms: ["subtitle", "medium"],
     icon: <Heading2 size={18} />,
     command: ({ editor, range }) => {
-      editor.chain().focus().deleteRange(range).setNode("heading", { level: 2 }).run();
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode("heading", { level: 2 })
+        .run();
     },
   },
   {
@@ -69,7 +84,12 @@ export const suggestionItems = createSuggestionItems([
     searchTerms: ["subtitle", "small"],
     icon: <Heading3 size={18} />,
     command: ({ editor, range }) => {
-      editor.chain().focus().deleteRange(range).setNode("heading", { level: 3 }).run();
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode("heading", { level: 3 })
+        .run();
     },
   },
   {
@@ -96,14 +116,21 @@ export const suggestionItems = createSuggestionItems([
     searchTerms: ["blockquote"],
     icon: <TextQuote size={18} />,
     command: ({ editor, range }) =>
-      editor.chain().focus().deleteRange(range).toggleNode("paragraph", "paragraph").toggleBlockquote().run(),
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .toggleNode("paragraph", "paragraph")
+        .toggleBlockquote()
+        .run(),
   },
   {
     title: "Code",
     description: "Capture a code snippet.",
     searchTerms: ["codeblock"],
     icon: <Code size={18} />,
-    command: ({ editor, range }) => editor.chain().focus().deleteRange(range).toggleCodeBlock().run(),
+    command: ({ editor, range }) =>
+      editor.chain().focus().deleteRange(range).toggleCodeBlock().run(),
   },
   {
     title: "Image",
@@ -135,7 +162,7 @@ export const suggestionItems = createSuggestionItems([
       const videoLink = prompt("Please enter Youtube Video Link");
       //From https://regexr.com/3dj5t
       const ytregex = new RegExp(
-        /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/,
+        /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/
       );
 
       if (ytregex.test(videoLink)) {
@@ -161,7 +188,9 @@ export const suggestionItems = createSuggestionItems([
     icon: <Twitter size={18} />,
     command: ({ editor, range }) => {
       const tweetLink = prompt("Please enter Twitter Link");
-      const tweetRegex = new RegExp(/^https?:\/\/(www\.)?x\.com\/([a-zA-Z0-9_]{1,15})(\/status\/(\d+))?(\/\S*)?$/);
+      const tweetRegex = new RegExp(
+        /^https?:\/\/(www\.)?x\.com\/([a-zA-Z0-9_]{1,15})(\/status\/(\d+))?(\/\S*)?$/
+      );
 
       if (tweetRegex.test(tweetLink)) {
         editor
--- examples/nextjs-tiptap-novel/src/components/TextEditor.tsx
+++ examples/nextjs-tiptap-novel/src/components/TextEditor.tsx
@@ -1,36 +1,9 @@
 "use client";
 
-import { ClientSideSuspense, useThreads } from "@liveblocks/react/suspense";
-import {
-  FloatingComposer,
-  FloatingThreads,
-  useLiveblocksExtension,
-} from "@liveblocks/react-tiptap";
-import Highlight from "@tiptap/extension-highlight";
-import { Image } from "@tiptap/extension-image";
-import Link from "@tiptap/extension-link";
-import Placeholder from "@tiptap/extension-placeholder";
-import TaskList from "@tiptap/extension-task-list";
-import { TextAlign } from "@tiptap/extension-text-align";
-import { Typography } from "@tiptap/extension-typography";
-import Youtube from "@tiptap/extension-youtube";
-import { useEditor } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
-import { EditorView } from "prosemirror-view";
+import { ClientSideSuspense } from "@liveblocks/react/suspense";
 import { DocumentSpinner } from "@/primitives/Spinner";
-import { CustomTaskItem } from "./CustomTaskItem";
-import { SelectionMenu } from "./SelectionMenu";
-import { Toolbar } from "./Toolbar";
 import styles from "./TextEditor.module.css";
 import { Avatars } from "@/components/Avatars";
-import {
-  EditorBubble,
-  EditorBubbleItem,
-  EditorCommand,
-  EditorCommandItem,
-  EditorContent,
-  EditorRoot,
-} from "novel";
 import { AdvancedEditor } from "@/components/Editor/advanced-editor";
 import { ThemeToggle } from "@/components/ThemeToggle";
 
@@ -46,144 +19,22 @@ export function Editor() {
   return (
     <div className={styles.container}>
       <div className={styles.editorHeader}>
-        {/*{editor && <Toolbar editor={editor} />}*/}
         <ThemeToggle />
         <Avatars />
       </div>
       <div className={styles.editorPanel}>
-        {/*{editor && <SelectionMenu editor={editor} />}*/}
         <div className={styles.editorContainer}>
           <AdvancedEditor />
         </div>
-
-        {/*<FloatingComposer editor={editor} style={{ width: 350 }} />*/}
-        {/*<FloatingThreads threads={threads} editor={editor} />*/}
-      </div>
-    </div>
-  );
-}
-
-// Collaborative text editor with simple rich text and live cursors
-function EditorOld() {
-  const liveblocks = useLiveblocksExtension();
-
-  // Set up editor with plugins, and place user info into Yjs awareness and cursors
-  const editor = useEditor({
-    editorProps: {
-      attributes: {
-        // Add styles to editor element
-        class: styles.editor,
-      },
-    },
-    extensions: [
-      liveblocks,
-      StarterKit.configure({
-        blockquote: {
-          HTMLAttributes: {
-            class: "tiptap-blockquote",
-          },
-        },
-        code: {
-          HTMLAttributes: {
-            class: "tiptap-code",
-          },
-        },
-        codeBlock: {
-          languageClassPrefix: "language-",
-          HTMLAttributes: {
-            class: "tiptap-code-block",
-            spellcheck: false,
-          },
-        },
-        heading: {
-          levels: [1, 2, 3],
-          HTMLAttributes: {
-            class: "tiptap-heading",
-          },
-        },
-        // The Collaboration extension comes with its own history handling
-        history: false,
-        horizontalRule: {
-          HTMLAttributes: {
-            class: "tiptap-hr",
-          },
-        },
-        listItem: {
-          HTMLAttributes: {
-            class: "tiptap-list-item",
-          },
-        },
-        orderedList: {
-          HTMLAttributes: {
-            class: "tiptap-ordered-list",
-          },
-        },
-        paragraph: {
-          HTMLAttributes: {
-            class: "tiptap-paragraph",
-          },
-        },
-      }),
-      Highlight.configure({
-        HTMLAttributes: {
-          class: "tiptap-highlight",
-        },
-      }),
-      Image.configure({
-        HTMLAttributes: {
-          class: "tiptap-image",
-        },
-      }),
-      Link.configure({
-        HTMLAttributes: {
-          class: "tiptap-link",
-        },
-      }),
-      Placeholder.configure({
-        placeholder: "Start writing…",
-        emptyEditorClass: "tiptap-empty",
-      }),
-      CustomTaskItem,
-      TaskList.configure({
-        HTMLAttributes: {
-          class: "tiptap-task-list",
-        },
-      }),
-      TextAlign.configure({
-        types: ["heading", "paragraph"],
-      }),
-      Typography,
-      Youtube.configure({
-        modestBranding: true,
-        HTMLAttributes: {
-          class: "tiptap-youtube",
-        },
-      }),
-    ],
-  });
-
-  const { threads } = useThreads();
-
-  return (
-    <div className={styles.container}>
-      <div className={styles.editorHeader}>
-        {editor && <Toolbar editor={editor} />}
-        <Avatars />
-      </div>
-      <div className={styles.editorPanel}>
-        {editor && <SelectionMenu editor={editor} />}
-        <EditorContent editor={editor} className={styles.editorContainer} />
-        <FloatingComposer editor={editor} style={{ width: 350 }} />
-        <FloatingThreads threads={threads} editor={editor} />
       </div>
     </div>
   );
 }
 
 // Prevents a matchesNode error on hot reloading
-EditorView.prototype.updateState = function updateState(state) {
-  // @ts-ignore
-  if (!this.docView) return;
-  // @ts-ignore
-  this.updateStateInner(state, this.state.plugins != state.plugins);
-};
+// EditorView.prototype.updateState = function updateState(state) {
+//   // @ts-ignore
+//   if (!this.docView) return;
+//   // @ts-ignore
+//   this.updateStateInner(state, this.state.plugins != state.plugins);
+// };
