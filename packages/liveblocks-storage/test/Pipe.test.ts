f65de40213a68695163f3a5f68da810849de89c2
Vincent Driessen <me@nvie.com>
2024-11-14 14:22:52 +0100

    Add Pipe helper construct, for tests

---
+++ packages/liveblocks-storage/test/Pipe.test.ts
@@ -0,0 +1,100 @@
+import { describe, expect, onTestFinished, test, vi } from "vitest";
+import { makePipe } from "./lib/Pipe.js";
+
+/**
+ * Sets up a pipe test:
+ * - Create a new Pipe instance
+ * - Connect a mock callback
+ *
+ * After test:
+ * - Automatically unsubscribes the callback
+ * - Automatically await dangling promises
+ *
+ * Returns:
+ * - The pipe, and the callback.
+ */
+function makePipeTest() {
+  const pipe = makePipe();
+  const callback = vi.fn();
+  const unsub = pipe.output.subscribe(callback);
+
+  onTestFinished(
+    async () => {
+      unsub();
+      await pipe.flush();
+    },
+
+    // Allow for max 1 second
+    1000
+  );
+
+  return { pipe, callback };
+}
+
+function useFakeTimers() {
+  vi.useFakeTimers();
+  onTestFinished(() => void vi.useRealTimers());
+}
+
+describe("Pipe (a testing utility, how meta)", () => {
+  test("empty", async () => {
+    const { callback } = makePipeTest();
+    expect(callback).not.toHaveBeenCalled();
+  });
+
+  test("auto flush control", async () => {
+    useFakeTimers();
+    const { pipe, callback } = makePipeTest();
+
+    pipe.send("one");
+    pipe.send("two");
+
+    expect(callback).not.toHaveBeenCalled();
+
+    // Callback will be invoked asynchronously
+    await vi.advanceTimersByTimeAsync(1); // Advance by 1ms
+    expect(callback).toHaveBeenCalledTimes(2);
+  });
+
+  test("manual flush control", async () => {
+    useFakeTimers();
+    const { pipe, callback } = makePipeTest();
+    pipe.setManual();
+
+    pipe.send("one");
+    pipe.send("two");
+
+    expect(callback).not.toHaveBeenCalled();
+
+    // Callback will be invoked asynchronously
+    await vi.advanceTimersByTimeAsync(1000); // Advance does nothing yet
+    expect(callback).not.toHaveBeenCalled();
+
+    // ...until flushed
+    pipe.flush();
+    await vi.advanceTimersByTimeAsync(1); // Advance by 1ms
+
+    expect(callback).toHaveBeenCalledTimes(2);
+  });
+
+  test("toggling to auto flushes immediately", async () => {
+    useFakeTimers();
+    const { pipe, callback } = makePipeTest();
+    pipe.setManual();
+
+    pipe.send("one");
+    pipe.send("two");
+
+    expect(callback).not.toHaveBeenCalled();
+
+    // Callback will be invoked asynchronously
+    await vi.advanceTimersByTimeAsync(1000); // Advance does nothing yet
+    expect(callback).not.toHaveBeenCalled();
+
+    // ...until flushed
+    pipe.setAuto();
+    await vi.advanceTimersByTimeAsync(1); // Advance by 1ms
+
+    expect(callback).toHaveBeenCalledTimes(2);
+  });
+});
---
+++ packages/liveblocks-storage/test/lib/Pipe.ts
@@ -0,0 +1,94 @@
+import type { EventSource, Observable } from "~/lib/EventSource.js";
+import { makeEventSource } from "~/lib/EventSource.js";
+import { raise } from "~/utils.js";
+
+/**
+ * Abstracts the network and provides control over how messages will be
+ * delivered between client and server on a per-test basis.
+ *
+ * Delivery will be fast and automatic by default, but never synchronous.
+ */
+export type Pipe<T> = {
+  send(data: T): void; // One end sends data into the pipe
+  output: Observable<T>; // Other end receives data from the pipe (which doesn't necessarily happen synchronously)
+
+  // Control pipe behavior by slowing down (simulate slow network)
+  // Set to -1 to drop messages entirely
+  // Only has an effect when pipe is in auto mode
+  // slow(delay: number | (() => number)): void;
+
+  // Manual pipe control options to pause/unpause delivery
+  setAuto(): void; // The default mode
+  setManual(): void;
+
+  // Control pipe by delivering all buffered messages to the pipe
+  flush(n?: number): Promise<void>;
+};
+
+export function makePipe<T>(): Pipe<T> {
+  let mode: "auto" | "manual" = "auto";
+
+  const buffer: T[] = [];
+  const output: EventSource<T> = makeEventSource<T>();
+
+  // Please note that `.send()` is a synchronous API. This can be deceiving,
+  // because it may lead to asynchronous side effects (messages delivered later).
+  // That's why it's important to close every test with an
+  // `await pipe.flush()`, and await all those promises!
+  function send(input: T) {
+    buffer.push(input);
+
+    if (mode === "auto" && !pendingFlush$) {
+      void flush();
+    }
+  }
+
+  function delay() {
+    // Or use callback slow() here?
+    return new Promise<void>((res) => setTimeout(res, 0));
+  }
+
+  let pendingFlush$: Promise<void> | null = null;
+
+  async function flushNow() {
+    if (pendingFlush$) raise("Internal corruption");
+
+    let data: T | undefined;
+    while ((data = buffer.shift()) !== undefined) {
+      // Add artificial delay between each loop?
+      await delay();
+      try {
+        output.notify(data);
+      } catch {
+        // Ignore
+      }
+    }
+
+    // Clear the pending
+    pendingFlush$ = null;
+  }
+
+  async function flush() {
+    pendingFlush$ ??= flushNow();
+    await pendingFlush$;
+  }
+
+  function setAuto() {
+    mode = "auto";
+    if (!pendingFlush$) {
+      void flush();
+    }
+  }
+
+  function setManual() {
+    mode = "manual";
+  }
+
+  return {
+    send,
+    output: output.observable,
+    setAuto,
+    setManual,
+    flush,
+  };
+}
