0dc3e1a83c3429217ad243b22ee59b9b84a4ec86
Vincent Driessen <me@nvie.com>
2024-11-15 15:22:59 +0100

    Fix all remaining typing issues

--- packages/liveblocks-storage/test/mutations.config.ts
+++ packages/liveblocks-storage/test/mutations.config.ts
@@ -1,20 +1,20 @@
-import type { LayeredCache as Stub } from "~/LayeredCache.js";
+import type { LayeredCache } from "~/LayeredCache.js";
 import type { Json } from "~/lib/Json.js";
 
-export function put(stub: Stub, key: string, value: Json): void {
+export function put(stub: LayeredCache, key: string, value: Json): void {
   stub.set(key, value);
 }
 
-export function putRandom(stub: Stub, key: string): void {
+export function putRandom(stub: LayeredCache, key: string): void {
   stub.set(key, Math.floor(Math.random() * 1_000_000));
 }
 
-export function putAndFail(stub: Stub, key: string, value: Json): void {
+export function putAndFail(stub: LayeredCache, key: string, value: Json): void {
   put(stub, key, value);
   throw new Error("b0rked");
 }
 
-export function dupe(stub: Stub, src: string, target: string): void {
+export function dupe(stub: LayeredCache, src: string, target: string): void {
   const value = stub.get(src);
   if (value === undefined) {
     throw new Error(`No such key '${src}'`);
@@ -22,12 +22,12 @@ export function dupe(stub: Stub, src: string, target: string): void {
   stub.set(target, value);
 }
 
-export function inc(stub: Stub, key: string): void {
+export function inc(stub: LayeredCache, key: string): void {
   const count = stub.getNumber(key) ?? 0;
   stub.set(key, count + 1);
 }
 
-export function dec(stub: Stub, key: string): void {
+export function dec(stub: LayeredCache, key: string): void {
   const count = stub.getNumber(key) ?? 0;
   if (count <= 0) {
     throw new Error("Cannot decrement beyond 0");
--- packages/liveblocks-storage/test/utils.ts
+++ packages/liveblocks-storage/test/utils.ts
@@ -1,9 +1,11 @@
-import type { ClientStore, ServerStore } from "~/index.js";
-import type { Json } from "~/lib/Json.js";
+import type { Client, Server } from "~/index.js";
 import type { LayeredCache } from "~/LayeredCache.js";
+import type { Json } from "~/lib/Json.js";
 
 export function fmt(
-  base: ClientStore<any> | ServerStore<any> | LayeredCache
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  base: Client<any> | Server<any> | LayeredCache
+  /* eslint-enable @typescript-eslint/no-explicit-any */
 ): Record<string, Json> {
   return "asObject" in base ? base.asObject() : Object.fromEntries(base);
 }
