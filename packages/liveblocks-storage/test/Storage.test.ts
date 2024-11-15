1298787c57c54a1a657dc0afff44a7bfd13c0b0e
Vincent Driessen <me@nvie.com>
2024-11-15 18:34:52 +0100

    Beautify test setup

--- packages/liveblocks-storage/test/Storage.test.ts
+++ packages/liveblocks-storage/test/Storage.test.ts
@@ -1,10 +1,7 @@
 import { describe, expect, test } from "vitest";
 
-import { Client } from "~/Client.js";
-import { Server } from "~/Server.js";
-
 import * as mutations from "./mutations.config.js";
-import { clientServerSetup } from "./utils.js";
+import { clientServerSetup, twoClientSetup } from "./utils.js";
 
 describe("Single Client/Server sync test", () => {
   test("most basic end to end test", async () => {
@@ -27,47 +24,43 @@ describe("Single Client/Server sync test", () => {
 
 describe("Multi-client storage synchronization tests", () => {
   test("most basic end to end test", async () => {
-    const client1 = new Client(mutations);
-    const client2 = new Client(mutations);
-    const server = new Server(mutations);
+    const { client1, client2, server, sync } = twoClientSetup(mutations);
 
-    const op1 = client1.mutate.put("a", 1);
-    const op2 = client2.mutate.put("a", 2);
+    expect(client1.asObject()).toEqual({});
+    expect(client2.asObject()).toEqual({});
+    expect(server.asObject()).toEqual({});
+
+    client1.mutate.put("a", 1);
+    client2.mutate.put("a", 2);
 
     expect(client1.asObject()).toEqual({ a: 1 });
     expect(client2.asObject()).toEqual({ a: 2 });
     expect(server.asObject()).toEqual({});
 
-    // Simulate client A op reaching server first
-    const delta1 = server.applyOp([op1, "put", ["a", 1]]);
-
-    // Apply the delta on both clients
-    client1.applyDelta(delta1);
-    client2.applyDelta(delta1);
+    await sync(client1);
 
     expect(client1.asObject()).toEqual({ a: 1 });
     expect(client2.asObject()).toEqual({ a: 2 });
     expect(server.asObject()).toEqual({ a: 1 });
 
-    // Simulate client B op reaching server next
-    const delta2 = server.applyOp([op2, "put", ["a", 2]]);
+    await sync(client2);
 
-    // Apply the delta on both clients
-    client1.applyDelta(delta2);
-    client2.applyDelta(delta2);
+    expect(client1.asObject()).toEqual({ a: 1 });
+    expect(client2.asObject()).toEqual({ a: 2 });
+    expect(server.asObject()).toEqual({ a: 2 });
+
+    await sync();
 
     expect(client1.asObject()).toEqual({ a: 2 });
     expect(client2.asObject()).toEqual({ a: 2 });
     expect(server.asObject()).toEqual({ a: 2 });
   });
 
-  test("basic end to end test with randomization in mutation", () => {
-    const client1 = new Client(mutations);
-    const client2 = new Client(mutations);
-    const server = new Server(mutations);
+  test("basic end to end test with randomization in mutation", async () => {
+    const { client1, client2, server, sync } = twoClientSetup(mutations);
 
-    const op1 = client1.mutate.put("a", 1);
-    const op2 = client2.mutate.putRandom("b");
+    client1.mutate.put("a", 1);
+    client2.mutate.putRandom("b");
 
     const b1 = client2.asObject()["b"]; // First random number (from first optimistic update)
 
@@ -75,12 +68,8 @@ describe("Multi-client storage synchronization tests", () => {
     expect(client2.asObject()).toEqual({ b: b1 });
     expect(server.asObject()).toEqual({});
 
-    // Simulate client A op reaching server first
-    const delta1 = server.applyOp([op1, "put", ["a", 1]]);
-
-    // Apply the delta on both clients
-    client1.applyDelta(delta1);
-    client2.applyDelta(delta1);
+    await sync(client1);
+    await sync(server);
 
     const b2 = client2.asObject()["b"]; // Second random number (after applying first op)
 
@@ -88,14 +77,14 @@ describe("Multi-client storage synchronization tests", () => {
     expect(client2.asObject()).toEqual({ a: 1, b: b2 });
     expect(server.asObject()).toEqual({ a: 1 });
 
-    // Simulate client B op reaching server next
-    const delta2 = server.applyOp([op2, "putRandom", ["b"]]);
+    await sync(client2);
 
-    // Apply the delta on both clients
-    client1.applyDelta(delta2);
-    client2.applyDelta(delta2);
+    expect(client1.asObject()).toEqual({ a: 1 });
+    expect(client2.asObject()).toEqual({ a: 1, b: b2 });
+    const b3 = server.asObject()["b"]; // Third random number (authoritative from server)
+    expect(server.asObject()).toEqual({ a: 1, b: b3 });
 
-    const b3 = client2.asObject()["b"]; // Third random number (authoritative from server)
+    await sync(server);
 
     expect(client1.asObject()).toEqual({ a: 1, b: b3 });
     expect(client2.asObject()).toEqual({ a: 1, b: b3 });
