d4f7feee21026ac5b861cd04a82a00f786d00201
Chris Nicholas <ctnicholasdev@gmail.com>
2024-11-15 13:36:15 +0000

    Better load behaviour in Lexical examples (#2056)
    Co-authored-by: Vincent Driessen <me@nvie.com>

---
+++ examples/nextjs-linear-like-issue-tracker/src/icons/SyncCompleteIcon.tsx
@@ -0,0 +1,18 @@
+import { SVGProps } from "react";
+
+export function SyncCompleteIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="m10.6 16.6l7.05-7.05l-1.4-1.4l-5.65 5.65l-2.85-2.85l-1.4 1.4zM12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22"
+      ></path>
+    </svg>
+  );
+}
