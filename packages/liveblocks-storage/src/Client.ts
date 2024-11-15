3a8d70625b4cc2e909f17abd4743916bd780385b
Vincent Driessen <me@nvie.com>
2024-11-15 17:10:50 +0100

    Add testing utilities

--- packages/liveblocks-storage/src/Client.ts
+++ packages/liveblocks-storage/src/Client.ts
@@ -1,14 +1,40 @@
+import type { Callback } from "./lib/EventSource.js";
 import type { Json } from "./lib/Json.js";
 import { Store } from "./Store.js";
 import type { ChangeReturnType, OmitFirstArg } from "./ts-toolkit.js";
-import type { Delta, Mutation, Mutations, Op, OpId } from "./types.js";
-import { opId } from "./utils.js";
+import type {
+  ClientMsg,
+  Delta,
+  Mutation,
+  Mutations,
+  Op,
+  OpId,
+  ServerMsg,
+  Socket,
+} from "./types.js";
+import { opId, raise } from "./utils.js";
 
 type BoundMutations<M extends Record<string, Mutation>> = {
   [K in keyof M]: ChangeReturnType<OmitFirstArg<M[K]>, OpId>;
 };
 
+let nextId = "A";
+
+function getClientId() {
+  const curr = nextId;
+  if (nextId.endsWith("Z")) {
+    nextId = "A".repeat(nextId.length + 1);
+  } else {
+    nextId =
+      nextId.slice(0, -1) +
+      String.fromCharCode(nextId[nextId.length - 1]!.charCodeAt(0) + 1);
+  }
+  return curr;
+}
+
 export class Client<M extends Mutations> {
+  #_debugClientId = getClientId();
+
   // #currentSession: Session; ??
   // #actor?: number;
   #store: Store<M>;
@@ -19,6 +45,8 @@ export class Client<M extends Mutations> {
   // *insertion order*, but it also allows us to efficiently remove acknowledgd
   // ops.
   #pendingOps: Map<OpId, Op>;
+
+  #socket: Socket<ClientMsg, ServerMsg> | null = null;
 
   // #eventSource: EventSource<Op>;
   // readonly events = {
@@ -39,12 +67,40 @@ export class Client<M extends Mutations> {
         const op: Op = [id, name, args];
         this.#pendingOps.set(id, op);
         this.#store.applyOp(op, false);
-        // this.#eventSource.notify(op);
+
+        // XXX Ultimately, we should not directly send this Op into the socket,
+        // we'll have to maybe throttle these, and also we should never send
+        // these out after we've gotten disconnected. Details for now, though!
+        const msg = op;
+        console.log(`[client ${this.#_debugClientId}] OUT`, msg);
+        this.#socket?.send(msg);
+
         return id;
       }) as any;
       /* eslint-enable @typescript-eslint/no-explicit-any */
       /* eslint-enable @typescript-eslint/no-unsafe-assignment */
     }
+  }
+
+  connect(socket: Socket<ClientMsg, ServerMsg>): Callback<void> {
+    if (this.#socket) raise("Already connected");
+
+    // Start listening to incoming ClientMsg messages on this socket
+    const disconnect = socket.recv.subscribe((msg) =>
+      this.#handleServerMsg(msg)
+    );
+
+    this.#socket = socket;
+    return () => {
+      // Tear down pipes
+      this.#socket = null;
+      disconnect();
+    };
+  }
+
+  #handleServerMsg(msg: ServerMsg): void {
+    console.log(`[client ${this.#_debugClientId}] IN`, msg);
+    this.applyDelta(msg);
   }
 
   applyDeltas(deltas: readonly Delta[]): void {
