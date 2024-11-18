9adabdb03d30f3fb3149eafd4114a3e5a44d51ed
Vincent Driessen <me@nvie.com>
2024-11-18 10:57:34 +0100

    Remove unneeded describe blocks

--- packages/liveblocks-storage/test/lib/Pipe.test.ts
+++ packages/liveblocks-storage/test/lib/Pipe.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, onTestFinished, test, vi } from "vitest";
+import { expect, onTestFinished, test, vi } from "vitest";
 
 import { makePipe } from "~/lib/Pipe.js";
 
@@ -37,91 +37,89 @@ function useFakeTimers() {
   onTestFinished(() => void vi.useRealTimers());
 }
 
-describe("Pipe (a testing utility, how meta)", () => {
-  test("empty", () => {
-    const { callback } = makePipeTest();
-    expect(callback).not.toHaveBeenCalled();
-  });
+test("empty", () => {
+  const { callback } = makePipeTest();
+  expect(callback).not.toHaveBeenCalled();
+});
 
-  test("auto flush control", async () => {
-    useFakeTimers();
-    const { pipe, callback } = makePipeTest();
+test("auto flush control", async () => {
+  useFakeTimers();
+  const { pipe, callback } = makePipeTest();
 
-    pipe.send("one");
-    pipe.send("two");
+  pipe.send("one");
+  pipe.send("two");
 
-    expect(callback).not.toHaveBeenCalled();
+  expect(callback).not.toHaveBeenCalled();
 
-    // Callback will be invoked asynchronously
-    await vi.advanceTimersByTimeAsync(1); // Advance by 1ms
-    expect(callback).toHaveBeenNthCalledWith(1, "one");
-    expect(callback).toHaveBeenNthCalledWith(2, "two");
-    expect(callback).toHaveBeenCalledTimes(2);
-  });
+  // Callback will be invoked asynchronously
+  await vi.advanceTimersByTimeAsync(1); // Advance by 1ms
+  expect(callback).toHaveBeenNthCalledWith(1, "one");
+  expect(callback).toHaveBeenNthCalledWith(2, "two");
+  expect(callback).toHaveBeenCalledTimes(2);
+});
 
-  test("manual flush control", async () => {
-    useFakeTimers();
-    const { pipe, callback } = makePipeTest();
-    pipe.setManual();
+test("manual flush control", async () => {
+  useFakeTimers();
+  const { pipe, callback } = makePipeTest();
+  pipe.setManual();
 
-    pipe.send("one");
-    pipe.send("two");
+  pipe.send("one");
+  pipe.send("two");
 
-    expect(callback).not.toHaveBeenCalled();
+  expect(callback).not.toHaveBeenCalled();
 
-    // Callback will be invoked asynchronously
-    await vi.advanceTimersByTimeAsync(1000); // Advance does nothing yet
-    expect(callback).not.toHaveBeenCalled();
+  // Callback will be invoked asynchronously
+  await vi.advanceTimersByTimeAsync(1000); // Advance does nothing yet
+  expect(callback).not.toHaveBeenCalled();
 
-    // ...until flushed
-    void pipe.flush();
-    await vi.advanceTimersByTimeAsync(1); // Advance by 1ms
+  // ...until flushed
+  void pipe.flush();
+  await vi.advanceTimersByTimeAsync(1); // Advance by 1ms
 
-    expect(callback).toHaveBeenCalledTimes(2);
-  });
+  expect(callback).toHaveBeenCalledTimes(2);
+});
 
-  test("toggling to auto flushes immediately", async () => {
-    useFakeTimers();
-    const { pipe, callback } = makePipeTest();
-    pipe.setManual();
+test("toggling to auto flushes immediately", async () => {
+  useFakeTimers();
+  const { pipe, callback } = makePipeTest();
+  pipe.setManual();
 
-    pipe.send("one");
-    pipe.send("two");
+  pipe.send("one");
+  pipe.send("two");
 
-    expect(callback).not.toHaveBeenCalled();
+  expect(callback).not.toHaveBeenCalled();
 
-    // Callback will be invoked asynchronously
-    await vi.advanceTimersByTimeAsync(1000); // Advance does nothing yet
-    expect(callback).not.toHaveBeenCalled();
+  // Callback will be invoked asynchronously
+  await vi.advanceTimersByTimeAsync(1000); // Advance does nothing yet
+  expect(callback).not.toHaveBeenCalled();
 
-    // ...until flushed
-    pipe.setAuto();
-    await vi.advanceTimersByTimeAsync(1); // Advance by 1ms
+  // ...until flushed
+  pipe.setAuto();
+  await vi.advanceTimersByTimeAsync(1); // Advance by 1ms
 
-    expect(callback).toHaveBeenCalledTimes(2);
-  });
+  expect(callback).toHaveBeenCalledTimes(2);
+});
 
-  test("pipes serialize/deserialize their payloads", async () => {
-    useFakeTimers();
-    const { pipe, callback } = makePipeTest();
-    pipe.setManual();
+test("pipes serialize/deserialize their payloads", async () => {
+  useFakeTimers();
+  const { pipe, callback } = makePipeTest();
+  pipe.setManual();
 
-    pipe.send(new Date("2024-01-01"));
-    pipe.send(new Error("I am an error"));
+  pipe.send(new Date("2024-01-01"));
+  pipe.send(new Error("I am an error"));
 
-    expect(callback).not.toHaveBeenCalled();
+  expect(callback).not.toHaveBeenCalled();
 
-    // Callback will be invoked asynchronously
-    await vi.advanceTimersByTimeAsync(1000); // Advance does nothing yet
-    expect(callback).not.toHaveBeenCalled();
+  // Callback will be invoked asynchronously
+  await vi.advanceTimersByTimeAsync(1000); // Advance does nothing yet
+  expect(callback).not.toHaveBeenCalled();
 
-    // ...until flushed
-    pipe.setAuto();
-    await vi.advanceTimersByTimeAsync(1); // Advance by 1ms
+  // ...until flushed
+  pipe.setAuto();
+  await vi.advanceTimersByTimeAsync(1); // Advance by 1ms
 
-    // They will come out as strings on the other end of the pipe
-    expect(callback).toHaveBeenNthCalledWith(1, "2024-01-01T00:00:00.000Z"); // Comes out as a string
-    expect(callback).toHaveBeenNthCalledWith(2, {}); // Comes out as an empty object
-    expect(callback).toHaveBeenCalledTimes(2);
-  });
+  // They will come out as strings on the other end of the pipe
+  expect(callback).toHaveBeenNthCalledWith(1, "2024-01-01T00:00:00.000Z"); // Comes out as a string
+  expect(callback).toHaveBeenNthCalledWith(2, {}); // Comes out as an empty object
+  expect(callback).toHaveBeenCalledTimes(2);
 });
