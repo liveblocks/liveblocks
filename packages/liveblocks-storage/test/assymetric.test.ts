81ca9794f2e9a8de81377fe1c17f5311df6989ab
Vincent Driessen <me@nvie.com>
2024-11-18 13:59:04 +0100

    Add assymmetric tests

--- packages/liveblocks-storage/test/assymetric.test.ts
+++ packages/liveblocks-storage/test/assymetric.test.ts
@@ -6,7 +6,7 @@
 import { expect, test } from "vitest";
 
 import { fail, inc, put, putAndInc } from "./mutations.config.js";
-import { clientServerSetup } from "./utils.js";
+import { clientServerSetup, twoClientSetup } from "./utils.js";
 
 test("assymmetric mutators (different behavior will sync)", async () => {
   const { client, server, sync } = clientServerSetup(
@@ -66,5 +66,47 @@ test("assymmetric mutators (rollback)", async () => {
 
   // Server will fail, and thus revert the mutation
   expect(client.data).toEqual({ a: 2, b: 1 });
+  expect(server.data).toEqual({ a: 2, b: 1 });
+});
+
+test("assymmetric mutators (rollback) (2 clients)", async () => {
+  const { client1, client2, server, sync } = twoClientSetup(
+    { put, assymmetricInc: inc }, // Client will not fail
+    { put, assymmetricInc: fail } // Inc fails on the server
+  );
+
+  expect(client1.data).toEqual({});
+  expect(client2.data).toEqual({});
+  expect(server.data).toEqual({});
+
+  client1.mutate.put("a", 2);
+
+  expect(client1.data).toEqual({ a: 2 });
+  expect(client2.data).toEqual({});
+  expect(server.data).toEqual({});
+
+  client1.mutate.assymmetricInc("a"); // Will fail on the server
+
+  expect(client1.data).toEqual({ a: 3 });
+  expect(client2.data).toEqual({});
+  expect(server.data).toEqual({});
+
+  client1.mutate.put("b", 1);
+
+  expect(client1.data).toEqual({ a: 3, b: 1 });
+  expect(client2.data).toEqual({});
+  expect(server.data).toEqual({});
+
+  await sync(client1);
+
+  expect(client1.data).toEqual({ a: 3, b: 1 });
+  expect(client2.data).toEqual({});
+  expect(server.data).toEqual({ a: 2, b: 1 });
+
+  await sync(server);
+
+  // Server will fail, and thus revert the mutation
+  expect(client1.data).toEqual({ a: 2, b: 1 });
+  expect(client2.data).toEqual({ a: 2, b: 1 });
   expect(server.data).toEqual({ a: 2, b: 1 });
 });
