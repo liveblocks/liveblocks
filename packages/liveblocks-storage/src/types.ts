54e7364e6470dc7a552055c176922d812407a903
Vincent Driessen <me@nvie.com>
2024-11-18 17:15:36 +0100

    Cleanups

--- packages/liveblocks-storage/src/types.ts
+++ packages/liveblocks-storage/src/types.ts
@@ -14,9 +14,10 @@ export type OpId = Brand<string, "OpId">;
 export type Op = readonly [id: OpId, name: string, args: readonly Json[]];
 
 export type Delta = readonly [
+  // XXX Maybe support multiple ops being acknowledged by the same delta?
   id: OpId,
   rem: readonly string[],
-  add: readonly (string | Json)[],
+  add: readonly (string | Json)[], // Alternated kv pairs, e.g. [key1,value1,key2,value2,etc...]
 ]; // Eventually, we'll need to compress this
 
 export type ClientMsg = Op;
@@ -24,7 +25,7 @@ export type ClientMsg = Op;
 export type ServerMsg = Delta;
 
 export type Mutation = (
-  stub: LayeredCache,
+  root: LayeredCache,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   ...args: readonly any[]
 ) => void;
