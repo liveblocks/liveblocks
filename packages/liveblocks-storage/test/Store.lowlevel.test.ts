9a7efe649ba421b3b2f8a0ee42091242e775370e
Vincent Driessen <me@nvie.com>
2024-11-18 09:45:27 +0100

    No need for describe block

--- packages/liveblocks-storage/test/Store.lowlevel.test.ts
+++ packages/liveblocks-storage/test/Store.lowlevel.test.ts
@@ -1,48 +1,46 @@
-import { describe, expect, test } from "vitest";
+import { expect, test } from "vitest";
 
 import { Store } from "~/Store.js";
 import { opId } from "~/utils.js";
 
 import * as mutations from "./mutations.config.js";
 
-describe("Store tests (relatively low-level)", () => {
-  test("can be mutated locally", () => {
-    const store = new Store(mutations);
-    expect(() => store.applyOp([opId(), "non-existing", []])).toThrow(
-      "Mutation not found: 'non-existing'"
-    );
-    expect(store.toObject()).toEqual({});
-  });
+test("can be mutated locally", () => {
+  const store = new Store(mutations);
+  expect(() => store.applyOp([opId(), "non-existing", []], false)).toThrow(
+    "Mutation not found: 'non-existing'"
+  );
+  expect(store.toObject()).toEqual({});
+});
 
-  test("can be mutated locally", () => {
-    const store = new Store(mutations);
-    store.applyOp([opId(), "put", ["a", 1]]);
-    store.applyOp([opId(), "put", ["b", 2]]);
-    store.applyOp([opId(), "put", ["c", 3]]);
-    store.applyOp([opId(), "inc", ["c"]]);
+test("can be mutated locally", () => {
+  const store = new Store(mutations);
+  store.applyOp([opId(), "put", ["a", 1]], false);
+  store.applyOp([opId(), "put", ["b", 2]], false);
+  store.applyOp([opId(), "put", ["c", 3]], false);
+  store.applyOp([opId(), "inc", ["c"]], false);
 
-    expect(store.toObject()).toEqual({ a: 1, b: 2, c: 4 });
-  });
+  expect(store.toObject()).toEqual({ a: 1, b: 2, c: 4 });
+});
 
-  test("mutations can fail", () => {
-    const store = new Store(mutations);
-    expect(() => store.applyOp([opId(), "dec", ["a"]])).toThrow(
-      "Cannot decrement beyond 0"
-    );
-    expect(store.toObject()).toEqual({});
-  });
+test("mutations can fail", () => {
+  const store = new Store(mutations);
+  expect(() => store.applyOp([opId(), "dec", ["a"]], false)).toThrow(
+    "Cannot decrement beyond 0"
+  );
+  expect(store.toObject()).toEqual({});
+});
 
-  test("all mutators should be executed atomically", () => {
-    const store = new Store(mutations);
-    store.applyOp([opId(), "put", ["a", 1]]);
-    store.applyOp([opId(), "put", ["b", 3]]);
-    try {
-      // Fails, so should be rolled back
-      store.applyOp([opId(), "putAndFail", ["a", 42]]);
-    } catch {
-      // Ignore
-    }
-    store.applyOp([opId(), "dupe", ["a", "c"]]);
-    expect(store.toObject()).toEqual({ a: 1, b: 3, c: 1 });
-  });
+test("all mutators should be executed atomically", () => {
+  const store = new Store(mutations);
+  store.applyOp([opId(), "put", ["a", 1]], false);
+  store.applyOp([opId(), "put", ["b", 3]], false);
+  try {
+    // Fails, so should be rolled back
+    store.applyOp([opId(), "putAndFail", ["a", 42]], false);
+  } catch {
+    // Ignore
+  }
+  store.applyOp([opId(), "dupe", ["a", "c"]], false);
+  expect(store.toObject()).toEqual({ a: 1, b: 3, c: 1 });
 });
