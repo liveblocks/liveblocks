2266a0ee2e125880ba1d382924a2575b9650a316
Florent <florent.lefebvre@liveblocks.io>
2024-11-27 16:21:00 -0500

    Add webhook endpoint

---
+++ examples/nextjs-channel-notifications-settings/src/app/api/liveblocks-webhook/route.ts
@@ -0,0 +1,36 @@
+import { Liveblocks } from "@liveblocks/node";
+import { WebhookHandler } from "@liveblocks/node";
+
+// Add your Liveblocks secret key from the dashboard
+const liveblocks = new Liveblocks({
+  secret: process.env.LIVEBLOCKS_SECRET_KEY as string,
+});
+
+// Add your webhook secret key from a project's webhooks dashboard
+const webhookHandler = new WebhookHandler(
+  process.env.LIVEBLOCKS_WEBHOOK_SECRET_KEY as string
+);
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const headers = request.headers;
+
+  // Verify if this is a real webhook request
+  let event;
+  try {
+    event = webhookHandler.verifyRequest({
+      headers: headers,
+      rawBody: JSON.stringify(body),
+    });
+  } catch (err) {
+    console.error(err);
+    return new Response("Could not verify webhook call", { status: 400 });
+  }
+
+  if (event.type === "notification") {
+    // Do stuff
+    return new Response("", { status: 200 });
+  }
+
+  return new Response("Event type not used", { status: 400 });
+}
