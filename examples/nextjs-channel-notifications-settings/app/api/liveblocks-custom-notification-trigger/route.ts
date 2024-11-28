f00eb65b0f74ace9bac4ca96c1a52b496517ba0c
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-11-28 14:15:48 +0100

    add button to generate a custom notification

---
+++ examples/nextjs-channel-notifications-settings/app/api/liveblocks-custom-notification-trigger/route.ts
@@ -0,0 +1,39 @@
+import { Liveblocks } from "@liveblocks/node";
+import { USER_INFO } from "../dummy-users";
+import { P } from "@liveblocks/react/dist/room-t_BUxm1-";
+
+const liveblocks = new Liveblocks({
+  secret: process.env.LIVEBLOCKS_SECRET_KEY as string,
+  // XXX
+  // @ts-expect-error
+  baseUrl: "https://dev.dev-liveblocks5948.workers.dev/",
+});
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  console.log("body", body);
+  const userIndex = Math.floor(Math.random() * USER_INFO.length);
+
+  try {
+    await liveblocks.triggerInboxNotification({
+      userId: `user-${userIndex}`,
+      kind: "$fileUploaded",
+      subjectId: "my-file",
+      activityData: {
+        file: "http://localhost:3000/my-file.zip",
+        size: 256,
+        success: true,
+      },
+      roomId: body.roomId,
+    });
+
+    return new Response(null, { status: 200 });
+  } catch (err) {
+    return Response.json(
+      {
+        err,
+      },
+      { status: 500 }
+    );
+  }
+}
