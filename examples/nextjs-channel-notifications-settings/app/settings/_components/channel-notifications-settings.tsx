5c00b3737af7690baec0fa44566839001188f44a
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-11-28 12:15:17 +0100

    add other channels in settings page

--- examples/nextjs-channel-notifications-settings/app/settings/_components/channel-notifications-settings.tsx
+++ examples/nextjs-channel-notifications-settings/app/settings/_components/channel-notifications-settings.tsx
@@ -20,6 +20,9 @@ function SubmitButton({ onClick }: { onClick: () => void }) {
 export function ChannelNotificationsSettings() {
   // TODO it will be replaced by the new hook
   const [emailNotifications, setEmailNotifications] = useState(true);
+  const [slackNotifications, setSlackNotifications] = useState(false);
+  const [$customNotifications, set$customNotifications] = useState(false);
+
   const handleSubmit = (): void => {
     // TODO CALL update
   };
@@ -51,6 +54,50 @@ export function ChannelNotificationsSettings() {
             className="ml-3 text-sm font-medium text-gray-700"
           >
             Receive email notifications
+          </label>
+        </div>
+      </div>
+      <div className="mb-6">
+        <div className="flex items-center">
+          <Switch.Root
+            className={cn(
+              "w-11 h-6 rounded-full relative inline-flex items-center transition-colors",
+              slackNotifications ? "bg-green-500" : "bg-gray-200"
+            )}
+            id="emailNotifications"
+            name="emailNotifications"
+            checked={slackNotifications}
+            onCheckedChange={setSlackNotifications}
+          >
+            <Switch.Thumb className="w-5 h-5 bg-white rounded-full shadow-md transform transition-transform duration-100 will-change-transform data-[state=checked]:translate-x-[22px]" />
+          </Switch.Root>
+          <label
+            htmlFor="emailNotifications"
+            className="ml-3 text-sm font-medium text-gray-700"
+          >
+            Receive Slack notifications
+          </label>
+        </div>
+      </div>
+      <div className="mb-6">
+        <div className="flex items-center">
+          <Switch.Root
+            className={cn(
+              "w-11 h-6 rounded-full relative inline-flex items-center transition-colors",
+              $customNotifications ? "bg-green-500" : "bg-gray-200"
+            )}
+            id="emailNotifications"
+            name="emailNotifications"
+            checked={$customNotifications}
+            onCheckedChange={set$customNotifications}
+          >
+            <Switch.Thumb className="w-5 h-5 bg-white rounded-full shadow-md transform transition-transform duration-100 will-change-transform data-[state=checked]:translate-x-[22px]" />
+          </Switch.Root>
+          <label
+            htmlFor="emailNotifications"
+            className="ml-3 text-sm font-medium text-gray-700"
+          >
+            Receive $customNotifications notifications
           </label>
         </div>
       </div>
