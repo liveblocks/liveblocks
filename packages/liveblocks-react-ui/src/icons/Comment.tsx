a1da00bb833b432c58d1f74ec17084a0d0958cf8
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2024-11-29 14:25:46 +0100

    Improve comment icon

--- packages/liveblocks-react-ui/src/icons/Comment.tsx
+++ packages/liveblocks-react-ui/src/icons/Comment.tsx
@@ -6,7 +6,7 @@ import { Icon } from "../components/internal/Icon";
 export function CommentIcon(props: ComponentProps<"svg">) {
   return (
     <Icon {...props}>
-      <path d="M10 16a6 6 0 1 0-5.686-4.08c.158.469.217.973.08 1.448l-.523 1.834a.75.75 0 0 0 .927.927l1.834-.524c.475-.136.979-.077 1.447.08A5.965 5.965 0 0 0 10 16Z" />
+      <path d="M10 16a6 6 0 1 0-5.552-3.72c.094.229.12.482.052.719l-.753 2.636a.5.5 0 0 0 .618.618l2.636-.753a1.1 1.1 0 0 1 .719.052A6.002 6.002 0 0 0 10 16Z" />
     </Icon>
   );
 }
