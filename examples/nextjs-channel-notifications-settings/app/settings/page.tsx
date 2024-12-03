97f80e24f143fa50219a905e45dd02f0ef865480
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-12-03 14:28:32 +0100

    update example to start using new react hook useChannelNotificationSettings()

--- examples/nextjs-channel-notifications-settings/app/settings/page.tsx
+++ examples/nextjs-channel-notifications-settings/app/settings/page.tsx
@@ -1,9 +1,44 @@
+"use client";
+
+import { LiveblocksProvider } from "@liveblocks/react";
 import { ChannelNotificationsSettings } from "./_components/channel-notifications-settings";
 
 export default function SettingsPage() {
   return (
-    <div className="flex flex-col w-full items-center justify-center h-screen">
-      <ChannelNotificationsSettings />
-    </div>
+    <LiveblocksProvider
+      authEndpoint="/api/liveblocks-auth"
+      // XXX
+      // @ts-expect-error
+      baseUrl="https://dev.dev-liveblocks5948.workers.dev/"
+      resolveUsers={async ({ userIds }) => {
+        const searchParams = new URLSearchParams(
+          userIds.map((userId) => ["userIds", userId])
+        );
+        const response = await fetch(`/api/users?${searchParams}`);
+
+        if (!response.ok) {
+          throw new Error("Problem resolving users");
+        }
+
+        const users = await response.json();
+        return users;
+      }}
+      resolveMentionSuggestions={async ({ text }) => {
+        const response = await fetch(
+          `/api/users/search?text=${encodeURIComponent(text)}`
+        );
+
+        if (!response.ok) {
+          throw new Error("Problem resolving mention suggestions");
+        }
+
+        const userIds = await response.json();
+        return userIds;
+      }}
+    >
+      <div className="flex flex-col w-full items-center justify-center h-screen">
+        <ChannelNotificationsSettings />
+      </div>
+    </LiveblocksProvider>
   );
 }
