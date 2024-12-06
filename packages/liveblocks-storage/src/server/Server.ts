7e291d0778863b02c2cf64929d888d0a121cd5b5
Vincent Driessen <me@nvie.com>
2024-12-06 15:45:00 +0100

    Toy with this Proxy idea

--- packages/liveblocks-storage/src/server/Server.ts
+++ packages/liveblocks-storage/src/server/Server.ts
@@ -210,7 +210,9 @@ export class Server {
     try {
       return {
         ok: true,
-        value: this.#cache.mutate((root) => mutationFn(root, ...args)),
+        value: this.#cache.mutate((root) =>
+          mutationFn({ root, rootProxy: root.makeProxy() }, ...args)
+        ),
       };
     } catch (e) {
       return {
