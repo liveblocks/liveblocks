1a8c5887dc1302a8c87f478d86ebfb0985065270
Vincent Driessen <me@nvie.com>
2024-12-17 17:20:43 +0100

    Allow MutableSignal.mutate() to be called without a callback function

--- packages/liveblocks-core/src/lib/signals.ts
+++ packages/liveblocks-core/src/lib/signals.ts
@@ -418,9 +418,9 @@ export class MutableSignal<T extends object> extends AbstractSignal<T> {
    * If the callback explicitly returns `false`, it's assumed that the state
    * was not changed.
    */
-  mutate(callback: (state: T) => unknown): void {
+  mutate(callback?: (state: T) => unknown): void {
     batch(() => {
-      const result = callback(this.#state);
+      const result = callback ? callback(this.#state) : true;
       if (result !== null && typeof result === "object" && "then" in result) {
         raise("MutableSignal.mutate() does not support async callbacks");
       }
