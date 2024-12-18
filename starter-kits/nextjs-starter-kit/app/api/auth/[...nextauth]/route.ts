e2205d25f9a75c5da89ba763013118884190d424
Chris Nicholas <ctnicholasdev@gmail.com>
2024-12-18 22:07:41 +0000

    feat: Start upgrading

--- starter-kits/nextjs-starter-kit/app/api/auth/[...nextauth]/route.ts
+++ starter-kits/nextjs-starter-kit/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,6 @@
-export { GET, POST } from "@/auth";
+export {
+  /* @next-codemod-error `GET` export is re-exported. Check if this component uses `params` or `searchParams`*/
+  GET /* @next-codemod-error `POST` export is re-exported. Check if this component uses `params` or `searchParams`*/,
+  POST,
+} from "@/auth";
 export const runtime = "edge"; // optional
--- starter-kits/nextjs-starter-kit/app/text/[id]/page.tsx
+++ starter-kits/nextjs-starter-kit/app/text/[id]/page.tsx
@@ -1,11 +1,13 @@
 import { getDocument } from "@/lib/actions";
 import { TextDocumentView } from "./TextDocumentView";
 
-export default async function Whiteboard({
-  params: { id },
-}: {
-  params: { id: string };
+export default async function Whiteboard(props: {
+  params: Promise<{ id: string }>;
 }) {
+  const params = await props.params;
+
+  const { id } = params;
+
   const { data = null, error = null } = await getDocument({ documentId: id });
 
   return <TextDocumentView initialDocument={data} initialError={error} />;
