455ae7ff2a1b0b2096efe53f11e9e8355e86ba58
Vincent Driessen <me@nvie.com>
2025-01-07 15:20:02 +0100

    Retain explicit-undefined in stringify results (for now)
    This is necessary due to the following mismatch between our front-end
    and back-end. Once this bug is fixed, we can remove this workaround from
    stringify/unstringify:
    
        https://linear.app/liveblocks/issue/LB-1333/officially-support-querying-for-absence-of-metadata

--- packages/liveblocks-core/src/lib/__tests__/stringify.test.ts
+++ packages/liveblocks-core/src/lib/__tests__/stringify.test.ts
@@ -1,4 +1,4 @@
-import { stringify } from "../stringify";
+import { stringify, unstringify } from "../stringify";
 
 describe("stringify", () => {
   it("returns the same result as JSON.stringify", () => {
@@ -16,7 +16,6 @@ describe("stringify", () => {
     expect(stringify(2)).toEqual(JSON.stringify(2));
     expect(stringify(true)).toEqual(JSON.stringify(true));
     expect(stringify(null)).toEqual(JSON.stringify(null));
-    expect(stringify(undefined)).toEqual(JSON.stringify(undefined));
   });
 
   it("supports objects in a stable way", () => {
@@ -42,9 +41,27 @@ describe("stringify", () => {
     );
   });
 
-  it("ignores undefined keys", () => {
-    expect(stringify([{ a: 2, b: true }])).toEqual(
+  it("maintains explicitly-undefined keys", () => {
+    expect(stringify(undefined)).toEqual(JSON.stringify("_explicit_undefined"));
+    expect(stringify([{ b: true, c: undefined, a: 2 }])).toEqual(
+      '[{"a":2,"b":true,"c":"_explicit_undefined"}]'
+    );
+    expect(stringify([{ a: 2, b: true }])).not.toEqual(
       stringify([{ b: true, c: undefined, a: 2 }])
     );
+  });
+
+  it("parse back explicit-undefined keys correctly", () => {
+    expect(unstringify(stringify(undefined))).toEqual(undefined);
+    expect(
+      unstringify(stringify([{ b: true, c: undefined, a: 2 }, 3]))
+    ).toEqual([{ b: true, a: 2 }, 3]);
+
+    // @ts-expect-error this is fine
+    const [parsed] = unstringify(stringify([{ a: 1, b: undefined }]));
+    expect(parsed).toEqual({ a: 1 });
+    expect(parsed.b).toEqual(undefined);
+    expect("b" in parsed).toEqual(true); // Retain explicit-undefined!
+    expect("non-existing" in parsed).toEqual(false);
   });
 });
