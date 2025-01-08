e041c9e40088cc167d38ec281abf71561c2e962a
yousefed <yousefdardiry@gmail.com>
2025-01-08 15:32:14 +0100

    options moved to hook instead of extension

--- packages/liveblocks-react-blocknote/src/BlockNoteLiveblocksExtension.ts
+++ packages/liveblocks-react-blocknote/src/BlockNoteLiveblocksExtension.ts
@@ -1,17 +1,10 @@
 
 import { useLiveblocksExtension as useTipTapLiveblocksExtension } from "@liveblocks/react-tiptap";
 
-export type LiveblocksExtensionOptions = Parameters<ReturnType<typeof useTipTapLiveblocksExtension>["configure"]>[0];
+export type LiveblocksExtensionOptions = Parameters<typeof useTipTapLiveblocksExtension>[0];
 
 export const useLiveblocksExtension = (options: LiveblocksExtensionOptions = {}) => {
-  const extension = useTipTapLiveblocksExtension();
+  const extension = useTipTapLiveblocksExtension({ mentions: false, ...options });
 
-  return extension.extend({
-    addExtensions() {
-      const ret = this.parent?.() ?? [];
-
-      // filter out the liveblocks mention extension, because this will be added via the schema instead of as an extension (see schema.ts)
-      return ret.filter((ext) => ext.name !== "liveblocksMention");
-    },
-  }).configure(options);
+  return extension;
 };
