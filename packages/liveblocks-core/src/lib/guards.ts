bc35a1f60c2c3103bb5032a9cb75ee2be87250a6
Vincent Driessen <me@nvie.com>
2025-01-09 08:46:43 +0100

    Move guards into core

--- packages/liveblocks-core/src/lib/guards.ts
+++ packages/liveblocks-core/src/lib/guards.ts
@@ -1,4 +1,4 @@
-import { isPlainObject } from "@liveblocks/core";
+import { isPlainObject } from "./utils";
 
 /**
  * Check if value is of shape { startsWith: string }
@@ -7,6 +7,8 @@ export function isStartsWith(blob: unknown): blob is { startsWith: string } {
   return isPlainObject(blob) && isString(blob.startsWith);
 }
 
+// Using inference for this type guard is better than manually annotating it
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function isString(value: unknown) {
   return typeof value === "string";
 }
