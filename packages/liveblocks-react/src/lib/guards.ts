2506d6366054cf69f576c36a04d7d4f8b113a3ab
Vincent Driessen <me@nvie.com>
2025-01-09 08:42:57 +0100

    TS will now automatically infer these ❤️
    Inference is better than using explicit type guards, since they will
    actually reflect the truth.

--- packages/liveblocks-react/src/lib/guards.ts
+++ packages/liveblocks-react/src/lib/guards.ts
@@ -7,6 +7,6 @@ export function isStartsWith(blob: unknown): blob is { startsWith: string } {
   return isPlainObject(blob) && isString(blob.startsWith);
 }
 
-export function isString(value: unknown): value is string {
+export function isString(value: unknown) {
   return typeof value === "string";
 }
