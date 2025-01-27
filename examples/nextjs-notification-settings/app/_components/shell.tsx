8c7452b4e52df14bca4350ded3f5d0c32367a879
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2025-01-27 14:23:49 +0100

    do not show settings button

--- examples/nextjs-notification-settings/app/_components/shell.tsx
+++ examples/nextjs-notification-settings/app/_components/shell.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
 import { NotificationsPopover } from "./notifications-popover";
 import { SettingsButton } from "./settings-button";
 
 export function Shell({ children }: { children?: React.ReactNode }) {
+  const pathname = usePathname();
   return (
     <main className="text-base bg-background/95 text-foreground flex flex-col w-full min-h-screen">
       <div className="flex flex-row w-full items-center justify-between h-[60px] flex-none px-4 border-b border-border/80 bg-background">
@@ -10,7 +15,7 @@ export function Shell({ children }: { children?: React.ReactNode }) {
         </div>
         <div className="flex items-center justify-end">
           <NotificationsPopover />
-          <SettingsButton />
+          {pathname !== "/settings" ? <SettingsButton /> : null}
         </div>
       </div>
       {children}
