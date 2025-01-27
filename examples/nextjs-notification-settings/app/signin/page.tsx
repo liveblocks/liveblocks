99717cbcbe0f67c3f40d3f5f6e3b58744388eff9
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2025-01-27 11:30:22 +0100

    scaffold signin page

---
+++ examples/nextjs-notification-settings/app/signin/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+  return (
+    <div className="flex flex-col w-full items-center justify-center h-screen">
+      <div className="flex flex-col gap-2">Sign in page</div>
+    </div>
+  );
+}
