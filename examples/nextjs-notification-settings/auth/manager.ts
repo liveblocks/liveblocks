9362bc2dd289b4201d076f6145d5f891301d8eb0
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2025-01-27 11:14:34 +0100

    add next auth + config + manager

---
+++ examples/nextjs-notification-settings/auth/manager.ts
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth";
+
+import { authConfig } from "./config";
+import { getUser } from "@/lib/database";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    // Get extra user info from your database to pass to front-end
+    // For front end, update next-auth.d.ts with session type
+    async session({ session }: { session: any }) {
+      const userInfo = getUser(session.user.email);
+
+      if (!userInfo) {
+        throw new Error("User not found");
+      }
+
+      session.user.info = userInfo;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/signin",
+  },
+
+  ...authConfig,
+});
