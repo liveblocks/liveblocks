e58aadd1b579ea7ae93b4799e80fecb1742280d5
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2025-01-27 15:30:57 +0100

    update redirec

--- examples/nextjs-notification-settings/app/settings/page.tsx
+++ examples/nextjs-notification-settings/app/settings/page.tsx
@@ -1,16 +1,6 @@
-"use client";
-
-import { redirect } from "next/navigation";
-
-import { auth } from "@/auth/manager";
 import { UserNotificationsSettings } from "./_components/user-notifications-settings";
 
-export default async function SettingsPage() {
-  const session = await auth();
-  if (session) {
-    redirect("/");
-  }
-
+export default function SettingsPage() {
   return (
     <div className="flex flex-col w-full items-center justify-center flex-1">
       <UserNotificationsSettings />
