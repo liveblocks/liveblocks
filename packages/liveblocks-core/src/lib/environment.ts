4a58f132377506ba18a76de0c7e9bd802ce9e0d7
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2025-01-27 18:16:01 +0100

    use stringified evaluation to read objects

--- packages/liveblocks-core/src/lib/environment.ts
+++ packages/liveblocks-core/src/lib/environment.ts
@@ -1,33 +1,58 @@
-// Declaring `import.meta.env` as a global type
-declare global {
-  interface ImportMetaEnv extends Record<string, string> {}
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
-}
 type EnvConfig = {
   LIVEBLOCKS_BASE_URL: string;
   NEXT_PUBLIC_LIVEBLOCKS_BASE_URL: string;
   VITE_LIVEBLOCKS_BASE_URL: string;
 };
 
+const getProcessEnv = (): Record<string, string> | undefined => {
+  try {
+    const parts = ["process", ".env"];
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-implied-eval
+    return new Function("return " + parts.join(""))();
+  } catch {
+    return undefined;
+  }
+};
+
+const getImportMeta = (): Record<string, string> | undefined => {
+  try {
+    // Split the string to prevent parser from detecting it
+    const parts = ["import", ".meta", ".env"];
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-implied-eval
+    return new Function("return " + parts.join(""))();
+  } catch {
+    return undefined;
+  }
+};
+
 export function getEnvVar<K extends keyof EnvConfig>(
   key: K
 ): EnvConfig[K] | undefined {
   switch (key) {
-    case "LIVEBLOCKS_BASE_URL":
-      return typeof process !== "undefined"
-        ? process.env.LIVEBLOCKS_BASE_URL
-        : undefined;
-    case "NEXT_PUBLIC_LIVEBLOCKS_BASE_URL":
-      return typeof process !== "undefined"
-        ? process.env.NEXT_PUBLIC_LIVEBLOCKS_BASE_URL
-        : undefined;
-    case "VITE_LIVEBLOCKS_BASE_URL":
-      return typeof import.meta !== "undefined" &&
-        typeof import.meta.env !== "undefined"
-        ? import.meta.env.VITE_LIVEBLOCKS_BASE_URL
-        : undefined;
+    case "LIVEBLOCKS_BASE_URL": {
+      const processEnv = getProcessEnv();
+      if (processEnv) {
+        return processEnv.LIVEBLOCKS_BASE_URL ?? undefined;
+      }
+
+      return undefined;
+    }
+    case "NEXT_PUBLIC_LIVEBLOCKS_BASE_URL": {
+      const processEnv = getProcessEnv();
+      if (processEnv) {
+        return processEnv.NEXT_PUBLIC_LIVEBLOCKS_BASE_URL ?? undefined;
+      }
+
+      return undefined;
+    }
+    case "VITE_LIVEBLOCKS_BASE_URL": {
+      const importMetaEnv = getImportMeta();
+      if (importMetaEnv) {
+        return importMetaEnv.VITE_LIVEBLOCKS_BASE_URL ?? undefined;
+      }
+
+      return undefined;
+    }
     default:
       return undefined;
   }
