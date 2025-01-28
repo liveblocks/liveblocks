a9cfd841551242d61bf11435d2bc7723e7db6afe
Aurélien Dupays Dexemple <3740000+SugarDarius@users.noreply.github.com>
2025-01-28 10:53:52 +0100

    Ensure not server side for every suspense hooks (#2203)
    Co-authored-by: Vincent Driessen <me@nvie.com>

--- packages/liveblocks-react/src/liveblocks.tsx
+++ packages/liveblocks-react/src/liveblocks.tsx
@@ -37,6 +37,7 @@ import { config } from "./config";
 import { useIsInsideRoom } from "./contexts";
 import { ASYNC_OK } from "./lib/AsyncResult";
 import { count } from "./lib/itertools";
+import { ensureNotServerSide } from "./lib/ssr";
 import { useInitial, useInitialUnlessFunction } from "./lib/use-initial";
 import { useLatest } from "./lib/use-latest";
 import { use } from "./lib/use-polyfill";
@@ -409,6 +410,9 @@ function useInboxNotifications_withClient<T>(
 }
 
 function useInboxNotificationsSuspense_withClient(client: OpaqueClient) {
+  // Throw error if we're calling this hook server side
+  ensureNotServerSide();
+
   const store = getLiveblocksExtrasForClient(client).store;
 
   // Suspend until there are at least some inbox notifications
@@ -433,6 +437,9 @@ function useUnreadInboxNotificationsCount_withClient(client: OpaqueClient) {
 function useUnreadInboxNotificationsCountSuspense_withClient(
   client: OpaqueClient
 ) {
+  // Throw error if we're calling this hook server side
+  ensureNotServerSide();
+
   const store = getLiveblocksExtrasForClient(client).store;
 
   // Suspend until there are at least some inbox notifications
@@ -979,6 +986,9 @@ function useUserThreads_experimental<M extends BaseMetadata>(
 function useUserThreadsSuspense_experimental<M extends BaseMetadata>(
   options: UseUserThreadsOptions<M> = {}
 ): ThreadsAsyncSuccess<M> {
+  // Throw error if we're calling this hook server side
+  ensureNotServerSide();
+
   const client = useClient();
   const { store } = getLiveblocksExtrasForClient<M>(client);
   const queryKey = makeUserThreadsQueryKey(options.query);
