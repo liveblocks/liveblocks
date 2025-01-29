f42849ee5793fe86ecb1417dabc4f4ebb6f975ae
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2025-01-29 10:59:41 +0100

    refinements

--- examples/nextjs-notification-settings/hooks/use-toast.ts
+++ examples/nextjs-notification-settings/hooks/use-toast.ts
@@ -1,8 +1,7 @@
-"use toast";
+"use client";
 
 import * as React from "react";
-
-import type { ToastActionElement, ToastProps } from "../components/ui/toast";
+import type { ToastActionElement, ToastProps } from "@/components/ui/toast";
 
 const TOAST_LIMIT = 1;
 const TOAST_REMOVE_DELAY = 1000000;
