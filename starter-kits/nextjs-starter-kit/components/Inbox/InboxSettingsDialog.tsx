a91bf02806fb15426e1e1c000913ba9fa209a274
Chris Nicholas <ctnicholasdev@gmail.com>
2025-02-19 15:11:44 +0000

    feat: Better notification settings in starter kit

--- starter-kits/nextjs-starter-kit/components/Inbox/InboxSettingsDialog.tsx
+++ starter-kits/nextjs-starter-kit/components/Inbox/InboxSettingsDialog.tsx
@@ -1,16 +1,23 @@
-import { useNotificationSettings } from "@liveblocks/react/suspense";
+import {
+  useNotificationSettings,
+  useRoomNotificationSettings,
+} from "@liveblocks/react/suspense";
 import { ReactNode } from "react";
 import { Dialog } from "@/primitives/Dialog";
+import { Select } from "@/primitives/Select";
 import { Spinner } from "@/primitives/Spinner";
 import { Switch } from "@/primitives/Switch";
 import styles from "./InboxSettingsDialog.module.css";
 
-// Render settings that change how notifications webhooks are sent
-// These webhooks can be sent on different channels, with different settings for each
-// Below we're just using the email channel, but you could also add slack or teams, for instance
 export function InboxSettingsDialog({ children }: { children: ReactNode }) {
+  // Render settings that change how notifications webhooks are sent
+  // These webhooks can be sent on different channels, with different settings for each
+  // Below we're just using the email channel, but you could also add slack or teams, for instance
   const [{ isLoading, settings, error }, updateSettings] =
     useNotificationSettings();
+
+  // Render which thread notifications should be received (for both the inbox, and also in webhooks)
+  const [roomSettings, updateRoomSettings] = useRoomNotificationSettings();
 
   return (
     <Dialog
@@ -27,7 +34,38 @@ export function InboxSettingsDialog({ children }: { children: ReactNode }) {
         ) : (
           <div className={styles.dialog}>
             <div className={styles.switches}>
-              <h3>Emails</h3>
+              <h3>Receive thread notifications for… </h3>
+              <div className={styles.selectBox}>
+                <Select
+                  aboveOverlay
+                  initialValue="replies_and_mentions"
+                  items={[
+                    {
+                      title: "All threads",
+                      value: "all",
+                      description: "All comment and thread activity",
+                    },
+                    {
+                      title: "Participating threads",
+                      value: "replies_and_mentions",
+                      description:
+                        "All mentions and replies in threads you're participating in",
+                    },
+                    {
+                      title: "No threads",
+                      value: "none",
+                      description: "No thread notifications are received",
+                    },
+                  ]}
+                  onChange={(value) => {
+                    updateRoomSettings({
+                      threads: value as any,
+                    });
+                  }}
+                  value={roomSettings.settings.threads}
+                />
+              </div>
+              <h3>Receive emails for…</h3>
               <div className={styles.switchBox}>
                 <Switch
                   label="Document invites"
