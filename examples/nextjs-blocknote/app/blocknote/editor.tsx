d49618d6520f6e65d6a73fbe9c3fafe86f99ad0a
Jonathan Rowny <jrowny@gmail.com>
2025-02-19 17:27:26 -0800

    add blocknote example skeleton

---
+++ examples/nextjs-blocknote/app/blocknote/editor.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import NotificationsPopover from "../notifications-popover";
+
+
+export default function TiptapEditor() {
+
+
+
+  return (
+    <div className="relative min-h-screen flex flex-col">
+      <div className="h-[60px] flex items-center justify-end px-4 border-b border-border/80 bg-background">
+
+        <NotificationsPopover />
+      </div>
+      <div className="border-b border-border/80 bg-background">
+      </div>
+      <div className="relative flex flex-row justify-between w-full py-16 xl:pl-[250px] pl-[100px] gap-[50px]">
+        <div className="relative flex flex-1 flex-col gap-2">
+
+        </div>
+
+        <div className="xl:[&:not(:has(.lb-tiptap-anchored-threads))]:pr-[200px] [&:not(:has(.lb-tiptap-anchored-threads))]:pr-[50px]">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
