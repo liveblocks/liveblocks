dce30931adc961dc4abb7dd0053112a7d06368fc
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2025-03-07 17:04:53 +0100

    create new generic protocol for comment body conversion

--- packages/liveblocks-emails/src/comment-body.tsx
+++ packages/liveblocks-emails/src/comment-body.tsx
@@ -4,6 +4,7 @@ import type {
   CommentBody,
   CommentBodyLink,
   CommentBodyMention,
+  CommentBodyParagraph,
   CommentBodyText,
   DU,
   ResolveUsersArgs,
@@ -23,6 +24,160 @@ import type { ComponentType, ReactNode } from "react";
 import { MENTION_CHARACTER } from "./lib/constants";
 import type { CSSProperties } from "./lib/css-properties";
 import { toInlineCSSString } from "./lib/css-properties";
+
+export type CommentBodyContainerElementArgs<T> = {
+  /**
+   * The blocks of the comment body
+   */
+  children: T[];
+};
+
+export type CommentBodyParagraphElementArgs<T> = {
+  /**
+   * The paragraph element.
+   */
+  element: CommentBodyParagraph;
+  /**
+   * The text content of the paragraph.
+   */
+  children: T[];
+};
+
+export type CommentBodyTextElementArgs = {
+  /**
+   * The text element.
+   */
+  element: CommentBodyText;
+};
+
+export type CommentBodyLinkElementArgs = {
+  /**
+   * The link element.
+   */
+  element: CommentBodyLink;
+
+  /**
+   * The absolute URL of the link.
+   */
+  href: string;
+};
+
+export type CommentBodyMentionElementArgs<U extends BaseUserMeta = DU> = {
+  /**
+   * The mention element.
+   */
+  element: CommentBodyMention;
+
+  /**
+   * The mention's user info, if the `resolvedUsers` option was provided.
+   */
+  user?: U["info"];
+};
+
+/**
+ * Protocol:
+ * Comment body elements to be converted to a custom format `T`
+ *
+ * Fallback to plain text as a `string`.
+ */
+export type ConvertCommentBodyElements<T, U extends BaseUserMeta = DU> = {
+  /**
+   * The container element used to display comment body blocks.
+   */
+  container: (args: CommentBodyContainerElementArgs<T>) => T;
+  /**
+   * The paragraph element used to display paragraphs.
+   */
+  paragraph: (args: CommentBodyParagraphElementArgs<T>, index: number) => T;
+  /**
+   * The text element used to display text elements.
+   */
+  text: (args: CommentBodyTextElementArgs, index: number) => T;
+  /**
+   * The link element used to display links.
+   */
+  link: (args: CommentBodyLinkElementArgs, index: number) => T;
+  /**
+   * The mention element used to display mentions.
+   */
+  mention: (args: CommentBodyMentionElementArgs<U>, index: number) => T;
+};
+
+export type ConvertCommentBody<T, U extends BaseUserMeta = DU> = {
+  /**
+   * A function that returns user info from user IDs.
+   * You should return a list of user objects of the same size, in the same order.
+   */
+  resolveUsers?: (
+    args: ResolveUsersArgs
+  ) => Awaitable<(U["info"] | undefined)[] | undefined>;
+
+  /**
+   * The elements used to customize the resulting format `T`.
+   */
+  elements: ConvertCommentBodyElements<T, U>;
+};
+
+/**
+ * Convert a `CommentBody` into a custom format `T`
+ */
+export async function convertCommentBody<T, U extends BaseUserMeta = DU>(
+  body: CommentBody,
+  options: ConvertCommentBody<T, U>
+): Promise<T> {
+  const resolvedUsers = await resolveUsersInCommentBody(
+    body,
+    options?.resolveUsers
+  );
+
+  const blocks: T[] = body.content
+    .map((block, index) => {
+      switch (block.type) {
+        case "paragraph": {
+          const children: T[] = block.children
+            .map((inline, inlineIndex) => {
+              if (isCommentBodyMention(inline)) {
+                return options.elements.mention(
+                  {
+                    element: inline,
+                    user: resolvedUsers.get(inline.id),
+                  },
+                  inlineIndex
+                );
+              }
+
+              if (isCommentBodyLink(inline)) {
+                const href = toAbsoluteUrl(inline.url) ?? inline.url;
+                return options.elements.link(
+                  { element: inline, href },
+                  inlineIndex
+                );
+              }
+
+              if (isCommentBodyText(inline)) {
+                return options.elements.text({ element: inline }, inlineIndex);
+              }
+
+              return null;
+            })
+            .filter((b) => b !== null);
+
+          return options.elements.paragraph(
+            { element: block, children },
+            index
+          );
+        }
+        default:
+          console.warn(
+            `Unsupported comment body block type: "${JSON.stringify(block.type)}"`
+          );
+          return null;
+      }
+    })
+    .filter((b) => b !== null);
+
+  return options.elements.container({ children: blocks });
+}
 
 export type CommentBodyContainerComponentProps = {
   /**
@@ -161,67 +316,42 @@ export async function convertCommentBodyAsReact(
     ...baseComponents,
     ...options?.components,
   };
-  const resolvedUsers = await resolveUsersInCommentBody(
-    body,
-    options?.resolveUsers
-  );
-
-  const blocks = body.content.map((block, index) => {
-    switch (block.type) {
-      case "paragraph": {
-        const children = block.children.map((inline, inlineIndex) => {
-          if (isCommentBodyMention(inline)) {
-            return inline.id ? (
-              <Components.Mention
-                key={`lb-comment-body-mention-${inlineIndex}`}
-                element={inline}
-                user={resolvedUsers.get(inline.id)}
-              />
-            ) : null;
-          }
-
-          if (isCommentBodyLink(inline)) {
-            const href = toAbsoluteUrl(inline.url) ?? inline.url;
-            return (
-              <Components.Link
-                key={`lb-comment-body-link-${inlineIndex}`}
-                element={inline}
-                href={href}
-              />
-            );
-          }
-
-          if (isCommentBodyText(inline)) {
-            return (
-              <Components.Text
-                key={`lb-comment-body-text-${inlineIndex}`}
-                element={inline}
-              />
-            );
-          }
-
-          return null;
-        });
-
-        return (
-          <Components.Paragraph key={`lb-comment-body-paragraph-${index}`}>
-            {children}
-          </Components.Paragraph>
-        );
-      }
-      default:
-        console.warn(
-          `Unsupported comment body block type: "${JSON.stringify(block.type)}"`
-        );
-        return null;
-    }
+  return await convertCommentBody<ReactNode, BaseUserMeta>(body, {
+    resolveUsers: options?.resolveUsers,
+    elements: {
+      container: ({ children }) => (
+        <Components.Container key={"lb-comment-body-container"}>
+          {children}
+        </Components.Container>
+      ),
+      paragraph: ({ children }, index) => (
+        <Components.Paragraph key={`lb-comment-body-paragraph-${index}`}>
+          {children}
+        </Components.Paragraph>
+      ),
+      text: ({ element }, index) => (
+        <Components.Text
+          key={`lb-comment-body-text-${index}`}
+          element={element}
+        />
+      ),
+      link: ({ element, href }, index) => (
+        <Components.Link
+          key={`lb-comment-body-link-${index}`}
+          element={element}
+          href={href}
+        />
+      ),
+      mention: ({ element, user }, index) =>
+        element.id ? (
+          <Components.Mention
+            key={`lb-comment-body-mention-${index}`}
+            element={element}
+            user={user}
+          />
+        ) : null,
+    },
   });
-
-  return (
-    <Components.Container key={"lb-comment-body-container"}>
-      {blocks}
-    </Components.Container>
-  );
 }
 
 export type ConvertCommentBodyAsHtmlStyles = {
