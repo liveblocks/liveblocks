b15d63319c8ede448cacd0f0928afb91fcd34044
Chris Nicholas <ctnicholasdev@gmail.com>
2025-03-10 15:44:41 +0000

    feat: Upgrade one to vite

--- tutorial/react/getting-started/joining-a-room/initial/App.tsx
+++ tutorial/react/getting-started/joining-a-room/initial/App.tsx
@@ -9,6 +9,8 @@ export default function App() {
   const roomId = "{% ROOM_ID %}";
   const publicApiKey = "{% LIVEBLOCKS_PUBLIC_KEY %}";
 
+  // vite pls
+
   return (
     <LiveblocksProvider publicApiKey={publicApiKey}>
       <div>Not in a room</div>
