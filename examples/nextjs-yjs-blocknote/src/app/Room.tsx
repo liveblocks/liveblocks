87a8fbd23109727cc11555b189b63ea0fdc26ae9
Chris Nicholas <ctnicholasdev@gmail.com>
2025-03-13 12:55:26 +0000

    chore: Tidy example

--- examples/nextjs-yjs-blocknote/src/app/Room.tsx
+++ examples/nextjs-yjs-blocknote/src/app/Room.tsx
@@ -7,7 +7,7 @@ import { ClientSideSuspense } from "@liveblocks/react";
 import { Loading } from "@/components/Loading";
 
 export function Room({ children }: { children: ReactNode }) {
-  const roomId = useExampleRoomId("liveblocks:examples:nextjs-yjs-blocknote");
+  const roomId = useExampleRoomId("liveblocks:examples:nextjs-blocknote");
 
   return (
     <RoomProvider
