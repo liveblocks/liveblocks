482f31f8208e5b81c6def702abf9228824f7d0e7
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2025-03-17 15:58:27 +0100

    Update textMentions settings

--- packages/liveblocks-core/src/types/RoomNotificationSettings.ts
+++ packages/liveblocks-core/src/types/RoomNotificationSettings.ts
@@ -1,6 +1,6 @@
 type RoomThreadsNotificationSettings = "all" | "replies_and_mentions" | "none";
 
-type RoomTextMentionsNotificationSettings = "all" | "self" | "none";
+type RoomTextMentionsNotificationSettings = "mine" | "none";
 
 export type RoomNotificationSettings = {
   threads: RoomThreadsNotificationSettings;
