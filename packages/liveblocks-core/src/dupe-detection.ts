6f840ecad00c3e49accda5ce74bb56f05fa15a7e
Vincent Driessen <me@nvie.com>
2025-03-19 10:56:07 +0100

    Only loosen dupe detection of exactly-same versions when inside a testing context
    In prod/dev contexts, we definitely still want to error when this
    situation occurs.

--- packages/liveblocks-core/src/dupe-detection.ts
+++ packages/liveblocks-core/src/dupe-detection.ts
@@ -39,7 +39,7 @@ export function detectDupes(
 
   if (!g[pkgId]) {
     g[pkgId] = pkgBuildInfo;
-  } else if (g[pkgId] === pkgBuildInfo) {
+  } else if (process.env.NODE_ENV === "test" && g[pkgId] === pkgBuildInfo) {
     // Allow it, see https://github.com/liveblocks/liveblocks/pull/1004
   } else {
     const msg = [
