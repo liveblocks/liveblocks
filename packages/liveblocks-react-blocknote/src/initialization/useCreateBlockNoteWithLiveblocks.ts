e4c32514a0e14e554fd13eb6ce8122a770085ed5
Jonathan <jrowny@gmail.com>
2025-03-19 09:30:54 -0700

    Blocknote package (#2239)
    Co-authored-by: Vincent Driessen <me@nvie.com>
    Co-authored-by: Chris Nicholas <ctnicholasdev@gmail.com>
    Co-authored-by: Marc Bouchenoire <bouchenoire.marc@gmail.com>

---
+++ packages/liveblocks-react-blocknote/src/initialization/useCreateBlockNoteWithLiveblocks.ts
@@ -0,0 +1,38 @@
+import type {
+  BlockNoteEditorOptions,
+  BlockSchema,
+  DefaultBlockSchema,
+  DefaultInlineContentSchema,
+  DefaultStyleSchema,
+  InlineContentSchema,
+  StyleSchema,
+} from "@blocknote/core";
+import { useCreateBlockNote } from "@blocknote/react";
+import type { DependencyList } from "react";
+
+import type { LiveblocksExtensionOptions } from "../BlockNoteLiveblocksExtension";
+import { useLiveblocksExtension } from "../BlockNoteLiveblocksExtension";
+import { withLiveblocksEditorOptions } from "./liveblocksEditorOptions";
+
+/**
+ * Function that can be used instead of standard useCreateBlockNote to add Liveblocks support
+ */
+export const useCreateBlockNoteWithLiveblocks = <
+  B extends BlockSchema = DefaultBlockSchema,
+  I extends InlineContentSchema = DefaultInlineContentSchema,
+  S extends StyleSchema = DefaultStyleSchema,
+>(
+  blocknoteOptions: Partial<BlockNoteEditorOptions<B, I, S>> = {},
+  liveblocksOptions: LiveblocksExtensionOptions = undefined,
+  deps: DependencyList = []
+) => {
+  const liveblocksExtension = useLiveblocksExtension(liveblocksOptions);
+  return useCreateBlockNote(
+    withLiveblocksEditorOptions(
+      liveblocksExtension,
+      blocknoteOptions,
+      liveblocksOptions
+    ),
+    [liveblocksExtension, ...deps]
+  );
+};
