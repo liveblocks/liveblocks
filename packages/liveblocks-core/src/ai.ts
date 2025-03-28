c9ee384b14bd100c5ef8cf7f0fb5d41da166a5d2
Vincent Driessen <me@nvie.com>
2025-03-28 12:23:19 +0100

    Connect to new AI socket URL

--- packages/liveblocks-core/src/ai.ts
+++ packages/liveblocks-core/src/ai.ts
@@ -467,8 +467,7 @@ export function makeCreateSocketDelegateForAi(
 
     const url = new URL(baseUrl);
     url.protocol = url.protocol === "http:" ? "ws" : "wss";
-    url.pathname = "/v7"; // Do we need this?
-    url.searchParams.set("ai", "true");
+    url.pathname = "/ai/v0"; // Do we need this?
     // TODO: don't allow public key to do this
     if (authValue.type === "secret") {
       url.searchParams.set("tok", authValue.token.raw);
