fea635893413a787d2a4fab6b88769ca354ff520
Chris Nicholas <ctnicholasdev@gmail.com>
2025-03-31 14:13:13 +0100

    chore: Move styles and make buttons work in prod

--- starter-kits/nextjs-starter-kit/app/note/[id]/page.tsx
+++ starter-kits/nextjs-starter-kit/app/note/[id]/page.tsx
@@ -1,7 +1,5 @@
 import { getDocument } from "@/lib/actions";
 import { NoteDocumentView } from "./NoteDocumentView";
-import "@blocknote/mantine/style.css";
-import "@/components/NoteEditor/blocknote.css";
 
 export default async function Note(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
