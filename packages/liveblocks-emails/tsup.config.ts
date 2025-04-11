346e700c3b3cafeb7733f565de3ee17750acb3db
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2025-04-11 14:29:39 +0200

    Add declaration maps for tsup projects

--- packages/liveblocks-emails/tsup.config.ts
+++ packages/liveblocks-emails/tsup.config.ts
@@ -1,8 +1,9 @@
+import { execSync } from "child_process";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  dts: true,
+  dts: false,
   splitting: true,
   clean: true,
   format: ["esm", "cjs"],
@@ -12,5 +13,11 @@ export default defineConfig({
     // Replace __VERSION__ globals with concrete version
     const pkg = require("./package.json");
     options.define.__VERSION__ = JSON.stringify(pkg.version);
+  },
+
+  async onSuccess() {
+    execSync("tsc --project tsconfig.dts.json", {
+      stdio: "inherit",
+    });
   },
 });
--- packages/liveblocks-react/tsup.config.ts
+++ packages/liveblocks-react/tsup.config.ts
@@ -1,9 +1,10 @@
+import { execSync } from "child_process";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts", "src/suspense.ts", "src/_private.ts"],
-  dts: true,
-  splitting: true,
+  dts: false,
+  splitting: false,
   clean: true,
   format: ["esm", "cjs"],
   sourcemap: true,
@@ -12,5 +13,11 @@ export default defineConfig({
     // Replace __VERSION__ globals with concrete version
     const pkg = require("./package.json");
     options.define.__VERSION__ = JSON.stringify(pkg.version);
+  },
+
+  async onSuccess() {
+    execSync("tsc --project tsconfig.dts.json", {
+      stdio: "inherit",
+    });
   },
 });
