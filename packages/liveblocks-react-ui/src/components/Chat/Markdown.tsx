73663919820accaca64254227525e9cf44fa3ba3
Nimesh Nayaju <nimeshnayaju@gmail.com>
2025-04-17 13:58:06 -0400

    Better handling of tokens from `marked.js`

--- packages/liveblocks-react-ui/src/components/Chat/Markdown.tsx
+++ packages/liveblocks-react-ui/src/components/Chat/Markdown.tsx
@@ -1,18 +1,104 @@
-import { Lexer, type Token } from "marked";
-import { Fragment, useMemo } from "react";
-
+import { Lexer, type Tokens } from "marked";
+import { type ReactNode, useMemo } from "react";
 export function Markdown({ content }: { content: string }) {
   const tokens = useMemo(() => {
     return new Lexer().lex(content);
   }, [content]);
 
-  return tokens.map((token, index) => (
-    <BlockTokenComp token={token} key={index} />
-  ));
+  return tokens.map((token, index) => {
+    return <BlockTokenComp token={token as BlockToken} key={index} />;
+  });
 }
 
-function BlockTokenComp({ token }: { token: Token }) {
+/**
+ * Block level tokens include:
+ * - space
+ * - code
+ * - blockquote
+ * - html
+ * - heading
+ * - hr
+ * - list
+ * - paragraph
+ * - table
+ */
+type BlockToken =
+  | Tokens.Space
+  | Tokens.Code
+  | Tokens.Blockquote
+  | Tokens.HTML
+  | Tokens.Heading
+  | Tokens.Hr
+  | Tokens.List
+  | Tokens.Paragraph
+  | Tokens.Table;
+
+function BlockTokenComp({ token }: { token: BlockToken }) {
   switch (token.type) {
+    case "space": {
+      return null;
+    }
+    case "code": {
+      let language: string | null = null;
+      if (token.lang !== undefined) {
+        language = token.lang.match(/^\S*/)?.[0] ?? null;
+      }
+      return (
+        <pre data-language={language || undefined}>
+          <code>{token.text}</code>
+        </pre>
+      );
+    }
+    case "blockquote": {
+      const tokens: BlockToken[] = [];
+      for (let i = 0; i < token.tokens.length; i++) {
+        switch (token.tokens[i]!.type) {
+          case "space":
+          case "code":
+          case "blockquote":
+          case "html":
+          case "heading":
+          case "hr":
+          case "list":
+          case "paragraph":
+          case "table": {
+            tokens.push(token.tokens[i] as BlockToken);
+            break;
+          }
+          case "text": {
+            const texts: Tokens.Text[] = [token.tokens[i] as Tokens.Text];
+            while (
+              i + 1 < token.tokens.length &&
+              token.tokens[i + 1]!.type === "text"
+            ) {
+              i++;
+              texts.push(token.tokens[i] as Tokens.Text);
+            }
+            tokens.push({
+              type: "paragraph",
+              tokens: texts,
+              raw: texts.map((text) => text.raw).join(""),
+              text: texts.map((text) => text.text).join(""),
+            } satisfies Tokens.Paragraph);
+            break;
+          }
+          default: {
+            continue;
+          }
+        }
+      }
+
+      return (
+        <blockquote>
+          {tokens.map((token, index) => {
+            return <BlockTokenComp token={token} key={index} />;
+          })}
+        </blockquote>
+      );
+    }
+    case "html": {
+      return token.text;
+    }
     case "heading": {
       let HeadingTag: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
       switch (token.depth) {
@@ -40,62 +126,299 @@ function BlockTokenComp({ token }: { token: Token }) {
       }
       return (
         <HeadingTag>
-          {(token.tokens ?? []).map((token, index) => (
-            <InlineTokenComp token={token} key={index} />
+          {token.tokens.map((token, index) => (
+            <InlineTokenComp key={index} token={token as InlineToken} />
           ))}
         </HeadingTag>
+      );
+    }
+    case "hr": {
+      return <hr />;
+    }
+    case "list": {
+      const ListTag = token.ordered ? "ol" : "ul";
+
+      return (
+        <ListTag>
+          {token.items.map((item, index) => {
+            // A 'loose' list item in Markdown is one where the content is wrapped in a paragraph (or potentially other block) token
+            if (item.loose) {
+              // If the list item is a task list item, we need to add a checkbox to the start of the token
+              if (item.task) {
+                const tokens = [...item.tokens];
+                if (tokens[0]?.type === "paragraph") {
+                  const token = tokens[0] as Tokens.Paragraph;
+                  token.tokens.unshift(
+                    {
+                      type: "checkbox",
+                      checked: item.checked,
+                      raw: "",
+                    },
+                    {
+                      type: "text",
+                      text: " ",
+                      raw: " ",
+                      escaped: false,
+                    }
+                  );
+                } else {
+                  tokens.unshift(
+                    {
+                      type: "checkbox",
+                      checked: item.checked,
+                      raw: "",
+                    },
+                    {
+                      type: "text",
+                      text: " ",
+                      raw: " ",
+                      escaped: false,
+                    }
+                  );
+                }
+
+                const items: BlockToken[] = [];
+                for (let i = 0; i < tokens.length; i++) {
+                  switch (tokens[i]!.type) {
+                    case "space":
+                    case "code":
+                    case "blockquote":
+                    case "html":
+                    case "heading":
+                    case "hr":
+                    case "list":
+                    case "paragraph":
+                    case "table": {
+                      items.push(tokens[i] as BlockToken);
+                      break;
+                    }
+                    case "text":
+                    case "checkbox": {
+                      const texts: (
+                        | Tokens.Text
+                        | {
+                            type: "checkbox";
+                            checked: boolean;
+                            raw: string;
+                            text: string;
+                          }
+                      )[] = [
+                        tokens[i] as
+                          | Tokens.Text
+                          | {
+                              type: "checkbox";
+                              checked: boolean;
+                              raw: string;
+                              text: string;
+                            },
+                      ];
+                      while (
+                        i + 1 < tokens.length &&
+                        tokens[i + 1]!.type === "text"
+                      ) {
+                        i++;
+                        texts.push(tokens[i] as Tokens.Text);
+                      }
+                      items.push({
+                        type: "paragraph",
+                        tokens: texts,
+                        raw: texts.map((text) => text.raw).join(""),
+                        text: texts.map((text) => text.text).join(""),
+                      } satisfies Tokens.Paragraph);
+                      break;
+                    }
+                    default: {
+                      continue;
+                    }
+                  }
+                }
+
+                return (
+                  <li key={index}>
+                    {items.map((token, index) => {
+                      return <BlockTokenComp token={token} key={index} />;
+                    })}
+                  </li>
+                );
+              } else {
+                const tokens: BlockToken[] = [];
+                for (let i = 0; i < item.tokens.length; i++) {
+                  switch (item.tokens[i]!.type) {
+                    case "space":
+                    case "code":
+                    case "blockquote":
+                    case "html":
+                    case "heading":
+                    case "hr":
+                    case "list":
+                    case "paragraph":
+                    case "table": {
+                      tokens.push(item.tokens[i] as BlockToken);
+                      break;
+                    }
+                    case "text": {
+                      const texts: Tokens.Text[] = [
+                        item.tokens[i] as Tokens.Text,
+                      ];
+                      while (
+                        i + 1 < item.tokens.length &&
+                        item.tokens[i + 1]!.type === "text"
+                      ) {
+                        i++;
+                        texts.push(item.tokens[i] as Tokens.Text);
+                      }
+                      tokens.push({
+                        type: "paragraph",
+                        tokens: texts,
+                        raw: texts.map((text) => text.raw).join(""),
+                        text: texts.map((text) => text.text).join(""),
+                      } satisfies Tokens.Paragraph);
+                      break;
+                    }
+                    default: {
+                      continue;
+                    }
+                  }
+                }
+
+                return (
+                  <li key={index}>
+                    {tokens.map((token, index) => {
+                      return <BlockTokenComp token={token} key={index} />;
+                    })}
+                  </li>
+                );
+              }
+            } else {
+              const Items: ReactNode = item.tokens.map((token, index) => {
+                switch (token.type) {
+                  case "space":
+                  case "code":
+                  case "blockquote":
+                  case "html":
+                  case "heading":
+                  case "hr":
+                  case "list":
+                  case "paragraph":
+                  case "table": {
+                    return (
+                      <BlockTokenComp token={token as BlockToken} key={index} />
+                    );
+                  }
+                  case "text": {
+                    return (
+                      <InlineTokenComp
+                        token={token as Tokens.Text}
+                        key={index}
+                      />
+                    );
+                  }
+                  default: {
+                    return null;
+                  }
+                }
+              });
+              if (item.task) {
+                return (
+                  <li key={index}>
+                    <input type="checkbox" disabled checked={item.checked} />{" "}
+                    {Items}
+                  </li>
+                );
+              } else {
+                return <li key={index}>{Items}</li>;
+              }
+            }
+          })}
+        </ListTag>
       );
     }
     case "paragraph": {
       return (
         <p>
-          {(token.tokens ?? []).map((token, index) => (
-            <InlineTokenComp token={token} key={index} />
+          {token.tokens.map((token, index) => (
+            <InlineTokenComp key={index} token={token as InlineToken} />
           ))}
         </p>
       );
     }
-    case "text": {
-      // TODO: Ask Nimesh about this. I had to add this `"tokens" in ...` check
-      // here to work around a TS issue.
-      if ("tokens" in token && token.tokens !== undefined) {
-        return (
-          <Fragment>
-            {(token.tokens ?? []).map((token, index) => (
-              <InlineTokenComp token={token} key={index} />
-            ))}
-          </Fragment>
-        );
-      }
-      if (typeof token.text !== "string") return null;
-      return token.text;
-    }
-    case "blockquote": {
+    case "table": {
       return (
-        <blockquote>
-          {(token.tokens ?? []).map((token, index) => (
-            <BlockTokenComp token={token} key={index} />
-          ))}
-        </blockquote>
+        <table>
+          <thead>
+            <tr>
+              {token.header.map((cell, index) => {
+                return (
+                  <th key={index} align={cell.align ?? undefined}>
+                    {cell.tokens.map((token, index) => (
+                      <InlineTokenComp
+                        key={index}
+                        token={token as InlineToken}
+                      />
+                    ))}
+                  </th>
+                );
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {token.rows.map((row, index) => {
+              return (
+                <tr key={index}>
+                  {row.map((cell, index) => {
+                    return (
+                      <td key={index} align={cell.align ?? undefined}>
+                        {cell.tokens.map((token, index) => (
+                          <InlineTokenComp
+                            key={index}
+                            token={token as InlineToken}
+                          />
+                        ))}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       );
     }
-    default:
-      return null;
   }
 }
 
-function InlineTokenComp({ token }: { token: Token }) {
+/**
+ * Inline tokens include:
+ * - strong
+ * - em
+ * - codespan
+ * - br
+ * - del
+ * - link
+ * - image
+ * - text
+ */
+type InlineToken =
+  | Tokens.Strong
+  | Tokens.Em
+  | Tokens.Codespan
+  | Tokens.Br
+  | Tokens.Del
+  | Tokens.Link
+  | Tokens.Image
+  | Tokens.Text
+  | Tokens.Escape;
+function InlineTokenComp({
+  token,
+}: {
+  token: InlineToken | { type: "checkbox"; checked: boolean };
+}) {
   switch (token.type) {
-    case "text":
-    case "escape": {
-      if (typeof token.text !== "string") return null;
-      return token.text;
-    }
     case "strong": {
       return (
         <strong>
-          {(token.tokens ?? []).map((token, index) => (
-            <InlineTokenComp token={token} key={index} />
+          {token.tokens.map((token, index) => (
+            <InlineTokenComp key={index} token={token as InlineToken} />
           ))}
         </strong>
       );
@@ -103,29 +426,10 @@ function InlineTokenComp({ token }: { token: Token }) {
     case "em": {
       return (
         <em>
-          {(token.tokens ?? []).map((token, index) => (
-            <InlineTokenComp token={token} key={index} />
+          {token.tokens.map((token, index) => (
+            <InlineTokenComp key={index} token={token as InlineToken} />
           ))}
         </em>
-      );
-    }
-    case "del": {
-      return (
-        <del>
-          {(token.tokens ?? []).map((token, index) => (
-            <InlineTokenComp token={token} key={index} />
-          ))}
-        </del>
-      );
-    }
-    case "link": {
-      if (typeof token.href !== "string") return null;
-      return (
-        <a href={token.href}>
-          {(token.tokens ?? []).map((token, index) => (
-            <InlineTokenComp token={token} key={index} />
-          ))}
-        </a>
       );
     }
     case "codespan": {
@@ -134,7 +438,85 @@ function InlineTokenComp({ token }: { token: Token }) {
     case "br": {
       return <br />;
     }
-    default:
+    case "del": {
+      return (
+        <del>
+          {token.tokens.map((token, index) => (
+            <InlineTokenComp key={index} token={token as InlineToken} />
+          ))}
+        </del>
+      );
+    }
+    case "link": {
+      let href: string | null;
+      try {
+        const url = new URL(token.href);
+        if (url.protocol === "http:" || url.protocol === "https:") {
+          href = url.toString();
+        } else {
+          href = null;
+        }
+      } catch {
+        href = null;
+      }
+
+      if (href === null) {
+        return token.tokens.map((token, index) => (
+          <InlineTokenComp key={index} token={token as InlineToken} />
+        ));
+      }
+
+      return (
+        <a
+          href={href}
+          title={token.title ? token.title : undefined}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {token.tokens.map((token, index) => (
+            <InlineTokenComp key={index} token={token as InlineToken} />
+          ))}
+        </a>
+      );
+    }
+    case "image": {
+      let href: string | null;
+      try {
+        const url = new URL(token.href);
+        if (url.protocol === "http:" || url.protocol === "https:") {
+          href = url.toString();
+        } else {
+          href = null;
+        }
+      } catch {
+        href = null;
+      }
+
+      if (href === null) {
+        return token.text;
+      }
+
+      return (
+        <img src={href} alt={token.text} title={token.title ?? undefined} />
+      );
+    }
+    case "text": {
+      if (token.tokens !== undefined) {
+        return token.tokens.map((token, index) => (
+          <InlineTokenComp key={index} token={token as InlineToken} />
+        ));
+      } else {
+        return token.text;
+      }
+    }
+    case "escape": {
+      return token.text;
+    }
+    case "checkbox": {
+      return <input type="checkbox" disabled checked={token.checked} />;
+    }
+    default: {
       return null;
+    }
   }
 }
