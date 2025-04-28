9ac864d096554d97130406c858bd17f5b87a6af2
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2025-04-28 16:37:28 +0200

    Docs: Subscribe/unsubscribe (#2323)
    Co-authored-by: Florent <florent.lefebvre@liveblocks.io>
    Co-authored-by: Chris Nicholas <ctnicholasdev@gmail.com>

--- examples/nextjs-notification-settings/app/settings/page.tsx
+++ examples/nextjs-notification-settings/app/settings/page.tsx
@@ -1,4 +1,4 @@
-import { UserNotificationsSettings } from "./_components/user-notifications-settings";
+import { NotificationsSettings } from "./_components/notifications-settings";
 
 // Settings that allow users to choose which notifications are received
 // https://liveblocks.io/docs/guides/how-to-create-a-notification-settings-panel
@@ -6,7 +6,7 @@ import { UserNotificationsSettings } from "./_components/user-notifications-sett
 export default function SettingsPage() {
   return (
     <div className="flex flex-col w-full items-center justify-center flex-1">
-      <UserNotificationsSettings />
+      <NotificationsSettings />
     </div>
   );
 }
