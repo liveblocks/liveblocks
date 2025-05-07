6945ceb24fb2d2578f3379d90369f099b935278d
Vincent Driessen <me@nvie.com>
2025-05-07 08:45:42 +0200

    Remove all schema language related packages
    We won't be maintaining these going forward, and the already published
    versions on NPM will get deprecated.

--- schema-lang/infer-schema/src/utils/object.ts
+++
@@ -1,8 +0,0 @@
-export function get<T, TKey extends keyof T>(
-  value: T,
-  key: TKey
-): T[TKey] | undefined {
-  return Object.prototype.hasOwnProperty.call(value, key)
-    ? value[key]
-    : undefined;
-}
