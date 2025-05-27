ec7d68642ec0f78203394c1098dff1c9ee00450c
Florent <florent.lefebvre@liveblocks.io>
2025-05-27 17:11:26 -0400

    clean up

--- examples/nextjs-multi-tenant-id-token/src/app/api/liveblocks-auth/route.ts
+++ examples/nextjs-multi-tenant-id-token/src/app/api/liveblocks-auth/route.ts
@@ -2,11 +2,6 @@ import { Liveblocks } from "@liveblocks/node";
 import { NextRequest, NextResponse } from "next/server";
 import { getUser } from "../../../database";
 
-/**
- * Authenticating your Liveblocks application
- * https://liveblocks.io/docs/authentication
- */
-
 const liveblocks = new Liveblocks({
   secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
@@ -25,16 +20,10 @@ export async function POST(request: NextRequest) {
     return new NextResponse("Invalid user", { status: 403 });
   }
 
-  // Create a session for the current user (access token auth), either with a provided user ID or a random one
   const res = await liveblocks.identifyUser(
     {
       userId,
-      groupIds: [
-        // This groupId is set on all public rooms for this tenant
-        `${tenantId}:all`,
-        // This groupId is set on private rooms the user has access to for this tenant
-        `${tenantId}:${userId}`,
-      ],
+      groupIds: [`${tenantId}:all`, `${tenantId}:${userId}`],
     },
     {
       userInfo: user.info,
