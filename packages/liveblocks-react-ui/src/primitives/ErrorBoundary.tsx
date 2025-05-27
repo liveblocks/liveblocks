44fdee8fd1dfd7b3a37de34bbe1db81880c0519c
Vincent Driessen <me@nvie.com>
2025-05-27 15:34:16 +0200

    Borrow Nimesh' implementation of a generic ErrorBoundary component
    Co-authored-by: Nimesh Nayaju <nimeshnayaju@gmail.com>

---
+++ packages/liveblocks-react-ui/src/primitives/ErrorBoundary.tsx
@@ -0,0 +1,68 @@
+import type { ErrorInfo, ReactNode } from "react";
+import { Component, createContext, createElement, useContext } from "react";
+
+const ErrorBoundaryContext = createContext<{
+  error: Error;
+  reset: () => void;
+} | null>(null);
+
+export interface ErrorBoundaryProps {
+  children?: ReactNode;
+  fallback?: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  error: Error | null;
+}
+
+export class ErrorBoundary extends Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { error: null };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { error };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    console.error(error, errorInfo);
+  }
+
+  reset() {
+    this.setState({ error: null });
+  }
+
+  render(): React.ReactNode {
+    if (this.state.error === null) return this.props.children;
+
+    return createElement(
+      ErrorBoundaryContext.Provider,
+      {
+        value: {
+          error: this.state.error,
+          reset: this.reset.bind(this),
+        },
+      },
+      this.props.fallback
+    );
+  }
+}
+
+export function useErrorBoundary(): {
+  error: Error;
+  reset: () => void;
+} {
+  const context = useContext(ErrorBoundaryContext);
+
+  if (context === null) {
+    throw new Error(
+      "useErrorBoundary must be used within an ErrorBoundary component"
+    );
+  }
+
+  return context;
+}
