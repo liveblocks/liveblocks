489ca4cb46b436923f01793ff02fc623cbf0dea5
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2025-05-28 13:56:52 +0200

    Fix unused import

--- packages/liveblocks-node/src/index.ts
+++ packages/liveblocks-node/src/index.ts
@@ -1,6 +1,5 @@
 import { detectDupes } from "@liveblocks/core";
 
-import type { RoomData } from "./client";
 import { PKG_FORMAT, PKG_NAME, PKG_VERSION } from "./version";
 
 detectDupes(PKG_NAME, PKG_VERSION, PKG_FORMAT);
