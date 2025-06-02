100896ce84e3102b775d1b8f8fe1238fd14220b0
Chris Nicholas <ctnicholasdev@gmail.com>
2025-06-02 17:11:03 +0100

    feat: Tidy

--- examples/nextjs-ai-popup/app/layout.tsx
+++ examples/nextjs-ai-popup/app/layout.tsx
@@ -1,12 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
-import { ChatList } from "./chat-list";
-import { redirect } from "next/navigation";
-import { nanoid } from "nanoid";
-import { ComponentProps } from "react";
 import "./globals.css";
-import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,48 +27,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         />
       </head>
       <body
-        className={`${inter.className} absolute inset-0 text-stone-900 flex justify-center items-center bg-stone-200`}
+        className={`${inter.className} absolute inset-0 text-gray-900 flex justify-center items-center bg-gray-200`}
       >
         <Providers>{children}</Providers>
       </body>
     </html>
-  );
-}
-
-function ChatsIcon(props: ComponentProps<"svg">) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={18}
-      height={18}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth={2}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M14 9a2 2 0 01-2 2H6l-4 4V4a2 2 0 012-2h8a2 2 0 012 2zM18 9h2a2 2 0 012 2v11l-4-4h-6a2 2 0 01-2-2v-1" />
-    </svg>
-  );
-}
-
-function PlusIcon(props: ComponentProps<"svg">) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={18}
-      height={18}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth={2}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M7.9 20A9 9 0 104 16.1L2 22zM8 12h8M12 8v8" />
-    </svg>
   );
 }
