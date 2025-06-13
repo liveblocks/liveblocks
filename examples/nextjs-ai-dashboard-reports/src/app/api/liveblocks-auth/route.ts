d6f03a13e8d7d9aaa675aa0d3041ce6d18b4cede
Chris Nicholas <ctnicholasdev@gmail.com>
2025-06-13 21:30:45 +0100

    feat: Unique ID in local session

--- examples/nextjs-ai-dashboard-reports/src/app/api/liveblocks-auth/route.ts
+++ examples/nextjs-ai-dashboard-reports/src/app/api/liveblocks-auth/route.ts
@@ -1,5 +1,5 @@
 import { Liveblocks } from "@liveblocks/node";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { users } from "@/data/users";
 
 /**
@@ -11,13 +11,13 @@ const liveblocks = new Liveblocks({
   secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
 
-export async function POST() {
+export async function POST(request: NextRequest) {
   if (!process.env.LIVEBLOCKS_SECRET_KEY) {
     return new NextResponse("Missing LIVEBLOCKS_SECRET_KEY", { status: 403 });
   }
 
   // Get the current user's unique id and info from your database
-  const user = users.find((user) => user.email === "charlie.layne@example.com");
+  const user = await getSession(request);
 
   if (!user) {
     return new NextResponse("User not found", { status: 404 });
@@ -39,4 +39,17 @@ export async function POST() {
   const { status, body } = await session.authorize();
 
   return new NextResponse(body, { status });
+}
+
+// Imagine this is your auth setup
+async function getSession(request: NextRequest) {
+  // Used to deploy to https://liveblocks.io/examples
+  const { userId } = await request.json();
+  const user = users.find((user) => user.email === "charlie.layne@example.com");
+
+  if (!user) {
+    return null;
+  }
+
+  return { ...user, email: (userId as string) || user.email };
 }
