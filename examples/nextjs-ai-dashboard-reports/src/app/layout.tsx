1517ab22855f074feeb09647d6e4ab0ecdbb9e27
Le Vaillant Pierre <pierre.levaillant@liveblocks.io>
2025-06-15 17:34:21 +0200

    Update font

--- examples/nextjs-ai-dashboard-reports/src/app/layout.tsx
+++ examples/nextjs-ai-dashboard-reports/src/app/layout.tsx
@@ -1,20 +1,22 @@
-import { GeistSans } from "geist/font/sans";
-import type { Metadata } from "next";
-import React from "react";
-import { Providers } from "./providers";
-import { AiPopup } from "@/ai-popup/AiPopup";
-import "@liveblocks/react-ui/styles.css";
-import "@liveblocks/react-ui/styles/dark/attributes.css";
-import "./globals.css";
+import { Inter } from "next/font/google"
+import type { Metadata } from "next"
+import React from "react"
+import { Providers } from "./providers"
+import { AiPopup } from "@/ai-popup/AiPopup"
+import "@liveblocks/react-ui/styles.css"
+import "@liveblocks/react-ui/styles/dark/attributes.css"
+import "./globals.css"
+
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Liveblocks",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -35,7 +37,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${GeistSans.className} overflow-x-hidden overflow-y-scroll scroll-auto bg-gray-50 antialiased selection:bg-blue-100 selection:text-blue-700 dark:bg-gray-950`}
+        className={`${inter.className} overflow-x-hidden overflow-y-scroll scroll-auto bg-gray-50 antialiased selection:bg-blue-100 selection:text-blue-700 dark:bg-gray-950`}
       >
         <Providers>
           <div>{children}</div>
@@ -43,5 +45,5 @@ export default function RootLayout({
         </Providers>
       </body>
     </html>
-  );
+  )
 }
