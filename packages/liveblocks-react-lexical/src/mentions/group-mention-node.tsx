73a8a17d620f3c533d258a73e464306846befaa5
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2025-06-27 13:07:51 +0200

    Finalize Lexical group mentions rendering and behavior

--- packages/liveblocks-react-lexical/src/mentions/group-mention-node.tsx
+++ packages/liveblocks-react-lexical/src/mentions/group-mention-node.tsx
@@ -1,4 +1,5 @@
 import { createInboxNotificationId } from "@liveblocks/core";
+import { Group } from "@liveblocks/react-ui/_private";
 import type {
   DOMConversionMap,
   DOMExportOutput,
@@ -16,17 +17,25 @@ import { Mention } from "./mention-component";
 export type SerializedGroupMentionNode = Spread<
   {
     groupId: string;
+    userIds?: string[];
   },
   SerializedLexicalNode
 >;
 export class GroupMentionNode extends DecoratorNode<JSX.Element> {
   __id: string;
   __groupId: string;
+  __userIds: string[] | undefined;
 
-  constructor(id: string, groupId: string, key?: NodeKey) {
+  constructor(
+    id: string,
+    groupId: string,
+    userIds: string[] | undefined,
+    key?: NodeKey
+  ) {
     super(key);
     this.__id = id;
     this.__groupId = groupId;
+    this.__userIds = userIds;
   }
 
   static getType(): string {
@@ -34,7 +43,12 @@ export class GroupMentionNode extends DecoratorNode<JSX.Element> {
   }
 
   static clone(node: GroupMentionNode): GroupMentionNode {
-    return new GroupMentionNode(node.__id, node.__groupId, node.__key);
+    return new GroupMentionNode(
+      node.__id,
+      node.__groupId,
+      node.__userIds,
+      node.__key
+    );
   }
 
   createDOM(): HTMLElement {
@@ -55,7 +69,7 @@ export class GroupMentionNode extends DecoratorNode<JSX.Element> {
           const value = atob(
             element.getAttribute("data-lexical-lb-group-mention")!
           );
-          const node = $createGroupMentionNode(value);
+          const node = $createGroupMentionNode(value, undefined);
           return { node };
         },
         priority: 1,
@@ -74,7 +88,10 @@ export class GroupMentionNode extends DecoratorNode<JSX.Element> {
   static importJSON(
     serializedNode: SerializedGroupMentionNode
   ): GroupMentionNode {
-    const node = $createGroupMentionNode(serializedNode.groupId);
+    const node = $createGroupMentionNode(
+      serializedNode.groupId,
+      serializedNode.userIds
+    );
     return node;
   }
 
@@ -96,12 +113,16 @@ export class GroupMentionNode extends DecoratorNode<JSX.Element> {
     return self.__id;
   }
 
+  getUserIds(): string[] | undefined {
+    const self = this.getLatest();
+    return self.__userIds;
+  }
+
   decorate(): JSX.Element {
     return (
       <Mention nodeKey={this.getKey()}>
         {MENTION_CHARACTER}
-        {/* TODO: Display group name */}
-        {/* <User userId={this.getUserId()} /> */}
+        <Group groupId={this.getGroupId()} />
       </Mention>
     );
   }
@@ -113,7 +134,14 @@ export function $isGroupMentionNode(
   return node instanceof GroupMentionNode;
 }
 
-export function $createGroupMentionNode(groupId: string): GroupMentionNode {
-  const node = new GroupMentionNode(createInboxNotificationId(), groupId);
+export function $createGroupMentionNode(
+  groupId: string,
+  userIds: string[] | undefined
+): GroupMentionNode {
+  const node = new GroupMentionNode(
+    createInboxNotificationId(),
+    groupId,
+    userIds
+  );
   return $applyNodeReplacement(node);
 }
