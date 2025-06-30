77b763c7468114726df7346975b1ae917e36f88c
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2025-06-30 13:23:05 +0200

    Improve DOM importing/exporting for Lexical’s Mention node

--- packages/liveblocks-react-lexical/src/mentions/mention-node.tsx
+++ packages/liveblocks-react-lexical/src/mentions/mention-node.tsx
@@ -54,8 +54,13 @@ export class MentionNode extends DecoratorNode<JSX.Element> {
     return {
       span: () => ({
         conversion: (element) => {
-          const value = atob(element.getAttribute("data-lexical-lb-mention")!);
-          const node = $createMentionNode(value);
+          const userId = element.getAttribute("data-lexical-lb-mention");
+
+          if (!userId) {
+            return null;
+          }
+
+          const node = $createMentionNode(userId);
           return { node };
         },
         priority: 1,
@@ -65,9 +70,8 @@ export class MentionNode extends DecoratorNode<JSX.Element> {
 
   exportDOM(): DOMExportOutput {
     const element = document.createElement("span");
-    const value = this.getTextContent();
-    element.setAttribute("data-lexical-lb-mention", btoa(value));
-    element.textContent = this.getTextContent();
+    element.setAttribute("data-lexical-lb-mention", this.getUserId());
+    element.textContent = this.getUserId();
     return { element };
   }
 
@@ -101,6 +105,10 @@ export class MentionNode extends DecoratorNode<JSX.Element> {
         <User userId={this.getUserId()} />
       </Mention>
     );
+  }
+
+  getTextContent(): string {
+    return MENTION_CHARACTER + this.getUserId();
   }
 }
 
