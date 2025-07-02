e5baacccf6c0274574e59527e1c6fa5c2bbf6ea3
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2025-07-02 10:12:31 +0200

    Finalize Lexical group mention nodes

--- packages/liveblocks-node-lexical/src/GroupMentionNodeLite.ts
+++ packages/liveblocks-node-lexical/src/GroupMentionNodeLite.ts
@@ -1,19 +1,32 @@
 import type { NodeKey, SerializedLexicalNode, Spread } from "lexical";
 import { $applyNodeReplacement, DecoratorNode } from "lexical";
 
+import { MENTION_CHARACTER } from "./constants";
+
 export type SerializedGroupMentionNode = Spread<
   {
-    value: string;
+    id: string;
+    groupId: string;
+    userIds: string[] | undefined;
   },
   SerializedLexicalNode
 >;
 
 export class GroupMentionNode extends DecoratorNode<null> {
   __id: string;
+  __groupId: string;
+  __userIds: string[] | undefined;
 
-  constructor(value: string, key?: NodeKey) {
+  constructor(
+    id: string,
+    groupId: string,
+    userIds: string[] | undefined,
+    key?: NodeKey
+  ) {
     super(key);
-    this.__id = value;
+    this.__id = id;
+    this.__groupId = groupId;
+    this.__userIds = userIds;
   }
 
   static getType(): string {
@@ -21,27 +34,49 @@ export class GroupMentionNode extends DecoratorNode<null> {
   }
 
   static clone(node: GroupMentionNode): GroupMentionNode {
-    return new GroupMentionNode(node.__id);
+    return new GroupMentionNode(node.__id, node.__groupId, node.__userIds);
   }
 
   static importJSON(
     serializedNode: SerializedGroupMentionNode
   ): GroupMentionNode {
-    const node = new GroupMentionNode(serializedNode.value);
+    const node = new GroupMentionNode(
+      serializedNode.id,
+      serializedNode.groupId,
+      serializedNode.userIds
+    );
     return $applyNodeReplacement(node);
   }
 
   exportJSON(): SerializedGroupMentionNode {
     return {
-      value: this.getTextContent(),
+      id: this.getId(),
+      groupId: this.getGroupId(),
+      userIds: this.getUserIds(),
       type: "lb-group-mention",
       version: 1,
     };
   }
 
-  getTextContent(): string {
+  getId(): string {
     const self = this.getLatest();
     return self.__id;
+  }
+
+  getGroupId(): string {
+    const self = this.getLatest();
+    return self.__groupId;
+  }
+
+  getUserIds(): string[] | undefined {
+    const self = this.getLatest();
+    return self.__userIds;
+  }
+
+  getTextContent(): string {
+    const groupId = this.getGroupId();
+
+    return MENTION_CHARACTER + groupId;
   }
 
   decorate(): null {
