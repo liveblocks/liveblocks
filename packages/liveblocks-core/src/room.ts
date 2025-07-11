a2d010bbf9feb583ddef638b47c3cd6163b92e94
Jonathan Rowny <jrowny@gmail.com>
2025-07-11 11:45:14 -0700

    don't throw

--- packages/liveblocks-core/src/room.ts
+++ packages/liveblocks-core/src/room.ts
@@ -1256,8 +1256,7 @@ export type RoomDelegates = Omit<Delegates<AuthValue>, "canZombie">;
 export type LargeMessageStrategy =
   | "default"
   | "split"
-  | "experimental-fallback-to-http"
-  | "throw";
+  | "experimental-fallback-to-http";
 
 /** @internal */
 export type RoomConfig<M extends BaseMetadata> = {
@@ -1734,8 +1733,14 @@ export function createRoom<
     // If message is too big for WebSockets, we need to follow a strategy
     switch (strategy) {
       case "default": {
-        console.error("Message is too large for websockets, not sending. Configure largeMessageStrategy option to deal with this."); // prettier-ignore
-        // Don't send the message
+        const type = "LARGE_MESSAGE_ERROR";
+        const err = new LiveblocksError("Message is too large for websockets", {
+          type,
+        });
+        const didNotify = config.errorEventSource.notify(err);
+        if (!didNotify) {
+          console.error("Message is too large for websockets");
+        }
         return;
       }
 
@@ -1761,10 +1766,6 @@ export function createRoom<
             }
           });
         return;
-      }
-
-      case "throw": {
-        throw new Error("Message is too large for websockets, not sending.");
       }
     }
   }
