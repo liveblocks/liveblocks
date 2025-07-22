8642ebd813625ca2c45f531f09be450daf4da01d
Florent Lefebvre <florent.lefebvre@liveblocks.io>
2025-07-22 17:15:13 -0400

    Group mentions node client (#2539)

--- packages/liveblocks-node/src/client.ts
+++ packages/liveblocks-node/src/client.ts
@@ -18,6 +18,8 @@ import type {
   DM,
   DS,
   DU,
+  GroupData,
+  GroupDataPlain,
   IdTuple,
   InboxNotificationData,
   InboxNotificationDataPlain,
@@ -53,6 +55,7 @@ import {
   ClientMsgCode,
   convertToCommentData,
   convertToCommentUserReaction,
+  convertToGroupData,
   convertToInboxNotificationData,
   convertToSubscriptionData,
   convertToThreadData,
@@ -2185,6 +2188,128 @@ export class Liveblocks {
     if (!res.ok) {
       throw await LiveblocksError.from(res);
     }
+  }
+
+  /**
+   * Create a group
+   * @param params.id The ID of the group to create.
+   * @param params.memberIds The IDs of the members to add to the group.
+   * @param params.tenantId The ID of the tenant to create the group for.
+   * @param options.signal (optional) An abort signal to cancel the request.
+   */
+  public async createGroup(
+    params: { id: string; memberIds?: string[]; tenantId?: string },
+    options?: RequestOptions
+  ): Promise<GroupData> {
+    const res = await this.#post(url`/v2/groups`, params, options);
+    if (!res.ok) {
+      throw await LiveblocksError.from(res);
+    }
+
+    const group = (await res.json()) as GroupDataPlain;
+    return convertToGroupData(group);
+  }
+
+  /**
+   * Get a group
+   * @param params.id The ID of the group to get.
+   * @param options.signal (optional) An abort signal to cancel the request.
+   */
+  public async getGroup(
+    params: { id: string },
+    options?: RequestOptions
+  ): Promise<GroupData> {
+    const res = await this.#get(
+      url`/v2/groups/${params.id}`,
+      undefined,
+      options
+    );
+    if (!res.ok) {
+      throw await LiveblocksError.from(res);
+    }
+
+    const group = (await res.json()) as GroupDataPlain;
+    return convertToGroupData(group);
+  }
+
+  /**
+   * Add members to a group
+   * @param params.id The ID of the group to add members to.
+   * @param params.memberIds The IDs of the members to add to the group.
+   * @param options.signal (optional) An abort signal to cancel the request.
+   */
+  public async addGroupMembers(
+    groupId: string,
+    params: { memberIds: string[] },
+    options?: RequestOptions
+  ): Promise<GroupData> {
+    const res = await this.#post(
+      url`/v2/groups/${groupId}/add-members`,
+      params,
+      options
+    );
+    if (!res.ok) {
+      throw await LiveblocksError.from(res);
+    }
+
+    const group = (await res.json()) as GroupDataPlain;
+    return convertToGroupData(group);
+  }
+
+  /**
+   * Remove members from a group
+   * @param params.id The ID of the group to remove members from.
+   * @param params.memberIds The IDs of the members to remove from the group.
+   * @param options.signal (optional) An abort signal to cancel the request.
+   */
+  public async removeGroupMembers(
+    groupId: string,
+    params: { memberIds: string[] },
+    options?: RequestOptions
+  ): Promise<GroupData> {
+    const res = await this.#post(
+      url`/v2/groups/${groupId}/remove-members`,
+      params,
+      options
+    );
+    if (!res.ok) {
+      throw await LiveblocksError.from(res);
+    }
+
+    const group = (await res.json()) as GroupDataPlain;
+    return convertToGroupData(group);
+  }
+
+  /**
+   * Delete a group
+   * @param params.id The ID of the group to delete.
+   * @param options.signal (optional) An abort signal to cancel the request.
+   */
+  public async deleteGroup(
+    params: { id: string },
+    options?: RequestOptions
+  ): Promise<void> {
+    const res = await this.#delete(url`/v2/groups/${params.id}`, options);
+    if (!res.ok) {
+      throw await LiveblocksError.from(res);
+    }
+  }
+
+  /**
+   * Get all groups
+   * @param options.signal (optional) An abort signal to cancel the request.
+   */
+  public async getGroups(options?: RequestOptions): Promise<Page<GroupData>> {
+    const res = await this.#get(url`/v2/groups`, undefined, options);
+    if (!res.ok) {
+      throw await LiveblocksError.from(res);
+    }
+
+    const page = (await res.json()) as Page<GroupDataPlain>;
+    return {
+      ...page,
+      data: page.data.map(convertToGroupData),
+    };
   }
 
   /**
