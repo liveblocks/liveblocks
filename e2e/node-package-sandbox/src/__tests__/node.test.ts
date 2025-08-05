dafebb99404dd5832f53116d10be3d8436b0e19b
Jonathan Rowny <jrowny@gmail.com>
2025-08-05 14:55:34 -0700

    new e2e for node-sandbox

---
+++ e2e/node-package-sandbox/src/__tests__/node.test.ts
@@ -0,0 +1,57 @@
+import { LiveList } from "@liveblocks/core";
+
+import { Liveblocks } from "@liveblocks/node";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+describe("node package e2e", () => {
+  test("create the room", async () => {
+    const client = new Liveblocks({
+      secret: process.env.LIVEBLOCKS_SECRET_KEY!,
+      // @ts-expect-error hidden config
+      baseUrl: process.env.LIVEBLOCKS_BASE_URL ?? "https://api.liveblocks.io",
+    });
+
+    expect(
+      await client.createRoom(
+        "node-e2e",
+        {
+          defaultAccesses: ["room:write"],
+        },
+        {
+          idempotent: true,
+        }
+      )
+    ).toBeDefined();
+  });
+  test("storage mutation should work in node environment", async () => {
+    const client = new Liveblocks({
+      secret: process.env.LIVEBLOCKS_SECRET_KEY!,
+      // @ts-expect-error hidden config
+      baseUrl: process.env.LIVEBLOCKS_BASE_URL ?? "https://api.liveblocks.io",
+    });
+
+    // delete existing data in the room
+    await expect(
+      client.mutateStorage("node-e2e", ({ root }) => {
+        root.delete("z");
+      })
+    ).resolves.toBeUndefined();
+
+    // add data to the room
+    await expect(
+      client.mutateStorage("node-e2e", ({ root }) => {
+        expect(root.toImmutable()).toEqual({});
+        // Mutate it!
+        root.set("z", new LiveList([1, 2, 3]));
+      })
+    ).resolves.toBeUndefined();
+
+    // add data to the room
+    await expect(
+      client.mutateStorage("node-e2e", ({ root }) => {
+        expect(root.toImmutable()).toEqual({ z: [1, 2, 3] });
+      })
+    ).resolves.toBeUndefined();
+  });
+});
