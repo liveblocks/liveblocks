0595a1f3fc02c5ddf34c65c8d955bcf154de9fff
Vincent Driessen <me@nvie.com>
2025-08-06 12:27:42 +0200

    Bump AI protocol to V5 which allows for tool call streaming

--- packages/liveblocks-core/src/ai.ts
+++ packages/liveblocks-core/src/ai.ts
@@ -1361,7 +1361,7 @@ export function makeCreateSocketDelegateForAi(
 
     const url = new URL(baseUrl);
     url.protocol = url.protocol === "http:" ? "ws" : "wss";
-    url.pathname = "/ai/v4";
+    url.pathname = "/ai/v5";
     // TODO: don't allow public key to do this
     if (authValue.type === "secret") {
       url.searchParams.set("tok", authValue.token.raw);
