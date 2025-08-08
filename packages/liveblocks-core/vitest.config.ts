c0c4e766387628175933b5d9ad5c93025354b841
Vincent Driessen <me@nvie.com>
2025-08-08 16:16:45 +0200

    Adopt to Vitest's timer mechanics

--- packages/liveblocks-core/vitest.config.ts
+++ packages/liveblocks-core/vitest.config.ts
@@ -1,3 +1,4 @@
+import { configDefaults } from "vitest/config";
 import { defaultLiveblocksVitestConfig } from "@liveblocks/vitest-config";
 
 export default defaultLiveblocksVitestConfig({
@@ -9,6 +10,14 @@ export default defaultLiveblocksVitestConfig({
         url: "http://dummy/",
       },
     },
+
+    // Gotcha! One key difference between Jest (our old test runner) and Vitest
+    // is that Vitest does not automatically mock the `performance.now()` method.
+    // See https://github.com/vitest-dev/vitest/issues/4004
+    fakeTimers: {
+      toFake: [...configDefaults.fakeTimers.toFake, "performance"],
+    },
+
     // Collect code coverage for this project, when using the --coverage flag
     coverage: {
       provider: "istanbul",
