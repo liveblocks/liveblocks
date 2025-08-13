b3ac7afd4773848bd1bdc6c91be1feca46159c12
Chris Nicholas <ctnicholasdev@gmail.com>
2025-08-13 12:55:15 +0100

    feat: Add buttons

--- examples/nextjs-comments/src/app/page.tsx
+++ examples/nextjs-comments/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { RoomProvider, useThreads } from "@liveblocks/react/suspense";
 import { Loading } from "../components/Loading";
@@ -15,14 +15,59 @@ import { ErrorBoundary } from "react-error-boundary";
 
 function Example() {
   const { threads } = useThreads();
+  const [state, setState] = useState<"oldest" | "newest" | "both" | "off">(
+    "newest"
+  );
 
   return (
-    <main>
+    <main style={{ maxWidth: "460px", margin: "0 auto" }}>
+      <div style={{ display: "flex", gap: "8px", marginBottom: "" }}>
+        <Button onClick={() => setState("newest")}>Show newest</Button>
+        <Button onClick={() => setState("oldest")}>Show oldest</Button>
+        <Button onClick={() => setState("both")}>Show both</Button>
+        <Button onClick={() => setState("off")}>Off</Button>
+      </div>
       {threads.map((thread) => (
-        <Thread key={thread.id} thread={thread} className="thread" />
+        <Thread
+          key={thread.id}
+          thread={thread}
+          className="thread"
+          maxVisibleComments={{
+            max: state === "off" ? 100000 : 4,
+            show: state,
+          }}
+        />
       ))}
       <Composer className="composer" />
     </main>
+  );
+}
+
+function Button({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick: () => void;
+}) {
+  return (
+    <button
+      onClick={onClick}
+      className="thread"
+      style={{
+        backgroundColor: "white",
+        border: "0",
+        padding: "8px 16px",
+        borderRadius: "8px",
+        fontFamily: "inherit",
+        appearance: "none",
+        cursor: "pointer",
+        fontSize: "15px",
+        fontWeight: "500",
+      }}
+    >
+      {children}
+    </button>
   );
 }
 
