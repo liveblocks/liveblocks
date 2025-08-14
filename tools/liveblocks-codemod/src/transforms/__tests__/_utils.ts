27da509e92e0e37dce4e5b7137f78a5e8b042ee4
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2025-08-14 09:06:47 +0200

    Improve `defineTest` reimplementation

--- tools/liveblocks-codemod/src/transforms/__tests__/_utils.ts
+++ tools/liveblocks-codemod/src/transforms/__tests__/_utils.ts
@@ -3,6 +3,8 @@ import { readFileSync, readdirSync } from "fs";
 import { describe, test, expect } from "vitest";
 import jscodeshift from "jscodeshift";
 
+const INPUT_FILE_REGEX = /\.input\.([jt]sx?)$/;
+
 // A Vitest-compatible simplified reimplementation of the `defineTest` helper from jscodeshift.
 // https://github.com/facebook/jscodeshift/blob/main/src/testUtils.js
 export function defineTestsForTransform(
@@ -14,22 +16,34 @@ export function defineTestsForTransform(
   const transformPath = path.resolve(__dirname, "..", transform);
   const fixturesPath = path.resolve(__dirname, "./__fixtures__", transform);
   const fixtures = readdirSync(fixturesPath)
-    .filter((file) => file.endsWith(".input.tsx"))
-    .map((file) => file.replace(".input.tsx", ""));
+    .map((file) => {
+      const match = file.match(INPUT_FILE_REGEX);
+
+      if (!match) {
+        return null;
+      }
+
+      const [, extension] = match;
+      const fixture = file.replace(INPUT_FILE_REGEX, "");
+
+      return {
+        name: fixture,
+        inputPath: path.join(fixturesPath, `${fixture}.input.${extension}`),
+        outputPath: path.join(fixturesPath, `${fixture}.output.${extension}`),
+      };
+    })
+    .filter((fixture) => fixture !== null);
 
   describe(transform, () => {
     for (const fixture of fixtures) {
-      test(`should transform ${fixture}`, async () => {
-        const inputPath = path.join(fixturesPath, `${fixture}.input.tsx`);
-        const outputPath = path.join(fixturesPath, `${fixture}.output.tsx`);
-
-        const input = readFileSync(inputPath, "utf8");
-        const output = readFileSync(outputPath, "utf8");
+      test(`should transform ${fixture.name}`, async () => {
+        const input = readFileSync(fixture.inputPath, "utf8");
+        const output = readFileSync(fixture.outputPath, "utf8");
 
         const transform = await import(transformPath);
         const result = transform.default(
           {
-            path: inputPath,
+            path: fixture.inputPath,
             source: input,
           },
           {
@@ -39,7 +53,7 @@ export function defineTestsForTransform(
           {
             parser: "tsx",
             ...(typeof options === "function"
-              ? options({ transform, fixture })
+              ? options({ transform, fixture: fixture.name })
               : options),
           }
         );
