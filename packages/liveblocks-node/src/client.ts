a209a48a9ce28a86a78d9ebf5064b3bf1a60983f
Florent <florent.lefebvre@liveblocks.io>
2025-08-25 11:05:13 -0400

    Add optional `scopes` parameter to `createGroup` method documentation

--- packages/liveblocks-node/src/client.ts
+++ packages/liveblocks-node/src/client.ts
@@ -2242,6 +2242,8 @@ export class Liveblocks {
    * @param params.groupId The ID of the group to create.
    * @param params.memberIds The IDs of the members to add to the group.
    * @param params.tenantId (optional) The tenant ID to create the group for.
+   * @param params.scopes (optional) The scopes to grant to the group. The default
+   * is `{ mention: true }`.
    * @param options.signal (optional) An abort signal to cancel the request.
    */
   public async createGroup(
@@ -2249,7 +2251,7 @@ export class Liveblocks {
       groupId: string;
       memberIds?: string[];
       tenantId?: string;
-      scopes: { mention: true };
+      scopes?: { mention: true };
     },
     options?: RequestOptions
   ): Promise<GroupData> {
