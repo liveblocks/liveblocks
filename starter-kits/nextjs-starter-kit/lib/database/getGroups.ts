0ad9738a3e9bc593045ece69b6388125a2933f07
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2025-09-09 10:31:50 +0200

    Use Liveblocks groups in the starter kit (#2652)

--- starter-kits/nextjs-starter-kit/lib/database/getGroups.ts
+++ starter-kits/nextjs-starter-kit/lib/database/getGroups.ts
@@ -1,10 +1,15 @@
 import { groups } from "@/data/groups";
 import { Group } from "@/types";
 import { getGroup } from "./getGroup";
+import { users } from "@/data/users";
 
 type Props = {
   groupIds?: string[];
   search?: string;
+};
+
+type GroupWithMembers = Group & {
+  memberIds: string[];
 };
 
 /**
@@ -15,10 +20,9 @@ type Props = {
  * @param groupIds - The group ids to get
  * @param search - The term to filter your users by, checks users' ids and names
  */
-export async function getGroups({
-  groupIds,
-  search,
-}: Props): Promise<(Group | null)[]> {
+export async function getGroups({ groupIds, search }: Props = {}): Promise<
+  (GroupWithMembers | null)[]
+> {
   const groupsPromises: Promise<Group | null>[] = [];
 
   // Filter by userIds or get all users
@@ -28,18 +32,19 @@ export async function getGroups({
     }
   } else {
     const allGroupIds = groups.map((group) => group.id);
+
     for (const groupId of allGroupIds) {
       groupsPromises.push(getGroup(groupId));
     }
   }
 
-  const groupList = await Promise.all(groupsPromises);
+  let groupList = await Promise.all(groupsPromises);
 
   // If search term, check if term is included in name or id, and filter
   if (search) {
     const term = search.toLowerCase();
 
-    return groupList.filter((group) => {
+    groupList = groupList.filter((group) => {
       if (!group) {
         return false;
       }
@@ -51,5 +56,14 @@ export async function getGroups({
     });
   }
 
-  return groupList;
+  return groupList.map((group) =>
+    group
+      ? {
+          ...group,
+          memberIds: users
+            .filter((user) => user.groupIds.includes(group.id))
+            .map((user) => user.id),
+        }
+      : group
+  );
 }
