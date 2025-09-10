d33432d08911ec9bf73bf92f8ec33c72ee68efdb
Jonathan Rowny <jrowny@gmail.com>
2025-09-10 11:38:01 -0700

    test

--- examples/nextjs-tiptap/app/tiptap/editor.tsx
+++ examples/nextjs-tiptap/app/tiptap/editor.tsx
@@ -10,13 +10,37 @@ import {
   Toolbar,
   FloatingToolbar,
 } from "@liveblocks/react-tiptap";
+import type { ComposerSubmitComment } from "@liveblocks/react-ui";
 import StarterKit from "@tiptap/starter-kit";
-import { useThreads } from "@liveblocks/react";
+import { useCreateThread, useSelf, useThreads } from "@liveblocks/react";
 import { useIsMobile } from "./use-is-mobile";
 import VersionsDialog from "../version-history-dialog";
+import { ThreadData } from "@liveblocks/core";
+import { FormEvent, useCallback, useEffect, useState } from "react";
+
+const useActiveThreads = () => {
+  const { threads = [], isLoading } = useThreads({ scrollOnLoad: false });
+  const filteredThreads = threads.filter((thread) => thread.comments[0] && !thread.comments[0].deletedAt);
+  return { threads: filteredThreads, isLoading };
+};
+
+const filterMyThreads = (threads: ThreadData[], myUserId: string | null) => {
+  if (!myUserId) {
+    return [];
+  }
+  return threads.filter((thread) => thread.comments[0] && thread.comments[0].userId === myUserId).filter((t) => t.comments.length === 1);;
+};
 
 export default function TiptapEditor() {
-  const liveblocks = useLiveblocksExtension();
+  const { threads } = useActiveThreads();
+  const myUserId = useSelf((me) => me.id);
+  const myFilteredThreads = filterMyThreads(threads, myUserId);
+  const createThread = useCreateThread();
+  const liveblocks = useLiveblocksExtension({
+    mentions: false,
+    threads_experimental: myFilteredThreads,
+  });
+  const [createdThreadId, setCreatedThreadId] = useState<string | null>(null);
 
   const editor = useEditor({
     editorProps: {
@@ -33,6 +57,33 @@ export default function TiptapEditor() {
     ],
   });
 
+
+  const handleComposerSubmit = useCallback(
+    (comment: ComposerSubmitComment, event: FormEvent<HTMLFormElement>): void => {
+      if (!editor) return;
+
+      event.preventDefault();
+
+      const thread = createThread({
+        body: comment.body,
+      });
+
+      editor.commands.addComment(thread.id);
+      setCreatedThreadId(thread.id);
+    },
+    [editor, createThread]
+  );
+
+
+  // After a thread is created, select it using useEffect
+  // so that editor will see the thread on re-render
+  useEffect(() => {
+    console.log('weeee createdThreadId', createdThreadId);
+    if (!createdThreadId) return;
+    console.log('weeee selecting thread it createdThreadId', createdThreadId);
+    editor?.commands.selectThread(createdThreadId);
+  }, [editor, createdThreadId]);
+
   return (
     <div className="relative min-h-screen flex flex-col">
       <div className="h-[60px] flex items-center justify-end px-4 border-b border-border/80 bg-background">
@@ -45,7 +96,7 @@ export default function TiptapEditor() {
       <div className="relative flex flex-row justify-between w-full py-16 xl:pl-[250px] pl-[100px] gap-[50px]">
         <div className="relative flex flex-1 flex-col gap-2">
           <EditorContent editor={editor} />
-          <FloatingComposer editor={editor} className="w-[350px]" />
+          <FloatingComposer onComposerSubmit={handleComposerSubmit} editor={editor} className="w-[350px]" />
           <FloatingToolbar editor={editor} />
         </div>
 
