6e06ebc422e9fd4c10eab705205cedaba2c9874b
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2025-09-11 15:02:29 +0200

    Add workaround for `act` warning

--- packages/liveblocks-react-ui/vitest.setup.ts
+++ packages/liveblocks-react-ui/vitest.setup.ts
@@ -1,1 +1,5 @@
 import "@testing-library/jest-dom/vitest";
+
+// Workaround to avoid `@testing-library/react` warnings about `act()`
+// https://github.com/testing-library/react-testing-library/issues/1061
+global.IS_REACT_ACT_ENVIRONMENT = true;
