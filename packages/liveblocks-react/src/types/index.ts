6fc6f9c878a490080e29a4e4342fb37c8375bf1d
Olivier Foucherot <olivier.foucherot@liveblocks.io>
2025-09-11 09:36:00 -0400

    update doc

--- packages/liveblocks-react/src/types/index.ts
+++ packages/liveblocks-react/src/types/index.ts
@@ -155,14 +155,12 @@ export type UseThreadsOptions<M extends BaseMetadata> = {
 
 export type InboxNotificationsQuery = {
   /**
-   * Whether to only return inbox notifications for a specific room. If not provided,
-   * all the user's inbox notifications will be returned.
+   * Whether to only return inbox notifications for a specific room.
    */
   roomId?: string;
 
   /**
-   * Whether to only return inbox notifications for a specific kind. If not provided,
-   * all the user's inbox notifications will be returned.
+   * Whether to only return inbox notifications for a specific kind.
    */
   kind?: string;
 };
