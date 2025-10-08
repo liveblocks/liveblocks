f6958e592fbad84930db460cc3b6ddecdbf6d87a
Chris Nicholas <ctnicholasdev@gmail.com>
2025-10-08 11:45:03 +0100

    feat: New chat button

--- examples/nextjs-ai-calendar/app/[chatId]/page.tsx
+++ examples/nextjs-ai-calendar/app/[chatId]/page.tsx
@@ -3,6 +3,8 @@
 import { Calendar } from "./_components/calendar";
 import { Room } from "./_components/room";
 import { Chat } from "./_components/chat";
+import Link from "next/link";
+import { nanoid } from "@liveblocks/core";
 
 export default function Page({ params }: { params: { chatId: string } }) {
   return (
@@ -12,10 +14,16 @@ export default function Page({ params }: { params: { chatId: string } }) {
           <div className="relative grow shadow rounded-xl overflow-hidden ring-1 ring-neutral-950/5 bg-white flex flex-col">
             <div className="flex items-center justify-between pr-4 border-b border-neutral-950/5">
               <div className="flex items-center p-2.5 gap-1.5 ">
-                <div className="text-sm font-medium py-1 px-2 rounded text-neutral-800">
+                <div className="text-sm font-medium py-1 px-1 rounded text-neutral-800">
                   August
                 </div>
               </div>
+              <Link
+                href={`/${nanoid()}`}
+                className="text-neutral-500 hover:text-neutral-900 text-sm bg-white hover:ring-1 ring-neutral-200 font-medium px-1.5 py-1 -mr-2 rounded-md hover:shadow-sm hover:bg-neutral-50"
+              >
+                New chat
+              </Link>
             </div>
 
             <div className="grow relative shrink">
