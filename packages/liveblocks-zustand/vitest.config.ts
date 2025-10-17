b716db8d667cfdb2d9bad48da392a5d350874c8f
Vincent Driessen <me@nvie.com>
2025-10-17 11:56:40 +0200

    Add coverage report

--- packages/liveblocks-zustand/vitest.config.ts
+++ packages/liveblocks-zustand/vitest.config.ts
@@ -3,5 +3,11 @@ import { defaultLiveblocksVitestConfig } from "@liveblocks/vitest-config";
 export default defaultLiveblocksVitestConfig({
   test: {
     environment: "jsdom",
+    // Collect code coverage for this project, when using the --coverage flag
+    coverage: {
+      provider: "istanbul",
+      include: ["src/**"],
+      exclude: ["**/__tests__/**"],
+    },
   },
 });
