478598ed822dcbde1983e57f0c6602a223cd1e81
Chris Nicholas <ctnicholasdev@gmail.com>
2025-11-28 13:05:13 +0000

    feat: Testing

--- examples/nextjs-comments/src/app/page.tsx
+++ examples/nextjs-comments/src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { RoomProvider, useThreads } from "@liveblocks/react/suspense";
 import { Loading } from "../components/Loading";
 import { Composer, Thread } from "@liveblocks/react-ui";
-import { ClientSideSuspense } from "@liveblocks/react";
+import { ClientSideSuspense, useSearchComments } from "@liveblocks/react";
 import { ErrorBoundary } from "react-error-boundary";
 
 /**
@@ -15,9 +15,29 @@ import { ErrorBoundary } from "react-error-boundary";
 
 function Example() {
   const { threads } = useThreads();
+  const [text, setText] = useState("");
+  const { results, isLoading, error } = useSearchComments({ query: { text } });
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <main>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        results.map((result) => (
+          <a key={result.commentId} href={"#" + result.commentId}>
+            {result.content}
+          </a>
+        ))
+      )}
       {threads.map((thread) => (
         <Thread key={thread.id} thread={thread} className="thread" />
       ))}
