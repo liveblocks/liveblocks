28fe1faf292665f2fcab7c933119dfc4bffdd9fa
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2026-01-05 11:49:36 +0100

    scaffold webhook function

---
+++ packages/liveblocks-nextjs/src/webhook.ts
@@ -0,0 +1,32 @@
+import {
+  Liveblocks,
+  type WebhookEvent,
+  WebhookHandler,
+} from "@liveblocks/node";
+
+export type WebhookOptions = {
+  /**
+   * The Liveblocks secret key provided on the dashboard's API keys page
+   * @example "sk_..."
+   */
+  liveblocksSecret: string;
+  /**
+   * The webhook signing secret provided on the dashboard's webhooks page
+   * @example "whsec_wPbvQ+u3VtN2e2tRPDKchQ1tBZ3svaHLm"
+   */
+  webhookSecret: string;
+  /**
+   * Catch-all handler for any incoming Webhook event
+   */
+  onEvent?: (event: WebhookEvent) => Promise<void>;
+};
+
+export function Webhook(options: WebhookOptions) {
+  const liveblocks = new Liveblocks({ secret: options.liveblocksSecret });
+  const webhookHandler = new WebhookHandler(options.webhookSecret);
+
+  return async function (req: Request): Promise<Response> {
+    const body = await req.json();
+    return new Response(null, { status: 200 });
+  };
+}
