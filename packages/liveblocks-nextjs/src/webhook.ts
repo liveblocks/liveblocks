1ab90a06c5f0c649a6f448c792b0429062de67f5
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2026-01-05 11:50:39 +0100

    add jsdoc

--- packages/liveblocks-nextjs/src/webhook.ts
+++ packages/liveblocks-nextjs/src/webhook.ts
@@ -21,6 +21,20 @@ export type WebhookOptions = {
   onEvent?: (event: WebhookEvent) => Promise<void>;
 };
 
+/**
+ * A simple utility which resolves incoming webhook payloads by signing the webhook secret properly.
+ *
+ * @example
+ * import { Webhook } from "@liveblocks/nextjs";
+ *
+ * export const POST = Webhook({
+ *   liveblocksSecret: process.env.LIVEBLOCKS_SECRET_KEY,
+ *   webhookSecret: process.env.WEBHOOK_SECRET,
+ *   onEvent: async (event) => {
+ *     console.log(event);
+ *   },
+ * })
+ */
 export function Webhook(options: WebhookOptions) {
   const liveblocks = new Liveblocks({ secret: options.liveblocksSecret });
   const webhookHandler = new WebhookHandler(options.webhookSecret);
