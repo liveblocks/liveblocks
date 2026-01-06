68244c4fdb590b434378a8958a886968393b1878
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2026-01-06 11:28:36 +0100

    update exports

--- packages/liveblocks-nextjs/src/index.ts
+++ packages/liveblocks-nextjs/src/index.ts
@@ -1,2 +1,24 @@
 export type { WebhookOptions } from "./webhook";
 export { Webhook } from "./webhook";
+export type {
+  CommentCreatedEvent,
+  CommentDeletedEvent,
+  CommentEditedEvent,
+  CommentReactionAdded,
+  CommentReactionRemoved,
+  CustomNotificationEvent,
+  RoomCreatedEvent,
+  RoomDeletedEvent,
+  StorageUpdatedEvent,
+  TextMentionNotificationEvent,
+  ThreadCreatedEvent,
+  ThreadDeletedEvent,
+  ThreadMarkedAsResolvedEvent,
+  ThreadMarkedAsUnresolvedEvent,
+  ThreadMetadataUpdatedEvent,
+  ThreadNotificationEvent,
+  UserEnteredEvent,
+  UserLeftEvent,
+  WebhookEvent,
+  YDocUpdatedEvent,
+} from "@liveblocks/node";
