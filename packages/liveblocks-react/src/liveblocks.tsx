8e3b3dfa4433a00b688bf01a8b1feaec1c847ae1
Vincent Driessen <me@nvie.com>
2026-01-07 09:55:27 +0100

    Prettier

--- packages/liveblocks-react/src/liveblocks.tsx
+++ packages/liveblocks-react/src/liveblocks.tsx
@@ -887,9 +887,9 @@ function useUpdateNotificationSettings_withClient(
 function useNotificationSettings_withClient(
   client: OpaqueClient
 ): [
-    NotificationSettingsAsyncResult,
-    (settings: PartialNotificationSettings) => void,
-  ] {
+  NotificationSettingsAsyncResult,
+  (settings: PartialNotificationSettings) => void,
+] {
   const updateNotificationSettings =
     useUpdateNotificationSettings_withClient(client);
 
@@ -926,9 +926,9 @@ function useNotificationSettings_withClient(
 function useNotificationSettingsSuspense_withClient(
   client: OpaqueClient
 ): [
-    NotificationSettingsAsyncSuccess,
-    (settings: PartialNotificationSettings) => void,
-  ] {
+  NotificationSettingsAsyncSuccess,
+  (settings: PartialNotificationSettings) => void,
+] {
   // Throw error if we're calling this hook server side
   ensureNotServerSide();
 
