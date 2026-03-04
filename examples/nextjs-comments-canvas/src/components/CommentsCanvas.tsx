4833e0404a400cfc7017da93f61446dc3767e601
Chris Nicholas <ctnicholasdev@gmail.com>
2026-03-04 11:31:37 +0000

    Example: Update comments canvas to use high-level components (#3143)

--- examples/nextjs-comments-canvas/src/components/CommentsCanvas.tsx
+++ examples/nextjs-comments-canvas/src/components/CommentsCanvas.tsx
@@ -1,21 +1,17 @@
-import { Thread } from "@liveblocks/react-ui";
-import { ThreadData } from "@liveblocks/core";
+import { ThreadData } from "@liveblocks/client";
 import { useThreads, useEditThreadMetadata } from "@liveblocks/react/suspense";
-import { useUser } from "@liveblocks/react";
 import {
   DataRef,
   DndContext,
   DragEndEvent,
   PointerSensor,
   TouchSensor,
-  useDraggable,
   useSensor,
   useSensors,
 } from "@dnd-kit/core";
-import { useCallback, useMemo, useRef, useState } from "react";
-import styles from "./CommentsCanvas.module.css";
-import { Toolbar } from "./Toolbar";
-import { useMaxZIndex, useNearEdge } from "../hooks";
+import { useCallback } from "react";
+import { PlaceThreadButton } from "./PlaceThreadButton";
+import { DraggableThread } from "./DraggableThread";
 
 export function CommentsCanvas() {
   const { threads } = useThreads();
@@ -36,103 +32,41 @@ export function CommentsCanvas() {
   );
 
   // On drag end, update thread metadata with new coords
-  const handleDragEnd = useCallback(({ active, delta }: DragEndEvent) => {
-    const thread = (active.data as DataRef<{ thread: ThreadData }>).current
-      ?.thread;
-
-    if (!thread) {
-      return;
-    }
-
-    editThreadMetadata({
-      threadId: thread.id,
-      metadata: {
-        x: thread.metadata.x + delta.x,
-        y: thread.metadata.y + delta.y,
-      },
-    });
-  }, []);
+  const handleDragEnd = useCallback(
+    ({ active, delta }: DragEndEvent) => {
+      const thread = (active.data as DataRef<{ thread: ThreadData }>).current
+        ?.thread;
+      if (!thread) {
+        return;
+      }
+      editThreadMetadata({
+        threadId: thread.id,
+        metadata: {
+          x: thread.metadata.x + delta.x,
+          y: thread.metadata.y + delta.y,
+        },
+      });
+    },
+    [editThreadMetadata]
+  );
 
   return (
-    <div className={`${styles.wrapper} lb-root`}>
-      <div className={styles.threads}>
+    <div
+      style={{
+        position: "relative",
+        width: "100vw",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      <div style={{ isolation: "isolate" }}>
         <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
           {threads.map((thread) => (
             <DraggableThread key={thread.id} thread={thread} />
           ))}
         </DndContext>
       </div>
-      <Toolbar />
-    </div>
-  );
-}
-
-// A draggable thread
-function DraggableThread({ thread }: { thread: ThreadData }) {
-  // Open threads that have just been created
-  const startOpen = useMemo(() => {
-    return Number(new Date()) - Number(new Date(thread.createdAt)) <= 100;
-  }, [thread]);
-  const [open, setOpen] = useState(startOpen);
-
-  // Enable drag
-  const { attributes, listeners, setNodeRef, transform, node } = useDraggable({
-    id: thread.id,
-    data: { thread }, // Pass thread to DndContext drag end event
-  });
-
-  // If currently dragging, add drag values to current metadata
-  const x = transform ? transform.x + thread.metadata.x : thread.metadata.x;
-  const y = transform ? transform.y + thread.metadata.y : thread.metadata.y;
-
-  // Get the creator of the thread
-  const { user: creator } = useUser(thread.comments[0].userId);
-
-  // Used to set z-index higher than other threads when pointer down
-  const editThreadMetadata = useEditThreadMetadata();
-  const maxZIndex = useMaxZIndex();
-
-  // Used to flip thread near edge of screen
-  const { nearRightEdge, nearBottomEdge } = useNearEdge(node);
-
-  return (
-    <div
-      ref={setNodeRef}
-      className={styles.draggableThread}
-      onPointerDown={() =>
-        editThreadMetadata({
-          threadId: thread.id,
-          metadata: { zIndex: maxZIndex + 1 },
-        })
-      }
-      style={{
-        transform: `translate3d(${x}px, ${y}px, 0)`,
-        zIndex: thread.metadata?.zIndex || 0,
-      }}
-    >
-      <div {...listeners} {...attributes}>
-        <div className={styles.avatar} onClick={() => setOpen(!open)}>
-          {creator ? (
-            <img
-              src={creator.avatar}
-              alt={creator.name}
-              width="28px"
-              height="28px"
-              draggable={false}
-            />
-          ) : (
-            <div />
-          )}
-        </div>
-      </div>
-      {open ? (
-        <Thread
-          thread={thread}
-          className="thread"
-          data-flip-vertical={nearBottomEdge || undefined}
-          data-flip-horizontal={nearRightEdge || undefined}
-        />
-      ) : null}
+      <PlaceThreadButton />
     </div>
   );
 }
