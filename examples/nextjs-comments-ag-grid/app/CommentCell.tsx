5e70a7945a33b4484cfb677980c6e4a20b799e95
Chris Nicholas <ctnicholasdev@gmail.com>
2026-03-06 10:32:26 +0000

    feat: Use comment pin

--- examples/nextjs-comments-ag-grid/app/CommentCell.tsx
+++ examples/nextjs-comments-ag-grid/app/CommentCell.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import {
-  Comment,
+  CommentPin,
   FloatingComposer,
   FloatingThread,
+  Icon,
 } from "@liveblocks/react-ui";
 import { CustomCellRendererProps } from "ag-grid-react";
 import { useCellThread } from "./CellThreadContext";
+import { CSSProperties } from "react";
 
 export function CommentCell(params: CustomCellRendererProps) {
   const { threads, openCell, setOpenCell } = useCellThread();
@@ -37,8 +39,7 @@ export function CommentCell(params: CustomCellRendererProps) {
       style={{
         display: "flex",
         alignItems: "center",
-        justifyContent: "space-between",
-        gap: 16,
+        gap: 12,
       }}
     >
       {/* Cell contents */}
@@ -52,20 +53,7 @@ export function CommentCell(params: CustomCellRendererProps) {
             onComposerSubmit={() => setOpenCell(metadata)}
             style={{ zIndex: 10 }}
           >
-            <button>
-              <svg
-                width={16}
-                height={16}
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M2.992 16.342a2 2 0 01.094 1.167l-1.065 3.29a1 1 0 001.236 1.168l3.413-.998a2 2 0 011.099.092 10 10 0 10-4.777-4.719M8 12h8M12 8v8" />
-              </svg>
-            </button>
+            <CustomCommentPin />
           </FloatingComposer>
         </div>
       ) : (
@@ -81,17 +69,45 @@ export function CommentCell(params: CustomCellRendererProps) {
           style={{ zIndex: 10 }}
           autoFocus
         >
-          <Comment.Avatar
-            style={{
-              width: 28,
-              height: 28,
-              borderRadius: "100%",
-              cursor: "pointer",
-            }}
-            userId={thread.comments[0]?.userId}
-          />
+          <CustomCommentPin userId={thread.comments[0]?.userId} />
         </FloatingThread>
       )}
     </div>
+  );
+}
+
+const COMMENT_PIN_SIZE = 24;
+
+// Show a plus icon in the pin if no user ID is passed
+function CustomCommentPin({ userId }: { userId?: string }) {
+  return (
+    <CommentPin
+      corner="top-left"
+      style={
+        {
+          "--lb-comment-pin-padding": "3px",
+          width: COMMENT_PIN_SIZE,
+          height: COMMENT_PIN_SIZE,
+          cursor: "pointer",
+          marginTop: 3,
+        } as CSSProperties
+      }
+      userId={userId}
+    >
+      {userId ? null : (
+        <svg
+          width={14}
+          height={14}
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M5 12h14M12 5v14" />
+        </svg>
+      )}
+    </CommentPin>
   );
 }
