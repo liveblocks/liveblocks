e5d8fb4b8b91f063631356cf99ce2b35922e6971
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2026-03-11 14:23:37 +0100

    Setup new package

---
+++ packages/liveblocks-react-flow/src/index.ts
@@ -0,0 +1,7 @@
+/* eslint-disable simple-import-sort/exports */
+import { detectDupes } from "@liveblocks/core";
+
+import { PKG_FORMAT, PKG_NAME, PKG_VERSION } from "./version";
+detectDupes(PKG_NAME, PKG_VERSION, PKG_FORMAT);
+
+export {};
