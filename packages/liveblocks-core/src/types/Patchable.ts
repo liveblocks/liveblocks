9c677c8238ca7413ce7b0f6c1390ee223fa2e181
Vincent Driessen <me@nvie.com>
2026-03-25 14:23:10 +0100

    Document OptionalKeys<T> and Patchable<T> types

--- packages/liveblocks-core/src/types/Patchable.ts
+++ packages/liveblocks-core/src/types/Patchable.ts
@@ -1,3 +1,6 @@
+/**
+ * Extracts the optional keys (whose values are allowed to be `undefined`).
+ */
 type OptionalKeys<T> = {
   [K in keyof T]-?: undefined extends T[K] ? K : never;
 }[keyof T];
@@ -6,4 +9,9 @@ type MakeOptionalFieldsNullable<T> = {
   [K in keyof T]: K extends OptionalKeys<T> ? T[K] | null : T[K];
 };
 
+/**
+ * Like Partial<T>, but also allows `null` for optional fields. Useful for
+ * representing patches where `null` means "remove this field" and `undefined`
+ * means "leave this field unchanged".
+ */
 export type Patchable<T> = Partial<MakeOptionalFieldsNullable<T>>;
