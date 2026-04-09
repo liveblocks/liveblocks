1fd82a016f1f51ae842b89b80446d79071f7b372
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2026-04-09 16:55:29 +0200

    Replace tsd by Vitest in `@liveblocks/react-ui`

--- packages/liveblocks-react-ui/test-d/augmentation.test-d.tsx
+++ packages/liveblocks-react-ui/test-d/augmentation.test-d.tsx
@@ -4,9 +4,9 @@ import type {
   LiveMap,
   LiveObject,
 } from "@liveblocks/core";
-import { expectError, expectType } from "tsd";
 import type { InboxNotificationCustomKindProps } from "@liveblocks/react-ui";
 import { InboxNotification } from "@liveblocks/react-ui";
+import { describe, expectTypeOf, test } from "vitest";
 
 // TODO: Create type tests for all components/props
 
@@ -65,66 +65,72 @@ declare global {
   }
 }
 
-// <InboxNotification />: `kinds` prop
-{
-  // Existing kinds have the expected props types
-  {
-    <InboxNotification
-      inboxNotification={{} as InboxNotificationData}
-      kinds={{
-        thread: (props) => {
-          expectType<string>(props.inboxNotification.threadId);
-          expectError(props.inboxNotification.activities);
-
-          return <InboxNotification.Thread {...props} />;
-        },
-        textMention: (props) => {
-          expectType<string>(props.inboxNotification.mentionId);
-          expectError(props.inboxNotification.activities);
-
-          return <InboxNotification.TextMention {...props} />;
-        },
-        $myErrorNotification: (props) => {
-          expectType<string>(
-            props.inboxNotification.activities[0]!.data.message
-          );
-          expectType<number>(props.inboxNotification.activities[0]!.data.code);
-          expectError(props.inboxNotification.activities[0]!.data.nonexisting);
-          expectError(props.inboxNotification.threadId);
-          expectError(props.inboxNotification.mentionId);
-
-          return (
-            <InboxNotification.Custom {...props} title="Title">
-              Content
-            </InboxNotification.Custom>
-          );
-        },
-      }}
-    />;
-  }
-
-  // Non-existing custom kinds are not allowed
-  {
-    expectError(
+describe("InboxNotification `kinds` (with Liveblocks augmentation)", () => {
+  test("existing kinds have the expected props types", () => {
+    void (
       <InboxNotification
         inboxNotification={{} as InboxNotificationData}
         kinds={{
+          thread: (props) => {
+            expectTypeOf(props.inboxNotification.threadId).toEqualTypeOf<string>();
+            // @ts-expect-error - `activities` is not on thread notifications
+            void props.inboxNotification.activities;
+
+            return <InboxNotification.Thread {...props} />;
+          },
+          textMention: (props) => {
+            expectTypeOf(props.inboxNotification.mentionId).toEqualTypeOf<string>();
+            // @ts-expect-error - `activities` is not on text-mention notifications
+            void props.inboxNotification.activities;
+
+            return <InboxNotification.TextMention {...props} />;
+          },
+          $myErrorNotification: (props) => {
+            expectTypeOf(
+              props.inboxNotification.activities[0]!.data.message
+            ).toEqualTypeOf<string>();
+            expectTypeOf(
+              props.inboxNotification.activities[0]!.data.code
+            ).toEqualTypeOf<number>();
+            // @ts-expect-error - invalid activity data field
+            void props.inboxNotification.activities[0]!.data.nonexisting;
+            // @ts-expect-error - `threadId` is not on this custom kind
+            void props.inboxNotification.threadId;
+            // @ts-expect-error - `mentionId` is not on this custom kind
+            void props.inboxNotification.mentionId;
+
+            return (
+              <InboxNotification.Custom {...props} title="Title">
+                Content
+              </InboxNotification.Custom>
+            );
+          },
+        }}
+      />
+    );
+  });
+
+  test("rejects custom kind keys that are not declared in ActivitiesData", () => {
+    void (
+      <InboxNotification
+        inboxNotification={{} as InboxNotificationData}
+        kinds={{
+          // @ts-expect-error - `$myNonExistingNotification` is not in ActivitiesData
           $myNonExistingNotification: (_props: any) => null,
         }}
       />
     );
-  }
+  });
 
-  // Custom kinds’ props types can be used as expected in external components
-  {
+  test("custom kind props types work in external components", () => {
     function MyCustomInboxNotification({
       inboxNotification,
       ...props
     }: InboxNotificationCustomKindProps) {
-      expectType<
+      expectTypeOf(inboxNotification.activities[0]!.data).toEqualTypeOf<
         | { message: string; code: number }
         | { duration: number; uploadId: string }
-      >(inboxNotification.activities[0]!.data);
+      >();
 
       return (
         <InboxNotification.Custom
@@ -141,9 +147,14 @@ declare global {
       inboxNotification,
       ...props
     }: InboxNotificationCustomKindProps<"$myErrorNotification">) {
-      expectType<string>(inboxNotification.activities[0]!.data.message);
-      expectType<number>(inboxNotification.activities[0]!.data.code);
-      expectError(inboxNotification.activities[0]!.data.nonexisting);
+      expectTypeOf(
+        inboxNotification.activities[0]!.data.message
+      ).toEqualTypeOf<string>();
+      expectTypeOf(
+        inboxNotification.activities[0]!.data.code
+      ).toEqualTypeOf<number>();
+      // @ts-expect-error - invalid activity data field
+      void inboxNotification.activities[0]!.data.nonexisting;
 
       return (
         <InboxNotification.Custom
@@ -156,23 +167,25 @@ declare global {
       );
     }
 
-    <InboxNotification
-      inboxNotification={{} as InboxNotificationData}
-      kinds={{
-        $myErrorNotification: MyCustomInboxNotification,
-        $myUploadNotification: MyCustomInboxNotification,
-      }}
-    />;
+    void (
+      <InboxNotification
+        inboxNotification={{} as InboxNotificationData}
+        kinds={{
+          $myErrorNotification: MyCustomInboxNotification,
+          $myUploadNotification: MyCustomInboxNotification,
+        }}
+      />
+    );
 
-    // When ActivitiesData is set with augmentation, specific custom kinds’ props types can only be used with their kinds
-    expectError(
+    void (
       <InboxNotification
         inboxNotification={{} as InboxNotificationData}
         kinds={{
           $myErrorNotification: MySpecificCustomInboxNotification,
+          // @ts-expect-error - handler is typed only for `$myErrorNotification`
           $myUploadNotification: MySpecificCustomInboxNotification,
         }}
       />
     );
-  }
-}
+  });
+});
--- packages/liveblocks-react-ui/test-d/no-augmentation.test-d.tsx
+++ packages/liveblocks-react-ui/test-d/no-augmentation.test-d.tsx
@@ -1,61 +1,69 @@
 import type { ActivityData, InboxNotificationData } from "@liveblocks/core";
-import { expectError, expectType } from "tsd";
 import type { InboxNotificationCustomKindProps } from "@liveblocks/react-ui";
 import { InboxNotification } from "@liveblocks/react-ui";
+import { describe, expectTypeOf, test } from "vitest";
 
 // TODO: Create type tests for all components/props
 
-// <InboxNotification />: `kinds` prop
-{
-  // Existing kinds have the expected props types
-  {
-    <InboxNotification
-      inboxNotification={{} as InboxNotificationData}
-      kinds={{
-        thread: (props) => {
-          expectType<string>(props.inboxNotification.threadId);
-          expectError(props.inboxNotification.activities);
+describe("InboxNotification `kinds` (no Liveblocks augmentation)", () => {
+  test("existing kinds have the expected props types", () => {
+    void (
+      <InboxNotification
+        inboxNotification={{} as InboxNotificationData}
+        kinds={{
+          thread: (props) => {
+            expectTypeOf(props.inboxNotification.threadId).toEqualTypeOf<string>();
+            // @ts-expect-error - `activities` is not on thread notifications
+            void props.inboxNotification.activities;
 
-          return <InboxNotification.Thread {...props} />;
-        },
-        textMention: (props) => {
-          expectType<string>(props.inboxNotification.mentionId);
-          expectError(props.inboxNotification.activities);
+            return <InboxNotification.Thread {...props} />;
+          },
+          textMention: (props) => {
+            expectTypeOf(props.inboxNotification.mentionId).toEqualTypeOf<string>();
+            // @ts-expect-error - `activities` is not on text-mention notifications
+            void props.inboxNotification.activities;
 
-          return <InboxNotification.TextMention {...props} />;
-        },
-        $myErrorNotification: (props) => {
-          expectType<ActivityData>(props.inboxNotification.activities[0]!.data);
-          expectError(props.inboxNotification.threadId);
-          expectError(props.inboxNotification.mentionId);
+            return <InboxNotification.TextMention {...props} />;
+          },
+          $myErrorNotification: (props) => {
+            expectTypeOf(
+              props.inboxNotification.activities[0]!.data
+            ).toEqualTypeOf<ActivityData>();
+            // @ts-expect-error - `threadId` is not on this custom kind
+            void props.inboxNotification.threadId;
+            // @ts-expect-error - `mentionId` is not on this custom kind
+            void props.inboxNotification.mentionId;
 
-          return (
-            <InboxNotification.Custom {...props} title="Title">
-              Content
-            </InboxNotification.Custom>
-          );
-        },
-      }}
-    />;
-  }
+            return (
+              <InboxNotification.Custom {...props} title="Title">
+                Content
+              </InboxNotification.Custom>
+            );
+          },
+        }}
+      />
+    );
+  });
 
-  // All custom kinds are allowed when ActivitiesData is not set with augmentation
-  {
-    <InboxNotification
-      inboxNotification={{} as InboxNotificationData}
-      kinds={{
-        $myNonExistingNotification: (_props: any) => null,
-      }}
-    />;
-  }
+  test("allows arbitrary custom kind keys when ActivitiesData is not augmented", () => {
+    void (
+      <InboxNotification
+        inboxNotification={{} as InboxNotificationData}
+        kinds={{
+          $myNonExistingNotification: (_props: any) => null,
+        }}
+      />
+    );
+  });
 
-  // Custom kinds’ props types can be used as expected in external components
-  {
+  test("custom kind props types work in external components", () => {
     function MyCustomInboxNotification({
       inboxNotification,
       ...props
     }: InboxNotificationCustomKindProps) {
-      expectType<ActivityData>(inboxNotification.activities[0]!.data);
+      expectTypeOf(
+        inboxNotification.activities[0]!.data
+      ).toEqualTypeOf<ActivityData>();
 
       return (
         <InboxNotification.Custom
@@ -72,7 +80,9 @@ import { InboxNotification } from "@liveblocks/react-ui";
       inboxNotification,
       ...props
     }: InboxNotificationCustomKindProps<"$myErrorNotification">) {
-      expectType<ActivityData>(inboxNotification.activities[0]!.data);
+      expectTypeOf(
+        inboxNotification.activities[0]!.data
+      ).toEqualTypeOf<ActivityData>();
 
       return (
         <InboxNotification.Custom
@@ -85,22 +95,26 @@ import { InboxNotification } from "@liveblocks/react-ui";
       );
     }
 
-    <InboxNotification
-      inboxNotification={{} as InboxNotificationData}
-      kinds={{
-        $myErrorNotification: MyCustomInboxNotification,
-        $myUploadNotification: MyCustomInboxNotification,
-      }}
-    />;
-
-    expectError(
+    void (
       <InboxNotification
         inboxNotification={{} as InboxNotificationData}
         kinds={{
+          $myErrorNotification: MyCustomInboxNotification,
+          $myUploadNotification: MyCustomInboxNotification,
+        }}
+      />
+    );
+
+    void (
+      <InboxNotification
+        inboxNotification={{} as InboxNotificationData}
+        kinds={{
+          // @ts-expect-error - handler is typed only for `$myErrorNotification`
           $myErrorNotification: MySpecificCustomInboxNotification,
+          // @ts-expect-error - handler is typed only for `$myErrorNotification`
           $myUploadNotification: MySpecificCustomInboxNotification,
         }}
       />
     );
-  }
-}
+  });
+});
