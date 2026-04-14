26b70c6b67e896c03e8c5f7c93cdb08328993cd2
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2026-04-14 10:42:17 +0200

    Fix `@liveblocks/react-flow` tests

--- packages/liveblocks-react-flow/vitest.config.ts
+++ packages/liveblocks-react-flow/vitest.config.ts
@@ -1,9 +1,16 @@
 import { defaultLiveblocksVitestConfig } from "@liveblocks/vitest-config";
+import { mergeConfig } from "vite";
 
-export default defaultLiveblocksVitestConfig({
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: ["vitest.setup.ts"],
-  },
-});
+export default mergeConfig(
+  defaultLiveblocksVitestConfig({
+    test: {
+      environment: "jsdom",
+      setupFiles: ["vitest.setup.ts"],
+    },
+  }),
+  {
+    resolve: {
+      dedupe: ["react", "react-dom"],
+    },
+  }
+);
