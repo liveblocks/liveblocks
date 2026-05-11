5b6a97574d5a71e1b40513723dcec12bd37b49cb
Jonathan Rowny <jrowny@gmail.com>
2026-05-11 15:52:35 -0700

    fix a few examples

--- examples/nextjs-ai-chats/app/[chatId]/page.tsx
+++ examples/nextjs-ai-chats/app/[chatId]/page.tsx
@@ -49,7 +49,9 @@ function ChatTitle({ chatId }: { chatId: string }) {
 
 // Overriding the empty chat state
 function Empty({ chatId }: AiChatComponentsEmptyProps) {
-  const sendMessage = useSendAiMessage(chatId);
+  const sendMessage = useSendAiMessage(chatId, {
+    copilotId: process.env.NEXT_PUBLIC_LIVEBLOCKS_COPILOT_ID || undefined,
+  });
 
   return (
     <div className="size-full mx-auto max-w-[--inner-app-width] flex items-end pb-[calc(3*var(--lb-spacing))]">
